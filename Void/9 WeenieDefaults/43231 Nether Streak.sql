88512d8c3b8187b00034d9c34610cf8d6bcc7937
gmriggs <gmriggs@gmail.com>
2018-09-29 13:54:56 -0400

    Adding prerequisites for Void magic (#4)
    * Adding prerequisites for Void magic
    
    * Updating Corruption to wcid 43344
    
    * Updating Festering Curse stat_Mod_Type
    
    * cleaning wcid insertion, netherbolt -> netherbot

---
+++ Void/9 WeenieDefaults/43231 Nether Streak.sql
@@ -0,0 +1,34 @@
+/* todo: find real wcid */
+delete from weenie where class_Id=43231;
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`)
+VALUES ('43231', 'netherstreak', 33) /* ProjectileSpell */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43231,   8,         25) /* Mass */
+     , (43231,   9,          0) /* ValidLocations - None */
+     , (43231,  66,          0) /* CheckpointStatus */
+     , (43231,  93,     133140) /* PhysicsState - Ethereal, IgnoreCollisions, LightingOn, Inelastic */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (43231,   1, True ) /* Stuck */
+     , (43231,  14, False) /* GravityStatus */
+     , (43231,  15, True ) /* LightsStatus */
+     , (43231,  16, True ) /* ScriptedCollision */
+     , (43231,  17, True ) /* Inelastic */
+     , (43231,  24, True ) /* UiHidden */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43231,  26,      45) /* MaximumVelocity */
+     , (43231,  78,       1) /* Friction */
+     , (43231,  79,       0) /* Elasticity */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43231,   1, 'Nether Streak') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43231,   1,   33561128) /* Setup */
+     , (43231,   3,  536871126) /* SoundTable */
+     , (43231,   8,  100667494) /* Icon */
+     , (43231,  22,  872415436) /* PhysicsEffectTable */
+     , (43231,  28,       5357) /* Spell - Nether Streak I */
+     , (43231,  30,         89) /* PhysicsScript - Destroy */;
---
+++ Void/9 WeenieDefaults/43232 Nether Arc.sql
@@ -0,0 +1,35 @@
+/* todo: find real wcid */
+delete from weenie where class_Id=43232;
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`)
+VALUES ('43232', 'netherarc', 33) /* ProjectileSpell */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43232,   8,         25) /* Mass */
+     , (43232,   9,          0) /* ValidLocations - None */
+     , (43232,  66,          0) /* CheckpointStatus */
+     , (43232,  93,     134164) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn, Inelastic */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (43232,   1, True ) /* Stuck */
+     , (43232,  14, True ) /* GravityStatus */
+     , (43232,  15, True ) /* LightsStatus */
+     , (43232,  16, True ) /* ScriptedCollision */
+     , (43232,  17, True ) /* Inelastic */
+     , (43232,  24, True ) /* UiHidden */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43232,  26,      40) /* MaximumVelocity */
+     , (43232,  39,     0.5) /* DefaultScale */
+     , (43232,  78,       1) /* Friction */
+     , (43232,  79,       0) /* Elasticity */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43232,   1, 'Nether Arc') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43232,   1,   33561128) /* Setup */
+     , (43232,   3,  536871126) /* SoundTable */
+     , (43232,   8,  100667494) /* Icon */
+     , (43232,  22,  872415436) /* PhysicsEffectTable */
+     , (43232,  28,       5369) /* Spell - Nether Arc I */
+     , (43232,  30,         89) /* PhysicsScript - Destroy */;
