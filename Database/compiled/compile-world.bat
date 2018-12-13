281ed0772786feb1fededf054084552da81ccf3c
Ty Conner <tyconner@itcproductions.com>
2018-12-13 04:18:14 -0500

    Update compile-world.bat

--- Database/compiled/compile-world.bat
+++ Database/compiled/compile-world.bat
@@ -17,17 +17,7 @@ call compile-patches.bat
 echo /* ACE-World-16PY-Patches */ >> ACE-World-Database.sql
 echo. >> ACE-World-Database.sql
 
-echo /*!40014 SET @OLD_SQL_SAFE_UPDATES=@@SQL_SAFE_UPDATES, SQL_SAFE_UPDATES=0 */; >> ACE-World-Database.sql
-echo /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */; >> ACE-World-Database.sql
-echo /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */; >> ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
-
 copy /b ACE-World-Database.sql + ..\Patches\*.sql ACE-World-Database.sql
-
-echo /*!40014 SET SQL_SAFE_UPDATES=@OLD_SQL_SAFE_UPDATES */; >> ACE-World-Database.sql
-echo /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */; >> ACE-World-Database.sql
-echo /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */; >> ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
 
 move ..\ACE-World-16PY\Database\compiled\*.sql
 move ..\Patches\*.sql
