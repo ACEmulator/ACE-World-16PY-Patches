b56e67b35f54c5d6919e4a16bfe7763f945af4df
Ty Conner <tyconner@itcproductions.com>
2018-12-13 13:36:33 -0500

    Update 30987 Letter From Home.sql

--- Database/Patches/TAWeaponsUpdates/9 WeenieDefaults/Book/30987 Letter From Home.sql
+++ Database/Patches/TAWeaponsUpdates/9 WeenieDefaults/Book/30987 Letter From Home.sql
@@ -9,10 +9,10 @@ VALUES (30987, 001 /* NAME_STRING */, 'Letter From Home')
      , (30987, 016 /* LONG_DESC_STRING */, 'Double-click this note to read it.');
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (30985, 001 /* SETUP_DID */, 33554773)
-     , (30985, 003 /* SOUND_TABLE_DID */, 536870932)
-     , (30985, 008 /* ICON_DID */, 100667503)
-     , (30985, 022 /* PHYSICS_EFFECT_TABLE_DID */, 872415275);
+VALUES (30987, 001 /* SETUP_DID */, 33554773)
+     , (30987, 003 /* SOUND_TABLE_DID */, 536870932)
+     , (30987, 008 /* ICON_DID */, 100667503)
+     , (30987, 022 /* PHYSICS_EFFECT_TABLE_DID */, 872415275);
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (30987, 001 /* ITEM_TYPE_INT */, 8192 /* TYPE_WRITABLE */)
