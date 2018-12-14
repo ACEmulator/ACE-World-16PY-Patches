7bdc7a2b70072b2851a8e57661ba7c5c14c57b34
Ty Conner <tyconner@itcproductions.com>
2018-12-14 01:04:42 -0500

    Remove TA patch temporarily

--- Database/Patches/Post16PYRecipes/06305 Academy Hand Axe.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM recipe WHERE id = 6305;
-
-INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`)
-VALUES (6305, 0, 0, 0, 0, 45532 /* Academy Hand Axe */, 1, 'As you pour the Oil of Rendering onto the Training Hand Axe, it slowly deepens to a lustrous red.', 0, 0, 'You were unable pour the Oil of Rendering onto the Training Hand Axe.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0);
-
-INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`)
-VALUES (6305, 12711 /* Oil of Rendering */, 45534 /* Training Hand Axe */);
--- Database/Patches/TrainingAcademy/Jonathan/8 QuestDefDB/AcademyExitToken.sql
+++
@@ -1,2 +0,0 @@
-REPLACE INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`)
-VALUES ('AcademeyExitTokenGiven', 0, 1, 'Recived the Acadmey Exit Token');
