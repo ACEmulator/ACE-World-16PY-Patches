ce5aba91422a1ed55a5591902aeb1382ac4540b3
Ty Conner <tyconner@itcproductions.com>
2018-12-14 02:03:42 -0500

    Update compile-world.bat

--- Database/compiled/compile-world.bat
+++ Database/compiled/compile-world.bat
@@ -1,17 +1,29 @@
 @echo off
 IF EXIST *.sql del *.sql
+echo.
 echo Creating ACE-World-Database...
 echo.
-echo Compiling ACE-World-16PY Base...
-call compile-base.bat
-@echo off
 echo /* ACE-World-Database */ >> ACE-World-Database.sql
 echo. >> ACE-World-Database.sql
 echo /* ACE-World-16PY */ >> ACE-World-Database.sql
 echo. >> ACE-World-Database.sql
-copy /b ACE-World-Database.sql + ..\ACE-World-16PY\Database\compiled\*.sql ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
-echo Compiling Patches, skipping patches marked optional...
+
+IF EXIST *.zip (
+    echo Found downloaded database zip file, skipping ACE-World-16PY Base compile...
+    echo.
+    7z x ACE-World-16PY-*.zip
+    copy /b ACE-World-Database.sql + ACE-World-16PY-*.sql ACE-World-Database.sql
+    echo. >> ACE-World-Database.sql
+) ELSE (
+    echo Compiling ACE-World-16PY Base...
+    call compile-base.bat
+    @echo off
+    copy /b ACE-World-Database.sql + ..\ACE-World-16PY\Database\compiled\*.sql ACE-World-Database.sql
+    echo. >> ACE-World-Database.sql
+)
+
+@echo off
+echo Compiling Patches, skipping patches marked as optional...
 call compile-patches.bat
 @echo off
 echo /* ACE-World-16PY-Patches */ >> ACE-World-Database.sql
