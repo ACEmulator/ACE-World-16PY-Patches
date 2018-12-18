065456421e7f7fbd595aa81a7e0f720425bbfc7e
Ty Conner <tyconner@itcproductions.com>
2018-12-18 04:34:24 -0500

    Updated Landblocks for Training Academy (#17)
    * Updated Landblocks for Training Academy
    
    * Fix Doors in Academy
    
    * Change Folder Struc
    
    * More Door updates
    
    * Update 30998 Academy Library.sql
    
    * Remove portals to force usage of exit npc for now

---
+++ Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Door/Misc/12705 Door.sql
@@ -0,0 +1,37 @@
+DELETE FROM `weenie` WHERE `class_Id` = 12705;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`)
+VALUES (12705, 'dooracademya', 19) /* Door */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (12705,   1,        128) /* ItemType - Misc */
+     , (12705,   8,        500) /* Mass */
+     , (12705,  16,         32) /* ItemUseable - Remote */
+     , (12705,  19,          0) /* Value */
+     , (12705,  93,         24) /* PhysicsState - ReportCollisions, IgnoreCollisions */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (12705,   1, True ) /* Stuck */
+     , (12705,   2, False) /* Open */
+     , (12705,   3, False ) /* Locked */
+     , (12705,  12, True ) /* ReportCollisions */
+     , (12705,  13, False) /* Ethereal */
+     , (12705,  14, False) /* GravityStatus */
+     , (12705,  33, False) /* ResetMessagePending */
+     , (12705,  34, False) /* DefaultOpen */
+     , (12705,  35, False ) /* DefaultLocked */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (12705,  11,      60) /* ResetInterval */
+     , (12705,  54,       2) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (12705,   1, 'Door') /* Name */
+     , (12705,  14, 'Double-click on these doors to open them.') /* Use */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (12705,   1,   33555930) /* Setup */
+     , (12705,   2,  150995078) /* MotionTable */
+     , (12705,   3,  536870946) /* SoundTable */
+     , (12705,   8,  100668183) /* Icon */
+     , (12705,  22,  872415275) /* PhysicsEffectTable */;
---
+++ Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Door/Misc/30998 Academy Library.sql
@@ -0,0 +1,39 @@
+DELETE FROM `weenie` WHERE `class_Id` = 30998;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`)
+VALUES (30998, 'doornewbieacademylibrary', 19) /* Door */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (30998,   1,        128) /* ItemType - Misc */
+     , (30998,   8,        500) /* Mass */
+     , (30998,  16,         32) /* ItemUseable - Remote */
+     , (30998,  19,          0) /* Value */
+     , (30998,  38,       9999) /* ResistLockpick */
+     , (30998,  93,         24) /* PhysicsState - ReportCollisions, IgnoreCollisions */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (30998,   1, True ) /* Stuck */
+     , (30998,   2, False) /* Open */
+     , (30998,   3, True ) /* Locked */
+     , (30998,  12, True ) /* ReportCollisions */
+     , (30998,  13, False) /* Ethereal */
+     , (30998,  14, False) /* GravityStatus */
+     , (30998,  33, False) /* ResetMessagePending */
+     , (30998,  34, False) /* DefaultOpen */
+     , (30998,  35, True ) /* DefaultLocked */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (30998,  11,      60) /* ResetInterval */
+     , (30998,  54,       2) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (30998,   1, 'Academy Library') /* Name */
+     , (30998,  12, 'keydooracademya') /* LockCode */
+     , (30998,  14, 'Use the Academy Library Key on this door to open it.') /* Use */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (30998,   1,   33555023) /* Setup */
+     , (30998,   2,  150994966) /* MotionTable */
+     , (30998,   3,  536870946) /* SoundTable */
+     , (30998,   8,  100668183) /* Icon */
+     , (30998,  22,  872415275) /* PhysicsEffectTable */;
