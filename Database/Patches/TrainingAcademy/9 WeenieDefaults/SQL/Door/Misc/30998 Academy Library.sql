2a9e7e0b3acb70c9297c92dbd6967cf56e903f02
Ty Conner <tyconner@itcproductions.com>
2018-12-18 04:22:15 -0500

    Update 30998 Academy Library.sql

--- Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Door/Misc/30998 Academy Library.sql
+++ Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Door/Misc/30998 Academy Library.sql
@@ -5,6 +5,7 @@ VALUES (30998, 'doornewbieacademylibrary', 19) /* Door */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (30998,   1,        128) /* ItemType - Misc */
+     , (30998,   8,        500) /* Mass */
      , (30998,  16,         32) /* ItemUseable - Remote */
      , (30998,  19,          0) /* Value */
      , (30998,  38,       9999) /* ResistLockpick */
@@ -27,6 +28,7 @@ VALUES (30998,  11,      60) /* ResetInterval */
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (30998,   1, 'Academy Library') /* Name */
+     , (30998,  12, 'keydooracademya') /* LockCode */
      , (30998,  14, 'Use the Academy Library Key on this door to open it.') /* Use */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
