1f5517e01f185a9ddfb73188dd1ee805102b6d12
Ty Conner <tyconner@itcproductions.com>
2018-12-21 05:28:04 -0500

    Update Part 1 NPCs
    Bring in more pcap data to update NPCs to reflect what was seen at end of retail

--- Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Creature/Human/29320 Training Master.sql
+++ Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Creature/Human/29320 Training Master.sql
@@ -1,90 +1,70 @@
-/* Weenie - Training Master (29320) */
-DELETE FROM weenie WHERE class_Id = 29320;
+DELETE FROM `weenie` WHERE `class_Id` = 29320;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`)
-VALUES ('29320', 'academyguardtrainingmaster', 10) /* Creature */;
+VALUES (29320, 'academyguardtrainingmaster', 10) /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (29320,   1,         16) /* ItemType - Creature */
      , (29320,   2,         31) /* CreatureType - Human */
-     , (29320,   3,          9) /* PaletteTemplate - Grey */
-     , (29320,   6,         -1) /* ItemsCapacity */
-     , (29320,   7,         -1) /* ContainersCapacity */
-     , (29320,   8,        120) /* Mass */
+     , (29320,   6,        255) /* ItemsCapacity */
+     , (29320,   7,        255) /* ContainersCapacity */
      , (29320,  16,         32) /* ItemUseable - Remote */
-     , (29320,  25,         10) /* Level */
-     , (29320,  27,          0) /* ArmorType */
+     , (29320,  25,         45) /* Level */
      , (29320,  93,    6292504) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
      , (29320,  95,          8) /* RadarBlipColor - Yellow */
+     , (29320, 113,          1) /* Gender - Male */
      , (29320, 133,          4) /* ShowableOnRadar - ShowAlways */
      , (29320, 134,         16) /* PlayerKillerStatus - RubberGlue */
-     , (29320, 146,        161) /* XpOverride */;
+     , (29320, 188,          3) /* HeritageGroup - Sho */
+     , (29320, 8007,          0) /* PCAPRecordedAutonomousMovement */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (29320,   1, True ) /* Stuck */
-     , (29320,   8, True ) /* AllowGive */
+     , (29320,  11, True ) /* IgnoreCollisions */
      , (29320,  12, True ) /* ReportCollisions */
      , (29320,  13, False) /* Ethereal */
+     , (29320,  14, True ) /* GravityStatus */
      , (29320,  19, False) /* Attackable */
      , (29320,  41, True ) /* ReportCollisionsAsEnvironment */
-     , (29320,  42, True ) /* AllowEdgeSlide */
-     , (29320,  52, True ) /* AiImmobile */;
+     , (29320,  42, True ) /* AllowEdgeSlide */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (29320,   3,    0.16) /* HealthRate */
-     , (29320,   4,       5) /* StaminaRate */
-     , (29320,   5,       1) /* ManaRate */
-     , (29320,  12,       1) /* Shade */
-     , (29320,  13,     0.9) /* ArmorModVsSlash */
-     , (29320,  14,       1) /* ArmorModVsPierce */
-     , (29320,  15,     1.1) /* ArmorModVsBludgeon */
-     , (29320,  16,     0.4) /* ArmorModVsCold */
-     , (29320,  17,     0.4) /* ArmorModVsFire */
-     , (29320,  18,       1) /* ArmorModVsAcid */
-     , (29320,  19,     0.6) /* ArmorModVsElectric */
-     , (29320,  54,       3) /* UseRadius */
-     , (29320,  64,       1) /* ResistSlash */
-     , (29320,  65,       1) /* ResistPierce */
-     , (29320,  66,       1) /* ResistBludgeon */
-     , (29320,  67,       1) /* ResistFire */
-     , (29320,  68,       1) /* ResistCold */
-     , (29320,  69,       1) /* ResistAcid */
-     , (29320,  70,       1) /* ResistElectric */
-     , (29320,  71,       1) /* ResistHealthBoost */
-     , (29320,  72,       1) /* ResistStaminaDrain */
-     , (29320,  73,       1) /* ResistStaminaBoost */
-     , (29320,  74,       1) /* ResistManaDrain */
-     , (29320,  75,       1) /* ResistManaBoost */
-     , (29320, 104,      10) /* ObviousRadarRange */
-     , (29320, 125,       1) /* ResistHealthDrain */;
+VALUES (29320,  54,       3) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (29320,   1, 'Training Master') /* Name */
-     , (29320,   3, 'Male') /* Sex */
-     , (29320,   4, 'Gharu''ndim') /* HeritageGroup */
-     , (29320,   5, 'Exploration Society Agent') /* Template */;
+     , (29320,   5, 'Exploration Society Agent') /* Template */
+     , (29320, 8006, 'AAA9AAAAAAA=') /* PCAPRecordedCurrentMotionState */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (29320,   1,   33554433) /* Setup */
      , (29320,   2,  150994945) /* MotionTable */
      , (29320,   3,  536870913) /* SoundTable */
-     , (29320,   4,  805306368) /* CombatTable */
      , (29320,   6,   67108990) /* PaletteBase */
-     , (29320,   7,  268435545) /* ClothingBase */
-     , (29320,   8,  100667446) /* Icon */;
+     , (29320,   8,  100667377) /* Icon */
+     , (29320, 8001,    9437238) /* PCAPRecordedWeenieHeader - ItemsCapacity, ContainersCapacity, Usable, UseRadius, RadarBlipColor, RadarBehavior */
+     , (29320, 8003,          4) /* PCAPRecordedObjectDesc - Stuck */
+     , (29320, 8005,     100355) /* PCAPRecordedPhysicsDesc - CSetup, MTable, STable, Position, Movement */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (29320, 8040, 2248344122, 56.078, -20.1217, 0.004999995, -0.675163, 0, 0, -0.737668) /* PCAPRecordedLocation */
+/* @teleloc 0x8603023A [56.078000 -20.121700 0.005000] -0.675163 0.000000 0.000000 -0.737668 */;
+
+INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
+VALUES (29320, 8000, 3692819195) /* PCAPRecordedObjectIID */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
-VALUES (29320,   1,  60, 0, 0) /* Strength */
-     , (29320,   2,  70, 0, 0) /* Endurance */
-     , (29320,   3,  80, 0, 0) /* Quickness */
-     , (29320,   4,  50, 0, 0) /* Coordination */
+VALUES (29320,   1, 180, 0, 0) /* Strength */
+     , (29320,   2, 150, 0, 0) /* Endurance */
+     , (29320,   3, 160, 0, 0) /* Quickness */
+     , (29320,   4, 150, 0, 0) /* Coordination */
      , (29320,   5, 120, 0, 0) /* Focus */
      , (29320,   6, 130, 0, 0) /* Self */;
 
 INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
-VALUES (29320,   1,    10, 0, 0, 45) /* MaxHealth */
-     , (29320,   3,    10, 0, 0, 80) /* MaxStamina */
-     , (29320,   5,    10, 0, 0, 140) /* MaxMana */;
+VALUES (29320,   1,   175, 0, 0, 175) /* MaxHealth */
+     , (29320,   3,   250, 0, 0, 250) /* MaxStamina */
+     , (29320,   5,   220, 0, 0, 220) /* MaxMana */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (29320,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
