0722e2cdfed61c77f0d4b88b3135294884ef1e7c
Keith McCormick <kdmxp512@gmail.com>
2019-01-06 14:17:19 -0800

    Updates1 : Fix-up Exit portals

--- Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Portal/29339 Exit to Sanamar.sql
+++ Database/Patches/TrainingAcademy/9 WeenieDefaults/SQL/Portal/29339 Exit to Sanamar.sql
@@ -9,7 +9,7 @@ VALUES (29339,   1,      65536) /* ItemType - Portal */
      , (29339,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
      , (29339, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */
      , (29339, 133,          4) /* ShowableOnRadar - ShowAlways */
-     , (29339, 8007,          0) /* PCAPRecordedAutonomousMovement */;
+     , (29339, 8007,         0) /* PCAPRecordedAutonomousMovement */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (29339,   1, True ) /* Stuck */
@@ -38,7 +38,17 @@ VALUES (29339,   1,   33554867) /* Setup */
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (29339, 8040, 1912799593, 158.641, -149.516, -6.063, 1, 0, 0, 0) /* PCAPRecordedLocation */
-/* @teleloc 0x72030169 [158.641000 -149.516000 -6.063000] 1.000000 0.000000 0.000000 0.000000 */;
+/* @teleloc 0x72030169 [158.641000 -149.516000 -6.063000] 1.000000 0.000000 0.000000 0.000000 */
+     , (29339, 2, 869859349, 59.099998, 100.300003, 52.005001, 0.000000, 0, 0, -1.000000) /* Destination */
+/* @teleloc 0x33D90015 [59.099998 100.300003 52.005001] 0.000000 0.000000 0.000000 -1.000000 */;
 
 INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
 VALUES (29339, 8000, 2614428451) /* PCAPRecordedObjectIID */;
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (29339,  4 /* Portal */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id,  7,  63 /* SetSanctuaryPosition */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3595894828, 133.717, 82.224, 47.458, 0.8053752, 0, 0, -0.5927655);
