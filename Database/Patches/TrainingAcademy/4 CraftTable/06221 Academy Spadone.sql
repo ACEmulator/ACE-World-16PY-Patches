2a28b9465d22df7a86d0bdf1e722cc65387c9948
Keith McCormick <kdmxp512@gmail.com>
2019-01-08 11:16:03 -0800

    Update2 : Quick Recipie changes

--- Database/Patches/TrainingAcademy/4 CraftTable/06221 Academy Spadone.sql
+++ Database/Patches/TrainingAcademy/4 CraftTable/06221 Academy Spadone.sql
@@ -1,7 +1,7 @@
-DELETE FROM recipe WHERE id = 6304;
+DELETE FROM recipe WHERE id = 6221;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`)
-VALUES (6304, 0, 0, 0, 0, 45536 /* Academy Dagger */, 1, 'As you pour the Oil of Rendering onto the Training Dagger, it slowly deepens to a lustrous red.', 0, 0, 'You were unable pour the Oil of Rendering onto the Training Dagger.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0);
+VALUES (6221, 0, 0, 0, 0, 41514 /* Academy Spadone */, 1, 'As you pour the Oil of Rendering onto the Training Spadone, it slowly deepens to a lustrous red.', 0, 0, 'You were unable pour the Oil of Rendering onto the Training Spadone.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0);
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`)
-VALUES (6304, 12711 /* Oil of Rendering */, 45538 /* Training Dagger */);
+VALUES (6221, 12711 /* Oil of Rendering */, 41512 /* Training Spadone */);
