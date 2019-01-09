9753b316b3037316d59e31abb4c791004222f447
kdmxp256 <kdmxp512@gmail.com>
2019-01-09 09:50:58 -0800

    Update2 : Quick Recipie changes (#22)

--- Database/Patches/TrainingAcademy/4 CraftTable/06203 Academy Budiaq.sql
+++ Database/Patches/TrainingAcademy/4 CraftTable/06203 Academy Budiaq.sql
@@ -1,7 +1,7 @@
-DELETE FROM recipe WHERE id = 6300;
+DELETE FROM recipe WHERE id = 6203;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`)
-VALUES (6300, 0, 0, 0, 0, 45552 /* Academy Broad Sword */, 1, 'As you pour the Oil of Rendering onto the Training Broad Sword, it slowly deepens to a lustrous red.', 0, 0, 'You were unable pour the Oil of Rendering onto the Training Broad Sword.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0);
+VALUES (6203, 0, 0, 0, 0, 45543 /* Academy Budiaq */, 1, 'As you pour the Oil of Rendering onto the Training Budiaq, it slowly deepens to a lustrous red.', 0, 0, 'You were unable pour the Oil of Rendering onto the Training Budiaq.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0);
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`)
-VALUES (6300, 12711 /* Oil of Rendering */, 45554 /* Training Broad Sword */);
+VALUES (6203, 12711 /* Oil of Rendering */, 45545 /* Training Budiaq */);
