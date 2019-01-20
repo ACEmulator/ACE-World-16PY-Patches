06357191c5048f9310b64cd61248098b0542cc66
Keith McCormick <kdmxp512@gmail.com>
2019-01-19 19:02:14 -0800

    Correct wording in a few recipes

--- Database/Patches/TODUpdates/4 CraftTable/06162 Major Sparking Atlan Sword.sql
+++ Database/Patches/TODUpdates/4 CraftTable/06162 Major Sparking Atlan Sword.sql
@@ -1,7 +1,7 @@
 DELETE FROM recipe WHERE id = 6162;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`)
-VALUES (6162, 0, 0, 0, 0, 46096 /* Major Sparking Atlan Sword */, 1, "The gem slips into the weapon altering its properties.", 46096 /* Major Sparking Atlan Sword */, 1, "The gem slips into the weapon altering its properties.", 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0);
+VALUES (6162, 0, 0, 0, 0, 46096 /* Major Sparking Atlan Sword */, 1, "The gem slips out of the weapon altering its properties.", 46096 /* Major Sparking Atlan Sword */, 1, "The gem slips out of the weapon altering its properties.", 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0);
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`)
 VALUES (6162, 6127 /* Stone Tool */,  46095 /* Blackfire Sparking Atlan Sword */);
