653f47c09a214148fd53b5918ea8b4d14eca3fd0
Keith McCormick <kdmxp512@gmail.com>
2019-01-26 22:20:55 -0800

    Add Sanamar landblock instance nodes

---
+++ Database/Patches/TODUpdates/6 LandBlockExtendedData/2AE3.sql
@@ -0,0 +1,72 @@
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018176,  7924, 719519758, 27.4197, 132.2896, 53.1225, 0.685966, 0, 0, -0.727633, False); /* Linkable Monster Generator ( 5 Min.) */
+/* @teleloc 0x2AE3000E [27.419737 132.289627 53.122459] 0.685966 0.000000 0.000000 -0.727633 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018177, 30535, 719519758, 36.3618, 131.723, 53.0545, -0.707107, 0, 0, -0.707107, False); /* Beacon Tower Exit */
+/* @teleloc 0x2AE3000E [36.361800 131.723000 53.054500] -0.707107 0.000000 0.000000 -0.707107 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018178, 30535, 719519790, 131.548, 132.248, 53.0543, 0.707107, 0, 0, -0.707107, False); /* Beacon Tower Exit */
+/* @teleloc 0x2AE3002E [131.548000 132.248000 53.054300] 0.707107 0.000000 0.000000 -0.707107 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018179, 30535, 719519776, 83.8575, 180.336, 53.0564, -0.999667, 0, 0, -0.0258187, False); /* Beacon Tower Exit */
+/* @teleloc 0x2AE30020 [83.857500 180.336000 53.056400] -0.999667 0.000000 0.000000 -0.025819 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018180, 30441, 719519769, 83.8046, 18.64673, 53.1806, -0.023773, 0, 0, -0.999717, True); /* Blue Phyntos Wasp */
+/* @teleloc 0x2AE30019 [83.804600 18.646730 53.180600] -0.023773 0.000000 0.000000 -0.999717 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018181, 30440, 719519771, 85.34198, 65.99232, 53.12326, 0.042952, 0, 0, 0.999077, True); /* Water Golem */
+/* @teleloc 0x2AE3001B [85.341980 65.992320 53.123260] 0.042952 0.000000 0.000000 0.999077 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018182, 30441, 719519773, 83.78499, 98.22715, 63.36037, -0.019711, 0, 0, 0.999806, True); /* Blue Phyntos Wasp */
+/* @teleloc 0x2AE3001D [83.784990 98.227150 63.360370] -0.019711 0.000000 0.000000 0.999806 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018183, 30440, 719519774, 85.00586, 126.5535, 63.12326, -0.029821, 0, 0, -0.999555, True); /* Water Golem */
+/* @teleloc 0x2AE3001E [85.005860 126.553500 63.123260] -0.029821 0.000000 0.000000 -0.999555 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018184, 28702, 719519774, 84.0645, 132.0857, 63.12326, 0.004913, 0, 0, 0.999988, True); /* Beacon */
+/* @teleloc 0x2AE3001E [84.064500 132.085700 63.123260] 0.004913 0.000000 0.000000 0.999988 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018185, 30441, 719519776, 87.42494, 174.5162, 53.12436, 0.326583, 0, 0, 0.945168, True); /* Blue Phyntos Wasp */
+/* @teleloc 0x2AE30020 [87.424940 174.516200 53.124360] 0.326583 0.000000 0.000000 0.945168 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018186, 30441, 719519776, 79.03531, 175.3815, 53.12436, -0.226693, 0, 0, 0.973966, True); /* Blue Phyntos Wasp */
+/* @teleloc 0x2AE30020 [79.035310 175.381500 53.124360] -0.226693 0.000000 0.000000 0.973966 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018187, 30440, 719519790, 126.6837, 135.2831, 53.12226, -0.696801, 0, 0, -0.717264, True); /* Water Golem */
+/* @teleloc 0x2AE3002E [126.683700 135.283100 53.122260] -0.696801 0.000000 0.000000 -0.717264 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018188, 30440, 719519790, 126.9016, 127.7563, 53.12226, -0.696801, 0, 0, -0.717264, True); /* Water Golem */
+/* @teleloc 0x2AE3002E [126.901600 127.756300 53.122260] -0.696801 0.000000 0.000000 -0.717264 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018189, 30440, 719519758, 40.77726, 136.0053, 53.12246, 0.480437, 0, 0, -0.877029, True); /* Water Golem */
+/* @teleloc 0x2AE3000E [40.777260 136.005300 53.122460] 0.480437 0.000000 0.000000 -0.877029 */
+
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924018190, 30440, 719519758, 40.81124, 127.1494, 53.12246, 0.810121, 0, 0, -0.586262, True); /* Water Golem */
+/* @teleloc 0x2AE3000E [40.811240 127.149400 53.122460] 0.810121 0.000000 0.000000 -0.586262 */
+
+REPLACE INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`)
+VALUES (1924018176, 1924018180) /* Blue Phyntos Wasp */
+	 , (1924018176, 1924018181) /* Water Golem */
+	 , (1924018176, 1924018182) /* Blue Phyntos Wasp */
+	 , (1924018176, 1924018183) /* Water Golem */
+	 , (1924018176, 1924018184) /* Beacon */
+	 , (1924018176, 1924018185) /* Blue Phyntos Wasp */
+	 , (1924018176, 1924018186) /* Blue Phyntos Wasp */
+	 , (1924018176, 1924018187) /* Water Golem */
+	 , (1924018176, 1924018188) /* Water Golem */
+	 , (1924018176, 1924018189) /* Water Golem */
+	 , (1924018176, 1924018190) /* Water Golem */
---
+++ Database/Patches/TODUpdates/6 LandBlockExtendedData/32D9.sql
@@ -0,0 +1,101 @@
+/* Linkable Monster Generators */
+REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1932365824,  1154, 853082164, 146.843, 83.011, 52.005, -0.670061, 0, 0, 0.742306, False); /* Linkable Monster Generator */
+/* @teleloc 0x32D90034 [146.843140 83.011261 52.005001] -0.670061 0.000000 0.000000 0.742306 */
+
+/* ContainersChests */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (143, 1932365825, 853082368, 126.473, 85.517, 52, -0.7071068, 0, 0, -0.7071068, False) /* Chest */
+     , (143, 1932365826, 853082400, 127.415, 127.864, 56, -4.37114E-08, 0, 0, -1, False) /* Chest */
+     , (143, 1932365827, 853082431, 131.857, 32.4426, 58, -4.37114E-08, 0, 0, -1, False) /* Chest */
+     , (143, 1932365828, 853082431, 130.595, 32.4694, 58, -4.37114E-08, 0, 0, -1, False) /* Chest */
+     , (143, 1932365829, 853082436, 161.45, 34.7341, 52.09459, -0.7071068, 0, 0, -0.7071068, False) /* Chest */
+     , (143, 1932365830, 853082436, 161.454, 33.6788, 52.09459, -0.7071068, 0, 0, -0.7071068, False) /* Chest */;
+
+/* DoorsAluvianHouse */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (412, 1932365831, 853082382, 158.636, 139.329, 52.082, 1, 0, 0, 0, False) /* Door */
+     , (412, 1932365832, 853082384, 160.786, 140.804, 52.082, 0.7071068, 0, 0, 0.7071068, False) /* Door */
+     , (412, 1932365833, 853082388, 151.161, 140.679, 56.082, 0.7071068, 0, 0, -0.7071068, False) /* Door */
+     , (412, 1932365834, 853082404, 124.925, 134.75, 52.082, -0.707107, 0, 0, -0.707107, False) /* Door */
+     , (412, 1932365835, 853082406, 123.45, 136.9, 52.082, 0, 0, 0, -1, False) /* Door */
+     , (412, 1932365836, 853082410, 123.575, 127.275, 56.082, -1, 0, 0, 0, False) /* Door */
+     , (412, 1932365837, 853082426, 124.84, 32.0111, 52.082, 1, 0, 0, 0, False) /* Door */
+     , (412, 1932365838, 853082427, 131.163, 41.4899, 52.082, -4.37114E-08, 0, 0, -1, False) /* Door */
+     , (412, 1932365839, 853082163, 148.616, 61.2369, 52.082, 0.7071068, 0, 0, -0.7071068, False) /* Door */
+     , (412, 1932365840, 853082163, 152.834, 65.2632, 52.082, -4.37114E-08, 0, 0, -1, False) /* Door */
+     , (412, 1932365841, 853082156, 129.855, 80.9885, 52.082, 1, 0, 0, 0, False) /* Door */
+     , (412, 1932365842, 853082156, 133.997, 85.05, 52.082, -0.7071068, 0, 0, -0.7071068, False) /* Door */
+     , (412, 1932365843, 853082166, 152.474, 134.269, 52.082, 1, 0, 0, 0, False) /* Door */
+     , (412, 1932365844, 853082166, 158.261, 126.754, 52.082, 0.7071068, 0, 0, 0.7071068, False) /* Door */
+     , (412, 1932365845, 853082166, 161.086, 126.279, 56.082, 0.7071068, 0, 0, 0.7071068, False) /* Door */
+     , (412, 1932365846, 853082158, 129.985, 128.588, 52.082, -0.7071068, 0, 0, -0.7071068, False) /* Door */
+     , (412, 1932365847, 853082158, 137.5, 134.375, 52.082, 0, 0, 0, -1, False) /* Door */
+     , (412, 1932365848, 853082158, 137.975, 137.2, 56.082, 0, 0, 0, -1, False) /* Door */
+     , (412, 1932365849, 853082382, 158.644, 139.547, 52.082, -4.37114E-08, 0, 0, -1, False) /* Door */
+     , (412, 1932365850, 853082404, 124.685, 134.762, 52.082, 0.707107, 0, 0, -0.707107, False) /* Door */;
+
+/* HotSpots */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (4179, 1932365851, 853082162, 155.867, 25.8664, 52.95, 0.08336759, 0, 0, -0.9965189, False) /* Bonfire */;
+
+/* Doors */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (30938, 1932365852, 853082162, 163.528, 36.0184, 52.17788, -4.37114E-08, 0, 0, -1, False) /* Door */;
+
+/* Vendors */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (30010, 1932365853, 853082378, 157.338, 138.145, 56, 0.671526, 0, 0, -0.740981, True) /* Apprentice Scrivener of Item and War Magic */
+     , (30009, 1932365854, 853082378, 164.19, 140.292, 56.005, -0.404737, 0, 0, -0.9144331, True) /* Apprentice Scrivener of Item and War Magic */
+     , (30012, 1932365856, 853082379, 162.609, 138.232, 52, 0.9997354, 0, 0, -0.0230045, True) /* Apprentice Scrivener of Life Magic */
+     , (30011, 1932365857, 853082381, 154.598, 136.964, 52.005, -0.7106092, 0, 0, -0.7035869, True) /* Apprentice Scrivener of Life Magic */
+     , (30021, 1932365858, 853082393, 164.38, 127.473, 52.005, -0.6755413, 0, 0, -0.7373221, True) /* Apprentice Scrivener of Creature Magic */
+     , (30019, 1932365859, 853082400, 128.105, 133.886, 56.005, -0.8374808, 0, 0, -0.5464667, True) /* Journeyman Scrivener of War Magic */
+     , (20210, 1932365860, 853082400, 123.625, 136.855, 56.005, 0.6642127, 0, 0, -0.7475436, True) /* Scrivener of Creature Magic */
+     , (20226, 1932365861, 853082400, 127.632, 129.203, 56.005, -0.9125881, 0, 0, -0.4088802, True) /* Scrivener of War Magic */
+     , (30025, 1932365862, 853082401, 123.97, 140.312, 52, -0.038784, 0, 0, -0.9992476, True) /* Journeyman Scrivener of Creature Magic */
+     , (30029, 1932365863, 853082403, 124.192, 127.708, 52.005, 0.7021591, 0, 0, -0.7120201, True) /* Journeyman Scrivener of Item Magic */
+     , (30015, 1932365864, 853082415, 136.766, 138.7, 52.005, 0.06261525, 0, 0, -0.9980378, True) /* Journeyman Scrivener of Life Magic */
+     , (30039, 1932365865, 853082417, 152.932, 59.0308, 52.005, -0.9672073, 0, 0, -0.2539883, True) /* Healer Deme du Laur */
+     , (30032, 1932365866, 853082425, 124.247, 36.735, 52.005, 0.872912, 0, 0, -0.4878777, True) /* Scribe Benno di Hergeaux */
+     , (30038, 1932365868, 853082430, 132.198, 36.9837, 55.005, -0.9254864, 0, 0, -0.3787807, True) /* Archmage Luchessa du Lamiere */
+     , (30040, 1932365869, 853082162, 157.084, 30.7475, 52.005, 0.9997516, 0, 0, -0.02228918, True) /* Blacksmith Karlus Loc */
+     , (30022, 1932365870, 853082166, 159.416, 128.755, 56.005, 0.01156479, 0, 0, -0.9999331, True) /* Apprentice Scrivener of Creature Magic */
+     , (20222, 1932365871, 853082158, 135.503, 135.641, 56.005, 0.6831837, 0, 0, -0.7302465, True) /* Scrivener of Life Magic */
+     , (20214, 1932365872, 853082158, 140.622, 135.596, 56.005, -0.661552, 0, 0, -0.7498993, True) /* Scrivener of Item Magic */;
+
+/* CreaturesTownCriers */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (30063, 1932365873, 853082171, 184.881, 54.456, 52.005, 0.833052, 0, 0, -0.553194, True) /* Town Crier */;
+
+/* CreaturesNPCs */
+REPLACE INTO `landblock_instance` (`weenie_Class_Id`, `guid`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (34145, 1932365874, 853082173, 175.303, 100.337, 52.005, -0.723443, 0, 0, -0.690384, True) /* Trilaine di Ricard */
+     , (30073, 1932365875, 853082368, 129.357, 87.2847, 52.005, 0.440398, 0, 0, -0.897803, True) /* Luressi di Maki */
+     , (30267, 1932365878, 853082162, 159.013, 46.1829, 52.005, 0.684133, 0, 0, -0.729357, True) /* Paissen du Kim */
+     , (30266, 1932365879, 853082157, 126.004, 109.671, 52.005, 0.6957896, 0, 0, -0.7182456, True) /* Stuven di Bonachien */;
+
+REPLACE INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`)
+VALUES (1932365824, 1932365873) /* Town Crier */
+	 , (1932365824, 1932365853) /* Apprentice Scrivener of Item and War Magic */
+	 , (1932365824, 1932365854) /* Apprentice Scrivener of Item and War Magic */
+	 , (1932365824, 1932365856) /* Apprentice Scrivener of Life Magic */
+	 , (1932365824, 1932365857) /* Apprentice Scrivener of Life Magic */
+	 , (1932365824, 1932365858) /* Apprentice Scrivener of Creature Magic */
+	 , (1932365824, 1932365859) /* Journeyman Scrivener of War Magic */
+	 , (1932365824, 1932365860) /* Scrivener of Creature Magic */
+	 , (1932365824, 1932365861) /* Scrivener of War Magic */
+	 , (1932365824, 1932365862) /* Journeyman Scrivener of Creature Magic */
+	 , (1932365824, 1932365863) /* Journeyman Scrivener of Item Magic */
+	 , (1932365824, 1932365864) /* Journeyman Scrivener of Life Magic */
+	 , (1932365824, 1932365865) /* Healer Deme du Laur */
+	 , (1932365824, 1932365866) /* Scribe Benno di Hergeaux */
+	 , (1932365824, 1932365868) /* Archmage Luchessa du Lamiere */
+	 , (1932365824, 1932365869) /* Blacksmith Karlus Loc */
+	 , (1932365824, 1932365870) /* Apprentice Scrivener of Creature Magic */
+	 , (1932365824, 1932365871) /* Scrivener of Life Magic */
+	 , (1932365824, 1932365872) /* Scrivener of Item Magic */
+	 , (1932365824, 1932365874) /* Trilaine di Ricard */
+	 , (1932365824, 1932365875) /* Luressi di Maki */
+	 , (1932365824, 1932365878) /* Paissen du Kim */
+	 , (1932365824, 1932365879) /* Stuven di Bonachien */;
