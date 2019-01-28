81e5dacd006b9aac8367122528d3630f904b5828
kdmxp256 <kdmxp512@gmail.com>
2019-01-27 19:48:32 -0800

    Add Four TOD+ Era Spells (#33)
    * Add Four TOD+ Era Spells
    
    * Minor change

---
+++ Database/Patches/TODUpdates/2 SpellTableExtendedData/05175 Facility Hub Recall.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell` WHERE `id` = 5175;
+
+INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`)
+VALUES (5175, 'Facility Hub Recall', 2315387410, 58.6391, -89.9231, 6.005, 0.995004, 0, 0, -0.099833);
+/* @teleloc 0x8A020212 [58.639100 -89.923100 6.005000] 0.995004 0.000000 0.000000 -0.099833 */
