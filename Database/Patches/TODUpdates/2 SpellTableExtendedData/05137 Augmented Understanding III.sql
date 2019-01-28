3d8ecc3e63216b028b817c1aba0adcbce27a7222
Ty Conner <tyconner@itcproductions.com>
2019-01-27 22:44:11 -0500

    Minor change

--- Database/Patches/TODUpdates/2 SpellTableExtendedData/05137 Augmented Understanding III.sql
+++ Database/Patches/TODUpdates/2 SpellTableExtendedData/05137 Augmented Understanding III.sql
@@ -1,2 +1,4 @@
-REPLACE INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`)
+DELETE FROM `spell` WHERE `id` = 5137;
+
+INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`)
 VALUES (5137, 'Augmented Understanding III', 20488 /* Float, SingleStat, Multiplicative */, 163 /* GlobalXpMod */, 1.06);
--- Database/Patches/TODUpdates/2 SpellTableExtendedData/05154 Augmented Understanding II.sql
+++ Database/Patches/TODUpdates/2 SpellTableExtendedData/05154 Augmented Understanding II.sql
@@ -1,2 +1,4 @@
-REPLACE INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`)
+DELETE FROM `spell` WHERE `id` = 5154;
+
+INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`)
 VALUES (5154, 'Augmented Understanding II', 20488 /* Float, SingleStat, Multiplicative */, 163 /* GlobalXpMod */, 1.04);
