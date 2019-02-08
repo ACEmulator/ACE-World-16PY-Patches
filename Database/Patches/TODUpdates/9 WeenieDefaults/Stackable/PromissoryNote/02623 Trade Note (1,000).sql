b37e4f98847c0e2f5cea70051d50a2e12f90891d
Ty Conner <tyconner@itcproductions.com>
2019-02-08 01:04:32 -0500

    fix ts

--- Database/Patches/TODUpdates/9 WeenieDefaults/Stackable/PromissoryNote/02623 Trade Note (1,000).sql
+++ Database/Patches/TODUpdates/9 WeenieDefaults/Stackable/PromissoryNote/02623 Trade Note (1,000).sql
@@ -1,5 +1,7 @@
+DELETE FROM `weenie` WHERE `class_Id` = 2623;
+
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (2623, 'tradenote1000', 51, '2005-02-09 10:00:00') /* Stackable */;
+VALUES (2623, 'tradenote1000', 51, '2019-02-08 00:45:15') /* Stackable */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (2623,   1,     262144) /* ItemType - PromissoryNote */
--- Database/Patches/TODUpdates/9 WeenieDefaults/Stackable/PromissoryNote/07376 Trade Note (25,000).sql
+++ Database/Patches/TODUpdates/9 WeenieDefaults/Stackable/PromissoryNote/07376 Trade Note (25,000).sql
@@ -1,5 +1,7 @@
+DELETE FROM `weenie` WHERE `class_Id` = 7376;
+
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (7376, 'tradenote25000', 51, '2005-02-09 10:00:00') /* Stackable */;
+VALUES (7376, 'tradenote25000', 51, '2019-02-08 00:45:15') /* Stackable */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (7376,   1,     262144) /* ItemType - PromissoryNote */
