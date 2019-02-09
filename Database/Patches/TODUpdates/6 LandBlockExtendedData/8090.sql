59fcb88a49b74fc63e9e8bbefc1a586c9f40b395
Keith McCormick <kdmxp512@gmail.com>
2019-02-09 15:26:57 -0800

    Fix added guids for location 8090

--- Database/Patches/TODUpdates/6 LandBlockExtendedData/8090.sql
+++ Database/Patches/TODUpdates/6 LandBlockExtendedData/8090.sql
@@ -19,93 +19,93 @@ VALUES (2013855832, 41525, 2156920853, 58.2231, 114.318, 132.805, 0.240812, 0, 0
 /* @teleloc 0x80900015 [58.223100 114.318000 132.805000] 0.240812 0.000000 0.000000 -0.970572 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855799, 38447, 2156921182, 12.0963, 13.8803, 117.655, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Advanced Equipment Chest */
+VALUES (2013855833, 38447, 2156921182, 12.0963, 13.8803, 117.655, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Advanced Equipment Chest */
 /* @teleloc 0x8090015E [12.096300 13.880300 117.655000] 0.000000 0.000000 0.000000 -1.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855800, 38447, 2156921182, 9.39006, 13.8432, 117.655, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Advanced Equipment Chest */
+VALUES (2013855834, 38447, 2156921182, 9.39006, 13.8432, 117.655, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Advanced Equipment Chest */
 /* @teleloc 0x8090015E [9.390060 13.843200 117.655000] 0.000000 0.000000 0.000000 -1.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855801, 38447, 2156921182, 14.9082, 13.8858, 117.655, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Advanced Equipment Chest */
+VALUES (2013855835, 38447, 2156921182, 14.9082, 13.8858, 117.655, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Advanced Equipment Chest */
 /* @teleloc 0x8090015E [14.908200 13.885800 117.655000] 0.000000 0.000000 0.000000 -1.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855802, 38443, 2156921182, 17.517, 18.12, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Mixed Equipment Chest */
+VALUES (2013855836, 38443, 2156921182, 17.517, 18.12, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Mixed Equipment Chest */
 /* @teleloc 0x8090015E [17.517000 18.120000 117.655000] 0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855803, 38443, 2156921182, 17.5196, 16.0198, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Mixed Equipment Chest */
+VALUES (2013855837, 38443, 2156921182, 17.5196, 16.0198, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Mixed Equipment Chest */
 /* @teleloc 0x8090015E [17.519600 16.019800 117.655000] 0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855804, 38443, 2156921182, 17.5108, 20.0197, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Mixed Equipment Chest */
+VALUES (2013855838, 38443, 2156921182, 17.5108, 20.0197, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Mixed Equipment Chest */
 /* @teleloc 0x8090015E [17.510800 20.019700 117.655000] 0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855805, 38445, 2156921182, 17.565, 25.947, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Magic Chest */
+VALUES (2013855839, 38445, 2156921182, 17.565, 25.947, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Magic Chest */
 /* @teleloc 0x8090015E [17.565000 25.947000 117.655000] 0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855806, 38445, 2156921182, 17.5408, 23.8472, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Magic Chest */
+VALUES (2013855840, 38445, 2156921182, 17.5408, 23.8472, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Magic Chest */
 /* @teleloc 0x8090015E [17.540800 23.847200 117.655000] 0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855807, 38445, 2156921182, 17.5617, 28.0072, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Magic Chest */
+VALUES (2013855841, 38445, 2156921182, 17.5617, 28.0072, 117.655, 0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Magic Chest */
 /* @teleloc 0x8090015E [17.561700 28.007200 117.655000] 0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855808, 38446, 2156921182, 6.414, 25.922, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Weapon Chest */
+VALUES (2013855842, 38446, 2156921182, 6.414, 25.922, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Weapon Chest */
 /* @teleloc 0x8090015E [6.414000 25.922000 117.655000] -0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855809, 38446, 2156921182, 6.42735, 27.9684, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Weapon Chest */
+VALUES (2013855843, 38446, 2156921182, 6.42735, 27.9684, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Weapon Chest */
 /* @teleloc 0x8090015E [6.427350 27.968400 117.655000] -0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855810, 38446, 2156921182, 6.4281, 23.9288, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Weapon Chest */
+VALUES (2013855844, 38446, 2156921182, 6.4281, 23.9288, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Weapon Chest */
 /* @teleloc 0x8090015E [6.428100 23.928800 117.655000] -0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855811, 38444, 2156921182, 6.39909, 18.0825, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Armor Chest */
+VALUES (2013855845, 38444, 2156921182, 6.39909, 18.0825, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Armor Chest */
 /* @teleloc 0x8090015E [6.399090 18.082500 117.655000] -0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855812, 38444, 2156921182, 6.40081, 20.2628, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Armor Chest */
+VALUES (2013855846, 38444, 2156921182, 6.40081, 20.2628, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Armor Chest */
 /* @teleloc 0x8090015E [6.400810 20.262800 117.655000] -0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855813, 38444, 2156921182, 6.3597, 16.09, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Armor Chest */
+VALUES (2013855847, 38444, 2156921182, 6.3597, 16.09, 117.655, -0.707107, 0, 0, -0.707107, False, '2019-02-09 06:17:23'); /* Mana Forge Armor Chest */
 /* @teleloc 0x8090015E [6.359700 16.090000 117.655000] -0.707107 0.000000 0.000000 -0.707107 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855814, 38457, 2156921182, 7.45872, 30.5271, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
+VALUES (2013855848, 38457, 2156921182, 7.45872, 30.5271, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
 /* @teleloc 0x8090015E [7.458720 30.527100 117.655000] 1.000000 0.000000 0.000000 0.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855815, 38457, 2156921182, 10.3755, 30.5465, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
+VALUES (2013855849, 38457, 2156921182, 10.3755, 30.5465, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
 /* @teleloc 0x8090015E [10.375500 30.546500 117.655000] 1.000000 0.000000 0.000000 0.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855816, 38457, 2156921182, 13.8522, 30.6479, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
+VALUES (2013855850, 38457, 2156921182, 13.8522, 30.6479, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
 /* @teleloc 0x8090015E [13.852200 30.647900 117.655000] 1.000000 0.000000 0.000000 0.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855817, 38457, 2156921182, 16.5013, 30.6595, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
+VALUES (2013855851, 38457, 2156921182, 16.5013, 30.6595, 117.655, 1, 0, 0, 0, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
 /* @teleloc 0x8090015E [16.501300 30.659500 117.655000] 1.000000 0.000000 0.000000 0.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855818, 38457, 2156921185, 14.3817, 33.0173, 124.05, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
+VALUES (2013855852, 38457, 2156921185, 14.3817, 33.0173, 124.05, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
 /* @teleloc 0x80900161 [14.381700 33.017300 124.050000] 0.000000 0.000000 0.000000 -1.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855819, 38457, 2156921185, 9.72385, 33.0282, 124.05, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
+VALUES (2013855853, 38457, 2156921185, 9.72385, 33.0282, 124.05, -4.37114E-08, 0, 0, -1, False, '2019-02-09 06:17:23'); /* Mana Forge Chest */
 /* @teleloc 0x80900161 [9.723850 33.028200 124.050000] 0.000000 0.000000 0.000000 -1.000000 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855830, 34726, 2156921182, 6.7585, 14.1265, 117.655, 0.92388, 0, 0, -0.382683, False, '2019-02-09 06:17:23'); /* Garbage Barrel */
+VALUES (2013855854, 34726, 2156921182, 6.7585, 14.1265, 117.655, 0.92388, 0, 0, -0.382683, False, '2019-02-09 06:17:23'); /* Garbage Barrel */
 /* @teleloc 0x8090015E [6.758500 14.126500 117.655000] 0.923880 0.000000 0.000000 -0.382683 */
 
 REPLACE INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2013855831, 34726, 2156921185, 9.3263, 38.6262, 124.064, 0.92388, 0, 0, -0.382683, False, '2019-02-09 06:17:23'); /* Garbage Barrel */
+VALUES (2013855855, 34726, 2156921185, 9.3263, 38.6262, 124.064, 0.92388, 0, 0, -0.382683, False, '2019-02-09 06:17:23'); /* Garbage Barrel */
 /* @teleloc 0x80900161 [9.326300 38.626200 124.064000] 0.923880 0.000000 0.000000 -0.382683 */
