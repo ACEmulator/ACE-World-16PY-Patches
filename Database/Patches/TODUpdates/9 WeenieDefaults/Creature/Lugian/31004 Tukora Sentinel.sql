d69b958dbe95c8ed2907c79011fe7ca922679203
Keith McCormick <kdmxp512@gmail.com>
2019-02-20 10:56:03 -0800

    Add missing semicolon

--- Database/Patches/TODUpdates/9 WeenieDefaults/Creature/Lugian/31004 Tukora Sentinel.sql
+++ Database/Patches/TODUpdates/9 WeenieDefaults/Creature/Lugian/31004 Tukora Sentinel.sql
@@ -18,7 +18,7 @@ VALUES (31004,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (31004,   1, True ) /* Stuck */
-     , (31004, 101, True ) /* CanGenerateRare */
+     , (31004, 101, True ) /* CanGenerateRare */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (31004,   1,       5) /* HeartbeatInterval */
