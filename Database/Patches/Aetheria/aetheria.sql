a8eea9506d739aa3cea3568c81f7f270a4b10115
gmriggs <gmriggs@gmail.com>
2019-03-19 03:15:42 -0400

    Adding aetheria objects to db

---
+++ Database/Patches/Aetheria/aetheria.sql
@@ -0,0 +1,309 @@
+DELETE FROM `weenie` WHERE `class_Id` = 42635;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42635, 'ace42635-aetheria', 38, '2019-03-17 00:00:00') /* Gem */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42635,   1,       2048) /* ItemType - Gem */
+     , (42635,   5,         50) /* EncumbranceVal */
+     , (42635,   9,          0) /* ValidLocations - None */
+     , (42635,  11,          1) /* MaxStackSize */
+     , (42635,  12,          1) /* StackSize */
+     , (42635,  13,         50) /* StackUnitEncumbrance */
+     , (42635,  15,      10000) /* StackUnitValue */
+     , (42635,  16,          1) /* ItemUseable - No */
+     , (42635,  18,          1) /* UiEffects - Magical */
+     , (42635,  19,      10000) /* Value */
+     , (42635,  65,        101) /* Placement - Resting */
+     , (42635,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (42635, 158,          7) /* WieldRequirements - Level */
+     , (42635, 159,          1) /* WieldSkillType - Axe */
+     , (42635, 160,         75) /* WieldDifficulty */
+     , (42635, 319,          1) /* ItemMaxLevel */
+     , (42635, 320,          2) /* ItemXpStyle - ScalesWithLevel */;
+
+INSERT INTO `weenie_properties_int64` (`object_Id`, `type`, `value`)
+VALUES (42635,   4,  750000000) /* ItemTotalXp */
+     , (42635,   5, 1000000000) /* ItemBaseXp */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42635,   1, False) /* Stuck */
+     , (42635,  11, True ) /* IgnoreCollisions */
+     , (42635,  13, True ) /* Ethereal */
+     , (42635,  14, True ) /* GravityStatus */
+     , (42635,  19, True ) /* Attackable */
+     , (42635,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42635,   1, 'Aetheria') /* Name */
+     , (42635,  16, 'A glowing ball of Coalesced Aetheria.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42635,   1,   33554809) /* Setup */
+     , (42635,   3,  536870932) /* SoundTable */
+     , (42635,   6,   67111919) /* PaletteBase */
+     , (42635,   8,  100690954) /* Icon */
+     , (42635,  22,  872415275) /* PhysicsEffectTable */
+     , (42635,  50,  100690996) /* IconOverlay */;
+DELETE FROM `weenie` WHERE `class_Id` = 42636;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42636, 'ace42636-aetheria', 38, '2019-02-10 00:00:00') /* Gem */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42636,   1,       2048) /* ItemType - Gem */
+     , (42636,   5,         50) /* EncumbranceVal */
+     , (42636,   9,          0) /* ValidLocations - None */
+     , (42636,  11,          1) /* MaxStackSize */
+     , (42636,  12,          1) /* StackSize */
+     , (42636,  13,         50) /* StackUnitEncumbrance */
+     , (42636,  15,      10000) /* StackUnitValue */
+     , (42636,  16,          1) /* ItemUseable - No */
+     , (42636,  18,          1) /* UiEffects - Magical */
+     , (42636,  19,      10000) /* Value */
+     , (42636,  65,        101) /* Placement - Resting */
+     , (42636,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (42636, 158,          7) /* WieldRequirements - Level */
+     , (42636, 159,          1) /* WieldSkillType - Axe */
+     , (42636, 160,        225) /* WieldDifficulty */
+     , (42636, 319,          1) /* ItemMaxLevel */
+     , (42636, 320,          2) /* ItemXpStyle - ScalesWithLevel */;
+
+INSERT INTO `weenie_properties_int64` (`object_Id`, `type`, `value`)
+VALUES (42636,   4,          0) /* ItemTotalXp */
+     , (42636,   5, 1000000000) /* ItemBaseXp */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42636,   1, False) /* Stuck */
+     , (42636,  11, True ) /* IgnoreCollisions */
+     , (42636,  13, True ) /* Ethereal */
+     , (42636,  14, True ) /* GravityStatus */
+     , (42636,  19, True ) /* Attackable */
+     , (42636,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42636,   1, 'Aetheria') /* Name */
+     , (42636,  16, 'A glowing ball of Coalesced Aetheria.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42636,   1,   33554809) /* Setup */
+     , (42636,   3,  536870932) /* SoundTable */
+     , (42636,   6,   67111919) /* PaletteBase */
+     , (42636,   8,  100690955) /* Icon */
+     , (42636,  22,  872415275) /* PhysicsEffectTable */
+     , (42636,  50,  100690996) /* IconOverlay */;
+DELETE FROM `weenie` WHERE `class_Id` = 42637;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42637, 'ace42637-aetheria', 38, '2019-02-10 00:00:00') /* Gem */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42637,   1,       2048) /* ItemType - Gem */
+     , (42637,   5,         50) /* EncumbranceVal */
+     , (42637,   9,          0) /* ValidLocations - None */
+     , (42637,  11,          1) /* MaxStackSize */
+     , (42637,  12,          1) /* StackSize */
+     , (42637,  13,         50) /* StackUnitEncumbrance */
+     , (42637,  15,      10000) /* StackUnitValue */
+     , (42637,  16,          1) /* ItemUseable - No */
+     , (42637,  18,          1) /* UiEffects - Magical */
+     , (42637,  19,      10000) /* Value */
+     , (42637,  65,        101) /* Placement - Resting */
+     , (42637,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (42637, 158,          7) /* WieldRequirements - Level */
+     , (42637, 159,          1) /* WieldSkillType - Axe */
+     , (42637, 160,        150) /* WieldDifficulty */
+     , (42637, 319,          1) /* ItemMaxLevel */
+     , (42637, 320,          2) /* ItemXpStyle - ScalesWithLevel */;
+
+INSERT INTO `weenie_properties_int64` (`object_Id`, `type`, `value`)
+VALUES (42637,   4,          0) /* ItemTotalXp */
+     , (42637,   5, 1000000000) /* ItemBaseXp */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42637,   1, False) /* Stuck */
+     , (42637,  11, True ) /* IgnoreCollisions */
+     , (42637,  13, True ) /* Ethereal */
+     , (42637,  14, True ) /* GravityStatus */
+     , (42637,  19, True ) /* Attackable */
+     , (42637,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42637,   1, 'Aetheria') /* Name */
+     , (42637,  16, 'A glowing ball of Coalesced Aetheria.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42637,   1,   33554809) /* Setup */
+     , (42637,   3,  536870932) /* SoundTable */
+     , (42637,   6,   67111919) /* PaletteBase */
+     , (42637,   8,  100690956) /* Icon */
+     , (42637,  22,  872415275) /* PhysicsEffectTable */
+     , (42637,  50,  100690996) /* IconOverlay */;
+DELETE FROM `weenie` WHERE `class_Id` = 42644;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42644, 'ace42644-aetheriapowder', 51, '2019-03-17 00:00:00') /* Stackable */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42644,   1,        128) /* ItemType - Misc */
+     , (42644,   5,          4) /* EncumbranceVal */
+     , (42644,  11,        100) /* MaxStackSize */
+     , (42644,  12,          1) /* StackSize */
+     , (42644,  13,          4) /* StackUnitEncumbrance */
+     , (42644,  15,          5) /* StackUnitValue */
+     , (42644,  16,          1) /* ItemUseable - No */
+     , (42644,  19,          5) /* Value */
+     , (42644,  65,        101) /* Placement - Resting */
+     , (42644,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42644,   1, False) /* Stuck */
+     , (42644,  11, True ) /* IgnoreCollisions */
+     , (42644,  13, True ) /* Ethereal */
+     , (42644,  14, True ) /* GravityStatus */
+     , (42644,  19, True ) /* Attackable */
+     , (42644,  69, False) /* IsSellable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42644,   1, 'Aetheria Powder') /* Name */
+     , (42644,  14, 'Ten Aetheria Powder may be given to Cypher in Xarabydun for a MFK.') /* Use */
+     , (42644,  20, 'Aetheria Powder') /* PluralName */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42644,   1,   33555208) /* Setup */
+     , (42644,   3,  536870932) /* SoundTable */
+     , (42644,   6,   67111919) /* PaletteBase */
+     , (42644,   8,  100668385) /* Icon */
+     , (42644,  22,  872415275) /* PhysicsEffectTable */;
+DELETE FROM `weenie` WHERE `class_Id` = 42645;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42645, 'ace42645-aetheriamanastone', 38, '2019-03-17 00:00:00') /* Gem */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42645,   1,       2048) /* ItemType - Gem */
+     , (42645,   5,          1) /* EncumbranceVal */
+     , (42645,  11,          1) /* MaxStackSize */
+     , (42645,  12,          1) /* StackSize */
+     , (42645,  13,          1) /* StackUnitEncumbrance */
+     , (42645,  15,          0) /* StackUnitValue */
+     , (42645,  16,     524296) /* ItemUseable - SourceContainedTargetContained */
+     , (42645,  18,         16) /* UiEffects - BoostStamina */
+     , (42645,  19,          0) /* Value */
+     , (42645,  65,        101) /* Placement - Resting */
+     , (42645,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (42645,  94,       2048) /* TargetType - Gem */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42645,   1, False) /* Stuck */
+     , (42645,  11, True ) /* IgnoreCollisions */
+     , (42645,  13, True ) /* Ethereal */
+     , (42645,  14, True ) /* GravityStatus */
+     , (42645,  19, True ) /* Attackable */
+     , (42645,  22, True ) /* Inscribable */
+     , (42645,  69, False) /* IsSellable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42645,   1, 'Aetheria Mana Stone') /* Name */
+     , (42645,  14, 'Use this on Coalesced Aetheria to bring its sigil to the surface and activate it.') /* Use */
+     , (42645,  16, 'An specially enhanced mana stone used to bathe Coalesced Aetheria in mana.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42645,   1,   33556407) /* Setup */
+     , (42645,   3,  536870932) /* SoundTable */
+     , (42645,   8,  100690958) /* Icon */
+     , (42645,  22,  872415275) /* PhysicsEffectTable */;
+DELETE FROM `weenie` WHERE `class_Id` = 42646;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42646, 'ace42646-aetheriadesiccant', 44, '2019-03-17 00:00:00') /* CraftTool */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42646,   1,   67108864) /* ItemType - CraftAlchemyIntermediate */
+     , (42646,   5,          0) /* EncumbranceVal */
+     , (42646,  11,       1000) /* MaxStackSize */
+     , (42646,  12,          1) /* StackSize */
+     , (42646,  13,          0) /* StackUnitEncumbrance */
+     , (42646,  15,       1000) /* StackUnitValue */
+     , (42646,  16,     524296) /* ItemUseable - SourceContainedTargetContained */
+     , (42646,  18,         16) /* UiEffects - BoostStamina */
+     , (42646,  19,       1000) /* Value */
+     , (42646,  65,        101) /* Placement - Resting */
+     , (42646,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (42646,  94,       2048) /* TargetType - Gem */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42646,   1, False) /* Stuck */
+     , (42646,  11, True ) /* IgnoreCollisions */
+     , (42646,  13, True ) /* Ethereal */
+     , (42646,  14, True ) /* GravityStatus */
+     , (42646,  19, True ) /* Attackable */
+     , (42646,  69, False) /* IsSellable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42646,   1, 'Aetheria Desiccant') /* Name */
+     , (42646,  14, 'Use this on aetheria to desiccate it and resulting in a powder of pure aetheria. This will destroy any ability to activate the aetheria.') /* Use */
+     , (42646,  16, 'A desiccant used to remove all moisture from aetheria destroying it. ') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42646,   1,   33556407) /* Setup */
+     , (42646,   3,  536870932) /* SoundTable */
+     , (42646,   8,  100690957) /* Icon */
+     , (42646,  22,  872415275) /* PhysicsEffectTable */;
+DELETE FROM `weenie` WHERE `class_Id` = 43133;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (43133, 'ace43133-aetheriapowder', 51, '2019-03-17 00:00:00') /* Stackable */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43133,   1,        128) /* ItemType - Misc */
+     , (43133,   5,          4) /* EncumbranceVal */
+     , (43133,  11,        100) /* MaxStackSize */
+     , (43133,  12,          1) /* StackSize */
+     , (43133,  13,          4) /* StackUnitEncumbrance */
+     , (43133,  15,          5) /* StackUnitValue */
+     , (43133,  16,          1) /* ItemUseable - No */
+     , (43133,  19,          5) /* Value */
+     , (43133,  65,        101) /* Placement - Resting */
+     , (43133,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (43133,   1, False) /* Stuck */
+     , (43133,  11, True ) /* IgnoreCollisions */
+     , (43133,  13, True ) /* Ethereal */
+     , (43133,  14, True ) /* GravityStatus */
+     , (43133,  19, True ) /* Attackable */
+     , (43133,  69, False) /* IsSellable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43133,   1, 'Aetheria Powder') /* Name */
+     , (43133,  14, 'Ten Aetheria Powder may be given to Cypher in Xarabydun for an MMD.') /* Use */
+     , (43133,  20, 'Aetheria Powder') /* PluralName */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43133,   1,   33555208) /* Setup */
+     , (43133,   3,  536870932) /* SoundTable */
+     , (43133,   6,   67111919) /* PaletteBase */
+     , (43133,   8,  100668383) /* Icon */
+     , (43133,  22,  872415275) /* PhysicsEffectTable */;
+
+DELETE FROM `recipe` WHERE `id` = 8001;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (8001, 0, 0 /* None */, 0, 0, 43133 /* Aetheria Powder */, 1, 'You use the desiccant on the aetheria to create an aetheria powder.', 43133 /* Aetheria Powder */, 1, 'You use the desiccant on the aetheria to create an aetheria powder.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-03-17 00:00:00');
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 8001;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (8001, 42646 /* Aetheria Desiccant */,  42635 /* Aetheria */, '2019-03-17 00:00:00')
+     , (8001, 42646 /* Aetheria Desiccant */,  42637 /* Aetheria */, '2019-03-17 00:00:00');
+
+DELETE FROM `recipe` WHERE `id` = 8002;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (8002, 0, 0 /* None */, 0, 0, 42644 /* Aetheria Powder */, 1, 'You use the desiccant on the aetheria to create an aetheria powder.', 42644 /* Aetheria Powder */, 1, 'You use the desiccant on the aetheria to create an aetheria powder.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-03-17 00:00:00');
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 8002;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (8002, 42646 /* Aetheria Desiccant */,  42636 /* Aetheria */, '2019-03-17 00:00:00');
