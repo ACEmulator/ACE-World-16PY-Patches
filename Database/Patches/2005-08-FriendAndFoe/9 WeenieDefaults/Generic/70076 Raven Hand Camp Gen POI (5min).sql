578d786410cb08170f68bccb3e94f7efeb7fcdda
Keith McCormick <22699181+kdmxp256@users.noreply.github.com>
2019-03-29 18:32:35 -0700

    33600 spell corrections and Raven Hand gen update

--- Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Generic/70076 Raven Hand Camp Gen POI (5min).sql
+++ Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Generic/70076 Raven Hand Camp Gen POI (5min).sql
@@ -26,15 +26,15 @@ VALUES (70076,   1,   33555051) /* Setup */
      , (70076,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (70076, 0.08333334, 31405, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+VALUES (70076, 0.08333334, 31404, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.1666667, 31400, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Conscript (31400) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.25, 31402, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Augur (31402) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (70076, 0.3333333, 31405, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (70076, 0.3333333, 31404, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.4166667, 31400, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Conscript (31400) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.5, 31402, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Augur (31402) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (70076, 0.5833333, 31405, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (70076, 0.5833333, 31404, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.6666667, 31400, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Conscript (31400) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.75, 31402, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Augur (31402) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (70076, 0.8333333, 31405, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (70076, 0.8333333, 31404, 900, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Hunter (31405) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 0.9166667, 31400, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Conscript (31400) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
      , (70076, 1, 31402, 900, 2, 2, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Raven Augur (31402) (x2 up to max of 2) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
