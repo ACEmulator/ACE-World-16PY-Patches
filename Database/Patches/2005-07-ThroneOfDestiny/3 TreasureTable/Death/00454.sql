798915487c402fbd322f8669d3da42471c953f94
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-02 03:31:05 -0700

    Adjust level 100 mobs' loot tiers (#72)

---
+++ Database/Patches/2005-07-ThroneOfDestiny/3 TreasureTable/Death/00454.sql
@@ -0,0 +1,4 @@
+DELETE FROM `treasure_death` WHERE `treasure_Type` = 454;
+
+INSERT INTO `treasure_death` (`treasure_Type`, `tier`, `loot_Quality_Mod`, `unknown_Chances`, `item_Chance`, `item_Min_Amount`, `item_Max_Amount`, `item_Treasure_Type_Selection_Chances`, `magic_Item_Chance`, `magic_Item_Min_Amount`, `magic_Item_Max_Amount`, `magic_Item_Treasure_Type_Selection_Chances`, `mundane_Item_Chance`, `mundane_Item_Min_Amount`, `mundane_Item_Max_Amount`, `mundane_Item_Type_Selection_Chances`, `last_Modified`)
+VALUES (454, 5, 0, 19, 100, 1, 2, 8, 100, 1, 2, 8, 100, 0, 1, 7, '2019-04-01 10:00:00');
