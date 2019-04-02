31b177b25ffb42c5b124d386b3c48765b11bf06a
Ty Conner <tyconner@itcproductions.com>
2019-04-02 05:31:28 -0500

    Update MadCowEvent (#73)
    * Update MadCowEvent
    
    * Update ACE-World-16PY
    
    * Update ACE-World-16PY

--- Database/Patches/2020-04-MadCowEvent/CowGenUpdates.sql
+++ Database/Patches/2020-04-MadCowEvent/CowGenUpdates.sql
@@ -1,8 +1,8 @@
 /* eventmadcowgen */
 UPDATE weenie_properties_int
-SET `value` = 1554112860 /* 04/01/2019 10:01:00 */
+SET `value` = 1585735260 /* 04/01/2020 10:01:00 */
 WHERE `object_Id` = 23631 AND `type` = 143 /* GeneratorStartTime */;
 
 UPDATE weenie_properties_int
-SET `value` = 1554199140 /* 04/02/2019 09:59:00 */
+SET `value` = 1585821540 /* 04/02/2020 09:59:00 */
 WHERE `object_Id` = 23631 AND `type` = 144 /* GeneratorEndTime */;
