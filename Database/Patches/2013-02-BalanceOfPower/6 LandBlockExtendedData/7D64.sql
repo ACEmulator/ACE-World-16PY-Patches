2ba467c7268194f1a8d6708ce8815190cc31a4bf
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-10 02:09:42 -0700

    Add a few spawn maps missed in previous PR

---
+++ Database/Patches/2013-02-BalanceOfPower/6 LandBlockExtendedData/7D64.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (2010529892, 49517, 2103705902, 85.3193, 89.8775, 15.20575, 0.999736, 0, 0, -0.0229798, False, '2019-02-10 00:00:00'); /* Iaret */
+/* @teleloc 0x7D64012E [85.319300 89.877500 15.205750] 0.999736 0.000000 0.000000 -0.022980 */
---
+++ Database/Patches/2013-02-BalanceOfPower/6 LandBlockExtendedData/DA55.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (2107986061, 49520, 3663003998, 44.2113, 28.8679, 20.00575, -0.9360951, 0, 0, -0.3517472, False, '2019-02-10 00:00:00'); /* Sacmisi */
+/* @teleloc 0xDA55015E [44.211300 28.867900 20.005750] -0.936095 0.000000 0.000000 -0.351747 */
