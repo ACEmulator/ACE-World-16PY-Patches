49f724eabc71f8095cf6c7adf21b441f1de310e3
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-13 18:52:58 -0700

    Add Trade Alliance stub quests and update SpellID 3535 (#94)

---
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/GonjokuAgreement.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'GonjokuAgreement';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('GonjokuAgreement', 0, 1, 'Flagged for Gonjoku Agreement quest', '2019-04-13 00:00:00');
---
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/LubziklanAgreement.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'LubziklanAgreement';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('LubziklanAgreement', 0, 1, 'Flagged for Lubziklan Agreement quest', '2019-04-13 00:00:00');
