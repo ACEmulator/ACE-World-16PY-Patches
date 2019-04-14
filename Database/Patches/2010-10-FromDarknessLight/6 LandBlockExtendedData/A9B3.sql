62f00d3830a6c4050d9b76bfb5953752d198747c
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-14 15:16:42 -0700

    Add four more spawn data points

---
+++ Database/Patches/2010-10-FromDarknessLight/6 LandBlockExtendedData/A9B3.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (2056990736, 43354, 2847080707, 177.444, 79.747, 116.005, 0.812668, 0, 0, -0.582727, False, '2019-04-14 03:38:31'); /* Scrivener of Void Magic */
+/* @teleloc 0xA9B30103 [177.444000 79.747000 116.005000] 0.812668 0.000000 0.000000 -0.582727 */
