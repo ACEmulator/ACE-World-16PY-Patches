4a3af09561e6249f325a6b563e73017875ac0194
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-13 19:58:13 -0700

    Add two missing spawns for Path of the Jojii quest

---
+++ Database/Patches/2005-07-ThroneOfDestiny/6 LandBlockExtendedData/E43D.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (2118373407, 25940, 3829202993, 150.118, 17.6175, 70.07899, -0.6113209, 0, 0, -0.7913828, False, '2019-02-10 00:00:00'); /* Scrawled Note */
+/* @teleloc 0xE43D0031 [150.118000 17.617500 70.078990] -0.611321 0.000000 0.000000 -0.791383 */
