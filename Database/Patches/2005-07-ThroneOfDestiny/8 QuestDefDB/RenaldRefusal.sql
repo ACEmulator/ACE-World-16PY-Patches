568a910b93de81a90a94221b58d7cf562b0bde56
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-13 18:43:50 -0700

    Add Trade Alliance stub quests and update SpellID 3535

---
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/RenaldRefusal.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'RenaldRefusal';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('RenaldRefusal', 0, 1, 'Flagged for Renald Refusal quest', '2019-04-13 00:00:00');
---
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/03535 Free Ride to Sanamar.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell` WHERE `id` = 3535;
+
+INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
+VALUES (3535, 'Free Ride to Sanamar', 869859336, 5.703595, 186.2831, 52.005, 0.300286, 0, 0, -0.953849, '2019-04-14 01:33:38');
+/* @teleloc 0x33D90008 [5.703595 186.283100 52.005000] 0.300286 0.000000 0.000000 -0.953849 */
