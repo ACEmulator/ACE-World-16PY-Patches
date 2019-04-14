81b60496ad39f5577a0e026f2136a9585b04934e
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-14 00:38:33 -0700

    Add newer landblocks

---
+++ Database/Patches/2005-07-ThroneOfDestiny/6 LandBlockExtendedData/F75D.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 63325;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (2138427392, 45726, 4150067232, 85.8427, 188.171, 19.937, -0.625612, 0, 0, -0.780135, False, '2019-04-14 03:38:31'); /* Shadowy Refuge */
+/* @teleloc 0xF75D0020 [85.842700 188.171000 19.937000] -0.625612 0.000000 0.000000 -0.780135 */
