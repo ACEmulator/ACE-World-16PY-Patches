babf705c413909d222cd4cd73ec8e07f0c8e65dd
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-17 19:19:41 -0700

    Create inscribedpurplegem.sql

---
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/inscribedpurplegem.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'inscribedpurplegem';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('inscribedpurplegem', 518400, -1, 'Inscribed Purple gem', '2019-04-17 20:34:27');
