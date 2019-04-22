ccc103fd6f092428a177715463a33aebc244b121
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-04-22 13:22:54 -0700

    Correct SQL syntax error

--- Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/03977 Salvaged Ivory.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/03977 Salvaged Ivory.sql
@@ -1,4 +1,5 @@
-﻿DELETE FROM `recipe` WHERE `id` = 3977;
+﻿
+DELETE FROM `recipe` WHERE `id` = 3977;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
 VALUES (3977, 0, 0, 0, 0, 0, 0, 'You apply the ivory.', 0, 0, 'You apply the ivory, but in the process you destroy the target.', 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 0, '2019-04-21 10:00:00');
