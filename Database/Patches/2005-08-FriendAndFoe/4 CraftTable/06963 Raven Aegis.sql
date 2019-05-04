7e30aafe34f1c97cb8a253a8bb966f40858a76ce
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-05-03 22:04:44 -0700

    Add missing property update to Raven Aegis recipe

--- Database/Patches/2005-08-FriendAndFoe/4 CraftTable/06963 Raven Aegis.sql
+++ Database/Patches/2005-08-FriendAndFoe/4 CraftTable/06963 Raven Aegis.sql
@@ -1,9 +1,20 @@
 DELETE FROM `recipe` WHERE `id` = 6963;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6963, 0, 48 /* Shield */, 0, 0, 31395 /* Raven Aegis */, 1, 'The shield crystalizes as you apply the Chorizite formula.', 0, 0, '', 1, 1, '', 1, 1, '', 1, 1, '', 1, 1, '', 0, '2019-04-15 18:20:10');
+VALUES (6963, 0, 48 /* Shield */, 0, 0, 31395 /* Raven Aegis */, 1, 'The shield crystalizes as you apply the Chorizite formula.', 0, 0, '', 1, 1, '', 1, 1, '', 1, 1, '', 1, 1, '', 0, '2019-05-03 00:00:00');
+
+INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
+VALUES (6963, True, 0, 0, 0, False, 0, 0, 0);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `recipe_mods_i_i_d` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`, `source`)
+VALUES (@parent_id, 3,  38, 0, 4, 0) /* On Player.SuccessResult CopyFromSourceToResult AllowedWielder to Result */;
+
+INSERT INTO `recipe_mods_string` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`, `source`)
+VALUES (@parent_id, 3,  25, NULL, 4, 0) /* On Player.SuccessResult CopyFromSourceToResult CraftsmanName to Result */;
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 6963;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (6963, 23361 /* Chorizite Formula */, 31396 /* Caliginous Aegis */, '2019-04-15 18:20:10');
+VALUES (6963, 23361 /* Chorizite Formula */, 31396 /* Caliginous Aegis */, '2019-05-03 00:00:00');
