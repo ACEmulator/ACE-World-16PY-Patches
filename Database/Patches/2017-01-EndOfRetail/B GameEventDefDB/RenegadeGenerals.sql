98e94256e7a6db4a82d9e12462c0be743ec55d37
Ty Conner <tyconner@itcproductions.com>
2019-05-27 02:24:58 -0500

    Create RenegadeGenerals.sql (#131)

---
+++ Database/Patches/2017-01-EndOfRetail/B GameEventDefDB/RenegadeGenerals.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'RenegadeGenerals';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('RenegadeGenerals', -1, -1, 4, '2019-05-11 00:00:00');
