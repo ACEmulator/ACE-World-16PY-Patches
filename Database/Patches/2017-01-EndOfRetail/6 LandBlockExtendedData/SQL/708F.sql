e76d5a3133f6bbfe3c1315177cfab9eb61f2d41b
Ty Conner <tyconner@itcproductions.com>
2019-05-29 13:01:09 -0500

    Add Hall of Metos Portal to Zaikhal area (#133)

---
+++ Database/Patches/2017-01-EndOfRetail/6 LandBlockExtendedData/SQL/708F.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 28815;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (1997074432,  7291, 1888419861, 50.4847, 98.0185, 239.937, 0.9434183, 0, 0, 0.3316051, False, '2019-02-10 00:00:00'); /* Halls of Metos */
+/* @teleloc 0x708F0015 [50.484700 98.018500 239.937000] 0.943418 0.000000 0.000000 0.331605 */
