a1816452d6816b4fd4b4aa309f743e60356b4d8d
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-05-30 21:32:06 -0700

    Correct Grievver Darter

--- Database/Patches/2014-02-Spring2014/9 WeenieDefaults/Grievver/52778 Grievver Darter.sql
+++ Database/Patches/2014-02-Spring2014/9 WeenieDefaults/Grievver/52778 Grievver Darter.sql
@@ -30,7 +30,7 @@ VALUES (52778,   1,   33556698) /* Setup */
      , (52778,   6,   67112927) /* PaletteBase */
      , (52778,   8,  100670960) /* Icon */
      , (52778,  22,  872415364) /* PhysicsEffectTable */
-     , (52778,  30,         87) /* PhysicsScript - BreatheLightning */;
+     , (52778,  30,         86) /* PhysicsScript - BreatheAcid */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (52778,   1, 500, 0, 0) /* Strength */
