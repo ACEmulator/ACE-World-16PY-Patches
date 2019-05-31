ad748763666e8760c2bb9737d5e03ace10be94cb
bryan <dirtyelf@users.noreply.github.com>
2019-05-31 00:45:48 -0400

    warden from give to refuse for explorer mace (#134)

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/Human/42140 Warden.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/Human/42140 Warden.sql
@@ -74,7 +74,7 @@ VALUES (42140,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
      , (42140,  8,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (42140,  6 /* Give */,      1, 8363 /* Mace of the Explorer */, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (42140,  1 /* Refuse */,      1, 8363 /* Mace of the Explorer */, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
