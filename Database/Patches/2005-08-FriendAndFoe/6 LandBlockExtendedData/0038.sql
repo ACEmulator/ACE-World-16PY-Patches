035b62a060479aeba7fd71d5c7a2d85d54ed2a96
Ty Conner <tyconner@itcproductions.com>
2019-06-16 17:39:39 -0500

    Some changes to Tomb of Adhorix (#143)
    * Some changes to Tomb of Adhorix
    
    * Remove generator profiles
    * Add event TombRubbleEvent
    * Add generator for TombRubbleEvent
    * Add event stop on Generation
    * Add event start on Death
    * Add generatortombrubble to 0038
    
    * Update 70077 Tomb Rubble Generator.sql
    
    * Change to NPC fake object
    
    * tweaks
    
    * corrections
    
    * Update 70078 Tomb Rubble.sql
    
    * Update 70024 Tomb Rubble.sql

--- Database/Patches/2005-08-FriendAndFoe/6 LandBlockExtendedData/0038.sql
+++ Database/Patches/2005-08-FriendAndFoe/6 LandBlockExtendedData/0038.sql
@@ -474,3 +474,7 @@ VALUES (1879277569, 1879277574, '2019-03-23 02:20:17') /* Great Revenant */
 	 , (1879277569, 1879277655, '2019-03-23 02:20:17') /* Elite Revenant */
 	 , (1879277569, 1879277656, '2019-03-23 02:20:17') /* Elite Revenant */
 	 , (1879277569, 1879277657, '2019-03-23 02:20:17') /* Elite Revenant */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (1879277664, 70077, 3670769, 229.368484, -30.032377, 0.005, 0.999921, 0, 0, -0.0126, False, '2019-06-16 02:20:17'); /* Tomb Rubble Generator */
+/* @teleloc 0x003802F1 [229.368484 -30.032377 0.005000] 0.999921 0.000000 0.000000 -0.012600 */
