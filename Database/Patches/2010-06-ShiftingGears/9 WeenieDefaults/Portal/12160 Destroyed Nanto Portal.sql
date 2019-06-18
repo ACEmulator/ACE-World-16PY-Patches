bec499155253f596b3599b9961e460a6e9fe4e0b
Ty Conner <tyconner@itcproductions.com>
2019-06-18 01:12:27 -0400

    Update 12160 Destroyed Nanto Portal.sql

--- Database/Patches/2010-06-ShiftingGears/9 WeenieDefaults/Portal/12160 Destroyed Nanto Portal.sql
+++ Database/Patches/2010-06-ShiftingGears/9 WeenieDefaults/Portal/12160 Destroyed Nanto Portal.sql
@@ -7,8 +7,7 @@ INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (12160,   1,      65536) /* ItemType - Portal */
      , (12160,  16,          1) /* ItemUseable - No */
      , (12160,  93,       3092) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
-     , (12160, 111,         49) /* PortalBitmask - Unrestricted */
-     , (12160, 133,          4) /* ShowableOnRadar - ShowAlways */;
+     , (12160, 111,         49) /* PortalBitmask - Unrestricted */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (12160,   1, True ) /* Stuck */
@@ -22,10 +21,13 @@ VALUES (12160,  54,    -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (12160,   1, 'Destroyed Nanto Portal') /* Name */
-     , (12160,  16, 'This portal was destroyed by Asheron''s foray into opening a portal to the lost Empyreans.') /* LongDesc */
-	 , (12160,  38, 'Destroyed Nanto Portal (52.2S, 82.5E).') /* Portal Destination String */;
+     , (12160,  16, 'This portal was destroyed by Asheron''s foray into opening a portal to the lost Empyreans.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (12160,   1,   33561060) /* Setup */
      , (12160,   2,  150995314) /* MotionTable */
      , (12160,   8,  100667499) /* Icon */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (12160, 2, 3862822946, 96.96, 37.722, 74.542, -4.371139E-08, 0, 0, -1) /* Destination */
+/* @teleloc 0xE63E0022 [96.960000 37.722000 74.542000] 0.000000 0.000000 0.000000 -1.000000 */;
