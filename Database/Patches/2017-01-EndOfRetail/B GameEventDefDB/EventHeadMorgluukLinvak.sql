b0e67158ce3bdf36eb34b2be8888d3ac62b6945e
Ty Conner <tyconner@itcproductions.com>
2019-06-19 01:05:03 -0500

    Create EventHeadMorgluukLinvak.sql (#148)

---
+++ Database/Patches/2017-01-EndOfRetail/B GameEventDefDB/EventHeadMorgluukLinvak.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'EventHeadMorgluukLinvak';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('EventHeadMorgluukLinvak', -1, -1, 4, '2019-06-19 10:00:00');
