1549104a2c32f5e3a853bc976d0600e6f9ba90a7
Ty Conner <tyconner@itcproductions.com>
2019-06-22 13:10:09 -0400

    Update 03930 Olthoi Hunter.sql

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/Human/03930 Olthoi Hunter.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/Human/03930 Olthoi Hunter.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 3930;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (3930, 'olthoihunter', 10, '2005-02-09 10:00:00') /* Creature */;
+VALUES (3930, 'olthoihunter', 10, '2019-06-17 04:18:21') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (3930,   1,         16) /* ItemType - Creature */
