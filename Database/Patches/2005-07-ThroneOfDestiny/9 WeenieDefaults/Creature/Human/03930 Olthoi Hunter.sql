c88fbc43cc102e42486caf3b9141c69e789f2dda
Ty Conner <tyconner@itcproductions.com>
2019-06-25 13:32:57 -0400

    Update 03930 Olthoi Hunter.sql

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/Human/03930 Olthoi Hunter.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/Human/03930 Olthoi Hunter.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 3930;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (3930, 'olthoihunter', 10, '2019-06-25 17:22:39') /* Creature */;
+VALUES (3930, 'olthoihunter', 10, '2019-06-17 04:18:21') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (3930,   1,         16) /* ItemType - Creature */
@@ -14,19 +14,15 @@ VALUES (3930,   1,         16) /* ItemType - Creature */
      , (3930,  27,          0) /* ArmorType - None */
      , (3930,  93,    6292504) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
      , (3930,  95,          8) /* RadarBlipColor - Yellow */
-     , (3930, 113,          2) /* Gender - Female */
      , (3930, 133,          4) /* ShowableOnRadar - ShowAlways */
      , (3930, 134,         16) /* PlayerKillerStatus - RubberGlue */
-     , (3930, 146,        516) /* XpOverride */
-     , (3930, 188,          1) /* HeritageGroup - Aluvian */;
+     , (3930, 146,        516) /* XpOverride */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (3930,   1, True ) /* Stuck */
      , (3930,   8, True ) /* AllowGive */
-     , (3930,  11, True ) /* IgnoreCollisions */
      , (3930,  12, True ) /* ReportCollisions */
      , (3930,  13, False) /* Ethereal */
-     , (3930,  14, True ) /* GravityStatus */
      , (3930,  19, False) /* Attackable */
      , (3930,  41, True ) /* ReportCollisionsAsEnvironment */
      , (3930,  42, True ) /* AllowEdgeSlide */
@@ -35,16 +31,16 @@ VALUES (3930,   1, True ) /* Stuck */
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (3930,   1,       5) /* HeartbeatInterval */
      , (3930,   2,       0) /* HeartbeatTimestamp */
-     , (3930,   3, 0.159999996423721) /* HealthRate */
+     , (3930,   3,    0.16) /* HealthRate */
      , (3930,   4,       5) /* StaminaRate */
      , (3930,   5,       1) /* ManaRate */
-     , (3930,  13, 0.899999976158142) /* ArmorModVsSlash */
+     , (3930,  13,     0.9) /* ArmorModVsSlash */
      , (3930,  14,       1) /* ArmorModVsPierce */
-     , (3930,  15, 1.10000002384186) /* ArmorModVsBludgeon */
-     , (3930,  16, 0.400000005960464) /* ArmorModVsCold */
-     , (3930,  17, 0.400000005960464) /* ArmorModVsFire */
+     , (3930,  15,     1.1) /* ArmorModVsBludgeon */
+     , (3930,  16,     0.4) /* ArmorModVsCold */
+     , (3930,  17,     0.4) /* ArmorModVsFire */
      , (3930,  18,       1) /* ArmorModVsAcid */
-     , (3930,  19, 0.600000023841858) /* ArmorModVsElectric */
+     , (3930,  19,     0.6) /* ArmorModVsElectric */
      , (3930,  54,       3) /* UseRadius */
      , (3930,  64,       1) /* ResistSlash */
      , (3930,  65,       1) /* ResistPierce */
@@ -72,14 +68,7 @@ VALUES (3930,   1,   33554510) /* Setup */
      , (3930,   2,  150994945) /* MotionTable */
      , (3930,   3,  536870914) /* SoundTable */
      , (3930,   4,  805306368) /* CombatTable */
-     , (3930,   6,   67108990) /* PaletteBase */
-     , (3930,   8,  100667446) /* Icon */
-     , (3930,   9,   83890281) /* EyesTexture */
-     , (3930,  10,   83890315) /* NoseTexture */
-     , (3930,  11,   83890356) /* MouthTexture */
-     , (3930,  15,   67116977) /* HairPalette */
-     , (3930,  16,   67109564) /* EyesPalette */
-     , (3930,  17,   67109562) /* SkinPalette */;
+     , (3930,   8,  100667446) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (3930,   1, 120, 0, 0) /* Strength */
@@ -90,14 +79,14 @@ VALUES (3930,   1, 120, 0, 0) /* Strength */
      , (3930,   6,  50, 0, 0) /* Self */;
 
 INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
-VALUES (3930,   1,     0, 0, 0, 56) /* MaxHealth */
-     , (3930,   3,     0, 0, 0, 112) /* MaxStamina */
-     , (3930,   5,     0, 0, 0, 50) /* MaxMana */;
+VALUES (3930,   1,   120, 0, 0, 176) /* MaxHealth */
+     , (3930,   3,   135, 0, 0, 247) /* MaxStamina */
+     , (3930,   5,    50, 0, 0, 100) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (3930,  6, 0, 2, 0,   1, 0, 0) /* MeleeDefense        Trained */
-     , (3930,  7, 0, 2, 0,   1, 0, 0) /* MissileDefense      Trained */
-     , (3930, 13, 0, 2, 0,   1, 0, 0) /* UnarmedCombat       Trained */;
+VALUES (3930,  6, 0, 2, 0,   1, 0, 373.815005535069) /* MeleeDefense        Trained */
+     , (3930,  7, 0, 2, 0,   1, 0, 373.815005535069) /* MissileDefense      Trained */
+     , (3930, 13, 0, 2, 0,   1, 0, 373.815005535069) /* UnarmedCombat       Trained */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (3930,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
