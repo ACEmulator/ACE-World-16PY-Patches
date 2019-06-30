5da7465209a0f40b58cc5666228817706ccaceeb
Ty Conner <tyconner@itcproductions.com>
2019-06-30 18:55:16 -0400

    Update 31384 Dark Dealings.sql

--- Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31384 Dark Dealings.sql
+++ Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31384 Dark Dealings.sql
@@ -33,7 +33,7 @@ VALUES (31384,   1,   33554773) /* Setup */
      , (31384,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
-VALUES (31384, 0, 0);
+VALUES (31384, 1, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
 VALUES (31384, 0, 4294967295, 'Sarkin Killcrane', '', False, 'Greetings,
