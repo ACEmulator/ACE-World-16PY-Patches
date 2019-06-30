bd2eb4388c3fb2977c75b435dfa1e7d4c66b7067
Ty Conner <tyconner@itcproductions.com>
2019-06-30 18:55:53 -0400

    Update 31415 Account of the Spear of Mukkir.sql

--- Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31415 Account of the Spear of Mukkir.sql
+++ Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31415 Account of the Spear of Mukkir.sql
@@ -35,7 +35,7 @@ VALUES (31415,   1,   33559593) /* Setup */
      , (31415,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
-VALUES (31415, 0, 0);
+VALUES (31415, 7, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
 VALUES (31415, 0, 4294967295, 'Kuyiza bint Zayi', '', False, 'With respect to His Eternal Splendor,
