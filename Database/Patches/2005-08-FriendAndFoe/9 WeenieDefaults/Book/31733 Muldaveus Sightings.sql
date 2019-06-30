547a8ee358f8231195d1544c3325ad27969b4fba
Ty Conner <tyconner@itcproductions.com>
2019-06-30 18:58:34 -0400

    Update 31733 Muldaveus Sightings.sql

--- Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31733 Muldaveus Sightings.sql
+++ Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31733 Muldaveus Sightings.sql
@@ -36,7 +36,7 @@ VALUES (31733,   1,   33554771) /* Setup */
      , (31733,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
-VALUES (31733, 0, 0);
+VALUES (31733, 5, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
 VALUES (31733, 0, 4294967295, 'Mancanza', '', False, 'The Renegade Muldaveus, long thought dead, has recently been sighted roaming the wilds of Dereth. Look for clues to his location in the areas described on the following pages:')
