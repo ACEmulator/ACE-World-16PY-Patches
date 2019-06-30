001310c7fd33033fd3d74bf86861f89ce096524c
Ty Conner <tyconner@itcproductions.com>
2019-06-30 19:00:24 -0400

    Update 70008 Lost Pet.sql

--- Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/70008 Lost Pet.sql
+++ Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/70008 Lost Pet.sql
@@ -33,7 +33,7 @@ VALUES (70008,   1,   33554773) /* Setup */
      , (70008,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
-VALUES (70008, 0, 0);
+VALUES (70008, 1, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
 VALUES (70008, 0, 4294967295, 'Xsao Lann', '', False, '**REWARD**
