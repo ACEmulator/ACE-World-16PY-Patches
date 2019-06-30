429137db7cd72f9c1ea5760598002bed4c5b0eb5
Ty Conner <tyconner@itcproductions.com>
2019-06-30 18:57:48 -0400

    Update 31661 Official Agents of the Arcanum Notice.sql

--- Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31661 Official Agents of the Arcanum Notice.sql
+++ Database/Patches/2005-08-FriendAndFoe/9 WeenieDefaults/Book/31661 Official Agents of the Arcanum Notice.sql
@@ -37,7 +37,7 @@ VALUES (31661,   1,   33554776) /* Setup */
      , (31661,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
-VALUES (31661, 0, 0);
+VALUES (31661, 1, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
 VALUES (31661, 0, 4294967295, 'Agents of the Arcanum', '', False, 'Let it be known that there is a critical shortage of Niffis Pearls. The Agents of the Arcanum will now pay double the normal reward for Niffis Pearls.');
