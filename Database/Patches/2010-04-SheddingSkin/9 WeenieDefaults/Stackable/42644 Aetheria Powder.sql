e6e9c1264b4ddc4976d614ab04df43e07981c0db
Ty Conner <tyconner@itcproductions.com>
2019-07-05 00:22:18 -0400

    Update Aetheria Powder

--- Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Stackable/42644 Aetheria Powder.sql
+++ Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Stackable/42644 Aetheria Powder.sql
@@ -1,10 +1,11 @@
 DELETE FROM `weenie` WHERE `class_Id` = 42644;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (42644, 'ace42644-aetheriapowder', 51, '2019-03-17 00:00:00') /* Stackable */;
+VALUES (42644, 'ace42644-aetheriapowder', 51, '2019-04-05 00:00:00') /* Stackable */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (42644,   1,        128) /* ItemType - Misc */
+     , (42644,   3,         14) /* PaletteTemplate - Red */
      , (42644,   5,          4) /* EncumbranceVal */
      , (42644,  11,        100) /* MaxStackSize */
      , (42644,  12,          1) /* StackSize */
@@ -12,16 +13,10 @@ VALUES (42644,   1,        128) /* ItemType - Misc */
      , (42644,  15,          5) /* StackUnitValue */
      , (42644,  16,          1) /* ItemUseable - No */
      , (42644,  19,          5) /* Value */
-     , (42644,  65,        101) /* Placement - Resting */
      , (42644,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (42644,   1, False) /* Stuck */
-     , (42644,  11, True ) /* IgnoreCollisions */
-     , (42644,  13, True ) /* Ethereal */
-     , (42644,  14, True ) /* GravityStatus */
-     , (42644,  19, True ) /* Attackable */
-     , (42644,  69, False) /* IsSellable */;
+VALUES (42644,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (42644,   1, 'Aetheria Powder') /* Name */
@@ -32,5 +27,6 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (42644,   1,   33555208) /* Setup */
      , (42644,   3,  536870932) /* SoundTable */
      , (42644,   6,   67111919) /* PaletteBase */
+     , (42644,   7,  268435723) /* ClothingBase */
      , (42644,   8,  100668385) /* Icon */
      , (42644,  22,  872415275) /* PhysicsEffectTable */;
