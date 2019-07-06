f2c43bf782d8dbe0575097b21a35b4d316e41c24
Ty Conner <tyconner@itcproductions.com>
2019-07-06 16:49:55 -0400

    Update 42710 Aligned Mana Stone.sql

--- Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/CraftTool/42710 Aligned Mana Stone.sql
+++ Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/CraftTool/42710 Aligned Mana Stone.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 42710;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (42710, 'ace42710-alignedmanastone', 44, '2019-07-03 00:00:00') /* CraftTool */;
+VALUES (42710, 'ace42710-alignedmanastone', 38, '2019-07-06 00:00:00') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (42710,   1,       2048) /* ItemType - Gem */
@@ -27,18 +27,11 @@ VALUES (42710, 167,      10) /* CooldownDuration */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (42710,   1, 'Aligned Mana Stone') /* Name */
-     , (42710,  16, 'This mana stone is incapable of holding a charge. Using this stone creates a conduit between the wielder and Dereth''s mana field.') /* LongDesc */;
+     , (42710,  16, 'This mana stone is incapable of holding a charge. Using this stone creates a conduit between the wielder and Dereth''s mana field.') /* LongDesc */
+     , (42710,  51, 'UseAlignedManaStone') /* UseSendsSignal */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (42710,   1,   33555641) /* Setup */
      , (42710,   3,  536870932) /* SoundTable */
      , (42710,   8,  100676308) /* Icon */
      , (42710,  22,  872415275) /* PhysicsEffectTable */;
-
-INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (42710,  7 /* Use */,   1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-SET @parent_id = LAST_INSERT_ID();
-
-INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  88 /* LocalSignal */, 0, 1, NULL, 'UseAlignedManaStone', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
