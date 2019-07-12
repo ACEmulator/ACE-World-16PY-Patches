4ba7b82475c443ff3fc2ad54f5188770c74eb50d
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-07-12 11:18:40 -0700

    Add more missed commas

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/02964 Aura of Hunter's Mark.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/02964 Aura of Hunter's Mark.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 2964;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (2964, 'Aura of Hunter''s Mark', 36872 /* Float, SingleStat, Additive */, 169 /* WeaponAuraDefense */ 0.18, '2019-07-11 00:00:00');
+VALUES (2964, 'Aura of Hunter''s Mark', 36872 /* Float, SingleStat, Additive */, 169 /* WeaponAuraDefense */, 0.18, '2019-07-11 00:00:00');
--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/03961 Defense of the Just.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/03961 Defense of the Just.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 3961;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (3961, 'Defense of the Just', 36872 /* Float, SingleStat, Additive */, 169 /* WeaponAuraDefense */ 0.6, '2019-07-11 00:00:00');
+VALUES (3961, 'Defense of the Just', 36872 /* Float, SingleStat, Additive */, 169 /* WeaponAuraDefense */, 0.6, '2019-07-11 00:00:00');
