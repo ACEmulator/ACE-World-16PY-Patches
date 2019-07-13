11045bedb2eb4f63238b760c7980a8cccc3e4c9f
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-07-13 12:41:02 -0700

    Mod spawns 3

--- Database/Patches/2005-09-UnderCoverOfNight/1 RegionDescExtendedData/C9E5.sql
+++ Database/Patches/2005-09-UnderCoverOfNight/1 RegionDescExtendedData/C9E5.sql
@@ -1,6 +1,19 @@
 DELETE FROM `encounter` WHERE `landblock` = 51685;
 
 INSERT INTO `encounter` (`landblock`, `weenie_Class_Id`, `cell_X`, `cell_Y`, `last_Modified`)
-VALUES (51685, 79995, 0, 1, '2019-06-30 00:00:00') /* Vissidal West Generator */
-     , (51685, 79995, 1, 5, '2019-06-30 00:00:00') /* Vissidal West Generator */
-     , (51685, 79995, 5, 1, '2019-06-30 00:00:00') /* Vissidal West Generator */;
+VALUES (51685, 79995, 0, 2, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 0, 5, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 0, 6, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 1, 5, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 1, 6, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 2, 4, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 2, 5, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 3, 4, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 4, 2, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 4, 7, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 5, 2, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 5, 7, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 6, 1, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 6, 2, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 6, 4, '2005-02-09 10:00:00') /* Vissidal West Generator */
+     , (51685, 79995, 7, 1, '2005-02-09 10:00:00') /* Vissidal West Generator */;
