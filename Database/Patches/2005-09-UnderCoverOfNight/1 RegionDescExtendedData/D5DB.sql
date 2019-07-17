041b7ac790ecc24f514e799e68f5d1f5793a55ae
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-07-17 12:59:37 -0700

    Update Jedetj Eckhart and add more Vissidal region spawn data

---
+++ Database/Patches/2005-09-UnderCoverOfNight/1 RegionDescExtendedData/D5DB.sql
@@ -0,0 +1,19 @@
+DELETE FROM `encounter` WHERE `landblock` = 54747;
+
+INSERT INTO `encounter` (`landblock`, `weenie_Class_Id`, `cell_X`, `cell_Y`, `last_Modified`)
+VALUES (54747, 79993, 0, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 0, 5, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 0, 6, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 1, 5, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 1, 6, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 2, 4, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 2, 5, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 3, 4, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 4, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 4, 7, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 5, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 5, 7, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 6, 1, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 6, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 6, 4, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (54747, 79993, 7, 1, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */;
---
+++ Database/Patches/2005-09-UnderCoverOfNight/1 RegionDescExtendedData/D6DA.sql
@@ -0,0 +1,19 @@
+DELETE FROM `encounter` WHERE `landblock` = 55002;
+
+INSERT INTO `encounter` (`landblock`, `weenie_Class_Id`, `cell_X`, `cell_Y`, `last_Modified`)
+VALUES (55002, 79993, 0, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 0, 5, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 0, 6, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 1, 5, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 1, 6, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 2, 4, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 2, 5, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 3, 4, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 4, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 4, 7, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 5, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 5, 7, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 6, 1, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 6, 2, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 6, 4, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */
+     , (55002, 79993, 7, 1, '2019-07-17 00:00:00') /* Vissidal Under Xik Minru Generator */;
