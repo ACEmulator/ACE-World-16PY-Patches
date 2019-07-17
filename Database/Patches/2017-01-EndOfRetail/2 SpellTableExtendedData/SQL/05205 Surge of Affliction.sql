23d97b86c0a636609d34f6cf13cd0cc0cb5aa406
gmriggs <gmriggs@gmail.com>
2019-07-16 21:42:07 -0400

    fixing aetheria surge spells

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05205 Surge of Affliction.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05205 Surge of Affliction.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 5205;
 
-INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (5205, 'Surge of Affliction', 36868 /* Int, SingleStat, Additive */, 318 /* DamageOverTime */, 150, '2019-03-18 09:00:00');
+INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `dot_Duration`, `last_Modified`)
+VALUES (5205, 'Surge of Affliction', 36868 /* Int, SingleStat, Additive */, 318 /* DamageOverTime */, 150, 19, '2019-07-16 09:00:00');
