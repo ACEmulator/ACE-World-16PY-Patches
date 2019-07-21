8ee6253e0030e5f78b84a2d087d1380aa2c59326
Ty Conner <tyconner@itcproductions.com>
2019-07-20 22:22:28 -0500

    V0.9.102 (#175)
    * Update 03818 Curse of Raven Fury.sql (#173)
    
    Adjust drain and damage modifiers to match retail spell description.
    
    * PatchesContent8.8 (#174)

---
+++ Database/Patches/2005-09-UnderCoverOfNight/3 TreasureTable/Wielded/02019.sql
@@ -0,0 +1,24 @@
+DELETE FROM `treasure_wielded` WHERE `treasure_Type` = 2019;
+
+INSERT INTO `treasure_wielded` (`treasure_Type`, `weenie_Class_Id`, `palette_Id`, `unknown_1`, `shade`, `stack_Size`, `stack_Size_Variance`, `probability`, `unknown_3`, `unknown_4`, `unknown_5`, `set_Start`, `has_Sub_Set`, `continues_Previous_Set`, `unknown_9`, `unknown_10`, `unknown_11`, `unknown_12`, `last_Modified`)
+VALUES (2019, 47852,0,0,0,0,0,0.16,0,0,0,TRUE,TRUE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Arbalest */
+     , (2019, 48020,0,0,0,20,0.25,1,0,0,0,TRUE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Quarrel */
+     , (2019, 47854,0,0,0,0,0,0.16,0,0,0,TRUE,TRUE,TRUE,0,0,0,0,'2019-07-20 00:00:00') /* Acid Crossbow */
+     , (2019, 48021,0,0,0,20,0.25,1,0,0,0,TRUE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Acid Quarrel */
+     , (2019, 47856,0,0,0,0,0,0.16,0,0,0,TRUE,TRUE,TRUE,0,0,0,0,'2019-07-20 00:00:00') /* Electric Crossbow */
+     , (2019, 48022,0,0,0,20,0.25,1,0,0,0,TRUE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Lightning Quarrel */
+     , (2019, 48026,0,0,0,0,0,0.04,0,0,0,TRUE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Nekode */
+     , (2019, 48028,0,0,0,0,0,0.04,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Lightning Nekode */
+     , (2019, 48027,0,0,0,0,0,0.04,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Acid Nekode */
+     , (2019, 48024,0,0,0,0,0,0.04,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Katar */
+     , (2019, 48025,0,0,0,0,0,0.04,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Lightning Katar */
+     , (2019, 48023,0,0,0,0,0,0.04,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Acid Katar */
+     , (2019, 48029,0,0,0,0,0,0.108,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Silifi */
+     , (2019, 48031,0,0,0,0,0,0.108,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Lightning Silifi */
+     , (2019, 48030,0,0,0,0,0,0.108,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Acid Silifi */
+     , (2019, 48032,0,0,0,0,0,0.108,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Ono */
+     , (2019, 48034,0,0,0,0,0,0.108,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Lightning Ono */
+     , (2019, 48033,0,0,0,0,0,0.108,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Acid Ono */
+     , (2019, 44,0,0,0,0,0,0.25,0,0,0,TRUE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Buckler */
+     , (2019, 91,0,0,0,0,0,0.25,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00') /* Kite Shield */
+     , (2019, 93,0,0,0,0,0,0.25,0,0,0,FALSE,FALSE,FALSE,0,0,0,0,'2019-07-20 00:00:00'); /* Round Shield */
