a958297edad236df165117b0baf4153f9de2f501
Slushnas <Slushnas@users.noreply.github.com>
2019-07-22 21:06:56 -0700

    Set NonTracking to True for later arc spells. (#177)

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05367 Nether Arc VII.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05367 Nether Arc VII.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 5367;
 
-INSERT INTO `spell` (`id`, `name`, `e_Type`, `base_Intensity`, `variance`, `wcid`, `num_Projectiles`, `spread_Angle`, `last_Modified`)
-VALUES (5367, 'Nether Arc VII', 1024 /* Nether */, 168, 94, 43232 /* Nether Arc */, 1, 0, '2019-03-18 09:00:00');
+INSERT INTO `spell` (`id`, `name`, `e_Type`, `base_Intensity`, `variance`, `wcid`, `num_Projectiles`, `spread_Angle`, `non_Tracking`, `last_Modified`)
+VALUES (5367, 'Nether Arc VII', 1024 /* Nether */, 168, 94, 43232 /* Nether Arc */, 1, 0, True, '2019-07-22 09:00:00');
