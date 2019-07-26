3b53e07d3b608e15b2d3d69ddb1f38cca294f38e
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-07-26 07:16:29 -0700

    Update cache version

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Gem/Gem/22135 Frest Greelving's Emerald.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Gem/Gem/22135 Frest Greelving's Emerald.sql
@@ -1,13 +1,14 @@
 DELETE FROM `weenie` WHERE `class_Id` = 22135;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (22135, 'jewelemeraldhauntedmansion', 38, '2019-07-24 00:00:00') /* Gem */;
+VALUES (22135, 'jewelemeraldhauntedmansion', 38, '2019-07-26 00:00:00') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (22135,   1,       2048) /* ItemType - Gem */
      , (22135,   3,          8) /* PaletteTemplate - Green */
      , (22135,   5,          5) /* EncumbranceVal */
      , (22135,   8,          5) /* Mass */
+     , (22135,   9,          0) /* ValidLocations - None */
      , (22135,  11,          1) /* MaxStackSize */
      , (22135,  12,          1) /* StackSize */
      , (22135,  13,          5) /* StackUnitEncumbrance */
@@ -16,16 +17,11 @@ VALUES (22135,   1,       2048) /* ItemType - Gem */
      , (22135,  16,          1) /* ItemUseable - No */
      , (22135,  19,       1000) /* Value */
      , (22135,  33,          1) /* Bonded - Bonded */
-     , (22135,  53,        101) /* PlacementPosition */
      , (22135,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (22135, 114,          1) /* Attuned - Attuned */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (22135,  11, True ) /* IgnoreCollisions */
-     , (22135,  13, True ) /* Ethereal */
-     , (22135,  14, True ) /* GravityStatus */
-     , (22135,  19, True ) /* Attackable */
-     , (22135,  22, True ) /* Inscribable */;
+VALUES (22135,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (22135,   1, 'Frest Greelving''s Emerald') /* Name */;
