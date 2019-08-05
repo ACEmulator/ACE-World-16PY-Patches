ea005d27518ff3b5ef6fc22de9df3afdd1db838a
Ty Conner <tyconner@itcproductions.com>
2019-08-04 22:32:04 -0400

    Update 2B11.sql
    Restore Morathe to Candeth Keep

--- Database/Patches/2017-01-EndOfRetail/6 LandBlockExtendedData/SQL/2B11.sql
+++ Database/Patches/2017-01-EndOfRetail/6 LandBlockExtendedData/SQL/2B11.sql
@@ -1,3 +1,7 @@
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
 VALUES (1924206668, 42852, 722534439, 114.677, 160.92, 48.19801, 0.05757342, 0, 0, -0.9983413, False); /* Portal to Town Network */
 /* @teleloc 0x2B110027 [114.677000 160.920000 48.198010] 0.057573 0.000000 0.000000 -0.998341 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1924206669, 43410, 722534684, 165.653, 173.53, 47.205, -0.3413002, 0, 0, -0.9399543, False); /* Morathe */
+/* @teleloc 0x2B11011C [165.653000 173.530000 47.205000] -0.341300 0.000000 0.000000 -0.939954 */
