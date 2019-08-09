8b64da0fa68d77ee11e670b2efa66838f912abc1
Ty Conner <tyconner@itcproductions.com>
2019-08-09 00:21:04 -0400

    Update Dark Tree Crystal Mine to 100 MinLevel

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/23076 Dark Tree Crystal Mine.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/23076 Dark Tree Crystal Mine.sql
@@ -1,12 +1,12 @@
 DELETE FROM `weenie` WHERE `class_Id` = 23076;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (23076, 'portaldarktreecrystalmine', 7, '2019-02-04 06:52:23') /* Portal */;
+VALUES (23076, 'portaldarktreecrystalmine', 7, '2019-08-08 10:00:00') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (23076,   1,      65536) /* ItemType - Portal */
      , (23076,  16,         32) /* ItemUseable - Remote */
-     , (23076,  86,         40) /* MinLevel */
+     , (23076,  86,        100) /* MinLevel */
      , (23076,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
      , (23076, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */
      , (23076, 133,          4) /* ShowableOnRadar - ShowAlways */;
@@ -16,17 +16,13 @@ VALUES (23076,   1, True ) /* Stuck */
      , (23076,  11, False) /* IgnoreCollisions */
      , (23076,  12, True ) /* ReportCollisions */
      , (23076,  13, True ) /* Ethereal */
-     , (23076,  14, True ) /* GravityStatus */
-     , (23076,  15, True ) /* LightsStatus */
-     , (23076,  19, True ) /* Attackable */
-     , (23076,  88, True ) /* PortalShowDestination */;
+     , (23076,  15, True ) /* LightsStatus */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (23076,  54, -0.100000001490116) /* UseRadius */;
+VALUES (23076,  54,    -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (23076,   1, 'Dark Tree Crystal Mine') /* Name */
-     , (23076,  38, 'Dark Tree Crystal Mine') /* AppraisalPortalDestination */;
+VALUES (23076,   1, 'Dark Tree Crystal Mine') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (23076,   1,   33555926) /* Setup */
