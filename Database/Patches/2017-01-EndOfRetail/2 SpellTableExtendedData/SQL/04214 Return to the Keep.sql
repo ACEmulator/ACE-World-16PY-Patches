8947f68694ce7b1f012a314d01453e14eedd9ae9
Ty Conner <tyconner@itcproductions.com>
2019-08-10 02:23:16 -0400

    Update 04214 Return to the Keep.sql

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04214 Return to the Keep.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04214 Return to the Keep.sql
@@ -2,4 +2,4 @@ DELETE FROM `spell` WHERE `id` = 4214;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
 VALUES (4214, 'Return to the Keep', 722599977, 120.64, 1.55, 48.005, 0.0871558, 0, 0, -0.9961947, '2019-08-09 13:00:00');
-/* @teleloc 0x2b120029 [120.64 1.55 48.005] 0.0871558 0 0 -0.9961947 */
+/* @teleloc 0x2B120029 [120.64 1.55 48.005] 0.0871558 0 0 -0.9961947 */
