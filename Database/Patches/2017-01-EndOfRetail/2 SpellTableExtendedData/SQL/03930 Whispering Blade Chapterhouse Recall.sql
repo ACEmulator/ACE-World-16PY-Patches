8f5cb8810b649597a467b9e743e5b7b4f872c984
Ty Conner <tyconner@itcproductions.com>
2019-08-10 02:24:53 -0400

    Update 03930 Whispering Blade Chapterhouse Recall.sql

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/03930 Whispering Blade Chapterhouse Recall.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/03930 Whispering Blade Chapterhouse Recall.sql
@@ -2,4 +2,4 @@ DELETE FROM `spell` WHERE `id` = 3930;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
 VALUES (3930, 'Whispering Blade Chapterhouse Recall', 8454440, 80, -10, 0, 0, 0, 0, -1, '2019-08-09 16:00:00');
-/* @teleloc 0x00810128 [80 -10 0] 0 0 0 */
+/* @teleloc 0x00810128 [80 -10 0] 0 0 0 -1 */
