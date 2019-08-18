8d7b271f554c4b0fa17d3d8a808152e1de83e684
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-08-17 21:51:54 -0700

    Change comment

--- Database/Patches/2007-07-AncientEnemies/1 RegionDescExtendedData/462C.sql
+++ Database/Patches/2007-07-AncientEnemies/1 RegionDescExtendedData/462C.sql
@@ -9,8 +9,8 @@ VALUES (17964, 1972, 1, 1, '2019-08-17 00:00:00') /* High Direlands Mountain Sha
      , (17964, 1972, 3, 3, '2019-08-17 00:00:00') /* High Direlands Mountain Shadow Generator */
      , (17964, 1972, 4, 0, '2019-08-17 00:00:00') /* High Direlands Mountain Shadow Generator */
      , (17964, 1972, 5, 7, '2019-08-17 00:00:00') /* High Direlands Mountain Shadow Generator */
-     , (17964, 80006, 6, 1, '2019-08-17 00:00:00') /* Vissidal West Area Gen */
-     , (17964, 80006, 6, 2, '2019-08-17 00:00:00') /* Vissidal West Area Gen */
-     , (17964, 80006, 6, 4, '2019-08-17 00:00:00') /* Vissidal West Area Gen */
-     , (17964, 80006, 7, 1, '2019-08-17 00:00:00') /* Vissidal West Area Gen */;
+     , (17964, 80006, 6, 1, '2019-08-17 00:00:00') /* Graveyard Master Gen */
+     , (17964, 80006, 6, 2, '2019-08-17 00:00:00') /* Graveyard Master Gen */
+     , (17964, 80006, 6, 4, '2019-08-17 00:00:00') /* Graveyard Master Gen */
+     , (17964, 80006, 7, 1, '2019-08-17 00:00:00') /* Graveyard Master Gen */;
 	 
