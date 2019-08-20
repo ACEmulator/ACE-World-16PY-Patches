b63a5876124b4f502f24fd8ee10ab6bf3c32b279
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-08-20 00:41:30 -0700

    Fix c/p error

--- Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/MeleeWeapon/48976 Acid Hatchet.sql
+++ Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/MeleeWeapon/48976 Acid Hatchet.sql
@@ -4,13 +4,13 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (48976, 'ace48976-acidhatchet', 6, '2019-08-19 00:00:00') /* MeleeWeapon */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (48973,   1,          1) /* ItemType - MeleeWeapon */
-     , (48973,   3,         21) /* PaletteTemplate - Gold */
-     , (48973,   5,        450) /* EncumbranceVal */
-     , (48973,   9,    1048576) /* ValidLocations - MeleeWeapon */
-     , (48973,  16,          1) /* ItemUseable - No */
-     , (48973,  18,        256) /* UiEffects - Acid */
-     , (48973,  19,        130) /* Value */
+VALUES (48976,   1,          1) /* ItemType - MeleeWeapon */
+     , (48976,   3,         21) /* PaletteTemplate - Gold */
+     , (48976,   5,        450) /* EncumbranceVal */
+     , (48976,   9,    1048576) /* ValidLocations - MeleeWeapon */
+     , (48976,  16,          1) /* ItemUseable - No */
+     , (48976,  18,        256) /* UiEffects - Acid */
+     , (48976,  19,        130) /* Value */
      , (48976,  33,         -2) /* Bonded - Destroy */
      , (48976,  45,         32) /* DamageType - Acid */
      , (48976,  46,          2) /* DefaultCombatStyle - OneHanded */
@@ -40,12 +40,12 @@ VALUES (48976,  21,       0) /* WeaponLength */
      , (48976, 157,       1) /* ResistanceModifier */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (48973,   1, 'Acid Hatchet') /* Name */;
+VALUES (48976,   1, 'Acid Hatchet') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (48973,   1,   33559451) /* Setup */
-     , (48973,   3,  536870932) /* SoundTable */
-     , (48973,   6,   67115558) /* PaletteBase */
-     , (48973,   7,  268436991) /* ClothingBase */
-     , (48973,   8,  100686914) /* Icon */
-     , (48973,  22,  872415275) /* PhysicsEffectTable */;
+VALUES (48976,   1,   33559451) /* Setup */
+     , (48976,   3,  536870932) /* SoundTable */
+     , (48976,   6,   67115558) /* PaletteBase */
+     , (48976,   7,  268436991) /* ClothingBase */
+     , (48976,   8,  100686914) /* Icon */
+     , (48976,  22,  872415275) /* PhysicsEffectTable */;
