a05d0449083bcae97af238a331ed93243650da21
starfox-ac <54785610+starfox-ac@users.noreply.github.com>
2019-09-02 17:54:39 -0700

    Exploration Society recipes

--- Database/Patches/2010-08-TheRisenPrincess/4 CraftTable/08019 Amateur Explorer Silifi.sql
+++ Database/Patches/2010-08-TheRisenPrincess/4 CraftTable/08019 Amateur Explorer Silifi.sql
@@ -1,9 +1,9 @@
-DELETE FROM `recipe` WHERE `id` = 1784;
+DELETE FROM `recipe` WHERE `id` = 8019;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (1784, 0, 0, 0, 0, 70312 /* Stamped Yanshi Scarlet Red Letter */, 1, 'You stamp your Scarlet Red Yanshi Letter.  It is now ready to take to an Agent of the Society.', 0, 0, 'You fail to make the pieces fit.', 0, 0, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-09-02 00:00:00');
+VALUES (8019, 0, 0, 0, 0, 70214 /* Amateur Explorer Silifi */, 1, 'You successfully create an Amateur Explorer Silifi.', 0, 0, 'You fail to make the pieces fit.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-09-02 00:00:00');
 
-DELETE FROM `cook_book` WHERE `recipe_Id` = 1784;
+DELETE FROM `cook_book` WHERE `recipe_Id` = 8019;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (1784, 8741 /* Yanshi Town Stamp */,  45876 /* Scarlet Red Letter */, '2019-09-02 00:00:00');
+VALUES (8019, 45990 /* Explorer Heavy Weapons Gem */,  70191 /* Amateur Explorer Axe Cast */, '2019-09-02 00:00:00');
