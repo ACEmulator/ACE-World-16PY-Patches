8d800dc233c7ee564e7b424e8416db0579217f90
starfox-ac <54785610+starfox-ac@users.noreply.github.com>
2019-09-02 21:03:26 -0700

    Correct recipe completion text

--- Database/Patches/2010-08-TheRisenPrincess/4 CraftTable/08054 Seasoned Explorer Khanjar.sql
+++ Database/Patches/2010-08-TheRisenPrincess/4 CraftTable/08054 Seasoned Explorer Khanjar.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 8054;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (8054, 0, 0, 0, 0, 70239 /* Seasoned Explorer Khanjar */, 1, 'You successfully create a Seasoned Explorer Khanjar.', 0, 0, 'You fail to make the pieces fit.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-09-02 00:00:00');
+VALUES (8054, 0, 0, 0, 0, 70239 /* Seasoned Explorer Khanjar */, 1, 'You place the Light Weapons Gem into the Dagger Cast.', 0, 0, 'You fail to make the pieces fit.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-09-02 00:00:00');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 8054;
 
