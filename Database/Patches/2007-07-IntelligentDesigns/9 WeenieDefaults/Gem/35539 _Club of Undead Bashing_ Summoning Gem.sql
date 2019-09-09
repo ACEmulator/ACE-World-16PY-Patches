9f2547ad298431d176216856f8412c928c17588d
starfox-ac <54785610+starfox-ac@users.noreply.github.com>
2019-09-09 03:48:40 -0700

    Requested changes

--- Database/Patches/2007-07-IntelligentDesigns/9 WeenieDefaults/Gem/35539 _Club of Undead Bashing_ Summoning Gem.sql
+++ Database/Patches/2007-07-IntelligentDesigns/9 WeenieDefaults/Gem/35539 _Club of Undead Bashing_ Summoning Gem.sql
@@ -15,14 +15,15 @@ VALUES (35539,   1,        128) /* ItemType - Misc */
      , (35539,  19,          1) /* Value */
      , (35539,  33,         -1) /* Bonded - Slippery */
      , (35539,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (35539,  94,         16) /* TargetType - Creature */;
+     , (35539,  94,         16) /* TargetType - Creature */
+     , (35539, 269,          1) /* UseCreateQuantity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (35539,  22, True ) /* Inscribable */
      , (35539,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (35539,  39, 0.800000011920929) /* DefaultScale */;
+VALUES (35539,  39, 0.8) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (35539,   1, '"Club of Undead Bashing" Summoning Gem') /* Name */
@@ -36,14 +37,6 @@ VALUES (35539,   1,   33556223) /* Setup */
      , (35539,   7,  268435841) /* ClothingBase */
      , (35539,   8,  100673039) /* Icon */
      , (35539,  22,  872415275) /* PhysicsEffectTable */
+     , (35539,  38,      80020) /* UseCreateItem - Club of Undead Bashing */;
      , (35539,  50,  100673774) /* IconOverlay */
      , (35539,  52,  100689403) /* IconUnderlay */;
-
-INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (35539,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-SET @parent_id = LAST_INSERT_ID();
-
-INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,   3 /* Give */, 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0 /* Undef */, 80020 /* Club of Undead Bashing */, 1, 0, 0, False, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-     , (@parent_id,  1,  77 /* DeleteSelf */, 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
