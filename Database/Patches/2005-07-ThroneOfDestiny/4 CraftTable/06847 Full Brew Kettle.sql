a852f9f0d456b1f34ee935df8bc30b82e67013c5
Ty Conner <tyconner@itcproductions.com>
2019-09-23 23:56:04 -0400

    More Fail Fixes

--- Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/06847 Full Brew Kettle.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/06847 Full Brew Kettle.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 6847;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6847, 0, 39 /* Cooking */, 200, 0, 29201 /* Full Brew Kettle */, 1, 'You combine the ingredients.', 0, 0, 'You fail to combine the ingredients.', 0, 0, '', 1, 1, '', 0, 0, '', 1, 1, '', 0, '2019-04-15 18:20:10');
+VALUES (6847, 0, 39 /* Cooking */, 200, 0, 29201 /* Full Brew Kettle */, 1, 'You combine the ingredients.', 0, 0, 'You fail to combine the ingredients.', 1, 1, '', 1, 1, '', 0, 0, '', 1, 1, '', 0, '2019-09-23 18:20:10');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 6847;
 
--- Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/06860 Tusker Spit Ale.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/06860 Tusker Spit Ale.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 6860;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6860, 0, 39 /* Cooking */, 300, 0, 29109 /* Tusker Spit Ale */, 50, 'You fill 50 empty bottles with Tusker Spit Ale!', 0, 0, 'You fail to combine the ingredients.', 0, 0, '', 1, 1, '', 1, 1, '', 1, 1, '', 0, '2019-04-15 18:20:10');
+VALUES (6860, 0, 39 /* Cooking */, 300, 0, 29109 /* Tusker Spit Ale */, 50, 'You fill 50 empty bottles with Tusker Spit Ale!', 0, 0, 'You fail to combine the ingredients.', 1, 1, '', 1, 1, '', 0, 0, '', 1, 1, '', 0, '2019-09-23 18:20:10');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 6860;
 
