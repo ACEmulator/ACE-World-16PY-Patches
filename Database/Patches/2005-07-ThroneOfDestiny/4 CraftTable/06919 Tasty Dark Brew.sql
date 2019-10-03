888f480cab26bec6ff97241865a8e314a32137aa
Ty Conner <tyconner@itcproductions.com>
2019-10-03 12:30:51 -0500

    Fix some issues with Beer Brewing (#224)

--- Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/06919 Tasty Dark Brew.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/4 CraftTable/06919 Tasty Dark Brew.sql
@@ -1,9 +1,9 @@
 DELETE FROM `recipe` WHERE `id` = 6919;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6919, 0, 39 /* Cooking */, 200, 0, 29190 /* Tasty Dark Brew */, 1, 'You create a Tasty Dark Brew.', 29161 /* Brew Kettle */, 0, 'You fail to combine the ingredients.', 1, 1, '', 1, 1, '', 1, 1, '', 1, 1, '', 0, '2019-09-23 18:20:10');
+VALUES (6919, 0, 39 /* Cooking */, 200, 0, 29190 /* Tasty Dark Brew */, 1, 'You create a Tasty Dark Brew.', 0, 0, 'You fail to combine the ingredients.', 1, 1, '', 1, 1, '', 1, 1, '', 1, 1, '', 0, '2019-10-01 18:20:10');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 6919;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (6919, 29160 /* Rancid Yeast */, 29168 /* Aromatic Dark Wort */, '2019-09-23 18:20:10');
+VALUES (6919, 29160 /* Rancid Yeast */, 29168 /* Aromatic Dark Wort */, '2019-10-01 18:20:10');
