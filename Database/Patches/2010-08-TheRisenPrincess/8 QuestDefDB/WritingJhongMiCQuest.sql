782962c9b5e653fe3319b5940097af9345f93aa4
starfox-ac <55265383+shadowroot-ac@users.noreply.github.com>
2019-10-04 07:34:37 -0700

    Use already updated quest DB files

--- Database/Patches/2010-08-TheRisenPrincess/8 QuestDefDB/WritingJhongMiCQuest.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'WritingJhongMiCQuest';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('WritingJhongMiCQuest', 72000, -1, 'Writing Jhong MiC Quest', '2019-10-03 00:00:00');
