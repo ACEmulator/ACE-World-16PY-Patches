5cff990d186e53e282454e47152e97b91f64eed4
starfox-ac <55265383+shadowroot-ac@users.noreply.github.com>
2019-10-05 10:03:29 -0700

    Update Distillery Nectar pickup timer

--- Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/distillerynectarquest.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/distillerynectarquest.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'distillerynectarquest';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('distillerynectarquest', 72000, -1, 'Distillery Nectar Quest', '2019-03-14 20:34:27');
+VALUES ('distillerynectarquest', 518400, -1, 'Distillery Nectar Quest', '2019-10-05 00:00:00');
