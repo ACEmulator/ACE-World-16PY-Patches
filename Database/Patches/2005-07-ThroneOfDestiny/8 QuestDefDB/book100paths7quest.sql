b8af5add8ec0fa0104b1873623720ef841008cdb
shadowroot-ac <55265383+shadowroot-ac@users.noreply.github.com>
2019-10-13 11:40:46 -0700

    Update Musansayn's Library quest (#229)

--- Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/book100paths7quest.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/book100paths7quest.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'book100paths7quest';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('book100paths7quest', 72000, -1, 'Book 100 Paths Quest', '2019-03-14 20:34:27');
+VALUES ('book100paths7quest', 518400, -1, 'Book 100 Paths Quest', '2019-10-05 00:00:00');
