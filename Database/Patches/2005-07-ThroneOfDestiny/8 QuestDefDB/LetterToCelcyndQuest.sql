01334f6707daf907018f3f952cec55209d72e1d5
shadowroot-ac <55265383+shadowroot-ac@users.noreply.github.com>
2019-10-13 11:37:32 -0700

    Update Letter to Celcynd quest (#226)
    * Update Letter To Celcynd quest
    
    * Update Leter to Celcynd with quest timer
    
    * c/p error correction

---
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/LetterToCelcyndQuest.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'LetterToCelcyndQuest';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('LetterToCelcyndQuest', 518400, -1, 'Letter To Celcynd Quest', '2019-10-04 00:00:00');
