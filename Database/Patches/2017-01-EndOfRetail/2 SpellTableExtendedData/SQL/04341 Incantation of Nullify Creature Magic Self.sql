b378619332b9cff40d156aff93287a465f861a81
gmriggs <gmriggs@gmail.com>
2019-10-24 03:55:40 -0400

    fixing level 8 dispels (#239)
    * pass 1
    
    * 285->435

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04341 Incantation of Nullify Creature Magic Self.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04341 Incantation of Nullify Creature Magic Self.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4341;
 
-INSERT INTO `spell` (`id`, `name`, `min_Power`, `max_Power`, `power_Variance`, `align`, `number`, `number_Variance`, `last_Modified`)
-VALUES (4341, 'Incantation of Nullify Creature Magic Self', 0, 400, 1, 2, 6, 0.5, '2019-03-18 09:00:00');
+INSERT INTO `spell` (`id`, `name`, `min_Power`, `max_Power`, `dispel_School`, `power_Variance`, `align`, `number`, `number_Variance`, `last_Modified`)
+VALUES (4341, 'Incantation of Nullify Creature Magic Self', 0, 435, 4 /* CreatureEnchantment */, 1, 0, 6, 0.666, '2019-10-23 09:00:00');
--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04342 Incantation of Nullify Item Magic.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04342 Incantation of Nullify Item Magic.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4342;
 
-INSERT INTO `spell` (`id`, `name`, `min_Power`, `max_Power`, `power_Variance`, `align`, `number`, `number_Variance`, `last_Modified`)
-VALUES (4342, 'Incantation of Nullify Item Magic', 0, 400, 1, 2, 3, 0.666, '2019-03-18 09:00:00');
+INSERT INTO `spell` (`id`, `name`, `min_Power`, `max_Power`, `dispel_School`, `power_Variance`, `align`, `number`, `number_Variance`, `last_Modified`)
+VALUES (4342, 'Incantation of Nullify Item Magic', 0, 435, 3 /* ItemEnchantment */, 1, 2, 6, 0.5, '2019-10-23 09:00:00');
