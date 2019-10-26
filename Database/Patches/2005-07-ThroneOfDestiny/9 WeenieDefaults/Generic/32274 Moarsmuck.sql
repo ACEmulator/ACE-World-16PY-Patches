c8414ca8aa0fc7b594713aa909a2929a330cdb27
Ty Conner <tyconner@itcproductions.com>
2019-10-26 14:01:31 -0400

    Update 32274 Moarsmuck.sql

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Generic/32274 Moarsmuck.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Generic/32274 Moarsmuck.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 32274;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (32274, 'ace32274-moarsmuck', 1, '2019-02-04 06:52:23') /* Generic */;
+VALUES (32274, 'ace32274-moarsmuck', 44, '2019-10-26 06:52:23') /* CraftTool */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32274,   1,          0) /* ItemType - None */
