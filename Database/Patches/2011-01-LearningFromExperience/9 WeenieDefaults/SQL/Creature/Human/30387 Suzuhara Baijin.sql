ce18f215ae683f908324c3d66fb63a7726e11655
Cyril Cressent <cyril@cressent.org>
2019-11-06 11:29:06 -0800

    Update XP rewards for Suzuhara Baijin's Delivery
    Rewards increased to 5,000,000 during 'Learning From Experience'.

---
+++ Database/Patches/2011-01-LearningFromExperience/9 WeenieDefaults/SQL/Creature/Human/30387 Suzuhara Baijin.sql
@@ -0,0 +1,6 @@
+UPDATE `ace_world`.`weenie_properties_emote_action`
+SET
+  `amount_64` = 5000000
+WHERE `emote_id` = (SELECT `id` FROM `weenie_properties_emote` WHERE `object_Id` = 30387 AND `category` = 6 AND `weenie_Class_Id` = 30397)
+  AND `order` = 4
+  AND `type` = 2;
