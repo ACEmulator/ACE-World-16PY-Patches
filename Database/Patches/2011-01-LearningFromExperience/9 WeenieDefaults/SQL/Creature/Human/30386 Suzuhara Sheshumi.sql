9086fb7945f600c1a1628739e2bef12f4cc08e76
Cyril Cressent <cyril@cressent.org>
2019-11-08 13:06:08 -0800

    Update last_Modified

--- Database/Patches/2011-01-LearningFromExperience/9 WeenieDefaults/SQL/Creature/Human/30386 Suzuhara Sheshumi.sql
+++ Database/Patches/2011-01-LearningFromExperience/9 WeenieDefaults/SQL/Creature/Human/30386 Suzuhara Sheshumi.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 30386;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (30386, 'beachfortsuzuharasheshumi', 10, '2005-02-09 10:00:00') /* Creature */;
+VALUES (30386, 'beachfortsuzuharasheshumi', 10, '2019-11-08 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (30386,   1,         16) /* ItemType - Creature */
--- Database/Patches/2011-01-LearningFromExperience/9 WeenieDefaults/SQL/Creature/Human/30387 Suzuhara Baijin.sql
+++ Database/Patches/2011-01-LearningFromExperience/9 WeenieDefaults/SQL/Creature/Human/30387 Suzuhara Baijin.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 30387;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (30387, 'stoneholdsuzuharabaijin', 10, '2005-02-09 10:00:00') /* Creature */;
+VALUES (30387, 'stoneholdsuzuharabaijin', 10, '2019-11-08 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (30387,   1,         16) /* ItemType - Creature */
