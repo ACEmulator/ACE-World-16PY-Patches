cb3685b73dce35bc720507fb31baa9532b739bd8
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2019-11-10 01:17:49 -0800

    Add pickup timers to encoded notes

--- Database/Patches/2005-10-TricksAndTreats/9 WeenieDefaults/Generic/70259 Halmera's Encoded Notes.sql
+++ Database/Patches/2005-10-TricksAndTreats/9 WeenieDefaults/Generic/70259 Halmera's Encoded Notes.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 70259;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (70259, 'ace70259-halmerasencodednotes', 1, '2019-09-09 14:38:55') /* Generic */;
+VALUES (70259, 'ace70259-halmerasencodednotes', 1, '2019-11-10 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (70259,   1,        128) /* ItemType - Misc */
@@ -23,7 +23,8 @@ VALUES (70259,   1, False) /* Stuck */
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (70259,   1, 'Halmera''s Encoded Notes') /* Name */
-     , (70259,  16, 'A set of notes written by Mistress Halmera in a strange, elaborate cipher.') /* LongDesc */;
+     , (70259,  16, 'A set of notes written by Mistress Halmera in a strange, elaborate cipher.') /* LongDesc */
+     , (70259,  33, 'solidmukkirorbnotespickup') /* Quest */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (70259,   1,   33554771) /* Setup */
--- Database/Patches/2005-10-TricksAndTreats/9 WeenieDefaults/Generic/70273 Gabille's Encoded Notes.sql
+++ Database/Patches/2005-10-TricksAndTreats/9 WeenieDefaults/Generic/70273 Gabille's Encoded Notes.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 70273;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (70273, 'ace70273-gabillesencodednotes', 1, '2019-09-09 14:38:55') /* Generic */;
+VALUES (70273, 'ace70273-gabillesencodednotes', 1, '2019-11-10 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (70273,   1,        128) /* ItemType - Misc */
@@ -23,7 +23,8 @@ VALUES (70273,   1, False) /* Stuck */
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (70273,   1, 'Gabille''s Encoded Notes') /* Name */
-     , (70273,  16, 'A set of notes written by Mistress Gabille in a strange, elaborate cipher.') /* LongDesc */;
+     , (70273,  16, 'A set of notes written by Mistress Gabille in a strange, elaborate cipher.') /* LongDesc */
+     , (70273,  33, 'detailmukkirorbnotespickup') /* Quest */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (70273,   1,   33554771) /* Setup */
