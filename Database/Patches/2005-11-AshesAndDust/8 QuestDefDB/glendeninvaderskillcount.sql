ae6cdb891a4a4ba4b8b82394ab456c8a3ae027bd
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2019-11-26 07:23:13 -0800

    .

--- Database/Patches/2005-11-AshesAndDust/8 QuestDefDB/glendeninvaderskillcount.sql
+++ Database/Patches/2005-11-AshesAndDust/8 QuestDefDB/glendeninvaderskillcount.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'glendeninvaderskillcount';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('glendeninvaderskillcount', 0, 20, 'Glenden Woods Invaders Kill Counter', '2019-02-04 06:51:50');
+VALUES ('glendeninvaderskillcount', 0, 20, 'Glenden Woods Invaders Kill Counter', '2019-11-21 00:00:00');
--- Database/Patches/2005-11-AshesAndDust/8 QuestDefDB/glendoninvadersblockadekillcount.sql
+++ Database/Patches/2005-11-AshesAndDust/8 QuestDefDB/glendoninvadersblockadekillcount.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'glendoninvadersblockadekillcount';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('glendoninvadersblockadekillcount', 0, 1, 'Blockade Guard Kill Task Counter', '2019-10-30 01:06:39');
+VALUES ('glendoninvadersblockadekillcount', 0, 1, 'Blockade Guard Kill Task Counter', '2019-11-21 00:00:00');
