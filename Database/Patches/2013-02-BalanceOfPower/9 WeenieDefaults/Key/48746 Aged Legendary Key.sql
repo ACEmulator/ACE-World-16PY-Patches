d52256ad8182bedcf05aac740407fe3f24431bec
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-01-04 20:11:24 -0500

    Add files via upload

---
+++ Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/Key/48746 Aged Legendary Key.sql
@@ -0,0 +1,43 @@
+DELETE FROM `weenie` WHERE `class_Id` = 48746;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (48746, 'ace48746-agedlegendarykey', 22, '2019-12-25 01:07:33') /* Key */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (48746,   1,      16384) /* ItemType - Key */
+     , (48746,   5,         30) /* EncumbranceVal */
+     , (48746,  16,    2097160) /* ItemUseable - SourceContainedTargetRemote */
+     , (48746,  18,         64) /* UiEffects - Lightning */
+     , (48746,  19,      10000) /* Value */
+     , (48746,  33,          0) /* Bonded - Normal */
+     , (48746,  53,        101) /* PlacementPosition - Resting */
+     , (48746,  91,          1) /* MaxStructure */
+     , (48746,  92,          1) /* Structure */
+     , (48746,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (48746,  94,        640) /* TargetType - LockableMagicTarget */
+     , (48746, 114,          0) /* Attuned - Normal */
+     , (48746, 369,        150) /* UseRequiresLevel */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (48746,  11, True ) /* IgnoreCollisions */
+     , (48746,  13, True ) /* Ethereal */
+     , (48746,  14, True ) /* GravityStatus */
+     , (48746,  19, True ) /* Attackable */
+     , (48746,  22, True ) /* Inscribable */
+     , (48746,  69, False) /* IsSellable */
+     , (48746,  99, False) /* Ivoryable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (48746,   1, 'Aged Legendary Key') /* Name */
+     , (48746,  13, 'keychestleg') /* KeyCode */
+     , (48746,  14, 'Use this key to open a Legendary Armor, Magic, or Weapon Chest.') /* Use */
+     , (48746,  16, 'This key has seen better days.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (48746,   1,   33554784) /* Setup */
+     , (48746,   3,  536870932) /* SoundTable */
+     , (48746,   8,  100693001) /* Icon */
+     , (48746,  22,  872415275) /* PhysicsEffectTable */;
+	 
+	 
+	 
---
+++ Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/Key/48748 Legendary Key 2 Use.sql
@@ -0,0 +1,46 @@
+DELETE FROM `weenie` WHERE `class_Id` = 48748;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (48748, 'ace48748-legendarykey', 22, '2019-02-10 00:00:00') /* Key */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (48748,   1,      16384) /* ItemType - Key */
+     , (48748,   5,         30) /* EncumbranceVal */
+     , (48748,  16,    2097160) /* ItemUseable - SourceContainedTargetRemote */
+     , (48748,  18,         64) /* UiEffects - Lightning */
+     , (48748,  19,      10000) /* Value */
+     , (48748,  33,          0) /* Bonded - Normal */
+     , (48748,  65,        101) /* Placement - Resting */
+     , (48748,  91,          2) /* MaxStructure */
+     , (48748,  92,          2) /* Structure */
+     , (48748,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (48748,  94,        640) /* TargetType - LockableMagicTarget */
+     , (48748, 114,          0) /* Attuned - Normal */
+     , (48748, 267,      86400) /* Lifespan */
+     , (48748, 268,      86400) /* RemainingLifespan */
+     , (48748, 369,        150) /* UseRequiresLevel */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (48748,   1, False) /* Stuck */
+     , (48748,  11, True ) /* IgnoreCollisions */
+     , (48748,  13, True ) /* Ethereal */
+     , (48748,  14, True ) /* GravityStatus */
+     , (48748,  19, True ) /* Attackable */
+     , (48748,  22, True ) /* Inscribable */
+     , (48748,  69, False) /* IsSellable */
+     , (48748,  99, False) /* Ivoryable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (48748,   1, 'Legendary Key') /* Name */
+	 , (48748,  13, 'keychestleg') /* KeyCode */
+     , (48748,  14, 'Use this key to open a Legendary Armor, Magic, or Weapon Chest.') /* Use */
+     , (48748,  16, 'A key only heard about in whispers and myths.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (48748,   1,   33554784) /* Setup */
+     , (48748,   3,  536870932) /* SoundTable */
+     , (48748,   8,  100693001) /* Icon */
+     , (48748,  22,  872415275) /* PhysicsEffectTable */;
+	 
+	 
+	 
