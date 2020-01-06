684ee712ee55cabd077ac1fc75631dd69bf11950
gmriggs <gmriggs@gmail.com>
2020-01-06 08:24:09 -0500

    adding num_Projectiles=1 to incantation life projectiles

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04428 Incantation of Martyr's Hecatomb.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04428 Incantation of Martyr's Hecatomb.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4428;
 
-INSERT INTO `spell` (`id`, `name`, `e_Type`, `wcid`, `drain_Percentage`, `damage_Ratio`, `last_Modified`)
-VALUES (4428, 'Incantation of Martyr''s Hecatomb', 128 /* Health */, 8607 /* Missile */, 0.25, 2, '2019-03-18 09:00:00');
+INSERT INTO `spell` (`id`, `name`, `e_Type`, `wcid`, `num_Projectiles`, `drain_Percentage`, `damage_Ratio`, `last_Modified`)
+VALUES (4428, 'Incantation of Martyr''s Hecatomb', 128 /* Health */, 8607 /* Missile */, 1, 0.25, 2, '2020-01-06 09:00:00');
--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04429 Incantation of Martyr's Blight.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04429 Incantation of Martyr's Blight.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4429;
 
-INSERT INTO `spell` (`id`, `name`, `e_Type`, `wcid`, `drain_Percentage`, `damage_Ratio`, `last_Modified`)
-VALUES (4429, 'Incantation of Martyr''s Blight', 512 /* Mana */, 8604 /* Missile */, 0.25, 2, '2019-03-18 09:00:00');
+INSERT INTO `spell` (`id`, `name`, `e_Type`, `wcid`, `num_projectiles`, `drain_Percentage`, `damage_Ratio`, `last_Modified`)
+VALUES (4429, 'Incantation of Martyr''s Blight', 512 /* Mana */, 8604 /* Missile */, 1, 0.25, 2, '2020-01-06 09:00:00');
