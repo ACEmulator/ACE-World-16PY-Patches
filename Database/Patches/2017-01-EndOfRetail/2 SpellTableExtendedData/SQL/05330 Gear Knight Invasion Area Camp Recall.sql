78bd69d650bb8ea008d2021fbb98436f38fd3d5e
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-01-11 12:47:19 -0800

    Update 05330 Gear Knight Invasion Area Camp Recall.sql

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05330 Gear Knight Invasion Area Camp Recall.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05330 Gear Knight Invasion Area Camp Recall.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell` WHERE `id` = 5330;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
-VALUES (5330, 'Gear Knight Invasion Area Camp Recall', 663158845, 173.4282, 96.90477, 113.7788, -0.9998896, 0, 0, -0.01486166, '2019-08-09 13:00:00');
-/* @teleloc 0x2787003D [173.4282 96.90477 113.7788] -0.9998896 0 0 -0.01486166 */
+VALUES (5330, 'Gear Knight Invasion Area Camp Recall', 560988209, 145.5065, 14.36704, 178.005, 1, 0, 0, 0, '2020-01-11 00:00:00');
+/* @teleloc 0x21700031 [145.5065 14.36704 178.005] 1 0 0 0 */
