b43c6191e7a7f299c7998bdc161376b9fcecf3ac
Ty Conner <tyconner@itcproductions.com>
2020-01-11 14:23:06 -0500

    Add Various Quest flags
    These flags are used in contract system validation testing

---
+++ Database/Patches/2017-01-EndOfRetail/8 QuestDefDB/SQL/DeewainGaveNote_0411.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'DeewainGaveNote_0411';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('DeewainGaveNote_0411', 0, 1, 'Player gave note to Deewain.', '2011-04-09 10:00:00');
---
+++ Database/Patches/2017-01-EndOfRetail/8 QuestDefDB/SQL/QuestRewardTimerOne.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'QuestRewardTimerOne';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('QuestRewardTimerOne', 3600, 1, '1 hour quest for contract testing.', '2005-02-09 10:00:00');
