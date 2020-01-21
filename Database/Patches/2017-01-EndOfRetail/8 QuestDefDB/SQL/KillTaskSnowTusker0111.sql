fa821d81d7fb6d63af65ee97168e7c4bddead655
Ty Conner <tyconner@itcproductions.com>
2020-01-21 03:09:30 -0500

    Add Various Quest flags (#282)
    * Add Various Quest flags
    
    These flags are used in contract system validation testing
    
    * Update QuestRewardTimerOne.sql

---
+++ Database/Patches/2017-01-EndOfRetail/8 QuestDefDB/SQL/KillTaskSnowTusker0111.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskSnowTusker0111';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskSnowTusker0111', 0, 20, 'Player killed x/20 Snow Tuskers.', '2011-01-09 10:00:00');
