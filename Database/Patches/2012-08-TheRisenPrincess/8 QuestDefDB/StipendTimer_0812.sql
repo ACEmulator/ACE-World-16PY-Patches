3f9c94c5fe220318803f2983f76ca6315348bc1d
Ty Conner <tyconner@itcproductions.com>
2020-01-27 03:11:15 -0500

    Update StipendTimer_0812.sql

--- Database/Patches/2012-08-TheRisenPrincess/8 QuestDefDB/StipendTimer_0812.sql
+++ Database/Patches/2012-08-TheRisenPrincess/8 QuestDefDB/StipendTimer_0812.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'StipendTimer_0812';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('StipendTimer_0812', 518400, -1, 'Amount of stipends recived.', '2020-01-27 00:00:00');
+VALUES ('StipendTimer_0812', 518400, -1, 'Amount of stipends received.', '2020-01-27 00:00:00');
