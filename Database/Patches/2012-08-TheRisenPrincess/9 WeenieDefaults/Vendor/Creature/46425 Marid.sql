c6cb0dd98a11620d0a362b3a1adf4cd42f824fee
Ty Conner <tyconner@itcproductions.com>
2020-01-26 23:59:16 -0500

    Update 46425 Marid.sql

--- Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/Vendor/Creature/46425 Marid.sql
+++ Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/Vendor/Creature/46425 Marid.sql
@@ -6,19 +6,21 @@ VALUES (46425, 'ace46425-marid', 12, '2019-02-10 00:00:00') /* Vendor */;
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (46425,   1,         16) /* ItemType - Creature */
      , (46425,   2,         31) /* CreatureType - Human */
-     , (46425,   6,        255) /* ItemsCapacity */
-     , (46425,   7,        255) /* ContainersCapacity */
+     , (46425,   6,         -1) /* ItemsCapacity */
+     , (46425,   7,         -1) /* ContainersCapacity */
      , (46425,  16,         32) /* ItemUseable - Remote */
      , (46425,  25,        250) /* Level */
+     , (46425,  27,          0) /* ArmorType - None */
      , (46425,  74,          0) /* MerchandiseItemTypes - None */
      , (46425,  75,          0) /* MerchandiseMinValue */
      , (46425,  76,     100000) /* MerchandiseMaxValue */
      , (46425,  93,    2098200) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment */
      , (46425, 113,          1) /* Gender - Male */
+     , (46425, 126,        125) /* VendorHappyMean */
+     , (46425, 127,        125) /* VendorHappyVariance */
      , (46425, 133,          4) /* ShowableOnRadar - ShowAlways */
      , (46425, 134,         16) /* PlayerKillerStatus - RubberGlue */
-     , (46425, 188,          2) /* HeritageGroup - Gharundim */
-     , (46425, 8007,          0) /* PCAPRecordedAutonomousMovement */;
+     , (46425, 188,          2) /* HeritageGroup - Gharundim */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (46425,   1, True ) /* Stuck */
@@ -26,14 +28,40 @@ VALUES (46425,   1, True ) /* Stuck */
      , (46425,  39, True ) /* DealMagicalItems */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (46425,  37,       1) /* BuyPrice */
+VALUES (46425,   1,       5) /* HeartbeatInterval */
+     , (46425,   2,       0) /* HeartbeatTimestamp */
+     , (46425,   3,    0.16) /* HealthRate */
+     , (46425,   4,       5) /* StaminaRate */
+     , (46425,   5,       1) /* ManaRate */
+     , (46425,  11,     300) /* ResetInterval */
+     , (46425,  13,     0.9) /* ArmorModVsSlash */
+     , (46425,  14,       1) /* ArmorModVsPierce */
+     , (46425,  15,     1.1) /* ArmorModVsBludgeon */
+     , (46425,  16,     0.4) /* ArmorModVsCold */
+     , (46425,  17,     0.4) /* ArmorModVsFire */
+     , (46425,  18,       1) /* ArmorModVsAcid */
+     , (46425,  19,     0.6) /* ArmorModVsElectric */
+     , (46425,  37,       1) /* BuyPrice */
      , (46425,  38,       1) /* SellPrice */
-     , (46425,  54,       3) /* UseRadius */;
+     , (46425,  54,       3) /* UseRadius */
+     , (46425,  64,       1) /* ResistSlash */
+     , (46425,  65,       1) /* ResistPierce */
+     , (46425,  66,       1) /* ResistBludgeon */
+     , (46425,  67,       1) /* ResistFire */
+     , (46425,  68,       1) /* ResistCold */
+     , (46425,  69,       1) /* ResistAcid */
+     , (46425,  70,       1) /* ResistElectric */
+     , (46425,  71,       1) /* ResistHealthBoost */
+     , (46425,  72,       1) /* ResistStaminaDrain */
+     , (46425,  73,       1) /* ResistStaminaBoost */
+     , (46425,  74,       1) /* ResistManaDrain */
+     , (46425,  75,       1) /* ResistManaBoost */
+     , (46425, 104,      10) /* ObviousRadarRange */
+     , (46425, 125,       1) /* ResistHealthDrain */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (46425,   1, 'Marid') /* Name */
-     , (46425,   5, 'Stipend Vendor') /* Template */
-     , (46425, 8006, 'AAA9AAAAAAA=') /* PCAPRecordedCurrentMotionState */;
+     , (46425,   5, 'Stipend Vendor') /* Template */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (46425,   1,   33554433) /* Setup */
@@ -41,23 +69,7 @@ VALUES (46425,   1,   33554433) /* Setup */
      , (46425,   3,  536870913) /* SoundTable */
      , (46425,   6,   67108990) /* PaletteBase */
      , (46425,   8,  100667446) /* Icon */
-     , (46425,   9,   83890483) /* EyesTexture */
-     , (46425,  10,   83890538) /* NoseTexture */
-     , (46425,  11,   83890617) /* MouthTexture */
-     , (46425,  15,   67117080) /* HairPalette */
-     , (46425,  16,   67110062) /* EyesPalette */
-     , (46425,  17,   67109550) /* SkinPalette */
-     , (46425,  57,      46423) /* AlternateCurrency - Stipend */
-     , (46425, 8001,    8388662) /* PCAPRecordedWeenieHeader - ItemsCapacity, ContainersCapacity, Usable, UseRadius, RadarBehavior */
-     , (46425, 8003,        516) /* PCAPRecordedObjectDesc - Stuck, Vendor */
-     , (46425, 8005,     100355) /* PCAPRecordedPhysicsDesc - CSetup, MTable, STable, Position, Movement */;
-
-INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (46425, 8040, 3332964380, 81.35, 94.862, 42.005, 0.04963332, 0, 0, -0.9987675) /* PCAPRecordedLocation */
-/* @teleloc 0xC6A9001C [81.350000 94.862000 42.005000] 0.049633 0.000000 0.000000 -0.998768 */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (46425, 8000, 2087358608) /* PCAPRecordedObjectIID */;
+     , (46425,  57,      46423) /* AlternateCurrency - Stipend */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (46425,   1, 220, 0, 0) /* Strength */
@@ -72,8 +84,25 @@ VALUES (46425,   1,   196, 0, 0, 331) /* MaxHealth */
      , (46425,   3,   196, 0, 0, 466) /* MaxStamina */
      , (46425,   5,   196, 0, 0, 486) /* MaxMana */;
 
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (46425,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (46425,  1,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (46425,  2,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (46425,  3,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (46425,  4,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (46425,  5,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (46425,  6,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (46425,  7,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (46425,  8,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
-VALUES (46425, 4, 46441, -1, 0, 0, False) /* Create Boxed Augmentation Gem (46441) for Shop */
+VALUES (46425, 2, 25642, -1, 4, 0, False) /* Create Leather Gauntlets (25642) for Wield */
+     , (46425, 2, 25641, -1, 4, 0, False) /* Create Leather Cuirass (25641) for Wield */
+     , (46425, 2, 25645, -1, 4, 0, False) /* Create Leather Leggings (25645) for Wield */
+     , (46425, 2, 25651, -1, 4, 0, False) /* Create Leather Sleeves (25651) for Wield */
+     , (46425, 2,   130, -1, 88, 1, False) /* Create Shirt (130) for Wield */
+     , (46425, 2,   107, -1, 4, 0, False) /* Create Sollerets (107) for Wield */
+     , (46425, 4, 46441, -1, 0, 0, False) /* Create Boxed Augmentation Gem (46441) for Shop */
      , (46425, 4, 46421, -1, 0, 0, False) /* Create Attribute Reset Certificate (46421) for Shop */
      , (46425, 4, 46420, -1, 0, 0, False) /* Create Skill Reset Certificate (46420) for Shop */
      , (46425, 4, 46422, -1, 0, 0, False) /* Create Mastery Reset Certificate (46422) for Shop */
@@ -87,64 +116,3 @@ VALUES (46425, 4, 46441, -1, 0, 0, False) /* Create Boxed Augmentation Gem (4644
      , (46425, 4, 46415, -1, 0, 0, False) /* Create Experience Certificate (46415) for Shop */
      , (46425, 4, 46435, -1, 0, 0, False) /* Create Box Of Ten Promissory Notes (46435) for Shop */;
 
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (46425, 67109550, 0, 24)
-     , (46425, 67110062, 32, 8)
-     , (46425, 67110547, 92, 4)
-     , (46425, 67113077, 40, 24)
-     , (46425, 67114609, 136, 24)
-     , (46425, 67114609, 80, 24)
-     , (46425, 67114609, 174, 66)
-     , (46425, 67114609, 96, 40)
-     , (46425, 67114646, 160, 8)
-     , (46425, 67117080, 24, 8);
-
-INSERT INTO `weenie_properties_texture_map` (`object_Id`, `index`, `old_Id`, `new_Id`)
-VALUES (46425, 0, 83889072, 83894829)
-     , (46425, 0, 83889342, 83894833)
-     , (46425, 1, 83894659, 83894839)
-     , (46425, 2, 83894832, 83894825)
-     , (46425, 2, 83894837, 83894823)
-     , (46425, 3, 83889344, 83894824)
-     , (46425, 4, 83887068, 83894824)
-     , (46425, 5, 83894659, 83894839)
-     , (46425, 6, 83892602, 83894825)
-     , (46425, 6, 83892601, 83894823)
-     , (46425, 7, 83889344, 83894824)
-     , (46425, 8, 83887068, 83894824)
-     , (46425, 9, 83887061, 83894835)
-     , (46425, 9, 83887060, 83894836)
-     , (46425, 10, 83887069, 83886782)
-     , (46425, 10, 83894513, 83894831)
-     , (46425, 10, 83894514, 83894838)
-     , (46425, 10, 83894510, 83894831)
-     , (46425, 11, 83887067, 83891213)
-     , (46425, 11, 83886788, 83894834)
-     , (46425, 13, 83887069, 83886782)
-     , (46425, 13, 83894513, 83894831)
-     , (46425, 13, 83894514, 83894838)
-     , (46425, 13, 83894510, 83894831)
-     , (46425, 14, 83887067, 83891213)
-     , (46425, 16, 83886232, 83890685)
-     , (46425, 16, 83886668, 83890483)
-     , (46425, 16, 83886837, 83890538)
-     , (46425, 16, 83886684, 83890617);
-
-INSERT INTO `weenie_properties_anim_part` (`object_Id`, `index`, `animation_Id`)
-VALUES (46425, 0, 16777294)
-     , (46425, 1, 16789345)
-     , (46425, 2, 16789640)
-     , (46425, 3, 16781841)
-     , (46425, 4, 16781838)
-     , (46425, 5, 16789351)
-     , (46425, 6, 16784628)
-     , (46425, 7, 16781840)
-     , (46425, 8, 16781839)
-     , (46425, 9, 16777300)
-     , (46425, 10, 16788992)
-     , (46425, 11, 16781812)
-     , (46425, 12, 16777304)
-     , (46425, 13, 16788995)
-     , (46425, 14, 16789659)
-     , (46425, 15, 16777307)
-     , (46425, 16, 16795650);
