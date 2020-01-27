316f1f98f26157cff7db927b1d60a139cfac7295
Ty Conner <tyconner@itcproductions.com>
2020-01-27 03:13:45 -0500

    .

--- Database/Patches/2012-08-TheRisenPrincess/8 QuestDefDB/StipendTimer_Monthly.sql
+++ Database/Patches/2012-08-TheRisenPrincess/8 QuestDefDB/StipendTimer_Monthly.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'StipendTimer_Monthly';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('StipendTimer_Monthly', 2332800, -1, 'Monthly timer for reciving up to 4 stipends.', '2020-01-27 00:00:00');
+VALUES ('StipendTimer_Monthly', 2332800, -1, 'Monthly timer for receiving up to 4 stipends.', '2020-01-27 00:00:00');
