18dba9df0b699a268c0c228388944b0b1995ce2e
Ty Conner <tyconner@itcproductions.com>
2020-01-30 13:12:51 -0500

    Update 39748 Exploration Marker.sql

--- Database/Patches/2009-01-ShiftingTactics/9 WeenieDefaults/Creature/Unsorted/39748 Exploration Marker.sql
+++ Database/Patches/2009-01-ShiftingTactics/9 WeenieDefaults/Creature/Unsorted/39748 Exploration Marker.sql
@@ -5,72 +5,157 @@ VALUES (39748, 'ace39748-explorationmarker', 10, '2019-02-10 00:00:00') /* Creat
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (39748,   1,         16) /* ItemType - Creature */
-     , (39748,   6,        255) /* ItemsCapacity */
-     , (39748,   7,        255) /* ContainersCapacity */
+     , (39748,   3,          2) /* PaletteTemplate - Blue */
+     , (39748,   6,         -1) /* ItemsCapacity */
+     , (39748,   7,         -1) /* ContainersCapacity */
      , (39748,  16,         32) /* ItemUseable - Remote */
-     , (39748,  93,    6292508) /* PhysicsState - Ethereal, ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
-     , (39748, 307,          0) /* DamageRating */
-     , (39748, 308,          0) /* DamageResistRating */
-     , (39748, 313,          0) /* CritRating */
-     , (39748, 314,          0) /* CritDamageRating */
-     , (39748, 315,          0) /* CritResistRating */
-     , (39748, 316,          0) /* CritDamageResistRating */
-     , (39748, 370,          0) /* GearDamage */
-     , (39748, 371,          0) /* GearDamageResist */
-     , (39748, 372,          0) /* GearCrit */
-     , (39748, 373,          0) /* GearCritResist */
-     , (39748, 374,          0) /* GearCritDamage */
-     , (39748, 375,          0) /* GearCritDamageResist */
-     , (39748, 376,          0) /* GearHealingBoost */
-     , (39748, 377,          0) /* GearNetherResist */
-     , (39748, 378,          0) /* GearLifeResist */
-     , (39748, 379,          0) /* GearMaxHealth */
-     , (39748, 381,          0) /* PKDamageRating */
-     , (39748, 382,          0) /* PKDamageResistRating */
-     , (39748, 383,          0) /* GearPKDamageRating */
-     , (39748, 384,          0) /* GearPKDamageResistRating */
-     , (39748, 386,          0) /* Overpower */
-     , (39748, 387,          0) /* OverpowerResist */
-     , (39748, 388,          0) /* GearOverpower */
-     , (39748, 389,          0) /* GearOverpowerResist */
-     , (39748, 8007,          0) /* PCAPRecordedAutonomousMovement */;
+     , (39748,  27,          0) /* ArmorType - None */
+     , (39748,  93,    6292508) /* PhysicsState - Ethereal, ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (39748,   1, True ) /* Stuck */
-     , (39748,  19, False) /* Attackable */;
+     , (39748,  19, False) /* Attackable */
+     , (39748,  52, True ) /* AiImmobile */
+     , (39748,  83, True ) /* NpcLooksLikeObject */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (39748,  54,       3) /* UseRadius */;
+VALUES (39748,   1,       5) /* HeartbeatInterval */
+     , (39748,   2,       0) /* HeartbeatTimestamp */
+     , (39748,   3,    0.16) /* HealthRate */
+     , (39748,   4,       5) /* StaminaRate */
+     , (39748,   5,       1) /* ManaRate */
+     , (39748,  13,     0.9) /* ArmorModVsSlash */
+     , (39748,  14,       1) /* ArmorModVsPierce */
+     , (39748,  15,     1.1) /* ArmorModVsBludgeon */
+     , (39748,  16,     0.4) /* ArmorModVsCold */
+     , (39748,  17,     0.4) /* ArmorModVsFire */
+     , (39748,  18,       1) /* ArmorModVsAcid */
+     , (39748,  19,     0.6) /* ArmorModVsElectric */
+     , (39748,  39,     0.5) /* DefaultScale */
+     , (39748,  54,       3) /* UseRadius */
+     , (39748,  64,       1) /* ResistSlash */
+     , (39748,  65,       1) /* ResistPierce */
+     , (39748,  66,       1) /* ResistBludgeon */
+     , (39748,  67,       1) /* ResistFire */
+     , (39748,  68,       1) /* ResistCold */
+     , (39748,  69,       1) /* ResistAcid */
+     , (39748,  70,       1) /* ResistElectric */
+     , (39748,  71,       1) /* ResistHealthBoost */
+     , (39748,  72,       1) /* ResistStaminaDrain */
+     , (39748,  73,       1) /* ResistStaminaBoost */
+     , (39748,  74,       1) /* ResistManaDrain */
+     , (39748,  75,       1) /* ResistManaBoost */
+     , (39748, 104,      10) /* ObviousRadarRange */
+     , (39748, 125,       1) /* ResistHealthDrain */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (39748,   1, 'Exploration Marker') /* Name */
-     , (39748,  14, 'Use this marker to declare this spot discovered.') /* Use */
-     , (39748, 8006, 'AAA9AAAAAAA=') /* PCAPRecordedCurrentMotionState */;
+     , (39748,  14, 'Use this marker to declare this spot discovered.') /* Use */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (39748,   1,   33560703) /* Setup */
      , (39748,   2,  150995429) /* MotionTable */
      , (39748,   3,  536870932) /* SoundTable */
      , (39748,   6,   67113133) /* PaletteBase */
+     , (39748,   7,  268436124) /* ClothingBase */
      , (39748,   8,  100671368) /* Icon */
-     , (39748,  22,  872415275) /* PhysicsEffectTable */
-     , (39748, 8001,         54) /* PCAPRecordedWeenieHeader - ItemsCapacity, ContainersCapacity, Usable, UseRadius */
-     , (39748, 8003,          4) /* PCAPRecordedObjectDesc - Stuck */
-     , (39748, 8005,     104451) /* PCAPRecordedPhysicsDesc - CSetup, MTable, STable, PeTable, Position, Movement */;
+     , (39748,  22,  872415275) /* PhysicsEffectTable */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (39748,   1,   33555499) /* Setup */
+     , (39748,   2,  150995147) /* MotionTable */
+     , (39748,   3,  536871052) /* SoundTable */
+     , (39748,   4,  805306368) /* CombatTable */
+     , (39748,   8,  100675745) /* Icon */;
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (39748,   1, 100, 0, 0) /* Strength */
+     , (39748,   2, 100, 0, 0) /* Endurance */
+     , (39748,   3, 120, 0, 0) /* Quickness */
+     , (39748,   4, 140, 0, 0) /* Coordination */
+     , (39748,   5, 120, 0, 0) /* Focus */
+     , (39748,   6, 120, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (39748,   1,    80, 0, 0, 130) /* MaxHealth */
+     , (39748,   3,   120, 0, 0, 220) /* MaxStamina */
+     , (39748,   5,    50, 0, 0, 170) /* MaxMana */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (39748,  6, 0, 2, 0,   1, 0, 1687.47761207598) /* MeleeDefense        Trained */
+     , (39748,  7, 0, 2, 0,   1, 0, 1687.47761207598) /* MissileDefense      Trained */
+     , (39748, 13, 0, 2, 0,   1, 0, 1687.47761207598) /* UnarmedCombat       Trained */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (39748,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (39748,  1,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (39748,  2,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (39748,  3,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (39748,  4,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (39748,  5,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (39748,  6,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (39748,  7,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (39748,  8,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (39748, 8040, 2847146018, 112.238, 46.3802, 94, -0.9999874, 0, 0, 0.005026132) /* PCAPRecordedLocation */
 /* @teleloc 0xA9B40022 [112.238000 46.380200 94.000000] -0.999987 0.000000 0.000000 0.005026 */;
 
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (39748, 8000, 3685519789) /* PCAPRecordedObjectIID */;
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 7 /* Use */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (39748, 67111092, 0, 0);
+SET @parent_id = LAST_INSERT_ID();
 
-INSERT INTO `weenie_properties_texture_map` (`object_Id`, `index`, `old_Id`, `new_Id`)
-VALUES (39748, 0, 83893054, 83893054)
-     , (39748, 0, 83893053, 83893053);
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 21 /* InqQuest */, 0, 1, NULL, 'FoundSeantheSpeedy', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-INSERT INTO `weenie_properties_anim_part` (`object_Id`, `index`, `animation_Id`)
-VALUES (39748, 0, 16794232);
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'FoundSeantheSpeedy', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 21 /* InqQuest */, 0, 1, NULL, 'FoundExplorationMarker1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'FoundExplorationMarker1', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 18 /* DirectBroadcast */, 0, 1, NULL, 'You discovered this marker already.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'FoundExplorationMarker1', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 18 /* DirectBroadcast */, 0, 1, NULL, 'You have discovered an Exploration Marker!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id, 1, 22 /* StampQuest */, 0, 1, NULL, 'FoundExplorationMarker1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id, 2, 22 /* StampQuest */, 0, 1, NULL, 'ExplorationMarkersFound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id, 3, 21 /* InqQuest */, 0, 1, NULL, 'ExplorationMarkersFound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'ExplorationMarkersFound', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 18 /* DirectBroadcast */, 0, 1, NULL, 'You have found a total of %tqc of the Exploration Markers.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'ExplorationMarkersFound', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 18 /* DirectBroadcast */, 0, 1, NULL, 'You have found a total of %tqc of the Exploration Markers.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (39748, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'FoundSeantheSpeedy', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 18 /* DirectBroadcast */, 0, 1, NULL, 'You examine the marker, but are unsure what to do with it. Engraved on a small plaque is the symbol of the Explorers Society, a long series of numbers, and the name Sean the Speedy.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
