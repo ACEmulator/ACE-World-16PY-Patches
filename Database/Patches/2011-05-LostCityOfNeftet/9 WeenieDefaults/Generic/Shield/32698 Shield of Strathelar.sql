5c93bc80b5b5bd65ed20af8c2a3eb32ba82e0ec8
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-01-31 02:20:47 -0500

    Update 32698 Shield of Strathelar.sql
    Corrected ace classname

--- Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Generic/Shield/32698 Shield of Strathelar.sql
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Generic/Shield/32698 Shield of Strathelar.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 32698;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (32698, 'ace32698-shieldofboreleansroyalguard', 1, '2019-02-10 00:00:00') /* Generic */;
+VALUES (32698, 'ace32698-shieldofstrathelar', 1, '2019-02-10 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32698,   1,          2) /* ItemType - Armor */
