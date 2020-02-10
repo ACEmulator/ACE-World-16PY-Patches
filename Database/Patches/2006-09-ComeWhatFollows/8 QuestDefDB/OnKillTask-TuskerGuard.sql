4d422a6464f5a21fa37b6382051bf0f834336727
Ty Conner <tyconner@itcproductions.com>
2020-02-10 03:29:57 -0500

    Add quests

---
+++ Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/OnKillTask-TuskerGuard.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'OnKillTask-TuskerGuard';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('OnKillTask-TuskerGuard', 0, 1, 'Player has Kill Task for Tusker Guards', '2020-02-10 00:00:00');
