0445397f0e77a727f8e8912c01d0f51f272ad0c7
nwg5817 <nwg5817@gmail.com>
2020-02-16 22:04:37 -0500

    Reprisals - Head of Baron Entemarre
    Still need creature spawns for Via Fortress, cleanup Hashina's emote. Quest tested and working but requires touch ups. Major work done.

---
+++ Database/Patches/2006-01-Reprisals/6 LandBlockExtendedData/1830.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x1830;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x71830004, 70752, 0x1830000E, 44.821598, 140.49011, 75.937004, -0.54216844, 0, 0, -0.84026986, False, '2020-02-16 16:48:49');
+/* @teleloc 0x1830000E [44.821598 140.490112 75.937004] -0.542168 0.000000 0.000000 -0.840270 */
