2588c41963e8d517039bab3be82a1956545b650c
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-02-22 09:46:24 -0800

    Londigul Ellic revisit (#295)

--- Database/Patches/2005-11-AshesAndDust/8 QuestDefDB/glendonorbgiven.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'glendonorbgiven';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('glendonorbgiven', 0, 1, 'Glenden Wood Recall Orb given', '2019-11-21 00:00:00');
