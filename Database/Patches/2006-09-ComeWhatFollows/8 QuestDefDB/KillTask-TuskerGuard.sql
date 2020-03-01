65a575f82ae5f4f7ae500778b1cbd9e4c7d300b3
Ty Conner <tyconner@itcproductions.com>
2020-02-29 22:44:36 -0500

    Delete KillTask-TuskerGuard.sql

--- Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/KillTask-TuskerGuard.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'KillTask-TuskerGuard';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('KillTask-TuskerGuard', 0, 500, 'Counter for Tusker Guard Kill Task', '2020-02-10 00:00:00');
