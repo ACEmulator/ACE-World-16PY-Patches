46a30996eac2bcbc9dbef6843a9f27d8e30446da
Ty Conner <tyconner@itcproductions.com>
2020-02-29 22:44:43 -0500

    Delete KillTask-TuskerGuardWait.sql

--- Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/KillTask-TuskerGuardWait.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'KillTask-TuskerGuardWait';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('KillTask-TuskerGuardWait', 72000, -1, 'Cooldown timer for Tusker Guard Kill Task', '2020-02-10 00:00:00');
