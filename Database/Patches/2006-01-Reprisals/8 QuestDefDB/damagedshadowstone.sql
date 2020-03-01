6f12bd3174a4559c80b222f2538f8932c88fa4ea
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-02-29 19:26:58 -0800

    Forgot corrected quest entry

---
+++ Database/Patches/2006-01-Reprisals/8 QuestDefDB/damagedshadowstone.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'damagedshadowstone';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('damagedshadowstone', 2332800, -1, 'Shadow Stone Quest', '2020-02-17 00:00:00');
