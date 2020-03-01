e0749e31750cf6a631bf389d248e55e84e494461
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-03-01 09:24:38 -0800

    Update 08360 Shattered Legendary Key.sql

--- Database/Patches/2013-02-BalanceOfPower/4 CraftTable/08360 Shattered Legendary Key.sql
+++ Database/Patches/2013-02-BalanceOfPower/4 CraftTable/08360 Shattered Legendary Key.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 8360;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (8360, 0, 23 /* Lockpick */, 670, 0, 48746 /* Aged Legendary Key */, 1, 'You carefully reassemble the shattered crystalline key, making it usable.', 0, 0, 'You fail to repair the Shattered Legendary Key', 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0, '2019-02-07 07:16:43');
+VALUES (8360, 0, 23 /* Lockpick */, 500, 0, 48746 /* Aged Legendary Key */, 1, 'You carefully reassemble the shattered crystalline key, making it usable.', 0, 0, 'You fail to repair the Shattered Legendary Key', 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0, '2019-02-07 07:16:43');
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
 VALUES (8360, 9295 /* Intricate Carving Tool */, 48908 /* Shattered Legendary Key */, '2019-02-07 07:16:43');
