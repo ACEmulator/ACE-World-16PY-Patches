9b04105a834867e25fa7984b2f2f4d20eec5b64a
nwg5817 <nwg5817@gmail.com>
2020-03-02 10:51:02 -0500

    Fienos Lunnum Disappearance Complete
    Conversion from Fienos' quest to Ace.
    Minor Changes
    
    Ruschk Totem Scale and direction its pointing Adjust empty pot setup did.
    Reprisals - Lunnums disappearance
    
    Landblocks and NPC updates
    Remove test area
    
    Clean
    
    Need to do the whole landblock
    
    or else night generator messes up creating instances in this landblock. Need to look at makinga change all the way down to ace-world.
    quest in quest db
    
    Rename for import
    
    Ruschk Totem Flag
    
    Make totem unpickable until flagged and only pick up once.
    Small Thiefs list change
    
    adjust more to retail. how do I fix the music icons?
    Fixes
    
    Andrilos Respawn and attackable
    Janda accepts note
    Fabian is not combatant
    False Morel combat table, needs more combat fixes
    
    Restore BC9F
    
    Accidently Deleted.
    Organization
    
    Landblock Fixes
    
    Landblock Adjust
    
    Fix IDs
    Landblock Compile Fix
    
    Some issue compiling with future GUIDs
    Allow totem pickup after flagged for quest.
    
    Minor Text fixes
    
    Opps
    
    Thrungus update
    
    Trim existing NPCs.
    
    Still working on Fuin Gaya.
    Return Fuin to ACE state
    
    Updated Script
    
    Script works with previous quest and lunnums disappearance
    Delete this mob
    
    Revert "Updated Script"
    
    This reverts commit 15635ac666ef03cc7c83a1dc0ce7a156b3d26b23.
    
    Delete fuin gaya

---
+++ Database/Patches/2006-01-Reprisals/9 WeenieDefaults/Generic/73043 Ruschk Totem.sql
@@ -0,0 +1,51 @@
+DELETE FROM `weenie` WHERE `class_Id` = 73043;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (73043, 'ace73043-ruschktotem', 1, '2020-02-23 18:43:28') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (73043,   1,        128) /* ItemType - Misc */
+     , (73043,   5,        300) /* EncumbranceVal */
+     , (73043,   8,        300) /* Mass */
+     , (73043,   9,          0) /* ValidLocations - None */
+     , (73043,  16,          8) /* ItemUseable - Contained */
+     , (73043,  19,          0) /* Value */
+     , (73043,  33,          1) /* Bonded - Bonded */
+     , (73043,  53,        101) /* PlacementPosition - Resting */
+     , (73043,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (73043, 114,          1) /* Attuned - Attuned */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (73043,   1, False) /* Stuck */
+     , (73043,  12, True ) /* ReportCollisions */
+     , (73043,  13, False) /* Ethereal */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (73043,  39, 0.07999999821186066) /* DefaultScale */
+     , (73043,  54,       1) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (73043,   1, 'Ruschk Totem') /* Name */
+     , (73043,  16, 'A small totem that you recognize as coming from one of the Southern Ruschk camps of Vanguard Isle.') /* LongDesc */
+     , (73043,  33, 'pickupruschktotem') /* Quest */ 
+     , (73043,  37, 'pickupruschktotemflag') /* QuestRestriction */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (73043,   1,   33559201) /* Setup */
+     , (73043,   8,  100688572) /* Icon */;
+
+/* Lifestoned Changelog:
+{
+  "LastModified": "2020-02-23T12:32:46.8264189-05:00",
+  "ModifiedBy": "MasterAilan",
+  "Changelog": [
+    {
+      "created": "0001-01-01T00:00:00",
+      "author": "ACE.Adapter",
+      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
+    }
+  ],
+  "UserChangeSummary": "ruschkflag",
+  "IsDone": false
+}
+*/
