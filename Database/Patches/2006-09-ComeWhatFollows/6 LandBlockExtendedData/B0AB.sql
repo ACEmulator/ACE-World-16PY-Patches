7581d08b74860e84eda38c5d90446dfe8ec4a254
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-03-01 19:57:53 -0500

    Complete landscape spawns

--- Database/Patches/2006-09-ComeWhatFollows/6 LandBlockExtendedData/B0AB.sql
+++ Database/Patches/2006-09-ComeWhatFollows/6 LandBlockExtendedData/B0AB.sql
@@ -44,3 +44,44 @@ INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `ori
 VALUES (0x7B0AB010, 33964, 0xB0AB0109, 109.603, 82.2278, 54.001, -0.605676, 0, 0, -0.79571, False, '2020-02-28 22:39:02'); /* Pet Ursuin Cub */
 /* @teleloc 0xB0AB0109 [109.603000 82.227800 54.001000] -0.605676 0.000000 0.000000 -0.795711 */
 
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB011, 33963, 0xB0AB0102, 115.456, 88.6296, 58, -0.108222, 0, 0, -0.994127, False, '2020-03-01 19:40:24'); /* Pet Baby Thrungus */
+/* @teleloc 0xB0AB0102 [115.456001 88.629601 58.000000] -0.108222 0.000000 0.000000 -0.994127 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB012, 33963, 0xB0AB0102, 118.054, 87.2528, 58, -0.756639, 0, 0, 0.653832, False, '2020-03-01 19:40:33'); /* Pet Baby Thrungus */
+/* @teleloc 0xB0AB0102 [118.054001 87.252800 58.000000] -0.756639 0.000000 0.000000 0.653832 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB013, 33963, 0xB0AB0102, 117.729, 89.8698, 58, 0.0917131, 0, 0, 0.995785, False, '2020-03-01 19:40:40'); /* Pet Baby Thrungus */
+/* @teleloc 0xB0AB0102 [117.728996 89.869797 58.000000] 0.091713 0.000000 0.000000 0.995785 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB014, 33964, 0xB0AB0109, 109.985, 77.3806, 54.001, -0.999943, 0, 0, -0.0106502, False, '2020-03-01 19:42:56'); /* Pet Ursuin Cub */
+/* @teleloc 0xB0AB0109 [109.985001 77.380600 54.000999] -0.999943 0.000000 0.000000 -0.010650 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB017, 33962, 0xB0AB010C, 109.539, 87.1419, 61.0023, 0.722017, 0, 0, -0.691875, False, '2020-03-01 19:44:33'); /* Pet Baby Drudge */
+/* @teleloc 0xB0AB010C [109.539001 87.141899 61.002300] 0.722017 0.000000 0.000000 -0.691875 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB018, 33962, 0xB0AB010E, 117.624, 87.5115, 61.0023, 0.44483, 0, 0, 0.895615, False, '2020-03-01 19:44:42'); /* Pet Baby Drudge */
+/* @teleloc 0xB0AB010E [117.624001 87.511497 61.002300] 0.444830 0.000000 0.000000 0.895615 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB01A, 33961, 0xB0AB010E, 117.547, 83.4876, 61.0053, 0.274001, 0, 0, 0.961729, False, '2020-03-01 19:45:22'); /* Pet Armoredillo Pup */
+/* @teleloc 0xB0AB010E [117.546997 83.487602 61.005299] 0.274001 0.000000 0.000000 0.961729 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB01B, 33961, 0xB0AB010E, 117.807, 80.5045, 61.0053, 0.944478, 0, 0, 0.328575, False, '2020-03-01 19:45:29'); /* Pet Armoredillo Pup */
+/* @teleloc 0xB0AB010E [117.806999 80.504501 61.005299] 0.944478 0.000000 0.000000 0.328575 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB01C, 33964, 0xB0AB0109, 109.51, 74.5888, 54.001, 0.298405, 0, 0, -0.954439, False, '2020-03-01 19:47:27'); /* Pet Ursuin Cub */
+/* @teleloc 0xB0AB0109 [109.510002 74.588799 54.000999] 0.298405 0.000000 0.000000 -0.954439 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B0AB01D, 33964, 0xB0AB0109, 107.06159, 75.39349, 54.001, -0.9891612, 0, 0, -0.14683375, False, '2020-03-01 19:47:40'); /* Pet Ursuin Cub */
+/* @teleloc 0xB0AB0109 [107.061592 75.393494 54.000999] -0.989161 0.000000 0.000000 -0.146834 */
+
+
