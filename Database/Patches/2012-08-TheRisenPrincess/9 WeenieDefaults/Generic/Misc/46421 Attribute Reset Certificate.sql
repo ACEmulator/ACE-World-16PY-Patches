297fbe563350d5b517a6d42321e4d9f8dc8ebf30
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-03-03 03:31:58 -0500

    Update 46421 Attribute Reset Certificate.sql

--- Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/Generic/Misc/46421 Attribute Reset Certificate.sql
+++ Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/Generic/Misc/46421 Attribute Reset Certificate.sql
@@ -7,7 +7,7 @@ INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (46421,   1,        128) /* ItemType - Misc */
      , (46421,   5,          0) /* EncumbranceVal */
      , (46421,  16,          1) /* ItemUseable - No */
-     , (46421,  19,          0) /* Value */
+     , (46421,  19,          1) /* Value */
      , (46421,  33,          1) /* Bonded - Bonded */
      , (46421,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (46421, 114,          1) /* Attuned - Attuned */;
