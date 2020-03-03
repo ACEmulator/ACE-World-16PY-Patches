6442c466be5e2b393039331c4532c3ee246ec826
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-03-03 00:13:24 -0800

    .

--- Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/CraftTool/32651 Shadowfire Stone.sql
+++ Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/CraftTool/32651 Shadowfire Stone.sql
@@ -19,7 +19,7 @@ VALUES (32651,   1,       2048) /* ItemType - Gem */
      , (32651, 114,          1) /* Attuned - Attuned */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (32651,  22, True ) /* Inscribable */;
+VALUES (32651,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (32651,   1, 'Shadowfire Stone') /* Name */
