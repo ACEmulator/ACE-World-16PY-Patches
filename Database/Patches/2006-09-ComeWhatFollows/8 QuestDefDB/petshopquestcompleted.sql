127e5a1edae6e481cf8b2a80164589c230a23862
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-03-04 03:02:27 -0500

    line endings

--- Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/petshopquestcompleted.sql
+++ Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/petshopquestcompleted.sql
@@ -2,3 +2,4 @@ DELETE FROM `quest` WHERE `name` = 'petshopquestcompleted';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
 VALUES ('petshopquestcompleted', 72000, -1, 'Player has given all four letters to Larinne Kerendova and completed quest', '2019-03-14 20:34:27');
+
--- Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/petshopquestcounter.sql
+++ Database/Patches/2006-09-ComeWhatFollows/8 QuestDefDB/petshopquestcounter.sql
@@ -2,3 +2,4 @@ DELETE FROM `quest` WHERE `name` = 'petshopquestcounter';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
 VALUES ('petshopquestcounter', 0, 4, 'Pet Shop Quest Turnins', '2019-03-14 20:34:27');
+
