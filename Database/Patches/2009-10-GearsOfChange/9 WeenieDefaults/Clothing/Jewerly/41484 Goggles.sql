0010a2703e7cd7801dd73c450f8181befe4ec298
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-03-06 21:52:42 -0500

    Fix bad file formatting
    Murdered by notepad++. Fixed the formatting.

--- Database/Patches/2009-10-GearsOfChange/9 WeenieDefaults/Clothing/Jewerly/41484 Goggles.sql
+++ Database/Patches/2009-10-GearsOfChange/9 WeenieDefaults/Clothing/Jewerly/41484 Goggles.sql
@@ -6,13 +6,13 @@ VALUES (41484, 'ace41484-goggles', 1, '2019-02-10 00:00:00') /* Generic */;
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (41484,   1,          8) /* ItemType - Jewelry */
 	 , (41484,   3,         21) /* PaletteTemplate - Gold */
-     , (41484,   5,        100) /* EncumbranceVal */
+	 , (41484,   5,        100) /* EncumbranceVal */
 	 , (41484,   8,         60) /* Mass */
-     , (41484,   9,   67108864) /* ValidLocations - TrinketOne */
-     , (41484,  16,          1) /* ItemUseable - No */
-     , (41484,  19,        100) /* Value */
-     , (41484,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (41484, 169,   33687297) /* TsysMutationData */;
+	 , (41484,   9,   67108864) /* ValidLocations - TrinketOne */
+	 , (41484,  16,          1) /* ItemUseable - No */
+	 , (41484,  19,        100) /* Value */
+	 , (41484,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+	 , (41484, 169,   33687297) /* TsysMutationData */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (41484,  22, True ) /* Inscribable */;
@@ -25,7 +25,7 @@ VALUES (41484,   1, 'Goggles') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (41484,   1,   33554680) /* Setup */
-     , (41484,   3,  536870932) /* SoundTable */
-     , (41484,   6,   67111919) /* PaletteBase */
-     , (41484,   8,  100690604) /* Icon */
-     , (41484,  36,  234881046) /* MutateFilter */;
+	 , (41484,   3,  536870932) /* SoundTable */
+	 , (41484,   6,   67111919) /* PaletteBase */
+	 , (41484,   8,  100690604) /* Icon */
+	 , (41484,  36,  234881046) /* MutateFilter */;
--- Database/Patches/2009-10-GearsOfChange/9 WeenieDefaults/Clothing/Jewerly/41487 Mechanical Scarab.sql
+++ Database/Patches/2009-10-GearsOfChange/9 WeenieDefaults/Clothing/Jewerly/41487 Mechanical Scarab.sql
@@ -6,13 +6,13 @@ VALUES (41487, 'ace41487-mechanicalscarab', 1, '2019-02-10 00:00:00') /* Generic
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (41487,   1,          8) /* ItemType - Jewelry */
 	 , (41487,   3,         21) /* PaletteTemplate - Gold */
-     , (41487,   5,        100) /* EncumbranceVal */
+	 , (41487,   5,        100) /* EncumbranceVal */
 	 , (41487,   8,         60) /* Mass */
-     , (41487,   9,   67108864) /* ValidLocations - TrinketOne */
-     , (41487,  16,          1) /* ItemUseable - No */
-     , (41487,  19,        100) /* Value */
-     , (41487,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (41487, 169,   33687297) /* TsysMutationData */;
+	 , (41487,   9,   67108864) /* ValidLocations - TrinketOne */
+	 , (41487,  16,          1) /* ItemUseable - No */
+	 , (41487,  19,        100) /* Value */
+	 , (41487,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+	 , (41487, 169,   33687297) /* TsysMutationData */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (41487,  22, True ) /* Inscribable */;
@@ -25,9 +25,9 @@ VALUES (41487,   1, 'Mechanical Scarab') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (41487,   1,   33555211) /* Setup */
-     , (41487,   3,  536870932) /* SoundTable */
-     , (41487,   6,   67111919) /* PaletteBase */
-     , (41487,   8,  100690707) /* Icon */
-     , (41487,  22,  872415275) /* PhysicsEffectTable */
+	 , (41487,   3,  536870932) /* SoundTable */
+	 , (41487,   6,   67111919) /* PaletteBase */
+	 , (41487,   8,  100690707) /* Icon */
+	 , (41487,  22,  872415275) /* PhysicsEffectTable */
 	 , (41487,  36,  234881046) /* MutateFilter */;
 	 
