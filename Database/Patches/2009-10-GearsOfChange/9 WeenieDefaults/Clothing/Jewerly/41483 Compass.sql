cd2dd7cdac83770f69078c9addd9ce02ead10f7c
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-03-06 21:40:18 -0500

    Add 2009-10-GearsOfChange
    This PR adds the folder structure and the base weenies for trinkets.

---
+++ Database/Patches/2009-10-GearsOfChange/9 WeenieDefaults/Clothing/Jewerly/41483 Compass.sql
@@ -0,0 +1,33 @@
+DELETE FROM `weenie` WHERE `class_Id` = 41483;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (41483, 'ace41483-compass', 1, '2019-02-10 00:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (41483,   1,          8) /* ItemType - Jewelry */
+	 , (41483,   3,         21) /* PaletteTemplate - Gold */
+     , (41483,   5,        100) /* EncumbranceVal */
+	 , (41483,   8,         60) /* Mass */
+     , (41483,   9,   67108864) /* ValidLocations - TrinketOne */
+     , (41483,  16,          1) /* ItemUseable - No */
+     , (41483,  19,        100) /* Value */
+     , (41483,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (41483, 169,   33687297) /* TsysMutationData */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (41483,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (41483,  39,    0.67) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (41483,   1, 'Compass') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (41483,   1,   33554680) /* Setup */
+     , (41483,   3,  536870932) /* SoundTable */
+     , (41483,   6,   67111919) /* PaletteBase */
+     , (41483,   8,  100690599) /* Icon */
+	 , (41483,  36,  234881046) /* MutateFilter */;
+
+
---
+++ Database/Patches/2009-10-GearsOfChange/9 WeenieDefaults/Clothing/Jewerly/41486 Puzzle Box.sql
@@ -0,0 +1,31 @@
+DELETE FROM `weenie` WHERE `class_Id` = 41486;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (41486, 'ace41486-puzzlebox', 1, '2019-02-10 00:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (41486,   1,          8) /* ItemType - Jewelry */
+	 , (41486,   3,         21) /* PaletteTemplate - Gold */
+     , (41486,   5,        100) /* EncumbranceVal */
+	 , (41486,   8,         60) /* Mass */
+     , (41486,   9,   67108864) /* ValidLocations - TrinketOne */
+     , (41486,  16,          1) /* ItemUseable - No */
+     , (41486,  19,        100) /* Value */
+     , (41486,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (41486, 169,   33687297) /* TsysMutationData */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (41486,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (41486,  39,    0.67) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (41486,   1, 'Puzzle Box') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (41486,   1,   33554817) /* Setup */
+     , (41486,   3,  536870932) /* SoundTable */
+     , (41486,   6,   67111919) /* PaletteBase */
+     , (41486,   8,  100690665) /* Icon */
+	 , (41486,  36,  234881046) /* MutateFilter */;
