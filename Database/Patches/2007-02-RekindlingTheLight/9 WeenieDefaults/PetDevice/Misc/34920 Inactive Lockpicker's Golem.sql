20c4dddd82e7f734fbce3c015b4350deee5f2c7c
Ty Conner <tyconner@itcproductions.com>
2020-03-08 17:01:15 -0400

    Update 34920 Inactive Lockpicker's Golem.sql

--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/PetDevice/Misc/34920 Inactive Lockpicker's Golem.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/PetDevice/Misc/34920 Inactive Lockpicker's Golem.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34920;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34920, 'ace34920-inactivelockpickersgolem', 44, '2019-02-10 00:00:00') /* CraftTool */;
+VALUES (34920, 'ace34920-inactivelockpickersgolem', 70, '2020-03-08 00:00:00') /* CraftTool */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34920,   1,        128) /* ItemType - Misc */
@@ -11,7 +11,8 @@ VALUES (34920,   1,        128) /* ItemType - Misc */
      , (34920,  33,          1) /* Bonded - Bonded */
      , (34920,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (34920,  94,         16) /* TargetType - Creature */
-     , (34920, 114,          1) /* Attuned - Attuned */;
+     , (34920, 114,          1) /* Attuned - Attuned */
+     , (34920, 266,      34902) /* PetClass - Artisan Lockpicker's Golem */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (34920,  22, True ) /* Inscribable */
@@ -29,10 +30,4 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (34920,   1,   33554718) /* Setup */
      , (34920,   3,  536870932) /* SoundTable */
      , (34920,   8,  100689354) /* Icon */
-     , (34920,  22,  872415275) /* PhysicsEffectTable */
-     , (34920, 8001,    2637840) /* PCAPRecordedWeenieHeader - Usable, Container, TargetType, Burden */
-     , (34920, 8003,         18) /* PCAPRecordedObjectDesc - Inscribable, Attackable */
-     , (34920, 8005,       6273) /* PCAPRecordedPhysicsDesc - CSetup, ObjScale, STable, PeTable */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (34920, 8000, 2149818579) /* PCAPRecordedObjectIID */;
+     , (34920,  22,  872415275) /* PhysicsEffectTable */;
