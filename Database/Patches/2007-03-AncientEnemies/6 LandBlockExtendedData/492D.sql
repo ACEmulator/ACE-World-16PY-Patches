6cd9262301e77e4e535fdce858d0d9e2a039444d
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-03-08 11:42:52 -0400

    Add Up to Arcanum Lookout and Down to Surface portal spawns

--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
@@ -1,5 +1,3 @@
-DELETE FROM `landblock_instance` WHERE `landblock` = 0x492D;
-
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x7492D000, 35488, 0x492D0017, 53.7138, 152.896, 29.58046, -0.509899, 0, 0, -0.860234, False, '2020-03-08 04:03:33'); /* Skettek the Wanderer */
 /* @teleloc 0x492D0017 [53.713799 152.895996 29.580460] -0.509899 0.000000 0.000000 -0.860234 */
