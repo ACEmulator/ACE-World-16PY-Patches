cb9dd2674f7f53152935cf2641a768dd357ef317
Ty Conner <tyconner@itcproductions.com>
2020-03-08 16:51:47 -0400

    Update 46023 Pet Mouf Igloo.sql

--- Database/Patches/2012-07-SoullessSisters/9 WeenieDefaults/PetDevice/Misc/46023 Pet Mouf Igloo.sql
+++ Database/Patches/2012-07-SoullessSisters/9 WeenieDefaults/PetDevice/Misc/46023 Pet Mouf Igloo.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 46023;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (46023, 'ace46023-petmoufigloo', 44, '2019-02-10 00:00:00') /* CraftTool */;
+VALUES (46023, 'ace46023-petmoufigloo', 70, '2020-03-08 00:00:00') /* PetDevice */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (46023,   1,        128) /* ItemType - Misc */
@@ -11,7 +11,8 @@ VALUES (46023,   1,        128) /* ItemType - Misc */
      , (46023,  33,          1) /* Bonded - Bonded */
      , (46023,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (46023,  94,         16) /* TargetType - Creature */
-     , (46023, 114,          1) /* Attuned - Attuned */;
+     , (46023, 114,          1) /* Attuned - Attuned */
+     , (46023, 266,      46024) /* PetClass - Pet Mouf */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (46023,  22, True ) /* Inscribable */
@@ -29,10 +30,4 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (46023,   1,   33560149) /* Setup */
      , (46023,   3,  536870932) /* SoundTable */
      , (46023,   8,  100689285) /* Icon */
-     , (46023,  22,  872415275) /* PhysicsEffectTable */
-     , (46023, 8001,    2637848) /* PCAPRecordedWeenieHeader - Value, Usable, Container, TargetType, Burden */
-     , (46023, 8003,         18) /* PCAPRecordedObjectDesc - Inscribable, Attackable */
-     , (46023, 8005,       6273) /* PCAPRecordedPhysicsDesc - CSetup, ObjScale, STable, PeTable */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (46023, 8000, 2199735648) /* PCAPRecordedObjectIID */;
+     , (46023,  22,  872415275) /* PhysicsEffectTable */;
