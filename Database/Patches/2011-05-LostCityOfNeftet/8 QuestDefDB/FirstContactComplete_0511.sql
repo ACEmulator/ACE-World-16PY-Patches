7b309953e65b1bcf9dde1c59fe8b13a6beea1113
Jim <jrock75@msn.com>
2020-03-11 01:05:05 -0500

    Add support for First Contact Contract

---
+++ Database/Patches/2011-05-LostCityOfNeftet/8 QuestDefDB/FirstContactComplete_0511.sql
@@ -0,0 +1,8 @@
+DELETE FROM ace_world.quest WHERE NAME = 'FirstContactComplete_0511';
+--
+INSERT INTO quest (quest.name, quest.min_Delta, quest.max_Solves, quest.message, quest.last_Modified)
+VALUES ('FirstContactComplete_0511', 72000, -1, 'Give Translator Aun Laokhe Orders to Contact the Burun', '2020-3-11');
+
+SELECT * 
+FROM ace_world.quest
+ORDER BY id DESC
---
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Gem/44175 Contract for First Contact.sql
@@ -0,0 +1,37 @@
+DELETE FROM `weenie` WHERE `class_Id` = 44175;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (44175, 'ace44175-contractforfirstcontact', 38, '2020-03-11 04:59:44') /* Gem */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (44175,   1,       2048) /* ItemType - Gem */
+     , (44175,  11,          1) /* MaxStackSize */
+     , (44175,  12,          1) /* StackSize */
+     , (44175,  16,          8) /* ItemUseable - Contained */
+     , (44175,  18,          2) /* UiEffects - Poisoned */
+     , (44175,  19,        100) /* Value */
+     , (44175,  53,        101) /* PlacementPosition - Resting */
+     , (44175,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (44175,  94,         16) /* TargetType - Creature */
+     , (44175, 280,        100) /* SharedCooldown */
+     , (44175, 349,         32) /* UseCreatesContractId - Contract_32_First_Contact */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (44175,  11, True ) /* IgnoreCollisions */
+     , (44175,  13, True ) /* Ethereal */
+     , (44175,  14, True ) /* GravityStatus */
+     , (44175,  19, True ) /* Attackable */
+     , (44175,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (44175, 167,       2) /* CooldownDuration */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (44175,   1, 'Contract for First Contact') /* Name */
+     , (44175,  16, 'Retrieve any information you can from the Renegade Fortress and bring it to Aun Laokhe in Arwic.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (44175,   1,   33554773) /* Setup */
+     , (44175,   3,  536870932) /* SoundTable */
+     , (44175,   8,  100691926) /* Icon */
+     , (44175,  22,  872415275) /* PhysicsEffectTable */;
