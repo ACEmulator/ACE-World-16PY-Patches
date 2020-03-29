77064ca99f447716e15d96305ed4dd9f605b7da5
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-03-29 16:50:16 -0400

    spell table entries provided by Ziang

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05435 Spectral Void Magic Mastery.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05435 Spectral Void Magic Mastery.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 5435;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (5435, 'Spectral Void Magic Mastery', 36880 /* Skill, SingleStat, Additive */, 22 /* Jump */, -20, '2019-03-18 09:00:00');
+VALUES (5435, 'Spectral Void Magic Mastery', 36880 /* Skill, SingleStat, Additive */, 43 /* VoidMagic */, 150, '2020-03-29 00:00:00');
