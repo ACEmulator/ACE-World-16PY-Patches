899ecd2535cdfe965b1f7dcaa320ee4ee8a3ca34
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-03-29 14:01:02 -0700

    Update Gear Knight recall drop point

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05330 Gear Knight Invasion Area Camp Recall.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05330 Gear Knight Invasion Area Camp Recall.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell` WHERE `id` = 5330;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
-VALUES (5330, 'Gear Knight Invasion Area Camp Recall', 560988209, 145.5065, 14.36704, 178.005, 1, 0, 0, 0, '2020-01-11 00:00:00');
-/* @teleloc 0x21700031 [145.5065 14.36704 178.005] 1 0 0 0 */
+VALUES (5330, 'Gear Knight Invasion Area Camp Recall', 0x21700103, 156, 12, 177.704987, 0.714421, 0, 0, 0.699716, '2020-03-29 00:00:00');
+/* @teleloc 0x21700103 [156.000000 12.000000 177.704987] 0.714421 0.000000 0.000000 0.699716 */
