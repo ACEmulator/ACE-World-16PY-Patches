b3d657925a458e7c6ee89350240bbf501c39ed33
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-03-31 12:10:58 -0400

    Hurnmel the Smith vendors (#335)
    * initial commit
    
    * remove /* DamageRating */ from hurnmel the smith
    
    * update hurnmel appearance
    
    * remove wielded clothing
    
    * move to vendor folder

---
+++ Database/Patches/2008-05-Introductions/6 LandBlockExtendedData/00EA.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x00EA;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x700EA000, 37477, 0x00EA0139, 120.753, -59.8032, -77.994, 0.709497, 0, 0, -0.704708, False, '2020-03-08 09:13:34'); /* Hurnmel the Smith */
+/* @teleloc 0x00EA0139 [120.752998 -59.803200 -77.994003] 0.709497 0.000000 0.000000 -0.704708 */
