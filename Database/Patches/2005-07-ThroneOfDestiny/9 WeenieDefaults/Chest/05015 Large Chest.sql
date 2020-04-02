a8213b780ab04c6fbdeaeb2d27939cf891906546
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-04-01 19:25:17 -0700

    Chest changes and remove two encounters (#362)
    * Chest changes and remove two encounters
    
    * Fix other chests
    
    * .

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Chest/05015 Large Chest.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Chest/05015 Large Chest.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 5015;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (5015, 'chestfolthiddagger', 20, '2005-02-09 10:00:00') /* Chest */;
+VALUES (5015, 'chestfolthiddagger', 20, '2020-03-30 00:00:00') /* Chest */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (5015,   1,        512) /* ItemType - Container */
@@ -45,5 +45,5 @@ VALUES (5015,   1,   33554556) /* Setup */
      , (5015,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (5015, 1,  5016, 1200, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Dull Dagger (5016) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
-     , (5015, 1, 34346, 1200, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ancient Shard of Metal (34346) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
+VALUES (5015, -1,  5016, 1200, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Dull Dagger (5016) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
+     , (5015, -1, 34346, 1200, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ancient Shard of Metal (34346) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Chest/29081 Old Chest.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Chest/29081 Old Chest.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 29081;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (29081, 'chestthrungussteaming1', 20, '2019-04-08 04:44:07') /* Chest */;
+VALUES (29081, 'chestthrungussteaming1', 20, '2020-03-30 00:00:00') /* Chest */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (29081,   1,        512) /* ItemType - Container */
@@ -11,6 +11,7 @@ VALUES (29081,   1,        512) /* ItemType - Container */
      , (29081,  16,         48) /* ItemUseable - ViewedRemote */
      , (29081,  19,        200) /* Value */
      , (29081,  38,        250) /* ResistLockpick */
+     , (29081,  81,          1) /* MaxGeneratedObjects */
      , (29081,  82,          1) /* InitGeneratedObjects */
      , (29081,  83,          2) /* ActivationResponse - Use */
      , (29081,  93,       1048) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity */
