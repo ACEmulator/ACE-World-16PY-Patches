e690b2f69aca4b3c8001083169715f554add997e
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-02 22:17:31 -0400

    Update 44283 Bracelet of Endurance.sql

--- Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Clothing/Jewerly/44283 Bracelet of Endurance.sql
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Clothing/Jewerly/44283 Bracelet of Endurance.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 44283;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (44283, 'ace44283-braceletofendurance', 1, '2019-11-02 06:14:33') /* Generic */;
+VALUES (44283, 'ace44283-braceletofendurance', 1, '2020-04-02 06:14:33') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (44283,   1,          8) /* ItemType - Jewelry */
