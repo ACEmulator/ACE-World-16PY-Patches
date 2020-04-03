c6d78cc547e62a6fe771316ed27fa0bc1b3270aa
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-04-02 23:25:36 -0700

    Clean Up :: Part Deux

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05122 Call of Leadership V.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05122 Call of Leadership V.sql
@@ -1,4 +1,5 @@
+  
 DELETE FROM `spell` WHERE `id` = 5122;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (5122, 'Call of Leadership V', 36865 /* Attribute, SingleStat, Additive */, 3 /* Quickness */, 40, '2019-03-18 09:00:00');
+VALUES (5122, 'Call of Leadership V', 36866 /* Attribute, SingleStat, Additive */, 1 /* Health */, 10, '2020-03-24 09:00:00');
--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05139 Augmented Damage II.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05139 Augmented Damage II.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 5139;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (5139, 'Augmented Damage II', 36880 /* Skill, SingleStat, Additive */, 22 /* Jump */, -20, '2019-03-18 09:00:00');
+VALUES (5139, 'Augmented Damage II', 33591300 /* Int, SingleStat, Additive, Beneficial */, 307 /* DamageRating */, 2, '2020-03-18 09:00:00');
