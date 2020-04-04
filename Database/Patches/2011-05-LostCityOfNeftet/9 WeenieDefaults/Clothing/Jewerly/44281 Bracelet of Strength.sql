cdcb5507dc04de1932bbe6f9c0bbb8dc0df2ec2b
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-04 08:57:45 -0400

    Cleanup Panhsj's Bracelets (#372)
    * initial commit
    
    * Update 44283 Bracelet of Endurance.sql
    
    * destroyonsell

--- Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Clothing/Jewerly/44281 Bracelet of Strength.sql
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Clothing/Jewerly/44281 Bracelet of Strength.sql
@@ -1,11 +1,11 @@
 DELETE FROM `weenie` WHERE `class_Id` = 44281;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (44281, 'ace44281-braceletofstrength', 1, '2019-11-02 06:14:23') /* Generic */;
+VALUES (44281, 'ace44281-braceletofstrength', 1, '2020-04-02 06:14:23') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (44281,   1,          8) /* ItemType - Jewelry */
-     , (44281,   3,         81) /* PaletteTemplate - LiteGreen */
+     , (44281,   3,          8) /* PaletteTemplate - Green */
      , (44281,   5,         60) /* EncumbranceVal */
      , (44281,   9,     196608) /* ValidLocations - WristWear */
      , (44281,  16,          1) /* ItemUseable - No */
@@ -13,7 +13,7 @@ VALUES (44281,   1,          8) /* ItemType - Jewelry */
      , (44281,  53,        101) /* PlacementPosition - Resting */
      , (44281,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (44281, 106,        450) /* ItemSpellcraft */
-     , (44281, 107,       2808) /* ItemCurMana */
+     , (44281, 107,       3000) /* ItemCurMana */
      , (44281, 108,       3000) /* ItemMaxMana */
      , (44281, 109,          0) /* ItemDifficulty */
      , (44281, 158,          7) /* WieldRequirements - Level */
@@ -21,16 +21,12 @@ VALUES (44281,   1,          8) /* ItemType - Jewelry */
      , (44281, 160,        150) /* WieldDifficulty */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (44281,  11, True ) /* IgnoreCollisions */
-     , (44281,  13, True ) /* Ethereal */
-     , (44281,  14, True ) /* GravityStatus */
-     , (44281,  19, True ) /* Attackable */
-     , (44281,  22, True ) /* Inscribable */
-     , (44281,  91, True ) /* Retained */;
+VALUES (44281,  22, True ) /* Inscribable */
+     , (44281,  23, True ) /* DestroyOnSell */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (44281,   5, -0.0333333015441895) /* ManaRate */
-     , (44281,  39, 0.670000016689301) /* DefaultScale */;
+     , (44281,  39,   0.670000016689301) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (44281,   1, 'Bracelet of Strength') /* Name */
@@ -40,6 +36,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (44281,   1,   33554683) /* Setup */
      , (44281,   3,  536870932) /* SoundTable */
      , (44281,   6,   67111919) /* PaletteBase */
+     , (44281,   7,  268435738) /* ClothingBase */
      , (44281,   8,  100668625) /* Icon */
      , (44281,  22,  872415275) /* PhysicsEffectTable */;
 
--- Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Clothing/Jewerly/44283 Bracelet of Endurance.sql
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Clothing/Jewerly/44283 Bracelet of Endurance.sql
@@ -1,11 +1,11 @@
 DELETE FROM `weenie` WHERE `class_Id` = 44283;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (44283, 'ace44283-braceletofendurance', 1, '2019-11-02 06:14:33') /* Generic */;
+VALUES (44283, 'ace44283-braceletofendurance', 1, '2020-04-02 06:14:33') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (44283,   1,          8) /* ItemType - Jewelry */
-     , (44283,   3,         81) /* PaletteTemplate - LiteGreen */
+     , (44283,   3,          8) /* PaletteTemplate - Green */
      , (44283,   5,         60) /* EncumbranceVal */
      , (44283,   9,     196608) /* ValidLocations - WristWear */
      , (44283,  16,          1) /* ItemUseable - No */
@@ -13,7 +13,7 @@ VALUES (44283,   1,          8) /* ItemType - Jewelry */
      , (44283,  53,        101) /* PlacementPosition - Resting */
      , (44283,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (44283, 106,        450) /* ItemSpellcraft */
-     , (44283, 107,       2808) /* ItemCurMana */
+     , (44283, 107,       3000) /* ItemCurMana */
      , (44283, 108,       3000) /* ItemMaxMana */
      , (44283, 109,          0) /* ItemDifficulty */
      , (44283, 158,          7) /* WieldRequirements - Level */
@@ -21,16 +21,12 @@ VALUES (44283,   1,          8) /* ItemType - Jewelry */
      , (44283, 160,        150) /* WieldDifficulty */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (44283,  11, True ) /* IgnoreCollisions */
-     , (44283,  13, True ) /* Ethereal */
-     , (44283,  14, True ) /* GravityStatus */
-     , (44283,  19, True ) /* Attackable */
-     , (44283,  22, True ) /* Inscribable */
-     , (44283,  91, True ) /* Retained */;
+VALUES (44283,  22, True ) /* Inscribable */
+     , (44283,  23, True ) /* DestroyOnSell */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (44283,   5, -0.0333333015441895) /* ManaRate */
-     , (44283,  39, 0.670000016689301) /* DefaultScale */;
+     , (44283,  39,   0.670000016689301) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (44283,   1, 'Bracelet of Endurance') /* Name */
@@ -40,6 +36,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (44283,   1,   33554683) /* Setup */
      , (44283,   3,  536870932) /* SoundTable */
      , (44283,   6,   67111919) /* PaletteBase */
+     , (44283,   7,  268435738) /* ClothingBase */
      , (44283,   8,  100668625) /* Icon */
      , (44283,  22,  872415275) /* PhysicsEffectTable */;
 
