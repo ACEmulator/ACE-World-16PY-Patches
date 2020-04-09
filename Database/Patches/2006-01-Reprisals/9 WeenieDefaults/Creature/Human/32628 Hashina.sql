109297d4c5d45f2055c8c479afd33fc7b5d6bfde
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-04-09 14:01:22 -0700

    .

--- Database/Patches/2006-01-Reprisals/9 WeenieDefaults/Creature/Human/32628 Hashina.sql
+++ Database/Patches/2006-01-Reprisals/9 WeenieDefaults/Creature/Human/32628 Hashina.sql
@@ -108,7 +108,7 @@ VALUES (31296, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'CampEntemarreSt
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  10 /* Tell */, 0, 1, NULL, 'Have you found Baron Entemarre at his encampment in the Direlands yet?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL;
+VALUES (@parent_id,  0,  10 /* Tell */, 0, 1, NULL, 'Have you found Baron Entemarre at his encampment in the Direlands yet?', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (31296, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'CampEntemarreStarted@head', NULL, NULL, NULL);
