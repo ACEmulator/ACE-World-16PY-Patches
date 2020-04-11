7624cee4c0940b6bbe15495584d9914bf4a2c09f
harliq <30803300+harliq@users.noreply.github.com>
2020-04-11 02:17:35 -0400

    Formatting
    Fixed formatting per a Picky Drid :)

--- Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Misc/35179 Egg.sql
+++ Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Misc/35179 Egg.sql
@@ -5,7 +5,7 @@ VALUES (35179, 'ace35179-egg', 33, '2019-02-10 00:00:00') /* Missile */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (35179,   1,          0) /* ItemType - None */
-	 , (35179,   8,         25) /* Mass */
+     , (35179,   8,         25) /* Mass */
      , (35179,   9,          0) /* ValidLocations - None */
      , (35179,  66,          0) /* CheckpointStatus */
      , (35179,  93,     133960) /* PhysicsState - ReportCollisions, Missile, AlignPath, PathClipped, LightingOn, Inelastic */;
@@ -20,7 +20,7 @@ VALUES (35179,   1, True ) /* Stuck */
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (35179,  39,     0.9) /* DefaultScale */
-	 , (35179,  26,      15) /* MaximumVelocity */
+     , (35179,  26,      15) /* MaximumVelocity */
      , (35179,  78,       1) /* Friction */
      , (35179,  79,       0) /* Elasticity */;
 
@@ -32,3 +32,4 @@ VALUES (35179,   1,   33554673) /* Setup */
      , (35179,   3,  536871113) /* SoundTable */
      , (35179,   8,  100667460) /* Icon */
      , (35179,  28,       4078) /* Spell - EggBolt */;
+     
