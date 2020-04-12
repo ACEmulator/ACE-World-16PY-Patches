00bddf7e557b347c1ff7fe6ffda7792ab8396585
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-12 14:15:17 -0400

    Update EC0F.sql

--- Database/Patches/2007-06-Evolution/6 LandBlockExtendedData/EC0F.sql
+++ Database/Patches/2007-06-Evolution/6 LandBlockExtendedData/EC0F.sql
@@ -1,3 +1,85 @@
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+ INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x7EC0F016, 35769, 0xEC0F002A, 133.68, 25.9624, 235.105, -0.15579003, 0, 0, -0.98779017, True, '2020-04-11 19:33:55'); /* King's Crown */
 /* @teleloc 0xEC0F002A [133.679993 25.962400 235.104996] -0.155790 0.000000 0.000000 -0.987790 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F017, 35769, 0xEC0F002A, 131.544449, 29.564585, 235.105, -0.893281, 0, 0, -0.449499, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F002A [131.544449 29.564585 235.104996] -0.893281 0.000000 0.000000 -0.449499 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F018, 35769, 0xEC0F002A, 127.549805, 32.774799, 235.104996, -0.804088, 0, 0, -0.594511, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F002A [127.549805 32.774799 235.104996] -0.804088 0.000000 0.000000 -0.594511 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F019, 35769, 0xEC0F002A, 124.746346, 35.532261, 235.105, -0.934131, 0, 0, -0.356931, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F002A [124.746346 35.532261 235.104996] -0.934131 0.000000 0.000000 -0.356931 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F020, 35769, 0xEC0F002A, 116.435028, 36.359100, 235.104996, -0.464153, 0, 0, -0.885755, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0022 [116.435028 36.359100 235.104996] -0.464153 0.000000 0.000000 -0.885755 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F021, 35769, 0xEC0F002A, 115.000610, 33.106567, 235.104996, -0.464153, 0, 0, -0.885755, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0022 [115.000610 33.106567 235.104996] -0.464153 0.000000 0.000000 -0.885755 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F022, 35769, 0xEC0F002A, 112.429413, 32.979172, 235.104996, -0.650071, 0, 0, -0.759873, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0022 [112.429413 32.979172 235.104996] -0.650071 0.000000 0.000000 -0.759873 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F023, 35769, 0xEC0F002A, 109.798256, 30.219139, 235.104996, -0.422633, 0, 0, -0.906301, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0022 [109.798256 30.219139 235.104996] -0.422633 0.000000 0.000000 -0.906301 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F024, 35769, 0xEC0F002A, 107.774689, 20.430330, 235.104996, 0.519266, 0, 0, -0.854612, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0021 [107.774689 20.430330 235.104996] 0.519266 0.000000 0.000000 -0.854612 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F025, 35769, 0xEC0F002A, 108.775993, 17.712944, 235.104996, 0.194740, 0, 0, -0.980855, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0021 [108.775993 17.712944 235.104996] 0.194740 0.000000 0.000000 -0.980855 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F026, 35769, 0xEC0F002A, 111.916420, 16.082745, 235.104996, 0.519266, 0, 0, -0.854612, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0021 [111.916420 16.082745 235.104996] 0.519266 0.000000 0.000000 -0.854612 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F027, 35769, 0xEC0F002A, 115.618820, 12.428805, 235.104996, 0.453771, 0, 0, -0.891118, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0021 [115.618820 12.428805 235.104996] 0.453771 0.000000 0.000000 -0.891118 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F028, 35769, 0xEC0F002A, 124.541260, 11.687528, 235.104996, 0.893421, 0, 0, -0.449221, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0029 [124.541260 11.687528 235.104996] 0.893421 0.000000 0.000000 -0.449221 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F029, 35769, 0xEC0F002A, 125.307297, 14.333665, 235.104996, 0.990091, 0, 0, -0.140427, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0029 [125.307297 14.333665 235.104996] 0.990091 0.000000 0.000000 -0.140427 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F030, 35769, 0xEC0F002A, 128.263077, 16.308483, 235.104996, 0.881912, 0, 0, -0.471413, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0029 [128.263077 16.308483 235.104996] 0.881912 0.000000 0.000000 -0.471413 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F031, 35769, 0xEC0F002A, 131.289688, 18.894655, 235.104996, 0.974720, 0, 0, -0.223432, True, '2020-04-11 19:33:55'); /* King's Crown */
+/* @teleloc 0xEC0F0029 [131.289688 18.894655 235.104996] 0.974720 0.000000 0.000000 -0.223432 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7EC0F032, 15759, 0xEC0F002A, 137.12059, 25.144468, 235.15504, 0.0021579808, 0, 0, 0.9999977, False, '2020-04-12 13:50:43'); /* Linkable Item Generator */
+/* @teleloc 0xEC0F002A [137.120590 25.144468 235.155045] 0.002158 0.000000 0.000000 0.999998 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x7EC0F032, 0x7EC0F016, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F017, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F018, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F019, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F020, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F021, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F022, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F023, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F024, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F025, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F026, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F027, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F028, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F029, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F030, '2005-02-09 10:00:00') /* King's Crown */
+     , (0x7EC0F032, 0x7EC0F031, '2005-02-09 10:00:00') /* King's Crown */;
