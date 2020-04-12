d9b02e1ec185111332d204efd94d811361c9fc2b
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-12 11:30:12 -0400

    Dress two nakey npcs (#386)
    * initial commit
    
    * move patch folder, readd skinpalette back
    
    * Update 34143 The Boar of Cinghalle.sql
    
    * Update 34143 The Boar of Cinghalle.sql

--- Database/Patches/2006-10-DanceOfTheDead/9 WeenieDefaults/Creature/Human/34062 Arialle du Bellenesse.sql
+++ Database/Patches/2006-10-DanceOfTheDead/9 WeenieDefaults/Creature/Human/34062 Arialle du Bellenesse.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34062;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34062, 'ace34062-arialledubellenesse', 10, '2019-04-09 23:37:09') /* Creature */;
+VALUES (34062, 'ace34062-arialledubellenesse', 10, '2020-04-07 23:37:09') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34062,   1,         16) /* ItemType - Creature */
@@ -41,22 +41,25 @@ VALUES (34062,   1,   33554510) /* Setup */
      , (34062,   3,  536870914) /* SoundTable */
      , (34062,   6,   67108990) /* PaletteBase */
      , (34062,   8,  100667377) /* Icon */
-     , (34062,   9,   83890278) /* EyesTexture */
-     , (34062,  10,   83890312) /* NoseTexture */
-     , (34062,  11,   83890356) /* MouthTexture */
-     , (34062,  15,   67117019) /* HairPalette */
-     , (34062,  16,   67110063) /* EyesPalette */
      , (34062,  17,   67115901) /* SkinPalette */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
-VALUES (34062,   1,  10, 0, 0) /* Strength */
-     , (34062,   2,  10, 0, 0) /* Endurance */
-     , (34062,   3,  10, 0, 0) /* Quickness */
-     , (34062,   4,  10, 0, 0) /* Coordination */
-     , (34062,   5,  10, 0, 0) /* Focus */
-     , (34062,   6,  10, 0, 0) /* Self */;
+VALUES (34062,   1,  240, 0, 0) /* Strength */
+     , (34062,   2,  210, 0, 0) /* Endurance */
+     , (34062,   3,  280, 0, 0) /* Quickness */
+     , (34062,   4,  280, 0, 0) /* Coordination */
+     , (34062,   5,  170, 0, 0) /* Focus */
+     , (34062,   6,  170, 0, 0) /* Self */;
 
 INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
-VALUES (34062,   1,     0, 0, 0, 5) /* MaxHealth */
-     , (34062,   3,     0, 0, 0, 0) /* MaxStamina */
-     , (34062,   5,     0, 0, 0, 0) /* MaxMana */;
+VALUES (34062,   1,     150, 0, 0, 255) /* MaxHealth */
+     , (34062,   3,     200, 0, 0, 410) /* MaxStamina */
+     , (34062,   5,     200, 0, 0, 370) /* MaxMana */;
+     
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (34062, 2, 32782,  0, 0,       0, False) /* Create Shield of Sanamar (32782) for Wield */
+     , (34062, 2, 28629,  0, 14, 0.0959, False) /* Create Alduressa Coat (28629) for Wield */
+     , (34062, 2, 28620,  0, 14, 0.0959, False) /* Create Alduressa Leggings (28620) for Wield */
+     , (34062, 2, 30951,  0, 14,      0, False) /* Create Alduressa Gauntlets (30951) for Wield */
+     , (34062, 2, 30950,  0, 14,      0, False) /* Create Alduressa Boots (30950) for Wield */;
+     
