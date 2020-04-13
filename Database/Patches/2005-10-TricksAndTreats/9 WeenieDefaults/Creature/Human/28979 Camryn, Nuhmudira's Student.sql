92a2ff3fa947661ac87a4a9c230fe6152ecd9547
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-04-12 22:37:03 -0700

    .

--- Database/Patches/2005-10-TricksAndTreats/9 WeenieDefaults/Creature/Human/28979 Camryn, Nuhmudira's Student.sql
+++ Database/Patches/2005-10-TricksAndTreats/9 WeenieDefaults/Creature/Human/28979 Camryn, Nuhmudira's Student.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 28979;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (28979, 'studentcamryn', 10, '2005-02-09 10:00:00') /* Creature */;
+VALUES (28979, 'studentcamryn', 10, '2020-04-12 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (28979,   1,         16) /* ItemType - Creature */
