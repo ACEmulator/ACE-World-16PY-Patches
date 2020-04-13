2077f23dfdb0df44c5b5001bb5108cfb23476378
harliq <30803300+harliq@users.noreply.github.com>
2020-04-13 15:04:33 -0400

    Update Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Misc/35179 Egg.sql
    Co-Authored-By: Ty Conner <tyconner@itcproductions.com>

--- Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Misc/35179 Egg.sql
+++ Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Misc/35179 Egg.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 35179;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (35179, 'ace35179-egg', 33, '2019-02-10 00:00:00') /* Missile */;
+VALUES (35179, 'ace35179-egg', 33, '2019-02-10 00:00:00') /* ProjectileSpell */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (35179,   1,          0) /* ItemType - None */
