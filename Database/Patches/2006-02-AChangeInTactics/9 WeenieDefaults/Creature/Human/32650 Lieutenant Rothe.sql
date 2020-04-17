267ec357c9cb19ac648c9b9d01986847634b4992
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-16 21:35:58 -0400

    Update 32650 Lieutenant Rothe.sql

--- Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/Creature/Human/32650 Lieutenant Rothe.sql
+++ Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/Creature/Human/32650 Lieutenant Rothe.sql
@@ -80,7 +80,7 @@ VALUES (32650, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'KilltaskTumerokGladi
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id, 0, 30 /* InqQuestSolves */, 0, 1, NULL, 'KilltaskTumerokGladiatorWait@KillTaskInProgress', NULL, 1, 2147483647, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id, 0, 30 /* InqQuestSolves */, 0, 1, NULL, 'KilltaskTumerokGladiatorWait@KillTaskInProgress', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (32650, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'KilltaskTumerokGladiatorWait@KillTaskInProgress', NULL, NULL, NULL);
