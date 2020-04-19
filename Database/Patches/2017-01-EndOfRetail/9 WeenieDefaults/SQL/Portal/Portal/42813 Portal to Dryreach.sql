f0f52f0529386aff0da02781c0a502bd0c64a40e
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-19 16:30:46 -0400

    Update 42813 Portal to Dryreach.sql

--- Database/Patches/2017-01-EndOfRetail/9 WeenieDefaults/SQL/Portal/Portal/42813 Portal to Dryreach.sql
+++ Database/Patches/2017-01-EndOfRetail/9 WeenieDefaults/SQL/Portal/Portal/42813 Portal to Dryreach.sql
@@ -19,8 +19,7 @@ VALUES (42813,  54, -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (42813,   1, 'Portal to Dryreach') /* Name */
-     , (42813,  16, 'This fortified town fell under siege in the earlier days of Aluvian settlement to Tumeroks ushered in from the Hea Village of Ahurenga by the Virindi. The townpeople are often in need of donations from traveling adventurers lest they starve. This town is good for characters over level 20.') /* LongDesc */
-     , (42813,  38, 'Portal to Dryreach (8.1S, 73.0E).') /* AppraisalPortalDestination */;
+     , (42813,  16, 'This fortified town fell under siege in the earlier days of Aluvian settlement to Tumeroks ushered in from the Hea Village of Ahurenga by the Virindi. The townpeople are often in need of donations from traveling adventurers lest they starve. This town is good for characters over level 20.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (42813,   1,   33555926) /* Setup */
@@ -28,5 +27,5 @@ VALUES (42813,   1,   33555926) /* Setup */
      , (42813,   8,  100667499) /* Icon */;
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (42813, 2, 0xDA75002B, 132, 60, 18.005, 0, 0, 0, 0) /* Destination */
+VALUES (42813, 2, 0xDA75002B, 132, 60, 18.005, 1, 0, 0, 0) /* Destination */
 /* @teleloc 0xDA75002B 132.000000 60.000000 18.004999 1.000000 0.000000 0.000000 0.000000 */;
