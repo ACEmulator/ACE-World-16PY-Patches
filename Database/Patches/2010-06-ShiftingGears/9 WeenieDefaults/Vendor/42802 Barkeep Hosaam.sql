3d709e4cf89779d1b33c26bdf055b9a451d016fb
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-19 23:47:43 -0400

    Dress some bartenders (#398)
    * move to correct patch folder
    
    * dressem

--- Database/Patches/2010-06-ShiftingGears/9 WeenieDefaults/Vendor/42802 Barkeep Hosaam.sql
+++ Database/Patches/2010-06-ShiftingGears/9 WeenieDefaults/Vendor/42802 Barkeep Hosaam.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 42802;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (42802, 'ace42802-barkeephosaam', 12, '2019-02-10 00:00:00') /* Vendor */;
+VALUES (42802, 'ace42802-barkeephosaam', 12, '2020-04-11 00:00:00') /* Vendor */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (42802,   1,         16) /* ItemType - Creature */
@@ -17,8 +17,7 @@ VALUES (42802,   1,         16) /* ItemType - Creature */
      , (42802, 113,          1) /* Gender - Male */
      , (42802, 133,          4) /* ShowableOnRadar - ShowAlways */
      , (42802, 134,         16) /* PlayerKillerStatus - RubberGlue */
-     , (42802, 188,          2) /* HeritageGroup - Gharundim */
-     , (42802, 8007,          0) /* PCAPRecordedAutonomousMovement */;
+     , (42802, 188,          2) /* HeritageGroup - Gharundim */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (42802,   1, True ) /* Stuck */
@@ -37,31 +36,14 @@ VALUES (42802,  37, 0.899999976158142) /* BuyPrice */
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (42802,   1, 'Barkeep Hosaam') /* Name */
-     , (42802,   5, 'Barkeeper') /* Template */
-     , (42802, 8006, 'AAA9AAAAAAA=') /* PCAPRecordedCurrentMotionState */;
+     , (42802,   5, 'Barkeeper') /* Template */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (42802,   1,   33554433) /* Setup */
      , (42802,   2,  150994945) /* MotionTable */
      , (42802,   3,  536870913) /* SoundTable */
      , (42802,   6,   67108990) /* PaletteBase */
-     , (42802,   8,  100667446) /* Icon */
-     , (42802,   9,   83890491) /* EyesTexture */
-     , (42802,  10,   83890532) /* NoseTexture */
-     , (42802,  11,   83890608) /* MouthTexture */
-     , (42802,  15,   67116998) /* HairPalette */
-     , (42802,  16,   67110062) /* EyesPalette */
-     , (42802,  17,   67109551) /* SkinPalette */
-     , (42802, 8001,    8388662) /* PCAPRecordedWeenieHeader - ItemsCapacity, ContainersCapacity, Usable, UseRadius, RadarBehavior */
-     , (42802, 8003,        516) /* PCAPRecordedObjectDesc - Stuck, Vendor */
-     , (42802, 8005,     100355) /* PCAPRecordedPhysicsDesc - CSetup, MTable, STable, Position, Movement */;
-
-INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (42802, 8040, 459028, 17.572, -69.4711, 0.004999995, 0.8072571, 0, 0, -0.5902) /* PCAPRecordedLocation */
-/* @teleloc 0x00070114 [17.572000 -69.471100 0.005000] 0.807257 0.000000 0.000000 -0.590200 */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (42802, 8000, 1879076878) /* PCAPRecordedObjectIID */;
+     , (42802,   8,  100667446) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (42802,   1, 100, 0, 0) /* Strength */
@@ -77,7 +59,11 @@ VALUES (42802,   1,   100, 0, 0, 140) /* MaxHealth */
      , (42802,   5,    60, 0, 0, 80) /* MaxMana */;
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
-VALUES (42802, 4,  4746, -1, 0, 0, False) /* Create Water (4746) for Shop */
+VALUES (42802, 2,  2598,  0, 17, 0.9818, False) /* Create Baggy Pants (2598) for Wield */
+     , (42802, 2,   124,  0, 7, 0.3000, False) /* Create Shirt (130) for Wield */
+     , (42802, 2,   115,  0, 17, 1, False) /* Create Leather Boots (115) for Wield */
+     , (42802, 2, 10696,  0, 9, 0.3333, False) /* Create Apron (10696) for Wield */
+     , (42802, 4,  4746, -1, 0, 0, False) /* Create Water (4746) for Shop */
      , (42802, 4,  5136, -1, 0, 0, False) /* Create Cove Apple Cider (5136) for Shop */
      , (42802, 4,  2465, -1, 0, 0, False) /* Create Palm Wine (2465) for Shop */
      , (42802, 4,  2459, -1, 0, 0, False) /* Create Kumiss (2459) for Shop */
@@ -99,50 +85,3 @@ VALUES (42802, 4,  4746, -1, 0, 0, False) /* Create Water (4746) for Shop */
      , (42802, 4, 48782, -1, 0, 0, False) /* Create Contract for Second Sister (48782) for Shop */
      , (42802, 4, 48783, -1, 0, 0, False) /* Create Contract for Third Sister (48783) for Shop */;
 
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (42802, 67109551, 0, 24)
-     , (42802, 67110026, 72, 8)
-     , (42802, 67110062, 32, 8)
-     , (42802, 67110317, 64, 8)
-     , (42802, 67110317, 160, 8)
-     , (42802, 67110355, 216, 24)
-     , (42802, 67110363, 40, 24)
-     , (42802, 67110548, 92, 4)
-     , (42802, 67116998, 24, 8);
-
-INSERT INTO `weenie_properties_texture_map` (`object_Id`, `index`, `old_Id`, `new_Id`)
-VALUES (42802, 0, 83889072, 83890012)
-     , (42802, 0, 83889342, 83890011)
-     , (42802, 1, 83887064, 83886241)
-     , (42802, 2, 83887066, 83887051)
-     , (42802, 3, 83889344, 83887054)
-     , (42802, 4, 83887068, 83887054)
-     , (42802, 5, 83887064, 83886241)
-     , (42802, 6, 83887066, 83887051)
-     , (42802, 7, 83889344, 83887054)
-     , (42802, 8, 83887068, 83887054)
-     , (42802, 9, 83887061, 83890009)
-     , (42802, 9, 83887060, 83890010)
-     , (42802, 16, 83886232, 83890359)
-     , (42802, 16, 83886668, 83890491)
-     , (42802, 16, 83886837, 83890532)
-     , (42802, 16, 83886684, 83890608);
-
-INSERT INTO `weenie_properties_anim_part` (`object_Id`, `index`, `animation_Id`)
-VALUES (42802, 0, 16781835)
-     , (42802, 1, 16781845)
-     , (42802, 2, 16781866)
-     , (42802, 3, 16781841)
-     , (42802, 4, 16781838)
-     , (42802, 5, 16781846)
-     , (42802, 6, 16781864)
-     , (42802, 7, 16781840)
-     , (42802, 8, 16781839)
-     , (42802, 9, 16777300)
-     , (42802, 10, 16777301)
-     , (42802, 11, 16777302)
-     , (42802, 12, 16777304)
-     , (42802, 13, 16777303)
-     , (42802, 14, 16777305)
-     , (42802, 15, 16777307)
-     , (42802, 16, 16795638);
