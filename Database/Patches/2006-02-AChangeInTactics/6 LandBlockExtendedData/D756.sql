e2c3f1a0d24e5ac91a7bffe9a7480bb5440c371f
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-04-20 18:52:24 -0400

    Create D756.sql

---
+++ Database/Patches/2006-02-AChangeInTactics/6 LandBlockExtendedData/D756.sql
@@ -0,0 +1,63 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xD756;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756000,  218, 0xD756011E, 96.0268, 65.6979, 34.805, 0.0162827, 0, 0, -0.999867, True, '2020-04-20 18:27:13'); /* Black Rat */
+/* @teleloc 0xD756011E [96.026802 65.697899 34.805000] 0.016283 0.000000 0.000000 -0.999867 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756001,  949, 0xD756010D, 119.025, 68.4029, 28.805, -0.801102, 0, 0, -0.598527, True, '2020-04-20 18:30:41'); /* Red Rat */
+/* @teleloc 0xD756010D [119.025002 68.402901 28.805000] -0.801102 0.000000 0.000000 -0.598527 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756002,  949, 0xD756010E, 125.097, 69.1461, 28.805, -0.731467, 0, 0, -0.681877, True, '2020-04-20 18:30:47'); /* Red Rat */
+/* @teleloc 0xD756010E [125.097000 69.146103 28.805000] -0.731467 0.000000 0.000000 -0.681877 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756003,  949, 0xD756010A, 118.05, 61.0052, 28.805, -0.9856, 0, 0, -0.169094, True, '2020-04-20 18:30:54'); /* Red Rat */
+/* @teleloc 0xD756010A [118.050003 61.005199 28.805000] -0.985600 0.000000 0.000000 -0.169094 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756004,  1625, 0xD756010B, 125.829, 56.1712, 28.805, -0.973811, 0, 0, -0.227358, True, '2020-04-20 18:31:02'); /* Mountain Rat */
+/* @teleloc 0xD756010B [125.829002 56.171200 28.805000] -0.973811 0.000000 0.000000 -0.227358 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756005,  1625, 0xD7560108, 127.761, 49.1997, 28.805, -0.982674, 0, 0, -0.185344, True, '2020-04-20 18:31:34'); /* Mountain Rat */
+/* @teleloc 0xD7560108 [127.761002 49.199699 28.805000] -0.982674 0.000000 0.000000 -0.185344 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756006,  1625, 0xD7560108, 129.186, 49.757, 28.805, -0.966023, 0, 0, -0.258455, True, '2020-04-20 18:31:36'); /* Mountain Rat */
+/* @teleloc 0xD7560108 [129.186005 49.757000 28.805000] -0.966023 0.000000 0.000000 -0.258455 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756007,  1626, 0xD7560109, 134.624, 48.2467, 28.805, -0.934915, 0, 0, -0.354871, True, '2020-04-20 18:31:41'); /* Silver Rat */
+/* @teleloc 0xD7560109 [134.623993 48.246700 28.805000] -0.934915 0.000000 0.000000 -0.354871 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756008,  1626, 0xD7560107, 134.193, 40.5637, 28.805, -0.95592, 0, 0, -0.293628, True, '2020-04-20 18:31:46'); /* Silver Rat */
+/* @teleloc 0xD7560107 [134.192993 40.563702 28.805000] -0.955920 0.000000 0.000000 -0.293628 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D756009,  1626, 0xD7560106, 128.0316, 40.73574, 28.804998, -0.9938989, 0, 0, -0.11029457, True, '2020-04-20 18:31:50'); /* Silver Rat */
+/* @teleloc 0xD7560106 [128.031601 40.735741 28.804998] -0.993899 0.000000 0.000000 -0.110295 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D75600A, 32727, 0xD7560102, 119.352, 22.5199, 28.82, -0.966036, 0, 0, 0.258409, True, '2020-04-20 18:42:14'); /* Rat King */
+/* @teleloc 0xD7560102 [119.351997 22.519899 28.820000] -0.966036 0.000000 0.000000 0.258409 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7D75600B,  7923, 0xD756011E, 96.0268, 65.6979, 34.805, 0.0162827, 0, 0, -0.999867, False, '2020-04-20 18:27:13'); /* Linkable Monster Generator ( 3 Min.) */
+/* @teleloc 0xF784031F [56.259200 277.711000 0.404999] 0.825336 0.000000 0.000000 -0.564642 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x7D75600B, 0x7D756000, '2020-04-20 10:00:00') /* Black Rat */
+     , (0x7D75600B, 0x7D756001, '2020-04-20 10:00:00') /* Red Rat */
+     , (0x7D75600B, 0x7D756002, '2020-04-20 10:00:00') /* Red Rat */
+     , (0x7D75600B, 0x7D756003, '2020-04-20 10:00:00') /* Red Rat */
+     , (0x7D75600B, 0x7D756004, '2020-04-20 10:00:00') /* Mountain Rat */
+     , (0x7D75600B, 0x7D756005, '2020-04-20 10:00:00') /* Mountain Rat */
+     , (0x7D75600B, 0x7D756006, '2020-04-20 10:00:00') /* Mountain Rat */
+     , (0x7D75600B, 0x7D756007, '2020-04-20 10:00:00') /* Silver Rat */
+     , (0x7D75600B, 0x7D756008, '2020-04-20 10:00:00') /* Silver Rat */
+     , (0x7D75600B, 0x7D756009, '2020-04-20 10:00:00') /* Silver Rat */
+     , (0x7D75600B, 0x7D75600A, '2020-04-20 10:00:00') /* Rat King */;
+     
