5a4c9d82a84e9cd9ef13847651362fdd516d0fdb
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-04-22 09:14:27 -0700

    initial commit

---
+++ Database/Patches/2006-03-DownTwistingPaths/8 QuestDefDB/swordofbellenese.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'swordofbellenese';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('swordofbellenese', 72000, -1, 'Sword of Bellenese Timer', '2019-11-21 00:00:00');
