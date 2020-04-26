64e27e73ed5f1b673e00aa6208fd5d8c556763d8
eviosdelam <53102393+eviosdelam@users.noreply.github.com>
2020-04-26 13:42:30 -0700

    Update BloodShrethKillTask.sql

--- Database/Patches/2005-08-FriendAndFoe/8 QuestDefDB/BloodShrethKillTask.sql
+++ Database/Patches/2005-08-FriendAndFoe/8 QuestDefDB/BloodShrethKillTask.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'BloodShrethKillTask';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('BloodShrethKillTask', 72000, -1, 'Blood Shreth Kill Task', '2019-02-04 06:51:50');
+VALUES ('KilltaskBloodShrethWait', 72000, -1, 'Blood Shreth Kill Task Timer', '2020-04-26 00:00:00');
