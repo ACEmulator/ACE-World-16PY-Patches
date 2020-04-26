7d5d4e1fd060365170d7f90c0d7278da308d7ee8
eviosdelam <53102393+eviosdelam@users.noreply.github.com>
2020-04-26 03:13:28 -0700

    Updated to standard naming

--- Database/Patches/2005-08-FriendAndFoe/8 QuestDefDB/bloodshrethkillcount.sql
+++ Database/Patches/2005-08-FriendAndFoe/8 QuestDefDB/bloodshrethkillcount.sql
@@ -1,4 +1,11 @@
-DELETE FROM `quest` WHERE `name` = 'bloodshrethkillcount';
+UPDATE `ace_world`.`weenie_properties_string` SET `value` = 'KilltaskBloodShreth' WHERE `id` = 58514;
+
+DELETE FROM `quest` WHERE `id` = 3746;
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('bloodshrethkillcount', 0, 10, 'Blood Shreth Kill Counter', '2019-02-04 06:51:50');
+VALUES ('KilltaskBloodShreth', 0, 10, 'Blood Shreth Kill Task', '2020-04-26 00:00:00');
+
+DELETE FROM `quest` WHERE `id` = 3747;
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KilltaskBloodShrethWait', 72000, -1, 'Blood Shreth Kill Task Timer', '2020-04-26 00:00:00');
