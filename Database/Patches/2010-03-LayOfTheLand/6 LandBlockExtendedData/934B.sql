b489f352e021439934924be1208f095ae8f44d01
freeLoadN <freeLoadN@gmail.com>
2020-04-29 22:03:30 -0600

    Create 934B.sql

---
+++ Database/Patches/2010-03-LayOfTheLand/6 LandBlockExtendedData/934B.sql
@@ -0,0 +1,9 @@
+/*
+Contains only Cara, missing all other 2010-03-LayOfTheLand content.
+*/
+
+#INSERT INTO landblock_instance (guid, weenie_Class_Id, obj_Cell_Id, origin_X, origin_Y, origin_Z, angles_W, angles_X, angles_Y, angles_Z, is_Link_Child, last_Modified)
+#VALUES (2471231487,  42519, 2471166244, 33.5201, 27.8121, 17.205, 0.275855, 0, 0, 0.961199, True, '2020-04-29 00:00:00'); / Cara the Apprentice */
+
+#INSERT INTO landblock_instance_link (parent_GUID, child_GUID, last_Modified)
+#VALUES (2033496074, 2471231487, '2020-04-29 10:00:00');
