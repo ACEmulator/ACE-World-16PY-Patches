6932c6b429281746574026490cbbe8c8830c871d
Ty Conner <tyconner@itcproductions.com>
2020-05-10 04:16:38 -0400

    Update 32715 Lo Raen.sql (#426)
    * Update 32715 Lo Raen.sql
    
    * Update and rename BloodShrethKillTask.sql to KilltaskBloodShreth.sql
    
    * Update and rename BloodShrethKillTaskWait.sql to KilltaskBloodShrethWait.sql
    
    * Update 32715 Lo Raen.sql
    
    * Update 32715 Lo Raen.sql

--- Database/Patches/2005-08-FriendAndFoe/8 QuestDefDB/KilltaskBloodShrethWait.sql
+++ Database/Patches/2005-08-FriendAndFoe/8 QuestDefDB/KilltaskBloodShrethWait.sql
@@ -1,4 +1,4 @@
-DELETE FROM `quest` WHERE `name` = 'BloodShrethKillTask';
+DELETE FROM `quest` WHERE `name` = 'KilltaskBloodShrethWait';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
 VALUES ('KilltaskBloodShrethWait', 72000, -1, 'Blood Shreth Kill Task Timer', '2020-04-26 00:00:00');
