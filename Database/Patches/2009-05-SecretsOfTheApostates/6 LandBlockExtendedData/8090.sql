6a5d40c1a1aed8873a1908d9b4f821d84f0c7381
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-05-15 12:38:08 -0400

    Component Exchanger - 2009-05-SecretsOfTheApostates (#429)
    * initial commit
    
    * other two spawn locations

---
+++ Database/Patches/2009-05-SecretsOfTheApostates/6 LandBlockExtendedData/8090.sql
@@ -0,0 +1,4 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x78090081, 40770, 0x80900161, 14.51279, 38.6134, 124.06022, 0.4153462, 0, 0, 0.9096634, False, '2020-05-12 23:57:58'); /* Component Exchanger */
+/* @teleloc 0x80900161 [14.512790 38.613400 124.060219] 0.415346 0.000000 0.000000 0.909663 */
+
