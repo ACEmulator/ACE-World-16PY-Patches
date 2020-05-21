06a5299283b0fb0f3e522949e4ccd843ddff076a
Ty Conner <tyconner@itcproductions.com>
2020-05-21 14:06:16 -0400

    Update 34604 Arena Bell.sql

--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Unsorted/34604 Arena Bell.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Unsorted/34604 Arena Bell.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34604;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34604, 'ace34604-arenabell', 10, '2019-02-10 00:00:00') /* Creature */;
+VALUES (34604, 'ace34604-arenabell', 10, '2020-05-21 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34604,   1,         16) /* ItemType - Creature */
