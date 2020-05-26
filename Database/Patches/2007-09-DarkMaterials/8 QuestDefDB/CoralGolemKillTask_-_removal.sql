d03d0f311f9aa6f1f207f2d87f757713198cba8c
Skeksis <skeksis_ac@hotmail.com>
2020-05-25 22:29:15 -0700

    Added Coral Golem Killtask NPC, Items, and quest string to golems. Updated coralgolemkillcount and removed CoralGolemKillTask cooldown as it is not needed.

---
+++ Database/Patches/2007-09-DarkMaterials/8 QuestDefDB/CoralGolemKillTask_-_removal.sql
@@ -0,0 +1,1 @@
+DELETE FROM `quest` WHERE `name` = 'CoralGolemKillTask'; /* Quest has no cooldown timer */
---
+++ Database/Patches/2007-09-DarkMaterials/8 QuestDefDB/coralgolemkillcount_-_update.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'coralgolemkillcount';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('coralgolemkillcount', 0, 50, 'Coral Golem Hunting: Coral Shield', '2020-05-25 13:46:50');
