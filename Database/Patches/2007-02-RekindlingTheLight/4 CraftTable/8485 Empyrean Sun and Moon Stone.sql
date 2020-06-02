29279ff831398f9a8f04d1214e4f535c87f2b869
Ty Conner <tyconner@itcproductions.com>
2020-06-02 14:01:38 -0400

    .

--- Database/Patches/2007-02-RekindlingTheLight/4 CraftTable/8485 Empyrean Sun and Moon Stone.sql
+++ Database/Patches/2007-02-RekindlingTheLight/4 CraftTable/8485 Empyrean Sun and Moon Stone.sql
@@ -1,10 +1,9 @@
 DELETE FROM `recipe` WHERE `id` = 8485;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (8485, 0, 0, 0, 0, 71080 /* Empyrean Moon and Sun Stone */, 1, 'The stones fit together to create part of a larger stone. There is still a section missing.', 0, 0, 'You fail.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2020-06-01 10:00:00');
+VALUES (8485, 0, 0, 0, 0, 71080 /* Empyrean Sun and Moon Stone */, 1, 'The stones fit together to create part of a larger stone. There is still a section missing.', 0, 0, 'You fail.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2020-06-01 10:00:00');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 8485;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
 VALUES (8485, 34911 /* Stone of Alb'arel */, 34913 /* Empyrean Sun Stone */, '2020-06-01 10:00:00');
-
