37399fa85be7e6d2c596c8d0645e4e541af8b368
Ty Conner <tyconner@itcproductions.com>
2020-06-05 03:57:01 -0400

    add keepalive to lobby

--- Database/Patches/2007-02-RekindlingTheLight/6 LandBlockExtendedData/SQL/00AF.sql
+++ Database/Patches/2007-02-RekindlingTheLight/6 LandBlockExtendedData/SQL/00AF.sql
@@ -53,10 +53,10 @@ VALUES (0x700AF093,  1154, 0x00AF0118, 37, -13.0434, 0, 1, 0, 0, 0, False, '2019
 /* @teleloc 0x00AF0118 [37.000000 -13.043400 0.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
-VALUES (0x700AF093, 0x700AF094, '2019-02-10 00:00:00') /* Garbage Barrel */
-     , (0x700AF093, 0x700AF095, '2019-02-10 00:00:00') /* Garbage Barrel */
-     , (0x700AF093, 0x700AF096, '2019-02-10 00:00:00') /* Colosseum Arena */
-     , (0x700AF093, 0x700AF097, '2019-02-10 00:00:00') /* Advanced Colosseum Arena */;
+VALUES (0x700AF093, 0x700AF094, '2019-02-10 00:00:00') /* Garbage Barrel (34726) */
+     , (0x700AF093, 0x700AF095, '2019-02-10 00:00:00') /* Garbage Barrel (34726) */
+     , (0x700AF093, 0x700AF096, '2019-02-10 00:00:00') /* Colosseum Arena (34727) */
+     , (0x700AF093, 0x700AF097, '2019-02-10 00:00:00') /* Advanced Colosseum Arena (34728) */;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x700AF094, 34726, 0x00AF0118, 37, -13.0434, 0, 1, 0, 0, 0,  True, '2019-02-10 00:00:00'); /* Garbage Barrel */
@@ -68,58 +68,59 @@ VALUES (0x700AF095, 34726, 0x00AF0118, 43, -13.0567, 0, 1, 0, 0, 0,  True, '2019
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x700AF096, 34727, 0x00AF010F, 30.8082, 4.51301, 0, 1, 0, 0, 0,  True, '2019-02-10 00:00:00'); /* Colosseum Arena */
-/* @teleloc 0x00AF010F [30.808200 4.513010 0.000000] 1.000000 0.000000 0.000000 0.000000 */
+/* @teleloc 0x00AF010F [30.808201 4.513010 0.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x700AF097, 34728, 0x00AF0138, 49.1829, 4.27901, 0, 1, 0, 0, 0,  True, '2019-02-10 00:00:00'); /* Advanced Colosseum Arena */
-/* @teleloc 0x00AF0138 [49.182900 4.279010 0.000000] 1.000000 0.000000 0.000000 0.000000 */
+/* @teleloc 0x00AF0138 [49.182899 4.279010 0.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF112,  1154, 0x00AF0117, 40, 0, 0.00750005, 0, 0, 0, -1,  False, '2019-02-10 00:00:00'); /* Linkable Monster Generator */
+VALUES (0x700AF112,  28282, 0x00AF0117, 40, 0, 0.00750005, 0, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Linkable Monster Gen - 10 sec. */
 /* @teleloc 0x00AF0117 [40.000000 0.000000 0.007500] 0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
-VALUES (0x700AF112, 0x700AF114, '2019-02-10 00:00:00') /* Master Arbitrator */
-     , (0x700AF112, 0x700AF115, '2019-02-10 00:00:00') /* Colosseum Ticket Vendor */
-     , (0x700AF112, 0x700AF116, '2019-02-10 00:00:00') /* Gladiator Diemos */
-     , (0x700AF112, 0x700AF117, '2019-02-10 00:00:00') /* The Master */
-     , (0x700AF112, 0x700AF11C, '2019-02-10 00:00:00') /* Arena of the Pumpkin King */;
+VALUES (0x700AF112, 0x700AF114, '2019-02-10 00:00:00') /* Master Arbitrator (34441) */
+     , (0x700AF112, 0x700AF115, '2019-02-10 00:00:00') /* Colosseum Ticket Vendor (34442) */
+     , (0x700AF112, 0x700AF116, '2019-02-10 00:00:00') /* Gladiator Diemos (35869) */
+     , (0x700AF112, 0x700AF117, '2019-02-10 00:00:00') /* The Master (35870) */
+     , (0x700AF112, 0x700AF11C, '2019-02-10 00:00:00') /* Arena of the Pumpkin King (36532) */
+     , (0x700AF112, 0x700AF128, '2020-06-05 03:24:29') /* Colo Lobby KeepAlive Generator (71812) */;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF113, 70302, 0x00AF0117, 38, 4.4, 0, -4.371139E-08, 0, 0, -1,  False, '2019-02-10 00:00:00'); /* Colo Arena One Stopgap Generator */
-/* @teleloc 0x00AF0117 [38.000000 4.400000 0.000000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x700AF113, 70302, 0x00AF0117, 38, 4.4, 0, -4.37114E-08, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Colo Arena One Stopgap Generator */
+/* @teleloc 0x00AF0117 [38.000000 4.400000 0.000000] -0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF114, 34441, 0x00AF0117, 40, 0, 0.007500052, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Master Arbitrator */
+VALUES (0x700AF114, 34441, 0x00AF0117, 40, 0, 0.00750005, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Master Arbitrator */
 /* @teleloc 0x00AF0117 [40.000000 0.000000 0.007500] 0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF115, 34442, 0x00AF0139, 50, -13, 0.006500006, 1, 0, 0, -4.37114E-08,  True, '2019-02-10 00:00:00'); /* Colosseum Ticket Vendor */
-/* @teleloc 0x00AF0139 [50.000000 -13.000000 0.006500] 1.000000 0.000000 0.000000 0.000000 */
+VALUES (0x700AF115, 34442, 0x00AF0139, 50, -13, 0.00650001, 1, 0, 0, -4.37114E-08,  True, '2019-02-10 00:00:00'); /* Colosseum Ticket Vendor */
+/* @teleloc 0x00AF0139 [50.000000 -13.000000 0.006500] 1.000000 0.000000 0.000000 -0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF116, 35869, 0x00AF0142, 60, -37.3, 0.008000016, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Gladiator Diemos */
-/* @teleloc 0x00AF0142 [60.000000 -37.300000 0.008000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x700AF116, 35869, 0x00AF0142, 60, -37.3, 0.00800002, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Gladiator Diemos */
+/* @teleloc 0x00AF0142 [60.000000 -37.299999 0.008000] 0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF117, 35870, 0x00AF0133, 42.5, -70, 0, -0.7071068, 0, 0, -0.7071068,  True, '2019-02-10 00:00:00'); /* The Master */
+VALUES (0x700AF117, 35870, 0x00AF0133, 42.5, -70, 0, -0.707107, 0, 0, -0.707107,  True, '2019-02-10 00:00:00'); /* The Master */
 /* @teleloc 0x00AF0133 [42.500000 -70.000000 0.000000] -0.707107 0.000000 0.000000 -0.707107 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF118, 70511, 0x00AF0117, 39, 4.4, 0, -4.371139E-08, 0, 0, -1,  False, '2019-02-10 00:00:00'); /* Colo Arena Two Stopgap Generator */
-/* @teleloc 0x00AF0117 [39.000000 4.400000 0.000000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x700AF118, 70511, 0x00AF0117, 39, 4.4, 0, -4.37114E-08, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Colo Arena Two Stopgap Generator */
+/* @teleloc 0x00AF0117 [39.000000 4.400000 0.000000] -0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF119, 70512, 0x00AF0117, 40, 4.39999, 0, -4.371139E-08, 0, 0, -1,  False, '2019-02-10 00:00:00'); /* Colo Arena Three Stopgap Generator */
-/* @teleloc 0x00AF0117 [40.000000 4.399990 0.000000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x700AF119, 70512, 0x00AF0117, 40, 4.39999, 0, -4.37114E-08, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Colo Arena Three Stopgap Generator */
+/* @teleloc 0x00AF0117 [40.000000 4.399990 0.000000] -0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF11A, 70513, 0x00AF0117, 41, 4.4, 0, -4.371139E-08, 0, 0, -1,  False, '2019-02-10 00:00:00'); /* Colo Arena Four Stopgap Generator */
-/* @teleloc 0x00AF0117 [41.000000 4.400000 0.000000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x700AF11A, 70513, 0x00AF0117, 41, 4.4, 0, -4.37114E-08, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Colo Arena Four Stopgap Generator */
+/* @teleloc 0x00AF0117 [41.000000 4.400000 0.000000] -0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF11B, 70514, 0x00AF0117, 42, 4.4, 0, -4.371139E-08, 0, 0, -1,  False, '2019-02-10 00:00:00'); /* Colo Arena Five Stopgap Generator */
-/* @teleloc 0x00AF0117 [42.000000 4.400000 0.000000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x700AF11B, 70514, 0x00AF0117, 42, 4.4, 0, -4.37114E-08, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Colo Arena Five Stopgap Generator */
+/* @teleloc 0x00AF0117 [42.000000 4.400000 0.000000] -0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x700AF11C, 36532, 0x00AF0152, 140, -42.528, 0, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Arena of the Pumpkin King */
@@ -162,5 +163,9 @@ VALUES (0x700AF126, 71078, 0x00AF015C, 199.817, -38.8083, 0.11, -0.0102795, 0, 0
 /* @teleloc 0x00AF015C [199.817001 -38.808300 0.110000] -0.010279 0.000000 0.000000 -0.999947 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AF127, 71075, 0x00AF015C, 201.01556, -39.828537, 0.161, 0.014719671, 0, 0, -0.99989164, False, '2020-05-29 18:54:45'); /* VeryMadCowWave1 */
-/* @teleloc 0x00AF015C [201.015564 -39.828537 0.161000] 0.014720 0.000000 0.000000 -0.999892 */
+VALUES (0x700AF127, 71075, 0x00AF015C, 201.016, -39.8285, 0.161, 0.0147197, 0, 0, -0.999892, False, '2020-05-29 18:54:45'); /* VeryMadCowWave1 */
+/* @teleloc 0x00AF015C [201.016006 -39.828499 0.161000] 0.014720 0.000000 0.000000 -0.999892 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x700AF128, 71812, 0x00AF0110, 27.289091, -6.0450277, 0.054999996, -0.6950934, 0, 0, 0.7189194,  True, '2020-06-05 03:24:29'); /* Colo Lobby KeepAlive Generator */
+/* @teleloc 0x00AF0110 [27.289091 -6.045028 0.055000] -0.695093 0.000000 0.000000 0.718919 */
