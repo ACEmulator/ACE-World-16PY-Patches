0b76ce7b88ae6a262566e324dbc32a53e4f680a0
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-06-06 03:27:53 -0400

    Diemos Update
    Balance Diemos and Guardian

--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Golem/34614 Gladiator Diemos.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Golem/34614 Gladiator Diemos.sql
@@ -38,23 +38,23 @@ VALUES (34614,   1,       5) /* HeartbeatInterval */
      , (34614,   7,    0.25) /* StaminaUponResurrection */
      , (34614,   8,     0.3) /* ManaUponResurrection */
      , (34614,  12,     0.5) /* Shade */
-     , (34614,  13,     0.8) /* ArmorModVsSlash */
+     , (34614,  13,     0.7) /* ArmorModVsSlash */
      , (34614,  14,     0.8) /* ArmorModVsPierce */
-     , (34614,  15,     0.8) /* ArmorModVsBludgeon */
-     , (34614,  16,       1) /* ArmorModVsCold */
+     , (34614,  15,     0.5) /* ArmorModVsBludgeon */
+     , (34614,  16,     0.6) /* ArmorModVsCold */
      , (34614,  17,       1) /* ArmorModVsFire */
      , (34614,  18,       1) /* ArmorModVsAcid */
      , (34614,  19,       1) /* ArmorModVsElectric */
-     , (34614,  31,      20) /* VisualAwarenessRange */
+     , (34614,  31,      22) /* VisualAwarenessRange */
      , (34614,  34,     2.3) /* PowerupTime */
      , (34614,  39,     1.6) /* DefaultScale */
      , (34614,  64,    0.33) /* ResistSlash */
      , (34614,  65,    0.33) /* ResistPierce */
      , (34614,  66,     0.8) /* ResistBludgeon */
-     , (34614,  67,    0.75) /* ResistFire */
-     , (34614,  68,    0.75) /* ResistCold */
-     , (34614,  69,     0.8) /* ResistAcid */
-     , (34614,  70,    0.75) /* ResistElectric */
+     , (34614,  67,     0.8) /* ResistFire */
+     , (34614,  68,     0.8) /* ResistCold */
+     , (34614,  69,     0.5) /* ResistAcid */
+     , (34614,  70,     0.4) /* ResistElectric */
      , (34614,  71,       1) /* ResistHealthBoost */
      , (34614,  72,       1) /* ResistStaminaDrain */
      , (34614,  73,       1) /* ResistStaminaBoost */
@@ -95,32 +95,38 @@ VALUES (34614,   1, 599500, 0, 0, 600000) /* MaxHealth */
      , (34614,   5,  9500, 0, 0, 10000) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (34614,  6, 0, 3, 0, 300, 0, 0) /* MeleeDefense        Specialized */
+VALUES (34614,  6, 0, 3, 0, 350, 0, 0) /* MeleeDefense        Specialized */
      , (34614,  7, 0, 3, 0, 429, 0, 0) /* MissileDefense      Specialized */
      , (34614, 14, 0, 3, 0, 300, 0, 0) /* ArcaneLore          Specialized */
-     , (34614, 15, 0, 3, 0, 265, 0, 0) /* MagicDefense        Specialized */
-     , (34614, 20, 0, 3, 0, 100, 0, 0) /* Deception           Specialized */
+     , (34614, 15, 0, 3, 0, 375, 0, 0) /* MagicDefense        Specialized */
+     , (34614, 20, 0, 3, 0, 500, 0, 0) /* Deception           Specialized */
      , (34614, 22, 0, 3, 0,  10, 0, 0) /* Jump                Specialized */
      , (34614, 24, 0, 3, 0,  10, 0, 0) /* Run                 Specialized */
-     , (34614, 31, 0, 3, 0, 190, 0, 0) /* CreatureEnchantment Specialized */
-     , (34614, 33, 0, 3, 0, 190, 0, 0) /* LifeMagic           Specialized */
-     , (34614, 34, 0, 3, 0, 190, 0, 0) /* WarMagic            Specialized */
+     , (34614, 31, 0, 3, 0, 255, 0, 0) /* CreatureEnchantment Specialized */
+     , (34614, 33, 0, 3, 0, 255, 0, 0) /* LifeMagic           Specialized */
+     , (34614, 34, 0, 3, 0, 255, 0, 0) /* WarMagic            Specialized */
      , (34614, 45, 0, 3, 0, 275, 0, 0) /* LightWeapons        Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
-VALUES (34614,  0,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
-     , (34614,  1,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
-     , (34614,  2,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
-     , (34614,  3,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
-     , (34614,  4,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
-     , (34614,  5,  4, 130, 0.75,  300,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
-     , (34614,  6,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
-     , (34614,  7,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
-     , (34614,  8,  4, 130, 0.75,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+VALUES (34614,  0,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (34614,  1,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (34614,  2,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (34614,  3,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (34614,  4,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (34614,  5,  4, 350, 0.75,  450,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (34614,  6,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (34614,  7,  4,  0,    0,   450,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (34614,  8,  4, 350, 0.75,  450,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
 VALUES (34614,  4101,  2.115)  /* Champion's Skullduggery */
-     , (34614,  4102,  2.115)  /* Champion's Clever Ruse */;
+     , (34614,  4102,  2.05)  /* Champion's Clever Ruse */
+     , (34614,  2146,  2.105)  /* Evisceration */
+     , (34614,  2144,  2.115)  /* Crushing Shame */
+     , (34614,  2136,  2.145)  /* Icy Torment */
+     , (34614,  2070,  2.115)  /* Heart Rend */
+     , (34614,  2128,  2.15)  /* Ilservian's Flame */
+     , (34614,  2140,  2.115)  /* Alset's Coil */;
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (34614, 9, 34452,  0, 0, 1, False) /* Create Gladiator Diemos Token (34452) for ContainTreasure */
--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Golem/35670 Guardian of Diemos.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Golem/35670 Guardian of Diemos.sql
@@ -10,13 +10,13 @@ VALUES (35670,   1,         16) /* ItemType - Creature */
      , (35670,   6,         -1) /* ItemsCapacity */
      , (35670,   7,         -1) /* ContainersCapacity */
      , (35670,  16,          1) /* ItemUseable - No */
-     , (35670,  25,        135) /* Level */
+     , (35670,  25,        750) /* Level */
      , (35670,  27,          0) /* ArmorType - None */
      , (35670,  40,          2) /* CombatMode - Melee */
      , (35670,  68,          9) /* TargetingTactic - Random, TopDamager */
      , (35670,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
      , (35670, 133,          2) /* ShowableOnRadar - ShowMovement */
-     , (35670, 146,     250000) /* XpOverride */;
+     , (35670, 146,    1750000) /* XpOverride */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (35670,   1, True ) /* Stuck */
@@ -31,7 +31,7 @@ VALUES (35670,   1, True ) /* Stuck */
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (35670,   1,       5) /* HeartbeatInterval */
      , (35670,   2,       0) /* HeartbeatTimestamp */
-     , (35670,   3,     1.2) /* HealthRate */
+     , (35670,   3,      30) /* HealthRate */
      , (35670,   4,     0.5) /* StaminaRate */
      , (35670,   5,       2) /* ManaRate */
      , (35670,   6,     0.1) /* HealthUponResurrection */
@@ -39,22 +39,22 @@ VALUES (35670,   1,       5) /* HeartbeatInterval */
      , (35670,   8,     0.3) /* ManaUponResurrection */
      , (35670,  12,     0.5) /* Shade */
      , (35670,  13,     0.8) /* ArmorModVsSlash */
-     , (35670,  14,     0.8) /* ArmorModVsPierce */
+     , (35670,  14,       1) /* ArmorModVsPierce */
      , (35670,  15,     0.8) /* ArmorModVsBludgeon */
      , (35670,  16,       1) /* ArmorModVsCold */
      , (35670,  17,       1) /* ArmorModVsFire */
-     , (35670,  18,       1) /* ArmorModVsAcid */
+     , (35670,  18,     0.8) /* ArmorModVsAcid */
      , (35670,  19,       1) /* ArmorModVsElectric */
-     , (35670,  31,      20) /* VisualAwarenessRange */
+     , (35670,  31,      22) /* VisualAwarenessRange */
      , (35670,  34,     2.3) /* PowerupTime */
      , (35670,  39,     1.2) /* DefaultScale */
      , (35670,  64,    0.33) /* ResistSlash */
      , (35670,  65,    0.33) /* ResistPierce */
-     , (35670,  66,     0.8) /* ResistBludgeon */
-     , (35670,  67,    0.75) /* ResistFire */
-     , (35670,  68,    0.75) /* ResistCold */
-     , (35670,  69,     0.8) /* ResistAcid */
-     , (35670,  70,    0.75) /* ResistElectric */
+     , (35670,  66,     0.7) /* ResistBludgeon */
+     , (35670,  67,    0.20) /* ResistFire */
+     , (35670,  68,    0.70) /* ResistCold */
+     , (35670,  69,    0.70) /* ResistAcid */
+     , (35670,  70,    0.45) /* ResistElectric */
      , (35670,  71,       1) /* ResistHealthBoost */
      , (35670,  72,       1) /* ResistStaminaDrain */
      , (35670,  73,       1) /* ResistStaminaBoost */
@@ -96,11 +96,11 @@ VALUES (35670,   1,   500, 0, 0, 1000) /* MaxHealth */
      , (35670,   5,   500, 0, 0, 1000) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (35670,  6, 0, 3, 0, 300, 0, 0) /* MeleeDefense        Specialized */
+VALUES (35670,  6, 0, 3, 0, 420, 0, 0) /* MeleeDefense        Specialized */
      , (35670,  7, 0, 3, 0, 429, 0, 0) /* MissileDefense      Specialized */
      , (35670, 14, 0, 3, 0, 300, 0, 0) /* ArcaneLore          Specialized */
-     , (35670, 15, 0, 3, 0, 265, 0, 0) /* MagicDefense        Specialized */
-     , (35670, 20, 0, 3, 0, 100, 0, 0) /* Deception           Specialized */
+     , (35670, 15, 0, 3, 0, 420, 0, 0) /* MagicDefense        Specialized */
+     , (35670, 20, 0, 3, 0, 999, 0, 0) /* Deception           Specialized */
      , (35670, 22, 0, 3, 0,  10, 0, 0) /* Jump                Specialized */
      , (35670, 24, 0, 3, 0,  10, 0, 0) /* Run                 Specialized */
      , (35670, 31, 0, 3, 0, 190, 0, 0) /* CreatureEnchantment Specialized */
@@ -109,30 +109,23 @@ VALUES (35670,  6, 0, 3, 0, 300, 0, 0) /* MeleeDefense        Specialized */
      , (35670, 45, 0, 3, 0, 275, 0, 0) /* LightWeapons        Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
-VALUES (35670,  0,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
-     , (35670,  1,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
-     , (35670,  2,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
-     , (35670,  3,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
-     , (35670,  4,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
-     , (35670,  5,  4, 130, 0.75,  300,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
-     , (35670,  6,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
-     , (35670,  7,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
-     , (35670,  8,  4, 130, 0.75,  300,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+VALUES (35670,  0,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (35670,  1,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (35670,  2,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (35670,  3,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (35670,  4,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (35670,  5,  4, 350, 0.75,  700,  240,  240,  240,  300,  300,  300,  300,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (35670,  6,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (35670,  7,  4,  0,    0,  700,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (35670,  8,  4, 350, 0.75,  700,  240,  240,  240,  300,  300,  300,  300,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
 VALUES (35670,    69,  2.055)  /* Shock Wave VI */
      , (35670,    91,  2.008)  /* Force Bolt VI */
-     , (35670,    97,  2.055)  /* Whirling Blade VI */
-     , (35670,   170,      2)  /* Regeneration Self VI */
      , (35670,   234,    2.1)  /* Vulnerability Other VI */
-     , (35670,  1242,   2.01)  /* Drain Health Other VI */
-     , (35670,  1254,   2.01)  /* Drain Stamina Other VI */
-     , (35670,  1265,   2.01)  /* Drain Mana Other VI */
      , (35670,  2164,   2.08)  /* Swordsman's Gift */
      , (35670,  2166,   2.08)  /* Tusker's Gift */
-     , (35670,  2174,   2.08)  /* Archer's Gift */
-     , (35670,  2763,   2.01)  /* Martyr's Hecatomb IV */;
-
+     , (35670,  2174,   2.08)  /* Archer's Gift */;
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (35670,  5 /* HeartBeat */,  0.075, NULL, 2147483708 /* HandCombat */, 1090519043 /* Ready */, NULL, NULL, NULL, NULL);
