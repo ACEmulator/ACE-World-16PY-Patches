74fed75329ef837e0b922588ee35384704529c61
Ty Conner <tyconner@itcproductions.com>
2020-06-06 04:17:29 -0400

    Update 36543 Gladiator Diemos Pass.sql

--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Stackable/Misc/36543 Gladiator Diemos Pass.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Stackable/Misc/36543 Gladiator Diemos Pass.sql
@@ -12,33 +12,23 @@ VALUES (36543,   1,        128) /* ItemType - Misc */
      , (36543,  15,         50) /* StackUnitValue */
      , (36543,  16,          1) /* ItemUseable - No */
      , (36543,  19,         50) /* Value */
-     , (36543,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+     , (36543,  33,          1) /* Bonded - Bonded */
+     , (36543,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (36543, 114,          1) /* Attuned - Attuned */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (36543,   1, False) /* Stuck */
-     , (36543,  11, True ) /* IgnoreCollisions */
-     , (36543,  13, True ) /* Ethereal */
-     , (36543,  14, True ) /* GravityStatus */
-     , (36543,  19, True ) /* Attackable */;
+VALUES (36543,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (36543,  39, 1.20000004768372) /* DefaultScale */;
+VALUES (36543,  39,    0.67) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (36543,   1, 'Gladiator Diemos Pass') /* Name */;
+VALUES (36543,   1, 'Gladiator Diemos Pass') /* Name */
+     , (36543,  14, 'Hand this ticket to the statue of Gladiator Diemos to gain access to his Arena for 24 hours.') /* Use */
+     , (36543,  16, 'A pass that grants limited access to a Gladiator Diemos'' Arena.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (36543,   1,   33556750) /* Setup */
+VALUES (36543,   1,   33554773) /* Setup */
      , (36543,   3,  536870932) /* SoundTable */
-     , (36543,   6,   67111919) /* PaletteBase */
      , (36543,   8,  100689631) /* Icon */
-     , (36543,  22,  872415275) /* PhysicsEffectTable */
-     , (36543, 8001,    2125848) /* PCAPRecordedWeenieHeader - Value, Usable, StackSize, MaxStackSize, Container, Burden */
-     , (36543, 8003,         16) /* PCAPRecordedObjectDesc - Attackable */
-     , (36543, 8005,       6273) /* PCAPRecordedPhysicsDesc - CSetup, ObjScale, STable, PeTable */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (36543, 8000,      36543) /* PCAPRecordedObjectIID */;
-
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (36543, 67111926, 0, 0);
+     , (36543,  22,  872415275) /* PhysicsEffectTable */;
