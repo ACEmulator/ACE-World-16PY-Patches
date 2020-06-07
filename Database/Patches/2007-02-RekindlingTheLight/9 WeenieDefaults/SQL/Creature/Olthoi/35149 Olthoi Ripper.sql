a98ce5f05172e3d2c6593f9f984702aa3d08188a
Ty Conner <tyconner@itcproductions.com>
2020-06-07 15:19:02 -0400

    Update Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Olthoi/35149 Olthoi Ripper.sql
    Co-authored-by: bobigor377 <61636128+bobigor377@users.noreply.github.com>

--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Olthoi/35149 Olthoi Ripper.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Olthoi/35149 Olthoi Ripper.sql
@@ -94,7 +94,6 @@ VALUES (35149,  6, 0, 3, 0, 380, 0, 0) /* MeleeDefense        Specialized */
      , (35149, 22, 0, 2, 0, 200, 0, 0) /* Jump                Trained */
      , (35149, 24, 0, 2, 0, 350, 0, 0) /* Run                 Trained */;
 
-
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (35149,  0,  4,  5,    0,  350,  242,  280,  210,  350,  350,  385,  350,    0, 1,  0.1,    0,    0,  0.1,    0,    0,  0.1,    0,    0,  0.1,    0,    0) /* Head */
      , (35149, 16,  4,  5,    0,  350,  242,  280,  210,  350,  350,  385,  350,    0, 2, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45) /* Torso */
