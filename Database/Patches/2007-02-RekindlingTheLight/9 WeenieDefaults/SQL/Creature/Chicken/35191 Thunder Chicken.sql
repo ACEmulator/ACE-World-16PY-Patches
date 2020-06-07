da58ee7ee2b25cabf1a8ea21cb2c1bc650f07e68
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-06-07 04:26:16 -0400

    More Matron Balance
    adjusted physcal resistances

--- Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Chicken/35191 Thunder Chicken.sql
+++ Database/Patches/2007-02-RekindlingTheLight/9 WeenieDefaults/SQL/Creature/Chicken/35191 Thunder Chicken.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 35191;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (35191, 'ace35191-thunderchicken', 10, '2019-02-10 00:00:00') /* Creature */;
+VALUES (35191, 'ace35191-thunderchicken', 10, '2020-05-24 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (35191,   1,         16) /* ItemType - Creature */
