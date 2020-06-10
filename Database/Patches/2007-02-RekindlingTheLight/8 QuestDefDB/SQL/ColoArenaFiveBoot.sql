46554cde743f80d89b9f532835d8b0af3b7dbe62
Ty Conner <tyconner@itcproductions.com>
2020-06-09 20:54:30 -0400

    update boot timing

--- Database/Patches/2007-02-RekindlingTheLight/8 QuestDefDB/SQL/ColoArenaFiveBoot.sql
+++ Database/Patches/2007-02-RekindlingTheLight/8 QuestDefDB/SQL/ColoArenaFiveBoot.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'ColoArenaFiveBoot';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('ColoArenaFiveBoot', 3590, -1, 'Boot Timer for Colo Arena Five', '2020-05-20 06:51:50');
+VALUES ('ColoArenaFiveBoot', 3588, -1, 'Boot Timer for Colo Arena Five', '2020-05-20 06:51:50');
--- Database/Patches/2007-02-RekindlingTheLight/8 QuestDefDB/SQL/ColoArenaTwoBoot.sql
+++ Database/Patches/2007-02-RekindlingTheLight/8 QuestDefDB/SQL/ColoArenaTwoBoot.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'ColoArenaTwoBoot';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('ColoArenaTwoBoot', 3590, -1, 'Boot Timer for Colo Arena Two', '2020-05-20 06:51:50');
+VALUES ('ColoArenaTwoBoot', 3588, -1, 'Boot Timer for Colo Arena Two', '2020-05-20 06:51:50');
