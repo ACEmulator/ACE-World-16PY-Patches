2d163ef91dbec27b9af0120f9f3be2411e636edd
Ty Conner <tyconner@itcproductions.com>
2020-06-10 03:34:33 -0400

    Update 04040 Master's Innervation.sql

--- Database/Patches/2007-02-RekindlingTheLight/2 SpellTableExtendedData/SQL/04040 Master's Innervation.sql
+++ Database/Patches/2007-02-RekindlingTheLight/2 SpellTableExtendedData/SQL/04040 Master's Innervation.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4040;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (4040, 'Master''s Innervation', 20488 /* Float, SingleStat, Multiplicative */, 3 /* HealthRate */, 1000, '2020-06-10 09:00:00');
+VALUES (4040, 'Master''s Innervation', 20488 /* Float, SingleStat, Multiplicative */, 3 /* HealthRate */, 11.0, '2020-06-10 09:00:00');
