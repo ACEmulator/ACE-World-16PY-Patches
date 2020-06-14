ed9bb182473bf0a2bb636ce214a1597b73710f3a
Ty Conner <tyconner@itcproductions.com>
2020-06-14 16:46:47 -0400

    Update 53150 Luminous Amber of the 5th Tier Paragon.sql

--- Database/Patches/2014-02-Spring2014/9 WeenieDefaults/SQL/CraftTool/Gem/53150 Luminous Amber of the 5th Tier Paragon.sql
+++ Database/Patches/2014-02-Spring2014/9 WeenieDefaults/SQL/CraftTool/Gem/53150 Luminous Amber of the 5th Tier Paragon.sql
@@ -5,6 +5,7 @@ VALUES (53150, 'ace53150-luminousamberofthe5thtierparagon', 44, '2019-02-10 00:0
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (53150,   1,       2048) /* ItemType - Gem */
+     , (53150,   3,         83) /* PaletteTemplate - Amber */
      , (53150,   5,        100) /* EncumbranceVal */
      , (53150,  11,          1) /* MaxStackSize */
      , (53150,  12,          1) /* StackSize */
@@ -15,8 +16,7 @@ VALUES (53150,   1,       2048) /* ItemType - Gem */
      , (53150,  19,         25) /* Value */
      , (53150,  33,          1) /* Bonded - Bonded */
      , (53150,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (53150,  94,      33025) /* TargetType - WeaponOrCaster */
-     , (53150, 8041,        101) /* PCAPRecordedPlacement - Resting */;
+     , (53150,  94,      33025) /* TargetType - WeaponOrCaster */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (53150,  22, True ) /* Inscribable */
@@ -32,22 +32,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (53150,   1,   33554809) /* Setup */
      , (53150,   3,  536870932) /* SoundTable */
      , (53150,   6,   67111919) /* PaletteBase */
+     , (53150,   7,  268435723) /* ClothingBase */
      , (53150,   8,  100693327) /* Icon */
      , (53150,  22,  872415275) /* PhysicsEffectTable */
-     , (53150,  52,  100691593) /* IconUnderlay */
-     , (53150, 8001,    2650265) /* PCAPRecordedWeenieHeader - PluralName, Value, Usable, UiEffects, StackSize, MaxStackSize, Container, TargetType, Burden */
-     , (53150, 8002,          1) /* PCAPRecordedWeenieHeader2 - IconUnderlay */
-     , (53150, 8003,   67108882) /* PCAPRecordedObjectDesc - Inscribable, Attackable, IncludesSecondHeader */
-     , (53150, 8005,     137217) /* PCAPRecordedPhysicsDesc - CSetup, STable, PeTable, AnimationFrame */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (53150, 8000, 3434237489) /* PCAPRecordedObjectIID */;
-
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (53150, 67111921, 0, 0);
-
-INSERT INTO `weenie_properties_texture_map` (`object_Id`, `index`, `old_Id`, `new_Id`)
-VALUES (53150, 0, 83890391, 83890391);
-
-INSERT INTO `weenie_properties_anim_part` (`object_Id`, `index`, `animation_Id`)
-VALUES (53150, 0, 16779181);
+     , (53150,  52,  100691593) /* IconUnderlay */;
