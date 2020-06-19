32cb195eceb13f02487f4a16438c32f69a453c8f
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-06-19 14:45:33 -0400

    initial commit

--- Database/Patches/2006-07-TowardAncientShores/9 WeenieDefaults/Clothing/Armor/33579 Ancient Relic Vestment and Bracers.sql
+++ Database/Patches/2006-07-TowardAncientShores/9 WeenieDefaults/Clothing/Armor/33579 Ancient Relic Vestment and Bracers.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 33579;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (33579, 'ace33579-ancientrelicvestmentandbracers', 2, '2019-08-12 00:40:47') /* Clothing */;
+VALUES (33579, 'ace33579-ancientrelicvestmentandbracers', 2, '2020-06-19 00:40:47') /* Clothing */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (33579,   1,          2) /* ItemType - Armor */
@@ -13,7 +13,6 @@ VALUES (33579,   1,          2) /* ItemType - Armor */
      , (33579,  18,          1) /* UiEffects - Magical */
      , (33579,  19,      20000) /* Value */
      , (33579,  28,        440) /* ArmorLevel */
-     , (33579,  53,        101) /* PlacementPosition - Resting */
      , (33579,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (33579, 106,        400) /* ItemSpellcraft */
      , (33579, 107,        800) /* ItemCurMana */
@@ -25,20 +24,16 @@ VALUES (33579,   1,          2) /* ItemType - Armor */
      , (33579, 265,          6) /* EquipmentSetId - AncientRelic */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (33579,  11, True ) /* IgnoreCollisions */
-     , (33579,  13, True ) /* Ethereal */
-     , (33579,  14, True ) /* GravityStatus */
-     , (33579,  19, True ) /* Attackable */
-     , (33579,  22, True ) /* Inscribable */;
+VALUES (33579,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (33579,   5, -0.0329999998211861) /* ManaRate */
-     , (33579,  13, 1.29999995231628) /* ArmorModVsSlash */
-     , (33579,  14, 0.800000011920929) /* ArmorModVsPierce */
-     , (33579,  15, 1.29999995231628) /* ArmorModVsBludgeon */
+VALUES (33579,   5,  -0.033) /* ManaRate */
+     , (33579,  13,     1.3) /* ArmorModVsSlash */
+     , (33579,  14,     0.8) /* ArmorModVsPierce */
+     , (33579,  15,     1.3) /* ArmorModVsBludgeon */
      , (33579,  16,       1) /* ArmorModVsCold */
      , (33579,  17,       1) /* ArmorModVsFire */
-     , (33579,  18, 1.10000002384186) /* ArmorModVsAcid */
+     , (33579,  18,     1.1) /* ArmorModVsAcid */
      , (33579,  19,     0.5) /* ArmorModVsElectric */
      , (33579, 165,       1) /* ArmorModVsNether */;
 
