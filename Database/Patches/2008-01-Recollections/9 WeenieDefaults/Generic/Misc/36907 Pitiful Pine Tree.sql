e6b99230a78fef92ae3afdae82c3f9dbf483d48a
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-06-22 15:29:24 -0400

    cleanup other weenies in folder

--- Database/Patches/2008-01-Recollections/9 WeenieDefaults/Generic/Misc/36907 Pitiful Pine Tree.sql
+++ Database/Patches/2008-01-Recollections/9 WeenieDefaults/Generic/Misc/36907 Pitiful Pine Tree.sql
@@ -1,22 +1,19 @@
 DELETE FROM `weenie` WHERE `class_Id` = 36907;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (36907, 'ace36907-pitifulpinetree', 1, '2020-03-07 00:00:00') /* Generic */;
+VALUES (36907, 'ace36907-pitifulpinetree', 1, '2020-06-22 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (36907,   1,       1024) /* ItemType - Useless */
      , (36907,   5,        500) /* EncumbranceVal */
-	 , (36907,   9,          0) /* ValidLocations - None */
      , (36907,  16,          1) /* ItemUseable - No */
      , (36907,  19,          5) /* Value */
-     , (36907,  65,        101) /* Placement - Resting */
      , (36907,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
 	 , (36907, 150,        103) /* HookPlacement - Hook */
      , (36907, 151,          9) /* HookType - Floor, Yard */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (36907,  13, True ) /* Ethereal */
-     , (36907,  22, True ) /* Inscribable */;
+VALUES (36907,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (36907,  39, 0.6) /* DefaultScale */;
