2db311eeb8bd68bfedfa8c54d92d78f151016649
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-06-24 00:20:59 -0700

    Dryreach Rescue update

---
+++ Database/Patches/2011-06-StiringsInTheDark/6 LandBlockExtendedData/DB74.sql
@@ -0,0 +1,18 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7DB7401E,   412, 0xDB740007, 17.325, 156, 18.082, -0.7071068, 0, 0, -0.7071068, False, '2019-02-10 00:00:00'); /* Door */
+/* @teleloc 0xDB740007 [17.325000 156.000000 18.082000] -0.707107 0.000000 0.000000 -0.707107 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7DB7401F,   412, 0xDB740007, 13.2, 160.275, 18.082, 0, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Door */
+/* @teleloc 0xDB740007 [13.200000 160.275000 18.082000] 0.000000 0.000000 0.000000 -1.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7DB74020,  1154, 0xDB740007, 4.058023, 163.134979, 18.005, 0.633421, 0, 0, -0.773808, False, '2019-02-10 00:00:00'); /* Linkable Monster Generator */
+/* @teleloc 0xDB740007 [4.058023 163.134979 18.004999] 0.633421 0.000000 0.000000 -0.773808 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x7DB74020, 0x7DB74021, '2019-02-10 00:00:00') /* Yu Vuo-Ki */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7DB74021,  6847, 0xDB740007, 11.00007, 161.6496, 18.005, 1, 0, 0, 0,  True, '2019-02-10 00:00:00'); /* Yu Vuo-Ki */
+/* @teleloc 0xDB740007 [11.000070 161.649600 18.005000] 1.000000 0.000000 0.000000 0.000000 */
---
+++ Database/Patches/2011-06-StiringsInTheDark/9 WeenieDefaults/Generic/Misc/06879 Dansha-Ki's Belongings.sql
@@ -0,0 +1,33 @@
+DELETE FROM `weenie` WHERE `class_Id` = 6879;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (6879, 'backpackdryreachprisoner', 1, '2005-02-09 10:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (6879,   1,        128) /* ItemType - Misc */
+     , (6879,   3,         14) /* PaletteTemplate - Red */
+     , (6879,   5,        400) /* EncumbranceVal */
+     , (6879,   8,        200) /* Mass */
+     , (6879,   9,          0) /* ValidLocations - None */
+     , (6879,  16,          1) /* ItemUseable - No */
+     , (6879,  19,          5) /* Value */
+     , (6879,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (6879,  22, True ) /* Inscribable */
+     , (6879,  23, True ) /* DestroyOnSell */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (6879,  39,    1.75) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (6879,   1, 'Dansha-Ki''s Belongings') /* Name */
+     , (6879,  33, 'PickedUpDanshaKiBelongings') /* Quest */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (6879,   1,   33554769) /* Setup */
+     , (6879,   3,  536870932) /* SoundTable */
+     , (6879,   6,   67111919) /* PaletteBase */
+     , (6879,   7,  268435867) /* ClothingBase */
+     , (6879,   8,  100670383) /* Icon */
+     , (6879,  22,  872415275) /* PhysicsEffectTable */;
