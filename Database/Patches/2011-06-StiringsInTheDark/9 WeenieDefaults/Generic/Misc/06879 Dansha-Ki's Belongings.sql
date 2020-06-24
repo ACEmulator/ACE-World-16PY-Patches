c90094bbe2647c8bcaae4fab3cadd5076d45ed4a
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-06-24 00:24:26 -0700

    .

--- Database/Patches/2011-06-StiringsInTheDark/9 WeenieDefaults/Generic/Misc/06879 Dansha-Ki's Belongings.sql
+++ Database/Patches/2011-06-StiringsInTheDark/9 WeenieDefaults/Generic/Misc/06879 Dansha-Ki's Belongings.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 6879;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (6879, 'backpackdryreachprisoner', 1, '2005-02-09 10:00:00') /* Generic */;
+VALUES (6879, 'backpackdryreachprisoner', 1, '2020-06-24 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (6879,   1,        128) /* ItemType - Misc */
