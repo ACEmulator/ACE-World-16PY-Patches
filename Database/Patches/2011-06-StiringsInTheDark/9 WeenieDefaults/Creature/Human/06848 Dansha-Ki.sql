4d7a9c7a524296cafa65de9ba391c005e5c61ea5
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-06-24 01:21:42 -0700

    .

--- Database/Patches/2011-06-StiringsInTheDark/9 WeenieDefaults/Creature/Human/06848 Dansha-Ki.sql
+++ Database/Patches/2011-06-StiringsInTheDark/9 WeenieDefaults/Creature/Human/06848 Dansha-Ki.sql
@@ -84,9 +84,9 @@ VALUES (6848,   1,    10, 0, 0, 60) /* MaxHealth */
      , (6848,   5,    10, 0, 0, 210) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (6848,  6, 0, 2, 0,   1, 0, 498.203319868331) /* MeleeDefense        Trained */
-     , (6848,  7, 0, 2, 0,   1, 0, 498.203319868331) /* MissileDefense      Trained */
-     , (6848, 13, 0, 2, 0,   1, 0, 498.203319868331) /* UnarmedCombat       Trained */;
+VALUES (6848,  6, 0, 2, 0,   1, 0, 0) /* MeleeDefense        Trained */
+     , (6848,  7, 0, 2, 0,   1, 0, 0) /* MissileDefense      Trained */
+     , (6848, 45, 0, 2, 0,   1, 0, 0) /* LightWeapons        Trained */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (6848,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
