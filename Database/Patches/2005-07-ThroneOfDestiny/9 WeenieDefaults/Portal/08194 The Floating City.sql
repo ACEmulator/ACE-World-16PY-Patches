c7fc1395f6104999fdfbc1431ff03a3d51d86302
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-07-09 18:39:20 -0400

    batch 3

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/08194 The Floating City.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/08194 The Floating City.sql
@@ -1,28 +1,22 @@
 DELETE FROM `weenie` WHERE `class_Id` = 8194;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (8194, 'portalfloatingcitydexit', 7, '2019-02-04 06:52:23') /* Portal */;
+VALUES (8194, 'portalfloatingcitydexit', 7, '2020-07-09 06:52:23') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (8194,   1,      65536) /* ItemType - Portal */
      , (8194,  16,         32) /* ItemUseable - Remote */
      , (8194,  86,        150) /* MinLevel */
      , (8194,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
-     , (8194, 111,         17) /* PortalBitmask - Unrestricted, NoSummon */
+     , (8194, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */
      , (8194, 133,          4) /* ShowableOnRadar - ShowAlways */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (8194,   1, True ) /* Stuck */
-     , (8194,  11, False) /* IgnoreCollisions */
-     , (8194,  12, True ) /* ReportCollisions */
-     , (8194,  13, True ) /* Ethereal */
-     , (8194,  14, True ) /* GravityStatus */
-     , (8194,  15, True ) /* LightsStatus */
-     , (8194,  19, True ) /* Attackable */
      , (8194,  88, True ) /* PortalShowDestination */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (8194,  54, -0.100000001490116) /* UseRadius */;
+VALUES (8194,  54, -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (8194,   1, 'The Floating City') /* Name */
--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/08196 The Floating City.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/08196 The Floating City.sql
@@ -1,28 +1,22 @@
 DELETE FROM `weenie` WHERE `class_Id` = 8196;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (8196, 'portalfloatingcityf', 7, '2019-02-04 06:52:23') /* Portal */;
+VALUES (8196, 'portalfloatingcityf', 7, '2020-07-09 06:52:23') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (8196,   1,      65536) /* ItemType - Portal */
      , (8196,  16,         32) /* ItemUseable - Remote */
      , (8196,  86,        150) /* MinLevel */
      , (8196,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
-     , (8196, 111,         17) /* PortalBitmask - Unrestricted, NoSummon */
+     , (8196, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */
      , (8196, 133,          4) /* ShowableOnRadar - ShowAlways */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (8196,   1, True ) /* Stuck */
-     , (8196,  11, False) /* IgnoreCollisions */
-     , (8196,  12, True ) /* ReportCollisions */
-     , (8196,  13, True ) /* Ethereal */
-     , (8196,  14, True ) /* GravityStatus */
-     , (8196,  15, True ) /* LightsStatus */
-     , (8196,  19, True ) /* Attackable */
      , (8196,  88, True ) /* PortalShowDestination */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (8196,  54, -0.100000001490116) /* UseRadius */;
+VALUES (8196,  54, -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (8196,   1, 'The Floating City') /* Name */
