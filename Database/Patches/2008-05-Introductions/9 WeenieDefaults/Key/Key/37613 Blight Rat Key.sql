c31f8851a0ffcdb996ae274fd9dc13d05864f713
Ty Conner <tyconner@itcproductions.com>
2020-07-09 01:59:16 -0400

    Apply suggestions from code review

--- Database/Patches/2008-05-Introductions/9 WeenieDefaults/Key/Key/37613 Blight Rat Key.sql
+++ Database/Patches/2008-05-Introductions/9 WeenieDefaults/Key/Key/37613 Blight Rat Key.sql
@@ -9,12 +9,10 @@ VALUES (37613,   1,      16384) /* ItemType - Key */
      , (37613,   8,         20) /* Mass */
      , (37613,  16,    2097160) /* ItemUseable - SourceContainedTargetRemote */
      , (37613,  19,          0) /* Value */
-     /*, (37613,  33,          1)*/ /* Bonded - Bonded */ /*Mag log says this wasn't attuned/bonded, but wiki does?*/
      , (37613,  91,          1) /* MaxStructure */
      , (37613,  92,          1) /* Structure */
      , (37613,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (37613,  94,        640) /* TargetType - LockableMagicTarget */
-     /*, (37613, 114,          1)*/ /* Attuned - Attuned */ /*Mag log says this wasn't attuned/bonded, but wiki does?*/;
+     , (37613,  94,        640) /* TargetType - LockableMagicTarget */;
      
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (37613,  22, True ) /* Inscribable */
@@ -31,4 +29,3 @@ VALUES (37613,   1,   33554784) /* Setup */
      , (37613,   3,  536870932) /* SoundTable */
      , (37613,   8,  100667485) /* Icon */
      , (37613,  22,  872415275) /* PhysicsEffectTable */;
-
