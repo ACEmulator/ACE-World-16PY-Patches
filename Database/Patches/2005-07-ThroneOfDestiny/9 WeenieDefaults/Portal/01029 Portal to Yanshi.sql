a1979759e7162680fe1ff84da6950b06a46d28af
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-07-09 18:04:18 -0400

    first batch

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/01029 Portal to Yanshi.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/01029 Portal to Yanshi.sql
@@ -1,36 +1,27 @@
 DELETE FROM `weenie` WHERE `class_Id` = 1029;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (1029, 'portalyanshi', 7, '2019-02-04 06:52:23') /* Portal */;
+VALUES (1029, 'portalyanshi', 7, '2020-07-09 06:52:23') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (1029,   1,      65536) /* ItemType - Portal */
-     , (1029,  16,         32) /* ItemUseable - Remote */
-     , (1029,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
-     , (1029, 111,          1) /* PortalBitmask - Unrestricted */
-     , (1029, 133,          4) /* ShowableOnRadar - ShowAlways */;
+     , (1029,  16,          1) /* ItemUseable - No */
+     , (1029,  93,       3092) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn */
+     , (1029, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (1029,   1, True ) /* Stuck */
-     , (1029,  11, False) /* IgnoreCollisions */
-     , (1029,  12, True ) /* ReportCollisions */
-     , (1029,  13, True ) /* Ethereal */
-     , (1029,  14, True ) /* GravityStatus */
-     , (1029,  15, True ) /* LightsStatus */
-     , (1029,  19, True ) /* Attackable */
-     , (1029,  88, True ) /* PortalShowDestination */;
+VALUES (1029,   1, True ) /* Stuck */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (1029,  54, -0.100000001490116) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (1029,   1, 'Portal to Yanshi') /* Name */
-     , (1029,  16, 'This portal goes to the Sho town of Yanshi on the edges of the Blackmire swamp. The original town was devastated by the Harbinger, who still lurks deep beneath the surface. This is a good town for characters over level 10. But stay away from the Harbinger.') /* LongDesc */
-     , (1029,  38, 'Destroyed Portal to Yanshi (12.6S, 42.4E).') /* AppraisalPortalDestination */;
+VALUES (1029,   1, 'Destroyed Portal to Yanshi') /* Name */
+     , (1029,  16, 'This portal was destroyed by Asheron''s foray into opening a portal to the lost Empyreans.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (1029,   1,   33554867) /* Setup */
-     , (1029,   2,  150994947) /* MotionTable */
+VALUES (1029,   1,   33561060) /* Setup */
+     , (1029,   2,  150995314) /* MotionTable */
      , (1029,   8,  100667499) /* Icon */;
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/04044 Stonehold Portal.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Portal/04044 Stonehold Portal.sql
@@ -1,36 +1,27 @@
 DELETE FROM `weenie` WHERE `class_Id` = 4044;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (4044, 'portalstonehold', 7, '2019-02-04 06:52:23') /* Portal */;
+VALUES (4044, 'portalstonehold', 7, '2020-07-09 06:52:23') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (4044,   1,      65536) /* ItemType - Portal */
-     , (4044,  16,         32) /* ItemUseable - Remote */
-     , (4044,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
-     , (4044, 111,          1) /* PortalBitmask - Unrestricted */
-     , (4044, 133,          4) /* ShowableOnRadar - ShowAlways */;
+     , (4044,  16,          1) /* ItemUseable - No */
+     , (4044,  93,       3092) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn */
+     , (4044, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (4044,   1, True ) /* Stuck */
-     , (4044,  11, False) /* IgnoreCollisions */
-     , (4044,  12, True ) /* ReportCollisions */
-     , (4044,  13, True ) /* Ethereal */
-     , (4044,  14, True ) /* GravityStatus */
-     , (4044,  15, True ) /* LightsStatus */
-     , (4044,  19, True ) /* Attackable */
-     , (4044,  88, True ) /* PortalShowDestination */;
+VALUES (4044,   1, True ) /* Stuck */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (4044,  54, -0.100000001490116) /* UseRadius */;
+VALUES (4044,  54, -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (4044,   1, 'Stonehold Portal') /* Name */
-     , (4044,  16, 'This portal was destroyed by Asheron''s foray into opening a portal to the lost Empyreans.') /* LongDesc */
-     , (4044,  38, 'Destroyed Stonehold Portal (68.7N, 21.8W).') /* AppraisalPortalDestination */;
+VALUES (4044,   1, 'Destroyed Stonehold Portal') /* Name */
+     , (4044,  16, 'This portal was destroyed by Asheron''s foray into opening a portal to the lost Empyreans.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (4044,   1,   33554867) /* Setup */
-     , (4044,   2,  150994947) /* MotionTable */
+VALUES (4044,   1,   33561060) /* Setup */
+     , (4044,   2,  150995314) /* MotionTable */
      , (4044,   8,  100667499) /* Icon */;
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
