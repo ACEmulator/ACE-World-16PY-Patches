bc79de8e50c950d1ad1823421bb1e62ef7d5ddff
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-07-13 23:52:47 -0400

    initial commit (#523)

--- Database/Patches/2007-10-Masked Memories/9 WeenieDefaults/Gem/Misc/36365 Chimeric Axe of the Quiddity Summoning Gem.sql
+++ Database/Patches/2007-10-Masked Memories/9 WeenieDefaults/Gem/Misc/36365 Chimeric Axe of the Quiddity Summoning Gem.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 36365;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (36365, 'ace36365-chimericaxeofthequidditysummoninggem', 38, '2020-04-09 00:00:00') /* Gem */;
+VALUES (36365, 'ace36365-chimericaxeofthequidditysummoninggem', 38, '2020-07-11 00:00:00') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (36365,   1,        128) /* ItemType - Misc */
@@ -27,7 +27,7 @@ VALUES (36365,  39,     0.8) /* DefaultScale */;
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (36365,   1, 'Chimeric Axe of the Quiddity Summoning Gem') /* Name */
      , (36365,  14, 'Use this gem to summon the weapon it is bound to.') /* Use */
-     , (36365,  16, 'A gem, seeming made of congealed portal energy. You know that using this gem will summon forth the weapon known as the Chimeric Atlatl of the Quiddity. Once summoned, the weapon will only remain in the world for 3 hours. (BE WARNED: 1 - This duration will continue to count down, even if the character bearing the weapon is logged out. 2 - Have a free space in your main pack when you use this gem. 3 - You must be at least level 120 to wield the weapon summoned by this gem.)') /* LongDesc */;
+     , (36365,  16, 'A gem, seeming made of congealed portal energy. You know that using this gem will summon forth the weapon known as the Chimeric Axe of the Quiddity. Once summoned, the weapon will only remain in the world for 3 hours. (BE WARNED: 1 - This duration will continue to count down, even if the character bearing the weapon is logged out. 2 - Have a free space in your main pack when you use this gem. 3 - You must be at least level 120 to wield the weapon summoned by this gem.)') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (36365,   1,   33556223) /* Setup */
