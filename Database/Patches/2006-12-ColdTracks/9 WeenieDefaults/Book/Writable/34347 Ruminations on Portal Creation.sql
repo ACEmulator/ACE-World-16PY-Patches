9284e78ff073646883515d0fe693a2d6bf545195
Relyc <65063770+RelycX@users.noreply.github.com>
2020-07-14 22:20:52 -0700

    Saving Asheron (under 80) and Fixes

---
+++ Database/Patches/2006-12-ColdTracks/9 WeenieDefaults/Book/Writable/34347 Ruminations on Portal Creation.sql
@@ -0,0 +1,36 @@
+DELETE FROM `weenie` WHERE `class_Id` = 34347;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (34347, 'ace34347-ruminationsonportalcreation', 8, '2020-04-10 00:00:00') /* Book */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (34347,   1,       8192) /* ItemType - Writable */
+     , (34347,   5,         15) /* EncumbranceVal */
+     , (34347,   9,          0) /* ValidLocations - None */
+     , (34347,  16,          8) /* ItemUseable - Contained */
+     , (34347,  19,       5000) /* Value */
+     , (34347,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (34347, 174,          1) /* AppraisalPages */
+     , (34347, 175,          1) /* AppraisalMaxPages */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (34347,  22, False) /* Inscribable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (34347,  54,       1) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (34347,   1, 'Ruminations on Portal Creation') /* Name */
+     , (34347,  16, 'A page of notes regarding the creation of a proper setting for portal summoning.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (34347,   1,   33554773) /* Setup */
+     , (34347,   3,  536870932) /* SoundTable */
+     , (34347,   8,  100668176) /* Icon */
+     , (34347,  22,  872415275) /* PhysicsEffectTable */;
+
+INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
+VALUES (34347, 1, 1000);
+
+INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
+VALUES (34347, 0, 4294967295, 'Asheron Realaidain', '', False, 'To create this portal, it will be of the utmost importance for the site to be arrayed properly.  A simple square will not do, as the equidistant sides will diffuse the power.  There needs to be a clear "direction of power", so to speak.\n\nPerhaps the golden ratio would suit. The spiral which may be derived from it occurs in certain magical creatures, most notably the niffis. A rectangle formed using this ratio may be the best base to work from, especially if the ley lines underneath were to match the internal divisions of it.\n\nThe golden ratio is, of course, approximately 1.61803398874989484820458683. That should be sufficiently accurate for my purposes. And it would be a good number to use to encode the other message I must concern myself as well.\n\nNote: The Mountain Sewer may be a good place to conduct further research and attempt to create the gems for these purposes.');
