ed5173c004931f2f52da9077f4ae653851701b63
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-07-14 23:10:07 -0700

    Update shield

--- Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/Generic/Shield/32698 Shield of Strathelar.sql
+++ Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/Generic/Shield/32698 Shield of Strathelar.sql
@@ -1,36 +1,35 @@
 DELETE FROM `weenie` WHERE `class_Id` = 32698;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (32698, 'ace32698-shieldofboreleansroyalguard', 1, '2020-06-11 00:00:00') /* Generic */;
+VALUES (32698, 'ace32698-shieldofstrathelar', 1, '2020-07-09 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32698,   1,          2) /* ItemType - Armor */
-     , (32698,   5,        600) /* EncumbranceVal */
+     , (32698,   5,       1125) /* EncumbranceVal */
      , (32698,   9,    2097152) /* ValidLocations - Shield */
      , (32698,  16,          1) /* ItemUseable - No */
      , (32698,  18,          1) /* UiEffects - Magical */
-     , (32698,  19,       6000) /* Value */
+     , (32698,  19,       7500) /* Value */
 	 , (32698,  28,        175) /* ArmorLevel */
 	 , (32698,  27,          2) /* ArmorType - Leather */
+     , (32698,  33,         -2) /* Bonded - Destroy */
      , (32698,  51,          4) /* CombatUse - Shield */
-     , (32698,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (32698, 158,          7) /* WieldRequirements - Level */
-     , (32698, 159,          1) /* WieldSkillType - Axe */
-     , (32698, 160,         40) /* WieldDifficulty */;
+     , (32698,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (32698,   1, False) /* Stuck */
      , (32698,  22, True ) /* Inscribable */;
-
+	 
 	 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (32698,   5,   -0.05) /* ManaRate */
      , (32698,  13,       1) /* ArmorModVsSlash */
-     , (32698,  14,       1) /* ArmorModVsPierce */
+     , (32698,  14,     0.7) /* ArmorModVsPierce */
      , (32698,  15,     1.2) /* ArmorModVsBludgeon */
-     , (32698,  16,     0.6) /* ArmorModVsCold */
-     , (32698,  17,     0.6) /* ArmorModVsFire */
-     , (32698,  18,     0.6) /* ArmorModVsAcid */
-     , (32698,  19,     0.6) /* ArmorModVsElectric */
+     , (32698,  16,     1.1) /* ArmorModVsCold */
+     , (32698,  17,     1.1) /* ArmorModVsFire */
+     , (32698,  18,       1) /* ArmorModVsAcid */
+     , (32698,  19,     0.7) /* ArmorModVsElectric */
+     , (32698, 165,       1) /* ArmorModVsNether */
      , (32698, 110,       1) /* BulkMod */
      , (32698, 111,       1) /* SizeMod */;
 
@@ -43,3 +42,9 @@ VALUES (32698,   1,   33559815) /* Setup */
      , (32698,   3,  536870932) /* SoundTable */
      , (32698,   8,  100688556) /* Icon */
      , (32698,  22,  872415275) /* PhysicsEffectTable */;
+	 
+	 
+	 
+	 
+	 
+	 
