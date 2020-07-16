771149b2df648f67cd5e965f7bcdc8e8d5c2f2f7
Ty Conner <tyconner@itcproductions.com>
2020-07-16 04:17:04 -0400

    Update 32087 Unstable Portal.sql

--- Database/Patches/2005-09-UnderCoverOfNight/9 WeenieDefaults/Portal/32087 Unstable Portal.sql
+++ Database/Patches/2005-09-UnderCoverOfNight/9 WeenieDefaults/Portal/32087 Unstable Portal.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 32087;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (32087, 'ace32087-unstableportal', 7, '2019-06-07 00:00:00') /* Portal */;
+VALUES (32087, 'ace32087-unstableportal', 7, '2019-02-10 00:00:00') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32087,   1,      65536) /* ItemType - Portal */
@@ -11,22 +11,14 @@ VALUES (32087,   1,      65536) /* ItemType - Portal */
      , (32087, 133,          4) /* ShowableOnRadar - ShowAlways */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (32087,   1, True ) /* Stuck */
-     , (32087,  11, False) /* IgnoreCollisions */
-     , (32087,  12, True ) /* ReportCollisions */
-     , (32087,  13, True ) /* Ethereal */
-     , (32087,  14, True ) /* GravityStatus */
-     , (32087,  15, True ) /* LightsStatus */
-     , (32087,  19, True ) /* Attackable */
-	 , (32087,  88, True ) /* PortalShowDestination */;
+VALUES (32087,   1, True ) /* Stuck */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (32087,  54, -0.100000001490116) /* UseRadius */;
+VALUES (32087,  54,    -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (32087,   1, 'Unstable Portal') /* Name */
-     , (32087,  16, 'This portal seems to have become unstable. It would be very unwise to use a portal in this state.') /* LongDesc */
-     , (32087,  38, 'Unstable Portal') /* AppraisalPortalDestination */;
+     , (32087,  16, 'This portal seems to have become unstable. It would be very unwise to use a portal in this state.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (32087,   1,   33556733) /* Setup */
