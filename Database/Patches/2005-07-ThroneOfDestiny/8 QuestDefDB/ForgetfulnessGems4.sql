e744937e68841ca2e19aa3850eeaeafc1e3a6e80
Ty Conner <tyconner@itcproductions.com>
2020-07-21 02:03:06 -0400

    Update ForgetfulnessGems4.sql

--- Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/ForgetfulnessGems4.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/8 QuestDefDB/ForgetfulnessGems4.sql
@@ -1,4 +1,4 @@
-DELETE FROM `quest` WHERE `name` = 'ForgetfulnessGems1';
+DELETE FROM `quest` WHERE `name` = 'ForgetfulnessGems4';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('ForgetfulnessGems1', 0, 9999999, 'Fourth Forgetfulness gems received counter', '2020-04-09 00:00:00');
+VALUES ('ForgetfulnessGems4', 0, 9999999, 'Fourth Forgetfulness gems received counter', '2020-04-09 00:00:00');
