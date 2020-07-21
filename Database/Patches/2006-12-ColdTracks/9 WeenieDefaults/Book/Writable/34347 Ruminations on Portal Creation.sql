11e163bd4a4d26914274b9fc45ca47d055b49b5b
Ty Conner <tyconner@itcproductions.com>
2020-07-21 00:24:39 -0400

    update books

--- Database/Patches/2006-12-ColdTracks/9 WeenieDefaults/Book/Writable/34347 Ruminations on Portal Creation.sql
+++ Database/Patches/2006-12-ColdTracks/9 WeenieDefaults/Book/Writable/34347 Ruminations on Portal Creation.sql
@@ -1,20 +1,14 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34347;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34347, 'ace34347-ruminationsonportalcreation', 8, '2020-04-10 00:00:00') /* Book */;
+VALUES (34347, 'ace34347-ruminationsonportalcreation', 8, '2019-02-10 00:00:00') /* Book */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34347,   1,       8192) /* ItemType - Writable */
      , (34347,   5,         15) /* EncumbranceVal */
-     , (34347,   9,          0) /* ValidLocations - None */
      , (34347,  16,          8) /* ItemUseable - Contained */
      , (34347,  19,       5000) /* Value */
-     , (34347,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (34347, 174,          1) /* AppraisalPages */
-     , (34347, 175,          1) /* AppraisalMaxPages */;
-
-INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (34347,  22, False) /* Inscribable */;
+     , (34347,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (34347,  54,       1) /* UseRadius */;
@@ -33,4 +27,11 @@ INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Cha
 VALUES (34347, 1, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
-VALUES (34347, 0, 4294967295, 'Asheron Realaidain', '', False, 'To create this portal, it will be of the utmost importance for the site to be arrayed properly.  A simple square will not do, as the equidistant sides will diffuse the power.  There needs to be a clear "direction of power", so to speak.\n\nPerhaps the golden ratio would suit. The spiral which may be derived from it occurs in certain magical creatures, most notably the niffis. A rectangle formed using this ratio may be the best base to work from, especially if the ley lines underneath were to match the internal divisions of it.\n\nThe golden ratio is, of course, approximately 1.61803398874989484820458683. That should be sufficiently accurate for my purposes. And it would be a good number to use to encode the other message I must concern myself as well.\n\nNote: The Mountain Sewer may be a good place to conduct further research and attempt to create the gems for these purposes.');
+VALUES (34347, 0, 4294967295, 'Asheron Realaidain', 'Password is cheese', False, 'To create this portal, it will be of the utmost importance for the site to be arrayed properly.  A simple square will not do, as the equidistant sides will diffuse the power.  There needs to be a clear "direction of power", so to speak.
+
+Perhaps the golden ratio would suit. The spiral which may be derived from it occurs in certain magical creatures, most notably the niffis. A rectangle formed using this ratio may be the best base to work from, especially if the ley lines underneath were to match the internal divisions of it.
+
+The golden ratio is, of course, approximately 1.61803398874989484820458683. That should be sufficiently accurate for my purposes. And it would be a good number to use to encode the other message I must concern myself as well.
+
+Note: The Mountain Sewer may be a good place to conduct further research and attempt to create the gems for these purposes. 
+');
