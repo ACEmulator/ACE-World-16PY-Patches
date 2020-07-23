370995a5c8648c542d9e8374dd16930a6649bfc2
Ty Conner <tyconner@itcproductions.com>
2020-07-23 17:38:24 -0400

    Update 00B7.sql

--- Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/00B7.sql
+++ Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/00B7.sql
@@ -69,8 +69,8 @@ VALUES (0x700B7043, 38688, 0x00B7026A, 136.773, -32.6153, -17.995, 0.7071068, 0,
 /* @teleloc 0x00B7026A [136.773000 -32.615300 -17.995000] 0.707107 0.000000 0.000000 -0.707107 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700B7044, 38574, 0x00B7026B, 136.802, -36.0819, -17.995, 0.5739695, 0, 0, -0.8188766, False, '2019-02-10 00:00:00'); /* Celestial Hand Quartermaster */
-/* @teleloc 0x00B7026B [136.802000 -36.081900 -17.995000] 0.573970 0.000000 0.000000 -0.818877 */
+VALUES (0x700B7044, 38574, 0x00B7026B, 136.802, -36.0819, -17.995, 0.7071068, 0, 0, -0.7071068, False, '2019-02-10 00:00:00'); /* Celestial Hand Quartermaster */
+/* @teleloc 0x00B7026B [136.802000 -36.081900 -17.995000] 0.707107 0.000000 0.000000 -0.707107 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x700B7045,  1154, 0x00B70295, 107.747, -34.4459, 0, 0, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Linkable Monster Generator */
