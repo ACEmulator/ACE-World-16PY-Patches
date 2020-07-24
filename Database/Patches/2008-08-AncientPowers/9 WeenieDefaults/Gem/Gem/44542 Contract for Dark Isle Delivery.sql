db2f1b32e482e1998cedc2d96c2cebf7b94b6ef7
Ty Conner <tyconner@itcproductions.com>
2020-07-24 01:55:47 -0400

    Update 44542 Contract for Dark Isle Delivery.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Gem/Gem/44542 Contract for Dark Isle Delivery.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Gem/Gem/44542 Contract for Dark Isle Delivery.sql
@@ -15,7 +15,7 @@ VALUES (44542,   1,       2048) /* ItemType - Gem */
      , (44542,  93,       3092) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn */
      , (44542,  94,         16) /* TargetType - Creature */
      , (44542, 280,        100) /* SharedCooldown */
-     , (44542, 349,        101) /* UseCreatesContractId - Contract_101_Soc__Dark_Isle_Delivery */;
+     , (44542, 349,        128) /* UseCreatesContractId - Contract_128_Soc__Dark_Isle_Delivery */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (44542,  22, True ) /* Inscribable */
@@ -27,7 +27,7 @@ VALUES (44542, 167,       2) /* CooldownDuration */;
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (44542,   1, 'Contract for Dark Isle Delivery') /* Name */
      , (44542,  14, 'Recommended Level: 180') /* Use */
-     , (44542,  16, 'Rheda the Watcher wants you to deliver a Dark Isle Remoran fin to the Deep.') /* LongDesc */;
+     , (44542,  16, 'Aisley Swiftfoot wants you to deliver a Dark Isle Remoran fin to the Deep.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (44542,   1,   33554773) /* Setup */
