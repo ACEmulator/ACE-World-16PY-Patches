295deefdd7b0426328b96f9c5ce73bad5e7994f5
Ty Conner <tyconner@itcproductions.com>
2020-07-25 15:22:46 -0400

    Society Gems

---
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Gem/Gem/38725 Radiant Blood Stronghold Portal Gem.sql
@@ -0,0 +1,45 @@
+DELETE FROM `weenie` WHERE `class_Id` = 38725;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (38725, 'ace38725-radiantbloodstrongholdportalgem', 38, '2019-02-10 00:00:00') /* Gem */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (38725,   1,       2048) /* ItemType - Gem */
+     , (38725,   3,         82) /* PaletteTemplate - PinkPurple */
+     , (38725,   5,         10) /* EncumbranceVal */
+     , (38725,  11,         25) /* MaxStackSize */
+     , (38725,  12,          1) /* StackSize */
+     , (38725,  13,         10) /* StackUnitEncumbrance */
+     , (38725,  15,       5000) /* StackUnitValue */
+     , (38725,  16,          8) /* ItemUseable - Contained */
+     , (38725,  18,          1) /* UiEffects - Magical */
+     , (38725,  19,       5000) /* Value */
+     , (38725,  33,          1) /* Bonded - Bonded */
+     , (38725,  93,       3092) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn */
+     , (38725,  94,         16) /* TargetType - Creature */
+     , (38725, 106,        300) /* ItemSpellcraft */
+     , (38725, 107,        100) /* ItemCurMana */
+     , (38725, 108,        100) /* ItemMaxMana */
+     , (38725, 109,          0) /* ItemDifficulty */
+     , (38725, 110,          0) /* ItemAllegianceRankLimit */
+     , (38725, 114,          1) /* Attuned - Attuned */
+     , (38725, 280,       1000) /* SharedCooldown */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (38725,  69, False) /* IsSellable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (38725, 167,      15) /* CooldownDuration */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (38725,   1, 'Radiant Blood Stronghold Portal Gem') /* Name */
+     , (38725,  16, 'This portal summoning gem works best if used outside in a relatively flat area.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (38725,   1,   33556769) /* Setup */
+     , (38725,   3,  536870932) /* SoundTable */
+     , (38725,   6,   67111919) /* PaletteBase */
+     , (38725,   8,  100672464) /* Icon */
+     , (38725,  22,  872415275) /* PhysicsEffectTable */
+     , (38725,  28,        157) /* Spell - SummonPortal1 */
+     , (38725,  31,      37164) /* LinkedPortalOne - Surface */;
--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Gem/Gem/38726 Black Market Gem of Dispelling.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Gem/Gem/38726 Black Market Gem of Dispelling.sql
@@ -1,56 +1,40 @@
 DELETE FROM `weenie` WHERE `class_Id` = 38726;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (38726, 'ace38726-blackmarketgemofdispelling', 38, '2019-08-11 01:59:46') /* Gem */;
+VALUES (38726, 'ace38726-blackmarketgemofdispelling', 38, '2019-02-10 00:00:00') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (38726,   1,       2048) /* ItemType - Gem */
-     , (38726,   3,          2) /* PaletteTemplate - Blue */
      , (38726,   5,         10) /* EncumbranceVal */
-     , (38726,   8,         10) /* Mass */
      , (38726,  11,         25) /* MaxStackSize */
      , (38726,  12,          1) /* StackSize */
      , (38726,  13,         10) /* StackUnitEncumbrance */
-     , (38726,  14,         10) /* StackUnitMass */
-     , (38726,  15,       1000) /* StackUnitValue */
+     , (38726,  15,     250000) /* StackUnitValue */
      , (38726,  16,          8) /* ItemUseable - Contained */
      , (38726,  18,          1) /* UiEffects - Magical */
      , (38726,  19,     250000) /* Value */
-     , (38726,  53,        101) /* PlacementPosition - Resting */
+     , (38726,  33,          1) /* Bonded - Bonded */
      , (38726,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (38726,  94,         16) /* TargetType - Creature */
      , (38726, 106,        210) /* ItemSpellcraft */
-     , (38726, 107,        450) /* ItemCurMana */
+     , (38726, 107,        500) /* ItemCurMana */
      , (38726, 108,        500) /* ItemMaxMana */
      , (38726, 109,          0) /* ItemDifficulty */
-     , (38726, 110,          0) /* ItemAllegianceRankLimit */;
-
-INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (38726,  11, True ) /* IgnoreCollisions */
-     , (38726,  13, True ) /* Ethereal */
-     , (38726,  14, True ) /* GravityStatus */
-     , (38726,  19, True ) /* Attackable */
-     , (38726,  23, True ) /* DestroyOnSell */;
+     , (38726, 110,          0) /* ItemAllegianceRankLimit */
+     , (38726, 114,          1) /* Attuned - Attuned */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (38726,  39, 1.20000004768372) /* DefaultScale */;
+VALUES (38726,  39,     1.2) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (38726,   1, 'Black Market Gem of Dispelling') /* Name */
      , (38726,  16, 'A small glowing gem, that seems to radiate light from an internal source.') /* LongDesc */
      , (38726,  20, 'Black Market Gems of Dispelling') /* PluralName */;
 
-
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (38726,   1,   33554809) /* Setup */
      , (38726,   3,  536870932) /* SoundTable */
      , (38726,   6,   67111919) /* PaletteBase */
-     , (38726,   7,  268435723) /* ClothingBase */
      , (38726,   8,  100671405) /* Icon */
      , (38726,  22,  872415275) /* PhysicsEffectTable */
-     , (38726,  28,       4331) /* Spell - Incantation of Nullify All Magic Self */
-     , (38726,  36,  234881046) /* MutateFilter */;
-	 
-	 
-	 
-	 
+     , (38726,  28,       4331) /* Spell - DispelAllBadSelf8 */;
