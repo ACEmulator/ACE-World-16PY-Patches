e510e59867c79b91601a5b5a33002088c41a4ca9
Ty Conner <tyconner@itcproductions.com>
2020-07-27 03:06:30 -0400

    Add quests

---
+++ Database/Patches/2008-08-AncientPowers/8 QuestDefDB/EldrytchWebMember.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'EldrytchWebMember';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('EldrytchWebMember', 0, 1, 'Player is a member of Eldrytch Web', '2020-03-31 20:34:27');
