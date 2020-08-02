1e1b3e427c4c30163a050aa6a66484a54f0f1556
Ty Conner <tyconner@itcproductions.com>
2020-08-02 01:36:44 -0400

    Update Radiant Blood BreastPlate Chest DT-2991.sql

--- Database/Patches/2008-08-AncientPowers/3 TreasureTable/Death/Radiant Blood BreastPlate Chest DT-2991.sql
+++ Database/Patches/2008-08-AncientPowers/3 TreasureTable/Death/Radiant Blood BreastPlate Chest DT-2991.sql
@@ -1,4 +1,4 @@
-DELETE FROM `treasure_death` WHERE `id` = 2971;
+DELETE FROM `treasure_death` WHERE `id` = 2991;
 
 INSERT INTO `treasure_death` (`id`, `treasure_Type`, `tier`, `loot_Quality_Mod`, `unknown_Chances`, `item_Chance`, `item_Min_Amount`, `item_Max_Amount`, `item_Treasure_Type_Selection_Chances`, `magic_Item_Chance`, `magic_Item_Min_Amount`, `magic_Item_Max_Amount`, `magic_Item_Treasure_Type_Selection_Chances`, `mundane_Item_Chance`, `mundane_Item_Min_Amount`, `mundane_Item_Max_Amount`, `mundane_Item_Type_Selection_Chances`, `last_Modified`)
 VALUES (2991 	/* id */
