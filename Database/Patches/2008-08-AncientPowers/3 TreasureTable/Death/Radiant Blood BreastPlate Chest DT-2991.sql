8bda5b44385d0f1ae1131836ebf8be7ee4ba7785
Ty Conner <tyconner@itcproductions.com>
2020-08-02 00:01:41 -0400

    Update Radiant Blood BreastPlate Chest DT-2991.sql

--- Database/Patches/2008-08-AncientPowers/3 TreasureTable/Death/Radiant Blood BreastPlate Chest DT-2991.sql
+++ Database/Patches/2008-08-AncientPowers/3 TreasureTable/Death/Radiant Blood BreastPlate Chest DT-2991.sql
@@ -1,8 +1,8 @@
 DELETE FROM `treasure_death` WHERE `id` = 2971;
 
-INSERT INTO `treasure_death` (`id`, `treasure_Type`, `tier`, `loot_Quality_Mod`, `unknown_Chances`, `item_Chance`, `item_Min_Amount`, `item_Max_Amount`, `item_Treasure_Type_Selection_Chances`, `magic_Item_Chance`, `magic_Item_Min_Amount`, `magic_Item_Max_Amount`, `magic_Item_Treasure_Type_Selection_Chances`, `mundane_Item_Chance`, `mundane_Item_Min_Amount`, `mundane_Item_Max_Amount`, `mundane_Item_Type_Selection_Chances`, `last_Modified`) 
-VALUES (2971 	/* id */
-	 , 2971		/* treasure_Type */
+INSERT INTO `treasure_death` (`id`, `treasure_Type`, `tier`, `loot_Quality_Mod`, `unknown_Chances`, `item_Chance`, `item_Min_Amount`, `item_Max_Amount`, `item_Treasure_Type_Selection_Chances`, `magic_Item_Chance`, `magic_Item_Min_Amount`, `magic_Item_Max_Amount`, `magic_Item_Treasure_Type_Selection_Chances`, `mundane_Item_Chance`, `mundane_Item_Min_Amount`, `mundane_Item_Max_Amount`, `mundane_Item_Type_Selection_Chances`, `last_Modified`)
+VALUES (2991 	/* id */
+	 , 2991		/* treasure_Type */
 	 , 8		/* tier */
 	 , 0		/* loot_Quality_Mod */
 	 , 0		/* unknown_Chances */
