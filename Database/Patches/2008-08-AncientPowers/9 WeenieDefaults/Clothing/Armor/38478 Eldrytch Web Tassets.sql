0aba62386b3a3a362b22125e1de88b30ece60ed6
Ty Conner <tyconner@itcproductions.com>
2020-08-02 00:12:26 -0400

    Update 38478 Eldrytch Web Tassets.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Clothing/Armor/38478 Eldrytch Web Tassets.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Clothing/Armor/38478 Eldrytch Web Tassets.sql
@@ -46,6 +46,6 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (38478,   1,   33554656) /* Setup */
      , (38478,   3,  536870932) /* SoundTable */
      , (38478,   7,  268437333) /* ClothingBase */
-     , (38478,   8,  100674556) /* Icon */
+     , (38478,   8,  100690250) /* Icon */
      , (38478,  22,  872415275) /* PhysicsEffectTable */
      , (38478,  36,  234881042) /* MutateFilter */;
