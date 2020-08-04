f2bd6aa1082db0eaf6731ad2ffd6229db359b05f
Ty Conner <tyconner@itcproductions.com>
2020-08-04 01:12:20 -0400

    Create 08326 Eldrytch Web Olthoi Shield.sql

---
+++ Database/Patches/2011-11-MasterOfDesign/4 CraftTable/08326 Eldrytch Web Olthoi Shield.sql
@@ -0,0 +1,28 @@
+DELETE FROM `recipe` WHERE `id` = 8326;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (8326, 0, 0, 0, 0, 0, 0, 'You tailor the heraldry of the Eldrytch Web onto the shield.', 0, 0, 'You fail! Impossible.', 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 0, '2020-07-25 10:00:00');
+
+INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
+VALUES (8326, True, 0, 0, 0, True, 0, 0, 0);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `recipe_mods_int` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`, `source`)
+VALUES (@parent_id, 0, 276, 9, 1, 1) /* On Source.SuccessTarget SetValue WieldRequirements4 - IntStat to Target */
+     , (@parent_id, 0, 277, 288, 1, 1) /* On Source.SuccessTarget SetValue WieldSkillType4 288 to Target */
+     , (@parent_id, 0, 278, 1, 1, 1) /* On Source.SuccessTarget SetValue WieldDifficulty4 1 to Target */;
+
+INSERT INTO `recipe_mods_string` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`, `source`)
+VALUES (@parent_id, 0,   1, 'Eldrytch Web Olthoi Shield', 1, 1) /* On Source.SuccessTarget SetValue Name to Target */
+     , (@parent_id, 0,  16, 'Olthoi Shield, bearing the heraldry of the Eldrytch Web', 1, 1) /* On Source.SuccessTarget SetValue LongDesc to Target */;
+
+INSERT INTO `recipe_mods_d_i_d` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`, `source`)
+VALUES (@parent_id, 1,   1, 0, 3, 1) /* On Source.SuccessSource CopyFromSourceToTarget Setup to Target */
+     , (@parent_id, 1,   7, 0, 3, 1) /* On Source.SuccessSource CopyFromSourceToTarget ClothingBase to Target */
+     , (@parent_id, 1,   8, 0, 3, 1) /* On Source.SuccessSource CopyFromSourceToTarget Icon to Target */;
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 8326;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (8326, 44601 /* Eldrytch Web Olthoi Shield Cover */, 37291 /* Olthoi Shield */, '2020-07-25 10:00:00');
