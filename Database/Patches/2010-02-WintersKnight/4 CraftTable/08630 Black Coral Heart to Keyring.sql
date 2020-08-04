ac6cc85f1cd184e613c73d1a903c858d81b7e9e1
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-08-04 00:51:42 -0400

    keyring and recipes

---
+++ Database/Patches/2010-02-WintersKnight/4 CraftTable/08630 Black Coral Heart to Keyring.sql
@@ -0,0 +1,13 @@
+DELETE FROM `recipe` WHERE `id` = 8630;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (8630, 0, 23 /* Lockpick */, 400, 0, 42347 /* Black Coral Keyring */, 1, 'You carve the black golem heart into a crude keyring.', 0, 0, 'You fail to carve the golem heart. The heart is destroyed.', 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0, '2005-02-09 10:00:00');
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 8630;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (8630, 9295 /* Intricate Carving Tool */,  42348 /* Black Coral Heart */, '2005-02-09 10:00:00');
+
+
+
+
---
+++ Database/Patches/2010-02-WintersKnight/9 WeenieDefaults/SQL/CraftTool/Misc/42347 Black Coral Keyring.sql
@@ -0,0 +1,45 @@
+DELETE FROM `weenie` WHERE `class_Id` = 42347;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42347, 'ace42347-blackcoralkeyring', 44, '2020-08-04 00:00:00') /* CraftTool */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42347,   1,        128) /* ItemType - Misc */
+     , (42347,   5,         40) /* EncumbranceVal */
+     , (42347,   8,         40) /* Mass */
+     , (42347,   9,          0) /* ValidLocations - None */
+     , (42347,  11,          1) /* MaxStackSize */
+     , (42347,  12,          1) /* StackSize */
+     , (42347,  13,         40) /* StackUnitEncumbrance */
+     , (42347,  14,         40) /* StackUnitMass */
+     , (42347,  15,        500) /* StackUnitValue */
+     , (42347,  16,     524296) /* ItemUseable - SourceContainedTargetContained */
+     , (42347,  19,        500) /* Value */
+     , (42347,  33,          0) /* Bonded - Normal */
+     , (42347,  91,         50) /* MaxStructure */
+     , (42347,  92,         50) /* Structure */
+     , (42347,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (42347,  94,      16384) /* TargetType - Key */
+     , (42347, 114,          0) /* Attuned - Normal */
+     , (42347, 150,        103) /* HookPlacement - Hook */
+     , (42347, 151,          2) /* HookType - Wall */
+     , (42347, 193,          0) /* NumKeys */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42347,  22, True ) /* Inscribable */
+     , (42347,  69, False) /* IsSellable */
+     , (42347,  99, False) /* Ivoryable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (42347,  39,    0.75) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42347,   1, 'Black Coral Keyring') /* Name */
+     , (42347,  14, 'Use this ring on a single use mana forge key to add the key to the ring. Use an intricate carving tool on the keyring to pop a key off again. Adding a key uses up one of the ring''s remaining uses, but removing a key does not.') /* Use */
+     , (42347,  16, 'A crude keyring roughly carved out of a black coral golem heart. ') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42347,   1,   33554790) /* Setup */
+     , (42347,   3,  536870932) /* SoundTable */
+     , (42347,   8,  100690880) /* Icon */
+     , (42347,  22,  872415275) /* PhysicsEffectTable */;
