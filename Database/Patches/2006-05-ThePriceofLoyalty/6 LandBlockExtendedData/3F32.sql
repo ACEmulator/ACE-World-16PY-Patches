abcb0dc5359abbcb0646ed3503f1ce0d8931a361
Ty Conner <tyconner@itcproductions.com>
2020-08-05 13:55:25 -0400

    .

--- Database/Patches/2006-05-ThePriceofLoyalty/6 LandBlockExtendedData/3F32.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x73F32027, 33277, 0x3F320129, 8.35993, 5.47571, 7.805001, 0.972123, 0, 0, -0.234471, False, '2020-02-29 18:24:41'); /* Riordin Nightbane */
-/* @teleloc 0x3F320129 [8.359930 5.475710 7.805001] 0.972123 0.000000 0.000000 -0.234471 */
--- Database/Patches/2006-08-ShatteringtheDark/6 LandBlockExtendedData/C95B.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7C95B06C, 33839, 0xC95B0131, 117.21, 54.326, 13.205, -0.707107, 0, 0, -0.707107, False, '2020-02-29 18:35:13'); /* Sergeant Huang */
-/* @teleloc 0xC95B0131 [117.209999 54.326000 13.205000] -0.707107 0.000000 0.000000 -0.707107 */
