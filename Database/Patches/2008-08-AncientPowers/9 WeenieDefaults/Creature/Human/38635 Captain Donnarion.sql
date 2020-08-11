ee774547310598ac4c7db17dfa40521a4edce69b
Ty Conner <tyconner@itcproductions.com>
2020-08-11 13:01:30 -0400

    Captain Donnarion

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/38635 Captain Donnarion.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/38635 Captain Donnarion.sql
@@ -128,3 +128,37 @@ VALUES (38635, 2,  2588,  0, 2, 0, False) /* Create Shirt (2588) for Wield */
      , (38635, 2, 38471,  0, 0, 0, False) /* Create Celestial Hand Sollerets (38471) for Wield */
      , (38635, 2, 29979,  1, 0, 0, False) /* Create Spadone (29979) for Wield */
      , (38635, 9, 38642,  0, 0, 0, False) /* Create  Donnarion key (38642) for ContainTreasure */;
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (38635, 7 /* Use */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 36 /* InqIntStat */, 0, 1, NULL, 'SocietyRankCelhan_95-95', NULL, 95, 95, NULL, NULL, NULL, NULL, 287 /* SocietyRankCelhan */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (38635, 22 /* TestSuccess */, 1, NULL, NULL, NULL, 'SocietyRankCelhan_95-95', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 12 /* TurnToTarget */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id, 1, 76 /* InqOwnsItems */, 0, 1, NULL, 'OwnsItem-38642', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 38642, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (38635, 22 /* TestSuccess */, 1, NULL, NULL, NULL, 'OwnsItem-38642', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 10 /* Tell */, 0, 1, NULL, 'Benedino is holed up in the southern wing of the Rogue Delvings. You''ll need three keys to get through all the gates to Benedino''s hideout. I gave you one key, but the captains from the Eldrytch Web and Radiant Blood have taken the other two. You''ll need to confront them in their own camps to get the keys you''ll need. Rest here if you need to, but you should get going as soon as you can.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (38635, 23 /* TestFailure */, 1, NULL, NULL, NULL, 'OwnsItem-38642', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id, 0, 10 /* Tell */, 0, 1, NULL, 'Aldrion sent you down here to hunt down Benedino, eh? More power to you. Perhaps you can bust through where the other Societies have held us. Benedino is holed up in the southern wing of the Rogue Delvings. You''ll need three keys to get through all the gates to Benedino''s hideout. I have one key, but the captains from the Eldrytch Web and Radiant Blood have taken the other two. You''ll need to confront them in their own camps to get the keys you''ll need. Rest here if you need to, but you should get going as soon as you can.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id, 1, 3 /* Give */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 38642, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
---
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/38635.es
@@ -0,0 +1,10 @@
+Use:
+    - InqIntStat: SocietyRankCelhan, 95 - 95
+        TestSuccess:
+            - TurnToTarget
+                - InqOwnsItems: Donnarion's Key (38642), 1
+                        TestSuccess:
+                            - Tell: Benedino is holed up in the southern wing of the Rogue Delvings. You'll need three keys to get through all the gates to Benedino's hideout. I gave you one key, but the captains from the Eldrytch Web and Radiant Blood have taken the other two. You'll need to confront them in their own camps to get the keys you'll need. Rest here if you need to, but you should get going as soon as you can.
+                        TestFailure:
+                            - Tell: Aldrion sent you down here to hunt down Benedino, eh? More power to you. Perhaps you can bust through where the other Societies have held us. Benedino is holed up in the southern wing of the Rogue Delvings. You'll need three keys to get through all the gates to Benedino's hideout. I have one key, but the captains from the Eldrytch Web and Radiant Blood have taken the other two. You'll need to confront them in their own camps to get the keys you'll need. Rest here if you need to, but you should get going as soon as you can.
+                            - Give: Donnarion's Key (38642)
