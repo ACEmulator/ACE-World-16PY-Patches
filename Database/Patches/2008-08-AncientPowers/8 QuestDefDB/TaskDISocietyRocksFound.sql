176e1852b6f1846e5c86a8fc9088f2ad31eba894
Ty Conner <tyconner@itcproductions.com>
2020-08-15 09:41:59 -0400

    Update TaskDISocietyRocksFound.sql

--- Database/Patches/2008-08-AncientPowers/8 QuestDefDB/TaskDISocietyRocksFound.sql
+++ Database/Patches/2008-08-AncientPowers/8 QuestDefDB/TaskDISocietyRocksFound.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'TaskDISocietyRocksFound';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('TaskDISocietyRocksFound', 0, 4, 'Player has touched some rocks', '2020-03-31 20:34:27');
+VALUES ('TaskDISocietyRocksFound', 0, 7, 'Player has touched some rocks', '2020-03-31 20:34:27');
