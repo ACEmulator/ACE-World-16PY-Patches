1cdb71f9fb315e9306d60ccb0878230ef011c6c1
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-08-15 10:36:18 -0700

    Renumber Throwing Phial Recipes

--- Database/Patches/2007-06-Evolution/4 CraftTable/08801 Slashing Infusion.sql
+++ Database/Patches/2007-06-Evolution/4 CraftTable/08801 Slashing Infusion.sql
@@ -1,7 +1,9 @@
-DELETE FROM recipe WHERE id = 7174;
+DELETE FROM `recipe` WHERE `id` = 8801;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (7174, 0, 38 /* Alchemy */, 325, 0, 80004 /* Platinum Phial of Acid Vulnerability */, 1, 'You create an Platinum Phial of Acid Vulnerability.', 0, 0, 'You fail to create an Platinum Phial of Acid Vulnerability.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-05-19 00:00:00');
+VALUES (8801, 0, 38 /* Alchemy */, 55, 0, 79989 /* Slashing Infusion */, 1, 'You make a Slashing infusion.', 0, 0, 'You fail to make a Slashing infusion.', 0, 0, NULL, 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 0, '2019-05-26 00:00:00');
 
-INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`)
-VALUES (7174, 35715 /* Enchanted Platinum Phial */,  5330 /* Acid Oil */);
+DELETE FROM `cook_book` WHERE `recipe_Id` = 8801;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (8801, 4747 /* Alembic */,   763 /* Verdigris */, '2019-05-26 00:00:00');
--- Database/Patches/2007-06-Evolution/4 CraftTable/08836 Mana Phial of Fester.sql
+++ Database/Patches/2007-06-Evolution/4 CraftTable/08836 Mana Phial of Fester.sql
@@ -1,7 +1,7 @@
-DELETE FROM recipe WHERE id = 7197;
+DELETE FROM recipe WHERE id = 8836;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (7197, 0, 38 /* Alchemy */, 425, 0, 39334 /* Mana Phial of Fire Vulnerability */, 1, 'You create an Mana Phial of Fire Vulnerability.', 0, 0, 'You fail to create an Mana Phial of Fire Vulnerability.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-05-19 00:00:00');
+VALUES (8836, 0, 38 /* Alchemy */, 425, 0, 41785 /* Mana Phial of Fester */, 1, 'You create an Mana Phial of Fester.', 0, 0, 'You fail to create an Mana Phial of Fester.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2019-05-19 00:00:00');
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`)
-VALUES (7197, 39328 /* Enchanted Mana Phial */,  4750 /* Fire Oil */);
+VALUES (8836, 39328 /* Enchanted Mana Phial */,  5333 /* Health Oil */);
