c2e6d71b381dfc53b3c0deebb1877f29ce9da628
Ty Conner <tyconner@itcproductions.com>
2020-08-16 01:20:39 -0400

    Update KeepFreebooterBlight.sql

--- Database/Patches/2008-08-AncientPowers/B GameEventDefDB/KeepFreebooterBlight.sql
+++ Database/Patches/2008-08-AncientPowers/B GameEventDefDB/KeepFreebooterBlight.sql
@@ -1,4 +1,4 @@
 DELETE FROM `event` WHERE `name` = 'KeepFreebooterBlight';
 
 INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('KeepFreebooterBlight', -1, -1, 3, '2019-06-16 10:00:00');
+VALUES ('KeepFreebooterBlight', -1, -1, 4, '2019-06-16 10:00:00');
