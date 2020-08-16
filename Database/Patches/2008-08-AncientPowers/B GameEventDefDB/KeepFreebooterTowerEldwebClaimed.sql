cbb3819d4cba231e9664bd3883350166eccf0f17
Ty Conner <tyconner@itcproductions.com>
2020-08-16 04:31:48 -0400

    Create KeepFreebooterTowerEldwebClaimed.sql

---
+++ Database/Patches/2008-08-AncientPowers/B GameEventDefDB/KeepFreebooterTowerEldwebClaimed.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'KeepFreebooterTowerEldwebClaimed';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('KeepFreebooterTowerEldwebClaimed', -1, -1, 3, '2019-06-16 10:00:00');
