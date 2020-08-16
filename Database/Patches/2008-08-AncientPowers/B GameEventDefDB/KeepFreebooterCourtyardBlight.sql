c7ef8b20403f233db41b3d7f7d120c945a9e2150
Ty Conner <tyconner@itcproductions.com>
2020-08-16 00:23:05 -0400

    Create KeepFreebooterCourtyardBlight.sql

---
+++ Database/Patches/2008-08-AncientPowers/B GameEventDefDB/KeepFreebooterCourtyardBlight.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'KeepFreebooterCourtyardBlight';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('KeepFreebooterCourtyardBlight', -1, -1, 3, '2019-06-16 10:00:00');
