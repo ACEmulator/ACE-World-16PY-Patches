02bb7b8f72743a6873e940ec944e3083cf69c394
Ty Conner <tyconner@itcproductions.com>
2020-08-19 03:44:41 -0400

    Update 80352 Freebooter Celhan Stopgap!.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/80352 Freebooter Celhan Stopgap!.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/80352 Freebooter Celhan Stopgap!.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 80352;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (80352, 'aerfallekeepnpcstopgap', 10, '2005-02-09 10:00:00') /* Creature */;
+VALUES (80352, 'FreebooterCelhannpcstopgap', 10, '2005-02-09 10:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (80352,   1,         16) /* ItemType - Creature */
