f56b97e4c4f9efdaf1072c9cd5ec0d58be490f9c
Ty Conner <tyconner@itcproductions.com>
2020-08-21 01:11:03 -0400

    Update 80342 Radiant Blood Crystal Array.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Unsorted/80342 Radiant Blood Crystal Array.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Unsorted/80342 Radiant Blood Crystal Array.sql
@@ -128,7 +128,7 @@ VALUES (80342, 9 /* Generation */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id, 0, 16 /* WorldBroadcast */, 0, 1, NULL, 'The Society of the Radiant Blood has erected a Crystal Array at Freebooter Keep to help keep the Creeping Blight at bay!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id, 0, 17 /* LocalBroadcast */, 0, 1, NULL, 'The %n becomes active.  If the %n is damaged to the point it falls, it can be destroyed by those of opposing Societies.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (80342, 18 /* Scream */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
