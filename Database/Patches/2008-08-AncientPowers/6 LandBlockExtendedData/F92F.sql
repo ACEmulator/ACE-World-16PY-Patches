5c2cb7ffb927569aed23254c0ccca2934af744be
Ty Conner <tyconner@itcproductions.com>
2020-08-22 17:20:18 -0400

    Update F92F.sql

--- Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/F92F.sql
+++ Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/F92F.sql
@@ -391,9 +391,9 @@ VALUES (0x7F92F0F5, 80355, 0xF92F0012, 58, 28, 148, 0.7071068, 0, 0, 0.7071068, 
 /* @teleloc 0xF92F0012 [58.000000 28.000000 148.000000] 0.707107 0.000000 0.000000 0.707107 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7F92F0F5, 80356, 0xF92F0012, 58, 28, 148, 0.7071068, 0, 0, 0.7071068, False, '2019-02-10 00:00:00'); /* Freebooter Eldweb Rewards Gen */
+VALUES (0x7F92F0F6, 80356, 0xF92F0012, 58, 28, 148, 0.7071068, 0, 0, 0.7071068, False, '2019-02-10 00:00:00'); /* Freebooter Eldweb Rewards Gen */
 /* @teleloc 0xF92F0012 [58.000000 28.000000 148.000000] 0.707107 0.000000 0.000000 0.707107 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7F92F0F5, 80357, 0xF92F0012, 58, 28, 148, 0.7071068, 0, 0, 0.7071068, False, '2019-02-10 00:00:00'); /* Freebooter Radblo Rewards Gen */
+VALUES (0x7F92F0F7, 80357, 0xF92F0012, 58, 28, 148, 0.7071068, 0, 0, 0.7071068, False, '2019-02-10 00:00:00'); /* Freebooter Radblo Rewards Gen */
 /* @teleloc 0xF92F0012 [58.000000 28.000000 148.000000] 0.707107 0.000000 0.000000 0.707107 */
