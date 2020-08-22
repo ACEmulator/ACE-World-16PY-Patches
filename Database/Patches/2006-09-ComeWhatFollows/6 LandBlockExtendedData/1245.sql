b2dc959a2256fa0919b885f85d2001bbf308a593
Patrick <phreekn.slacker@gmail.com>
2020-08-22 01:49:59 -0500

    Create 1245.sql

---
+++ Database/Patches/2006-09-ComeWhatFollows/6 LandBlockExtendedData/1245.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x1245;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x71245001, 33985, 0x12450100, 92.8098, 173.87, -5.263, 0.998036, 0, 0, -0.06264389, False, '2019-02-10 00:00:00'); /* Ancient Caves */
+/* @teleloc 0x12450100 [92.809800 173.870000 -5.263000] 0.998036 0.000000 0.000000 -0.062644 */
