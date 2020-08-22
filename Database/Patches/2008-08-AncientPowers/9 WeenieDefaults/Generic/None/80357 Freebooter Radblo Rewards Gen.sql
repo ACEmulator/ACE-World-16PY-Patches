b195322b6b84ed0205dd14a9d592fb7587af8e96
Ty Conner <tyconner@itcproductions.com>
2020-08-22 14:41:50 -0400

    Update 80357 Freebooter Radblo Rewards Gen.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Generic/None/80357 Freebooter Radblo Rewards Gen.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Generic/None/80357 Freebooter Radblo Rewards Gen.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 80357;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (80357, 'FreebooterCelhanRewardsGen', 1, '2005-02-09 10:00:00') /* Generic */;
+VALUES (80357, 'FreebooterRadbloRewardsGen', 1, '2005-02-09 10:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (80357,  66,          1) /* CheckpointStatus */
@@ -22,8 +22,8 @@ VALUES (80357,  41,      60) /* RegenerationInterval */
      , (80357, 121,       5) /* GeneratorInitialDelay */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (80357,   1, 'Freebooter Celhan Rewards Gen') /* Name */
-     , (80357,  34, 'KeepFreebooterCelhanRewards') /* GeneratorEvent */;
+VALUES (80357,   1, 'Freebooter Radblo Rewards Gen') /* Name */
+     , (80357,  34, 'KeepFreebooterRadbloRewards') /* GeneratorEvent */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (80357,   1,   33555051) /* Setup */
