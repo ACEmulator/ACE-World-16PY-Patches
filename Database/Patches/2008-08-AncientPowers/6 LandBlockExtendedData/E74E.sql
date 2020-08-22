773de0d8c6bb9e8753af1fcae6df1743083feb70
Ty Conner <tyconner@itcproductions.com>
2020-08-22 18:12:31 -0400

    Create E74E.sql

---
+++ Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/E74E.sql
@@ -0,0 +1,11 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E74E0D1, 80349, 0xE74E0000, 100, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Freebooter Celhan Stopgap Gen */
+/* @teleloc 0xE74E0000 [100.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E74E0D2, 80350, 0xE74E0000, 96, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Freebooter Eldweb Stopgap Gen */
+/* @teleloc 0xE74E0000 [96.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E74E0D3, 80351, 0xE74E0000, 92, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Freebooter Radblo Stopgap Gen */
+/* @teleloc 0xE74E0000 [92.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
