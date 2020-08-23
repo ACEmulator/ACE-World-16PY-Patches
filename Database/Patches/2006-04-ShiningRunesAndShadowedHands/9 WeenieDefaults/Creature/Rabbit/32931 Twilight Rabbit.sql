c85a009f02fed9af02bebe0fb96d0585142659f0
Patrick <phreekn.slacker@gmail.com>
2020-08-23 11:30:45 -0500

    Update 32931 Twilight Rabbit.sql

--- Database/Patches/2006-04-ShiningRunesAndShadowedHands/9 WeenieDefaults/Creature/Rabbit/32931 Twilight Rabbit.sql
+++ Database/Patches/2006-04-ShiningRunesAndShadowedHands/9 WeenieDefaults/Creature/Rabbit/32931 Twilight Rabbit.sql
@@ -47,6 +47,14 @@ INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`
 VALUES (32931,   1,   250, 0, 0, 430) /* MaxHealth */
      , (32931,   3,   250, 0, 0, 610) /* MaxStamina */
      , (32931,   5,     0, 0, 0, 40) /* MaxMana */;
+     
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (32931,  6, 0, 3, 0,   0, 0, 0) /* MeleeDefense        Specialized */
+     , (32931,  7, 0, 3, 0,   0, 0, 0) /* MissileDefense      Specialized */
+     , (32931, 15, 0, 3, 0,   0, 0, 0) /* MagicDefense        Specialized */
+     , (32931, 22, 0, 2, 0,  10, 0, 0) /* Jump                Trained */
+     , (32931, 24, 0, 2, 0,  10, 0, 0) /* Run                 Trained */
+     , (32931, 45, 0, 3, 0,   0, 0, 0) /* LightWeapons        Specialized */;     
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (32931,  0,  2,  2, 0.75,  240,  240,  240,  240,  240,  240,  240,  240,    0, 1, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0) /* Head */
@@ -56,24 +64,3 @@ VALUES (32931,  0,  2,  2, 0.75,  240,  240,  240,  240,  240,  240,  240,  240,
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (32931, 9,  5633,  0, 0, 0.05, False) /* Create Rabbit Carcass (5633) for ContainTreasure */
      , (32931, 9,     0,  0, 0, 0.95, False) /* Create nothing for ContainTreasure */;
-
-/* Lifestoned Changelog:
-{
-  "LastModified": "2020-08-22T18:50:19.287911Z",
-  "ModifiedBy": "ACE.Adapter",
-  "Changelog": [
-    {
-      "created": "2020-08-22T15:58:06.1825234Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    },
-    {
-      "created": "2020-08-22T18:50:19.2851938Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    }
-  ],
-  "UserChangeSummary": "Weenie exported from ACEmulator world database using ACE.Adapter",
-  "IsDone": false
-}
-*/
