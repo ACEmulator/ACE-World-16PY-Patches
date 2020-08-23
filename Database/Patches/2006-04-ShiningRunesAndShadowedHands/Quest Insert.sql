e1710929bb81f4b131137fbeb8c1252348c2fa92
Patrick <phreekn.slacker@gmail.com>
2020-08-23 00:49:50 -0500

    Delete Quest Insert.sql

--- Database/Patches/2006-04-ShiningRunesAndShadowedHands/Quest Insert.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `quest` WHERE `id`=5000;
-INSERT INTO `quest` (`id`, `name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`) VALUES (5000, 'rabbithutchkey', 1123200, -1, 'Picked up Rabbit Hutch Key', '2020-08-20 12:11:01');
