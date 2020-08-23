e519c74abf51273eb0b144e1f1c30334a577191b
Patrick <phreekn.slacker@gmail.com>
2020-08-23 12:26:04 -0500

    Update 32929 Dusk Rabbit.sql

--- Database/Patches/2006-04-ShiningRunesAndShadowedHands/9 WeenieDefaults/Creature/Rabbit/32929 Dusk Rabbit.sql
+++ Database/Patches/2006-04-ShiningRunesAndShadowedHands/9 WeenieDefaults/Creature/Rabbit/32929 Dusk Rabbit.sql
@@ -6,19 +6,64 @@ VALUES (32929, 'ace32929-duskrabbit', 10, '2020-08-22 19:45:20') /* Creature */;
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32929,   1,         16) /* ItemType - Creature */
      , (32929,   2,         25) /* CreatureType - Rabbit */
-     , (32929,   3,         21) /* PaletteTemplate - Gold */
+     , (32929,   3,         21) /* PaletteTemplate - Brown */
      , (32929,   6,         -1) /* ItemsCapacity */
      , (32929,   7,         -1) /* ContainersCapacity */
      , (32929,  16,          1) /* ItemUseable - No */
      , (32929,  25,        135) /* Level */
+     , (32929,  27,          0) /* ArmorType - None */
+     , (32929,  40,          2) /* CombatMode - Melee */
+     , (32929,  68,          9) /* TargetingTactic - Random, TopDamager */
+     , (32929,  72,         41) /* FriendType - Bunny */
+     , (32929,  81,          2) /* MaxGeneratedObjects */
+     , (32929,  82,          2) /* InitGeneratedObjects */
      , (32929,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
-     , (32929, 133,          4) /* ShowableOnRadar - ShowAlways */;
+     , (32929, 103,          1) /* GeneratorDestructionType - Nothing */
+     , (32929, 133,          4) /* ShowableOnRadar - ShowAlways */
+     , (32929, 146,     250000) /* XpOverride */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (32929,   1, True ) /* Stuck */;
+VALUES (32929,   1, True ) /* Stuck */
+     , (32929,  11, False) /* IgnoreCollisions */
+     , (32929,  12, True ) /* ReportCollisions */
+     , (32929,  13, False) /* Ethereal */
+     , (32929,  14, True ) /* GravityStatus */
+     , (32929,  19, True ) /* Attackable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (32929,  39, 1.399999976158142) /* DefaultScale */;
+VALUES (32929,  39,     1.5) /* DefaultScale */
+	  , (32929,   1,       5) /* HeartbeatInterval */
+     , (32929,   2,       0) /* HeartbeatTimestamp */
+     , (32929,   3,   0.067) /* HealthRate */
+     , (32929,   4,       5) /* StaminaRate */
+     , (32929,   5,       2) /* ManaRate */
+     , (32929,  12,     0.5) /* Shade */
+     , (32929,  13,       1) /* ArmorModVsSlash */
+     , (32929,  14,       1) /* ArmorModVsPierce */
+     , (32929,  15,       1) /* ArmorModVsBludgeon */
+     , (32929,  16,       1) /* ArmorModVsCold */
+     , (32929,  17,       1) /* ArmorModVsFire */
+     , (32929,  18,       1) /* ArmorModVsAcid */
+     , (32929,  19,       1) /* ArmorModVsElectric */
+     , (32929,  31,      18) /* VisualAwarenessRange */
+     , (32929,  34,       3) /* PowerupTime */
+     , (32929,  36,       1) /* ChargeSpeed */
+     , (32929,  41,    3600) /* RegenerationInterval */
+     , (32929,  43,       1) /* GeneratorRadius */
+     , (32929,  64,       1) /* ResistSlash */
+     , (32929,  65,       1) /* ResistPierce */
+     , (32929,  66,       1) /* ResistBludgeon */
+     , (32929,  67,       1) /* ResistFire */
+     , (32929,  68,       1) /* ResistCold */
+     , (32929,  69,       1) /* ResistAcid */
+     , (32929,  70,       1) /* ResistElectric */
+     , (32929,  71,       1) /* ResistHealthBoost */
+     , (32929,  72,       1) /* ResistStaminaDrain */
+     , (32929,  73,       1) /* ResistStaminaBoost */
+     , (32929,  74,       1) /* ResistManaDrain */
+     , (32929,  75,       1) /* ResistManaBoost */
+     , (32929, 104,      10) /* ObviousRadarRange */
+     , (32929, 125,       1) /* ResistHealthDrain */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (32929,   1, 'Dusk Rabbit') /* Name */;
@@ -50,6 +95,14 @@ INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`
 VALUES (32929,   1,   250, 0, 0, 445) /* MaxHealth */
      , (32929,   3,   250, 0, 0, 640) /* MaxStamina */
      , (32929,   5,     0, 0, 0, 40) /* MaxMana */;
+     
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (32929,  6, 0, 3, 0,   0, 0, 0) /* MeleeDefense        Specialized */
+     , (32929,  7, 0, 3, 0,   0, 0, 0) /* MissileDefense      Specialized */
+     , (32929, 15, 0, 3, 0,   0, 0, 0) /* MagicDefense        Specialized */
+     , (32929, 22, 0, 2, 0,  10, 0, 0) /* Jump                Trained */
+     , (32929, 24, 0, 2, 0,  10, 0, 0) /* Run                 Trained */
+     , (32929, 45, 0, 3, 0,   0, 0, 0) /* LightWeapons        Specialized */;          
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (32929,  0,  2,  2, 0.75,  240,  240,  240,  240,  240,  240,  240,  240,    0, 1, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0) /* Head */
@@ -59,29 +112,3 @@ VALUES (32929,  0,  2,  2, 0.75,  240,  240,  240,  240,  240,  240,  240,  240,
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (32929, 9,  5633,  0, 0, 0.05, False) /* Create Rabbit Carcass (5633) for ContainTreasure */
      , (32929, 9,     0,  0, 0, 0.95, False) /* Create nothing for ContainTreasure */;
-
-/* Lifestoned Changelog:
-{
-  "LastModified": "2020-08-22T18:50:10.1368906Z",
-  "ModifiedBy": "ACE.Adapter",
-  "Changelog": [
-    {
-      "created": "2020-08-22T15:13:36.6003036Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    },
-    {
-      "created": "2020-08-22T18:33:27.9730583Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    },
-    {
-      "created": "2020-08-22T18:50:10.1094137Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    }
-  ],
-  "UserChangeSummary": "Weenie exported from ACEmulator world database using ACE.Adapter",
-  "IsDone": false
-}
-*/
