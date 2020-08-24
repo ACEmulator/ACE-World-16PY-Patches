844606a0e21558f6e7b48560ab59a140cc19f27f
Patrick <phreekn.slacker@gmail.com>
2020-08-24 11:29:04 -0500

    Update RabbitHutchKey.sql

--- Database/Patches/2006-04-ShiningRunesAndShadowedHands/RabbitHutchKey.sql
+++ Database/Patches/2006-04-ShiningRunesAndShadowedHands/RabbitHutchKey.sql
@@ -1,4 +1,4 @@
 DELETE FROM quest WHERE name = 'RabbitHutchKey';
 
 INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
-VALUES ('RabbitHutchKequesty', 1123200, -1, 'Picked up Rabbit Hutch Key', '2019-03-14 20:34:27');
+VALUES ('rabbithutchkey', 1123200, -1, 'Picked up Rabbit Hutch Key', '2019-03-14 20:34:27');
