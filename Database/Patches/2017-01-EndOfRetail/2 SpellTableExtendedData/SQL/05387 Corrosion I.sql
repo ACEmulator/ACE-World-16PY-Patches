30e13db54bb0d8a1ff8cbb0b50e6b60f0e0acf32
gmriggs <gmriggs@gmail.com>
2020-08-24 14:32:14 -0400

    updating dot statmodvals to match retail (per tick, instead of total)

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05387 Corrosion I.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/05387 Corrosion I.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 5387;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `e_Type`, `base_Intensity`, `variance`, `wcid`, `num_Projectiles`, `dot_Duration`, `last_Modified`)
-VALUES (5387, 'Corrosion I', 36868 /* Int, SingleStat, Additive */, 330 /* NetherOverTime */, 76, 1024 /* Nether */, 76, 0, 43344 /* Nether Bolt */, 1, 15, '2019-03-18 09:00:00');
+VALUES (5387, 'Corrosion I', 36868 /* Int, SingleStat, Additive */, 330 /* NetherOverTime */, 19, 1024 /* Nether */, 76, 0, 43344 /* Nether Bolt */, 1, 15, '2019-03-18 09:00:00');
