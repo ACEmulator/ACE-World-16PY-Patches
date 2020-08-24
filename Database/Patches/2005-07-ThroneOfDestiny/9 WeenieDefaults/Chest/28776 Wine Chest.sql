7a739fd4c5dede8681f5b2f48185c65623f7414f
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2020-08-24 00:55:01 -0700

    Update 28776 Wine Chest.sql (#601)

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Chest/28776 Wine Chest.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Chest/28776 Wine Chest.sql
@@ -48,5 +48,5 @@ VALUES (28776,   1,   33554556) /* Setup */
      , (28776,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (28776, 1, 28765, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Cove Apple Wine (28765) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
-     , (28776, 1, 28816, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Glorious Apple (28816) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
+VALUES (28776, -1, 28765, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Cove Apple Wine (28765) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
+     , (28776, -1, 28816, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Glorious Apple (28816) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
