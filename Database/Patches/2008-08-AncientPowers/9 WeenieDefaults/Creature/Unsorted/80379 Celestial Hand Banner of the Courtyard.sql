f83748156388ae7dce34bdcdafdaa61ea250df5c
Ty Conner <tyconner@itcproductions.com>
2020-08-24 01:38:33 -0400

    Update 80379 Celestial Hand Banner of the Courtyard.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Unsorted/80379 Celestial Hand Banner of the Courtyard.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Unsorted/80379 Celestial Hand Banner of the Courtyard.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 80379;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (80379, 'ace80312-CelestialHandbannerofthecourtyard', 10, '2019-02-10 00:00:00') /* Creature */;
+VALUES (80379, 'ace80379-CelestialHandbannerofthecourtyard', 10, '2019-02-10 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (80379,   1,         16) /* ItemType - Creature */
