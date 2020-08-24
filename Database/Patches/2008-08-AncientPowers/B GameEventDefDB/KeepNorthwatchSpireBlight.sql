a8a128f934715499978d1fbc5864b8975f285fdb
Ty Conner <tyconner@itcproductions.com>
2020-08-24 00:48:53 -0400

    Create KeepNorthwatchSpireBlight.sql

---
+++ Database/Patches/2008-08-AncientPowers/B GameEventDefDB/KeepNorthwatchSpireBlight.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'KeepNorthwatchSpireBlight';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('KeepNorthwatchSpireBlight', -1, -1, 4, '2019-06-16 10:00:00');
