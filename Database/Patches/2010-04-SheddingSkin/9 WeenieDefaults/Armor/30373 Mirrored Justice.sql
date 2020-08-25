0d62030e5cffc76cb38e41aef84d00d2614e4dcb
Patrick <phreekn.slacker@gmail.com>
2020-08-24 21:11:07 -0500

    Update 30373 Mirrored Justice.sql

--- Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Armor/30373 Mirrored Justice.sql
+++ Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Armor/30373 Mirrored Justice.sql
@@ -5,11 +5,13 @@ VALUES (30373, 'shieldraremirroredjustice', 1, '2019-05-03 00:00:00') /* Generic
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (30373,   1,          2) /* ItemType - Armor */
+     , (30373,   3,          4) /* PaletteTemplate - Brown */
      , (30373,   5,        690) /* EncumbranceVal */
      , (30373,   9,    2097152) /* ValidLocations - Shield */
      , (30373,  16,          1) /* ItemUseable - No */
      , (30373,  17,        228) /* RareId */
      , (30373,  19,      50000) /* Value */
+     , (30373,  27,          2) /* ArmorType - Leather */     
      , (30373,  28,        480) /* ArmorLevel */
      , (30373,  51,          4) /* CombatUse - Shield */
      , (30373,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
@@ -28,17 +30,21 @@ VALUES (30373,   4,          0) /* ItemTotalXp */
      , (30373,   5, 2000000000) /* ItemBaseXp */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (30373,  22, True ) /* Inscribable */;
+VALUES (30373,  22, True ) /* Inscribable */
+     , (30373, 100, False) /* Dyable */;;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (30373,   5, -0.0333333015441895) /* ManaRate */
-     , (30373,  13, 1.10000002384186) /* ArmorModVsSlash */
-     , (30373,  14, 0.800000011920929) /* ArmorModVsPierce */
-     , (30373,  15, 1.39999997615814) /* ArmorModVsBludgeon */
-     , (30373,  16, 0.899999976158142) /* ArmorModVsCold */
-     , (30373,  17, 0.899999976158142) /* ArmorModVsFire */
-     , (30373,  18, 0.899999976158142) /* ArmorModVsAcid */
-     , (30373,  19, 0.899999976158142) /* ArmorModVsElectric */
+VALUES (30373,   5, -0.03) /* ManaRate */
+     , (30373,  12, 0.7) /* Shade */
+     , (30373,  13, 1.1) /* ArmorModVsSlash */
+     , (30373,  14, 0.8) /* ArmorModVsPierce */
+     , (30373,  15, 1.4) /* ArmorModVsBludgeon */
+     , (30373,  16, 0.9) /* ArmorModVsCold */
+     , (30373,  17, 0.9) /* ArmorModVsFire */
+     , (30373,  18, 0.9) /* ArmorModVsAcid */
+     , (30373,  19, 0.9) /* ArmorModVsElectric */
+     , (30373, 110, 1.7) /* BulkMod */
+     , (30373, 111,       1) /* SizeMod */	       
      , (30373, 165,       1) /* ArmorModVsNether */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
@@ -48,8 +54,11 @@ VALUES (30373,   1, 'Mirrored Justice') /* Name */
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (30373,   1,   33559421) /* Setup */
      , (30373,   3,  536870932) /* SoundTable */
+     , (30373,   6,   67108990) /* PaletteBase */     
      , (30373,   8,  100686844) /* Icon */
      , (30373,  22,  872415275) /* PhysicsEffectTable */
+     , (30373,  36,  234881042) /* MutateFilter */
+     , (30373,  46,  939524146) /* TsysMutationFilter */     
      , (30373,  52,  100686604) /* IconUnderlay */;
 
 INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
