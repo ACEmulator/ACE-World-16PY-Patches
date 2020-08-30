1c752712674fd4365567051cc08837d361b10b89
Ty Conner <tyconner@itcproductions.com>
2020-08-30 13:16:55 -0400

    Update E74E.sql

--- Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/E74E.sql
+++ Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/E74E.sql
@@ -11,13 +11,13 @@ VALUES (0x7E74E0D3, 80351, 0xE74E0000, 92, 83, 32.005, 1, 0, 0, 0, False, '2020-
 /* @teleloc 0xE74E0000 [92.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7E74E0D4, 80391, 0xE74E0000, 90, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Northwatch Celhan Stopgap Gen */
+VALUES (0x7E74E0D4, 80397, 0xE74E0000, 90, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Northwatch Celhan Stopgap Gen */
 /* @teleloc 0xE74E0000 [90.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7E74E0D5, 80392, 0xE74E0000, 86, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Northwatch Eldweb Stopgap Gen */
+VALUES (0x7E74E0D5, 80398, 0xE74E0000, 86, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Northwatch Eldweb Stopgap Gen */
 /* @teleloc 0xE74E0000 [86.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7E74E0D6, 80393, 0xE74E0000, 82, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Northwatch Radblo Stopgap Gen */
+VALUES (0x7E74E0D6, 80399, 0xE74E0000, 82, 83, 32.005, 1, 0, 0, 0, False, '2020-08-22 10:00:00'); /* Northwatch Radblo Stopgap Gen */
 /* @teleloc 0xE74E0000 [82.000000 83.000000 32.005000] 1.000000 0.000000 0.000000 0.000000 */
