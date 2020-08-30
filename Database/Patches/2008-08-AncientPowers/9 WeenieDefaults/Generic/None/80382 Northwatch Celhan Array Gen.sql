a3d153e36f8bc51eb5293466add471e836f5b2be
Ty Conner <tyconner@itcproductions.com>
2020-08-30 13:39:38 -0400

    Update 80382 Northwatch Celhan Array Gen.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Generic/None/80382 Northwatch Celhan Array Gen.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Generic/None/80382 Northwatch Celhan Array Gen.sql
@@ -30,4 +30,4 @@ VALUES (80382,   1,   33555051) /* Setup */
      , (80382,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (80382, -1, 80406, 300, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Celestial Hand Crystal Array (80406) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
+VALUES (80382, -1, 40543, 300, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Celestial Hand Crystal Array (40543) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
