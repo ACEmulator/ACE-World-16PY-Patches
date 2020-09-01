1e51ff6551ab9cfdbbd5f9b1525bdd7be32240ca
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-09-01 00:34:14 -0400

    Restore Heritage Masks
    24859 Vincadi
    30447 Virindi Desecrator
    32151 Virindi Desecrator Mask
    32175 Broken Virindi Desecrator Mask
    34100 The Red Bull
    34143 The Boar of Cinghalle
    34144 The Stag of Bellenesse
    34145 Trilaine di Ricard ES.es
    34145 Trilaine di Ricard
    34214 The Dragon of Power
    34217 Mask of the Malik
    34218 The Helm of the Golden Flame
    34255 Karlun's Visage
    71142 The Royal Bull of Sanamar

---
+++ Database/Patches/2006-11-RememberingThePast/8 QuestDefDB/regaliamaskdesecrator.sql
@@ -0,0 +1,5 @@
+DELETE FROM `quest` WHERE `name` = 'regaliamaskdesecrator';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('regaliamaskdesecrator', 72000, -1, 'Picked up Virindi Desectrator Mask', '2020-08-31 20:34:27');
+
