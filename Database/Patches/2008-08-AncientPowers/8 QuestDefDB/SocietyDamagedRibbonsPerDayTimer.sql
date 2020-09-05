4006793ca82b455e9a95ee22e988d093326acfa3
Ty Conner <tyconner@itcproductions.com>
2020-09-05 18:33:16 -0400

    Update SocietyDamagedRibbonsPerDayTimer.sql

--- Database/Patches/2008-08-AncientPowers/8 QuestDefDB/SocietyDamagedRibbonsPerDayTimer.sql
+++ Database/Patches/2008-08-AncientPowers/8 QuestDefDB/SocietyDamagedRibbonsPerDayTimer.sql
@@ -1,4 +1,4 @@
-DELETE FROM `quest` WHERE `name` = 'SocietyRibbonsPerDayTimer';
+DELETE FROM `quest` WHERE `name` = 'SocietyDamagedRibbonsPerDayTimer';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('SocietyRibbonsPerDayTimer', 216000, -1, 'Timer for how often a player can turn in the limit of damaged ribbons', '2020-03-31 20:34:27');
+VALUES ('SocietyDamagedRibbonsPerDayTimer', 216000, -1, 'Timer for how often a player can turn in the limit of damaged ribbons', '2020-03-31 20:34:27');
