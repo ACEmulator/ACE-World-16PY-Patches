a0082896a43da9a92609fe1f36263b2e4756e841
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:16:13 -0500

    Create CrystallineArrayFragmentPickupTimer.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/CrystallineArrayFragmentPickupTimer.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'CrystalFinalPickup0509';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystalFinalPickup0509', 518400, -1, 'Picked up Crystalline Array Fragment', '2019-03-14 20:34:27');
