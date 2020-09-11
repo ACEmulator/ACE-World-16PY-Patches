497082d344e9f5745ad80a1dda97449d8754eb08
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:16:08 -0500

    Create CrystallineSkillTest10109Wait.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/CrystallineSkillTest10109Wait.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'CrystallineSkillTest10109Wait';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineSkillTest10109Wait', 72000, -1, 'Found Crystalline Bound Wisp', '2019-03-14 20:34:27');
