71314eac8c83129a69cf49c2650e2be8ed557a86
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:39:57 -0500

    Update 40779 Lever.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/40779 Lever.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/40779 Lever.sql
@@ -9,31 +9,7 @@ VALUES (40779,   1,         16) /* ItemType - Creature */
      , (40779,   7,         -1) /* ContainersCapacity */
      , (40779,  16,         32) /* ItemUseable - Remote */
      , (40779,  93,    2098200) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment */
-     , (40779,  95,          3) /* RadarBlipColor - White */
-     , (40779, 307,          0) /* DamageRating */
-     , (40779, 308,          0) /* DamageResistRating */
-     , (40779, 313,          0) /* CritRating */
-     , (40779, 314,          0) /* CritDamageRating */
-     , (40779, 315,          0) /* CritResistRating */
-     , (40779, 316,          0) /* CritDamageResistRating */
-     , (40779, 370,          0) /* GearDamage */
-     , (40779, 371,          0) /* GearDamageResist */
-     , (40779, 372,          0) /* GearCrit */
-     , (40779, 373,          0) /* GearCritResist */
-     , (40779, 374,          0) /* GearCritDamage */
-     , (40779, 375,          0) /* GearCritDamageResist */
-     , (40779, 376,          0) /* GearHealingBoost */
-     , (40779, 377,          0) /* GearNetherResist */
-     , (40779, 378,          0) /* GearLifeResist */
-     , (40779, 379,          0) /* GearMaxHealth */
-     , (40779, 381,          0) /* PKDamageRating */
-     , (40779, 382,          0) /* PKDamageResistRating */
-     , (40779, 383,          0) /* GearPKDamageRating */
-     , (40779, 384,          0) /* GearPKDamageResistRating */
-     , (40779, 386,          0) /* Overpower */
-     , (40779, 387,          0) /* OverpowerResist */
-     , (40779, 388,          0) /* GearOverpower */
-     , (40779, 389,          0) /* GearOverpowerResist */;
+     , (40779,  95,          3) /* RadarBlipColor - White */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (40779,   1, True ) /* Stuck */
