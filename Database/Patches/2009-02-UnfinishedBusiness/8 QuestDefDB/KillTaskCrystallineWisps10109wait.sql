6221c178eb8448ab541d67d498ef8cdd9f67ef49
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:15:57 -0500

    Create KillTaskCrystallineWisps10109wait.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/KillTaskCrystallineWisps10109wait.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskCrystallineWisps10109Wait';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskCrystallineWisps10109Wait', 72000, -1, 'Crystalline Wisp Kill Task', '2020-07-20 06:51:50');
