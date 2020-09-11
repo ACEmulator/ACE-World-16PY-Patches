1cdba2b47ac225ffd7b0afb405b358034322c253
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:11:44 -0500

    Create KillTaskIntenseWispsShiVolwait.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/KillTaskIntenseWispsShiVolwait.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskIntenseWispsShiVolwait';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskIntenseWispsShiVolwait', 0, 1, 'Crystalline Intense Wisp Kill Task Shivering and Voltaic', '2020-07-20 06:51:50');
