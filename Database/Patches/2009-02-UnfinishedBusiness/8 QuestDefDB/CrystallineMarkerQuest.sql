4b2d772e6f97b3da27097ff05990a20985615ba6
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:16:10 -0500

    Create CrystallineMarkerQuest.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/CrystallineMarkerQuest.sql
@@ -0,0 +1,159 @@
+DELETE FROM quest WHERE name = 'CrystallineMarkerCounter0109';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarkerCounter0109', 0, 25, 'Found Crystalline Bound Wisp', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'CrystallineMarker5x';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarker5x', 0, 1, 'Found 5 Crystalline Markers', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'CrystallineMarker10x';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarker10x', 0, 1, 'Found 10 Crystalline Markers', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'CrystallineMarker15x';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarker15x', 0, 1, 'Found 15 Crystalline Markers', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'CrystallineMarker20x';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarker20x', 0, 1, 'Found 20 Crystalline Markers', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'CrystallineMarker25x';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarker25x', 0, 1, 'Found 25 Crystalline Markers', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'CrystallineMarkerStart';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('CrystallineMarkerStart', 0, 1, 'Started Crystalline Markers Quest', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker1';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker1', 0, 1, 'Found 1st Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker2';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker2', 0, 1, 'Found 2nd Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker3';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker3', 0, 1, 'Found 3rd Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker4';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker4', 0, 1, 'Found 4th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker5';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker5', 0, 1, 'Found 5th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker6';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker6', 0, 1, 'Found 6th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker7';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker7', 0, 1, 'Found 7th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker8';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker8', 0, 1, 'Found 8th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker9';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker9', 0, 1, 'Found 9th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker10';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker10', 0, 1, 'Found 10th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker11';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker11', 0, 1, 'Found 11th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker12';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker12', 0, 1, 'Found 12th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker13';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker13', 0, 1, 'Found 13th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker14';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker14', 0, 1, 'Found 14th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker15';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker15', 0, 1, 'Found 15th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker16';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker16', 0, 1, 'Found 16th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker17';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker17', 0, 1, 'Found 17th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker18';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker18', 0, 1, 'Found 18th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker19';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker19', 0, 1, 'Found 19th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker20';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker20', 0, 1, 'Found 20th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker21';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker21', 0, 1, 'Found 21st Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker22';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker22', 0, 1, 'Found 22nd Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker23';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker23', 0, 1, 'Found 23rd Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker24';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker24', 0, 1, 'Found 24th Crystalline Marker', '2019-03-14 20:34:27');
+
+DELETE FROM quest WHERE name = 'FoundCrystallineMarker25';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FoundCrystallineMarker25', 0, 1, 'Found 25th Crystalline Marker', '2019-03-14 20:34:27');
