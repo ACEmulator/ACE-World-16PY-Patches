0405c09e4250d0caf2e8174424aa5ec5b0c73818
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:16:05 -0500

    Create EssenceLockUsed.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/EssenceLockUsed.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'EssenceLockUsed';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('EssenceLockUsed', 0, 5, 'Used Essence Lock', '2019-03-14 20:34:27');
