c6478dd42a85f0753f094ae2ef66ce6e53417b49
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:11:36 -0500

    Create KillTaskIntenseWispsCorIncawait.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/KillTaskIntenseWispsCorIncawait.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskIntenseWispsCorIncawait';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskIntenseWispsCorIncawait', 0, 1, 'Crystalline Intense Wisp Kill Task Corroding and Incalescent', '2020-07-20 06:51:50');
