783952eb05a0d5b4829545e1e19df07228a96724
Patrick <phreekn.slacker@gmail.com>
2020-09-10 19:15:54 -0500

    Create CrystallineArrayFragmentEvent.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/B GameEventDefDB/CrystallineArrayFragmentEvent.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'CrystallineArrayFragmentEvent';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('CrystallineArrayFragmentEvent', -1, -1, 3, '2019-06-23 00:00:00');
