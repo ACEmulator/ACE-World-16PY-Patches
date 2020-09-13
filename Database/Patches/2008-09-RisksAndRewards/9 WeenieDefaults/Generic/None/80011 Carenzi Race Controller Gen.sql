196a3bb10d4e126db03491906022e56bbd947a3f
Ty Conner <tyconner@itcproductions.com>
2020-09-13 04:05:07 -0400

    Update 80011 Carenzi Race Controller Gen.sql

--- Database/Patches/2008-09-RisksAndRewards/9 WeenieDefaults/Generic/None/80011 Carenzi Race Controller Gen.sql
+++ Database/Patches/2008-09-RisksAndRewards/9 WeenieDefaults/Generic/None/80011 Carenzi Race Controller Gen.sql
@@ -4,8 +4,8 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (80011, 'CarenziRaceControllerGen', 1, '2005-02-09 10:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (80011,  81,          1) /* MaxGeneratedObjects */
-     , (80011,  82,          1) /* InitGeneratedObjects */
+VALUES (80011,  81,          2) /* MaxGeneratedObjects */
+     , (80011,  82,          2) /* InitGeneratedObjects */
      , (80011,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (80011, 142,          3) /* GeneratorTimeType - Event */
      , (80011, 145,          2) /* GeneratorEndDestructionType - Destroy */;
@@ -29,4 +29,5 @@ VALUES (80011,   1,   33555051) /* Setup */
      , (80011,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (80011, 1, 80015, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Carenzi Race Controller (80015) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
+VALUES (80011, 1, 80015, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Carenzi Race Controller (80015) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */
+     , (80011, 1, 80007, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, -2, 0, 1, 0, 0, 0) /* Generate Landblock KeepAlive (80007) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
