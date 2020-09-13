db2ef9d79649aad531414521005685c3c6badab2
Ty Conner <tyconner@itcproductions.com>
2020-09-13 06:06:46 -0400

    Update 38993 Carenzi Race Bookie.sql

--- Database/Patches/2008-09-RisksAndRewards/9 WeenieDefaults/Creature/Human/38993 Carenzi Race Bookie.sql
+++ Database/Patches/2008-09-RisksAndRewards/9 WeenieDefaults/Creature/Human/38993 Carenzi Race Bookie.sql
@@ -590,10 +590,10 @@ VALUES (38993, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'CarenziRaceCup', NUL
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id, 0, 21 /* InqQuest */, 0, 1, NULL, 'CarenziRaceWins', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id, 0, 30 /* InqQuestSolves */, 0, 1, NULL, 'CarenziRaceWins@10-10', NULL, 10, 10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (38993, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'CarenziRaceWins', NULL, NULL, NULL);
+VALUES (38993, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'CarenziRaceWins@10-10', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -603,7 +603,7 @@ VALUES (@parent_id, 0, 10 /* Tell */, 0, 1, NULL, 'For having backed so many win
      , (@parent_id, 2, 22 /* StampQuest */, 0, 1, NULL, 'CarenziRaceCup', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (38993, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'CarenziRaceWins', NULL, NULL, NULL);
+VALUES (38993, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'CarenziRaceWins@10-10', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
