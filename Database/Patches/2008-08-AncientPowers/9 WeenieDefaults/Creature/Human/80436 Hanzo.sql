6a309753533ba7b183b6a6a1ce468781544072e4
Ty Conner <tyconner@itcproductions.com>
2020-09-13 16:58:35 -0400

    Update 80436 Hanzo.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/80436 Hanzo.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/80436 Hanzo.sql
@@ -64,6 +64,13 @@ VALUES (80436,   1, 'Hanzo') /* Name */
      , (80436,   4, 'Sho') /* HeritageGroup */
      , (80436,   5, 'Blighted Golem Hunter') /* Template */;
 
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (80436,   1,   33554433) /* Setup */
+     , (80436,   2,  150994945) /* MotionTable */
+     , (80436,   3,  536870913) /* SoundTable */
+     , (80436,   4,  805306368) /* CombatTable */
+     , (80436,   8,  100667446) /* Icon */;
+
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (80436,   1, 290, 0, 0) /* Strength */
      , (80436,   2, 200, 0, 0) /* Endurance */
