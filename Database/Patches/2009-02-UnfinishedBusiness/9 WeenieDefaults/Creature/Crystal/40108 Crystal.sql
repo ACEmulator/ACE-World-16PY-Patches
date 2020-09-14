1f40c82c5254b2a7da96c5430d4e3a6cce59bfae
Patrick <phreekn.slacker@gmail.com>
2020-09-14 16:30:43 -0500

    Update 40108 Crystal.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40108 Crystal.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40108 Crystal.sql
@@ -9,31 +9,7 @@ VALUES (40108,   1,         16) /* ItemType - Creature */
      , (40108,   7,         -1) /* ContainersCapacity */
      , (40108,  16,         32) /* ItemUseable - Remote */
      , (40108,  93,    6292508) /* PhysicsState - Ethereal, ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
-     , (40108,  95,          8) /* RadarBlipColor - Yellow */
-     , (40108, 307,          0) /* DamageRating */
-     , (40108, 308,          0) /* DamageResistRating */
-     , (40108, 313,          0) /* CritRating */
-     , (40108, 314,          0) /* CritDamageRating */
-     , (40108, 315,          0) /* CritResistRating */
-     , (40108, 316,          0) /* CritDamageResistRating */
-     , (40108, 370,          0) /* GearDamage */
-     , (40108, 371,          0) /* GearDamageResist */
-     , (40108, 372,          0) /* GearCrit */
-     , (40108, 373,          0) /* GearCritResist */
-     , (40108, 374,          0) /* GearCritDamage */
-     , (40108, 375,          0) /* GearCritDamageResist */
-     , (40108, 376,          0) /* GearHealingBoost */
-     , (40108, 377,          0) /* GearNetherResist */
-     , (40108, 378,          0) /* GearLifeResist */
-     , (40108, 379,          0) /* GearMaxHealth */
-     , (40108, 381,          0) /* PKDamageRating */
-     , (40108, 382,          0) /* PKDamageResistRating */
-     , (40108, 383,          0) /* GearPKDamageRating */
-     , (40108, 384,          0) /* GearPKDamageResistRating */
-     , (40108, 386,          0) /* Overpower */
-     , (40108, 387,          0) /* OverpowerResist */
-     , (40108, 388,          0) /* GearOverpower */
-     , (40108, 389,          0) /* GearOverpowerResist */;
+     , (40108,  95,          8) /* RadarBlipColor - Yellow */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (40108,   1, True ) /* Stuck */
