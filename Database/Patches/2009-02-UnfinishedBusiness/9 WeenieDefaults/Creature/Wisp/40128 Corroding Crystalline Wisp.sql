4cfb168c2cb6982aa5a9a0319b978e3c8c56eaa8
Patrick <phreekn.slacker@gmail.com>
2020-09-14 08:54:16 -0500

    Create 40128 Corroding Crystalline Wisp.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Wisp/40128 Corroding Crystalline Wisp.sql
@@ -0,0 +1,117 @@
+DELETE FROM `weenie` WHERE `class_Id` = 40128;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (40128, 'ace40128-corrodingcrystallinewisp', 10, '2019-02-10 00:00:00') /* Creature */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (40128,   1,         16) /* ItemType - Creature */
+     , (40128,   2,         20) /* CreatureType - Wisp */
+     , (40128,   6,         -1) /* ItemsCapacity */
+     , (40128,   7,         -1) /* ContainersCapacity */
+     , (40128,  16,          1) /* ItemUseable - No */
+     , (40128,  25,        115) /* Level */
+     , (40128,  40,          2) /* CombatMode - Melee */  
+	  , (40128,  68,          9) /* TargetingTactic - Random, TopDamager */       
+     , (40128,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
+     , (40128, 133,          4) /* ShowableOnRadar - ShowAlways */
+     , (40128, 146,     125000) /* XpOverride */	  ;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (40128,   1, True ) /* Stuck */
+     , (40128,   6, True ) /* AiUsesMana */
+     , (40128,  11, False) /* IgnoreCollisions */
+     , (40128,  12, True ) /* ReportCollisions */
+     , (40128,  13, False) /* Ethereal */
+     , (40128,  14, True ) /* GravityStatus */
+     , (40128,  19, True ) /* Attackable */
+     , (40128,  50, True ) /* NeverFailCasting */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (40128,   1,       5) /* HeartbeatInterval */
+     , (40128,   2,       0) /* HeartbeatTimestamp */
+     , (40128,   3,     0.4) /* HealthRate */
+     , (40128,   4,       5) /* StaminaRate */
+     , (40128,   5,       1) /* ManaRate */
+     , (40128,  13,    0.84) /* ArmorModVsSlash */
+     , (40128,  14,       2) /* ArmorModVsPierce */
+     , (40128,  15,    0.84) /* ArmorModVsBludgeon */
+     , (40128,  16,       2) /* ArmorModVsCold */
+     , (40128,  17,       2) /* ArmorModVsFire */
+     , (40128,  18,       2) /* ArmorModVsAcid */
+     , (40128,  19,       2) /* ArmorModVsElectric */
+     , (40128,  31,      30) /* VisualAwarenessRange */
+     , (40128,  34,       1) /* PowerupTime */
+     , (40128,  36,       1) /* ChargeSpeed */
+     , (40128,  39,     1.3) /* DefaultScale */
+     , (40128,  64,     0.9) /* ResistSlash */
+     , (40128,  65,     0.3) /* ResistPierce */
+     , (40128,  66,     0.3) /* ResistBludgeon */
+     , (40128,  67,     0.9) /* ResistFire */
+     , (40128,  68,     0.3) /* ResistCold */
+     , (40128,  69,     0.3) /* ResistAcid */
+     , (40128,  70,     0.3) /* ResistElectric */
+     , (40128,  71,       1) /* ResistHealthBoost */
+     , (40128,  72,       1) /* ResistStaminaDrain */
+     , (40128,  73,       1) /* ResistStaminaBoost */
+     , (40128,  74,       1) /* ResistManaDrain */
+     , (40128,  75,       1) /* ResistManaBoost */
+     , (40128,  80,       3) /* AiUseMagicDelay */
+     , (40128, 104,      10) /* ObviousRadarRange */
+     , (40128, 125,       1) /* ResistHealthDrain */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (40128,   1, 'Corroding Crystalline Wisp') /* Name */
+	  , (40128,  45, 'KillTaskCrystallineWisps10109');
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (40128,   1,   33557068) /* Setup */
+     , (40128,   2,  150995087) /* MotionTable */
+     , (40128,   4,  805306368) /* CombatTable */        
+     , (40128,   3,  536870985) /* SoundTable */
+     , (40128,   8,  100671683) /* Icon */
+     , (40128,  22,  872415274) /* PhysicsEffectTable */
+     , (40128,  35,         32) /* DeathTreasureType - Loot Tier: 6 */;     
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (40128,   1, 150, 0, 0) /* Strength */
+     , (40128,   2, 200, 0, 0) /* Endurance */
+     , (40128,   3, 220, 0, 0) /* Quickness */
+     , (40128,   4, 150, 0, 0) /* Coordination */
+     , (40128,   5, 330, 0, 0) /* Focus */
+     , (40128,   6, 330, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (40128,   1,   620, 0, 0, 720) /* MaxHealth */
+     , (40128,   3,   620, 0, 0, 820) /* MaxStamina */
+     , (40128,   5,   120, 0, 0, 450) /* MaxMana */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (40128,  6, 0, 3, 0, 385, 0, 0) /* MeleeDefense        Specialized */
+     , (40128,  7, 0, 3, 0, 440, 0, 0) /* MissileDefense      Specialized */
+     , (40128, 45, 0, 3, 0, 180, 0, 0) /* LightWeapons        Specialized */
+     , (40128, 14, 0, 3, 0, 300, 0, 0) /* ArcaneLore          Specialized */
+     , (40128, 15, 0, 3, 0, 230, 0, 0) /* MagicDefense        Specialized */
+     , (40128, 20, 0, 3, 0, 100, 0, 0) /* Deception           Specialized */
+     , (40128, 24, 0, 3, 0,  50, 0, 0) /* Run                 Specialized */
+     , (40128, 31, 0, 3, 0, 100, 0, 0) /* CreatureEnchantment Specialized */
+     , (40128, 33, 0, 3, 0, 100, 0, 0) /* LifeMagic           Specialized */
+     , (40128, 34, 0, 3, 0, 100, 0, 0) /* WarMagic            Specialized */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (40128,  0, 32, 50,  0.5,   140,   200,   200,   200,   200,   200,  200,   200,    0, 1,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Head */
+     , (40128, 16, 32,  0,    0,   140,   200,   200,   200,   200,   200,  200,   200,    0, 2,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4) /* Torso */
+     , (40128, 17, 32,  5, 0.75,   120,   200,   200,   200,   200,   200,  200,   200,    0, 2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Tail */
+     , (40128, 21, 32,  0,    0,   110,    40,    34,    34,    34,    34,   80,    34,    0, 2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Wings */;
+
+INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
+VALUES (40128,    63,   2.17)  /* Acid VI */
+     , (40128,   1160,    2.1)  /* Heal Self V */
+     , (40128,   1240,      2)  /* Drain Health Other IV */
+     , (40128,   1252,      2)  /* Drain Stamina Other IV */
+     , (40128,   1263,      2)  /* Drain Mana Other IV */;
+
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (40128, 9, 40100,  0, 0, 0.33, False)	/* Create Crystalline Shard for ContainTreasure */
+	  , (40128, 9,  6876,  0, 0, 0.66, False) /* Create Sturdy Iron Key (6876) for ContainTreasure */
+     , (40128, 9,     0,  0, 0, 0.99, False) /* Create nothing for ContainTreasure */ ;
+     
