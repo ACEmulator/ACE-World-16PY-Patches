84048edc554aa669257cf5d211709a132fae7d04
Patrick <phreekn.slacker@gmail.com>
2020-09-14 16:53:55 -0500

    Create crystalcorestart.sql

---
+++ Database/Patches/2012-12-AGrowingTwilight/8 QuestDefDB/crystalcorestart.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'crystalcorestart';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('crystalcorestart', 0, 1, 'Started Crystal Core Quest', '2019-03-14 20:34:27');
