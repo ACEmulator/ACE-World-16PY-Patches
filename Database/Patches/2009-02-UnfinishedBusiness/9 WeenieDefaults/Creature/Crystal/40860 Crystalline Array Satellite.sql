d58b340ec2149eeadfd3f657dafed748071c399b
Patrick <phreekn.slacker@gmail.com>
2020-09-14 08:59:48 -0500

    Create 40860 Crystalline Array Satellite.sql

---
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40860 Crystalline Array Satellite.sql
@@ -0,0 +1,108 @@
+DELETE FROM `weenie` WHERE `class_Id` = 40860;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (40860, 'ace40860-crystallinearraysatellite', 10, '2019-02-10 00:00:00') /* Creature */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (40860,   1,         16) /* ItemType - Creature */
+     , (40860,   2,         47) /* CreatureType - Crystal */
+     , (40860,   6,         -1) /* ItemsCapacity */
+     , (40860,   7,         -1) /* ContainersCapacity */
+     , (40860,  16,          1) /* ItemUseable - No */
+     , (40860,  25,        100) /* Level */
+     , (40860,  81,          8) /* MaxGeneratedObjects */
+     , (40860,  82,          8) /* InitGeneratedObjects */     
+     , (40860,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
+     , (40860, 103,          3) /* GeneratorDestructionType - Kill */ 
+     , (40860, 133,          4) /* ShowableOnRadar - ShowAlways */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (40860,   1, True ) /* Stuck */
+     , (40860,   6, True ) /* AiUsesMana */
+     , (40860,  11, False) /* IgnoreCollisions */
+     , (40860,  12, True ) /* ReportCollisions */
+     , (40860,  13, False) /* Ethereal */
+     , (40860,  14, True ) /* GravityStatus */
+     , (40860,  19, True ) /* Attackable */
+     , (40860,  29, True ) /* NoCorpse */
+     , (40860,  50, True ) /* NeverFailCasting */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (40860,   1,       5) /* HeartbeatInterval */
+     , (40860,   2,       0) /* HeartbeatTimestamp */
+     , (40860,   3,     400) /* HealthRate */
+     , (40860,   4,       5) /* StaminaRate */
+     , (40860,   5,       1) /* ManaRate */
+     , (40860,  13,       1) /* ArmorModVsSlash */
+     , (40860,  14,       1) /* ArmorModVsPierce */
+     , (40860,  15,       1) /* ArmorModVsBludgeon */
+     , (40860,  16,       1) /* ArmorModVsCold */
+     , (40860,  17,       1) /* ArmorModVsFire */
+     , (40860,  18,       1) /* ArmorModVsAcid */
+     , (40860,  19,       1) /* ArmorModVsElectric */
+     , (40860,  31,     0.3) /* VisualAwarenessRange */
+     , (40860,  34,       1) /* PowerupTime */
+     , (40860,  36,       1) /* ChargeSpeed */
+     , (40860,  39,     0.4) /* DefaultScale */
+     , (40860,  41,     300) /* RegenerationInterval */
+     , (40860,  43,      15) /* GeneratorRadius */     
+     , (40860,  64,     0.3) /* ResistSlash */
+     , (40860,  65,     0.3) /* ResistPierce */
+     , (40860,  66,    0.75) /* ResistBludgeon */
+     , (40860,  67,     0.3) /* ResistFire */
+     , (40860,  68,     0.3) /* ResistCold */
+     , (40860,  69,     0.3) /* ResistAcid */
+     , (40860,  70,     0.3) /* ResistElectric */
+     , (40860,  71,       1) /* ResistHealthBoost */
+     , (40860,  72,       1) /* ResistStaminaDrain */
+     , (40860,  73,       1) /* ResistStaminaBoost */
+     , (40860,  74,       1) /* ResistManaDrain */
+     , (40860,  75,       1) /* ResistManaBoost */
+     , (40860, 104,      10) /* ObviousRadarRange */
+     , (40860, 125,       1) /* ResistHealthDrain */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (40860,   1, 'Crystalline Array Satellite') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (40860,   1,   33558690) /* Setup */
+     , (40860,   2,  150995290) /* MotionTable */
+     , (40860,   3,  536871001) /* SoundTable */
+     , (40860,   6,   67113876) /* PaletteBase */
+     , (40860,   8,  100676420) /* Icon */
+     , (40860,  22,  872415389) /* PhysicsEffectTable */;
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (40860,   1, 100, 0, 0) /* Strength */
+     , (40860,   2, 100, 0, 0) /* Endurance */
+     , (40860,   3, 100, 0, 0) /* Quickness */
+     , (40860,   4, 100, 0, 0) /* Coordination */
+     , (40860,   5, 600, 0, 0) /* Focus */
+     , (40860,   6, 600, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (40860,   1,   950, 0, 0, 1000) /* MaxHealth */
+     , (40860,   3,  1000, 0, 0, 1100) /* MaxStamina */
+     , (40860,   5, 30000, 0, 0, 30600) /* MaxMana */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (40860, 15, 0, 3, 0, 275, 0, 0)/* MagicDefense        Specialized */
+     , (40860, 45, 0, 3, 0, 180, 0, 0) /* LightWeapons        Specialized */
+     , (40860, 34, 0, 3, 0, 100, 0, 0) /* WarMagic            Specialized */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (40860,  0,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (40860,  1,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (40860,  2,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (40860,  3,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (40860,  4,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (40860,  5,  4,  1, 0.75,  250,  250,  250,  250,  250,  250,  250,  250,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (40860,  6,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (40860,  7,  4,  0,    0,  250,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (40860,  8,  4,  1, 0.75,  250,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+
+INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
+VALUES (40860,    73,   3)  /* Frost V */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (40860, -1, 40864, 10, 8, 8, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Wisps - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
