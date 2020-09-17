1a6e5d988a729a6a22ea4ecf6219ef09d661b4ab
Patrick <phreekn.slacker@gmail.com>
2020-09-17 07:57:33 -0500

    Update 40094 Crystal.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40094 Crystal.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40094 Crystal.sql
@@ -13,7 +13,9 @@ VALUES (40094,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (40094,   1, True ) /* Stuck */
-     , (40094,  19, False) /* Attackable */;
+     , (40094,  19, False) /* Attackable */
+     , (40094,  52, True) /* AIImmobile */
+     , (40094,  83, True) /* NPCLookLike */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (40094,  54,       3) /* UseRadius */;
