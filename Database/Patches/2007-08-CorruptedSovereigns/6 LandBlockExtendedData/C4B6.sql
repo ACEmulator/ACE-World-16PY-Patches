35d993f17c1070f88e4c646e0bbc39b5e8614ca3
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-09-16 21:28:56 -0400

    landblock fixes

--- Database/Patches/2007-08-CorruptedSovereigns/6 LandBlockExtendedData/C4B6.sql
+++ Database/Patches/2007-08-CorruptedSovereigns/6 LandBlockExtendedData/C4B6.sql
@@ -5,8 +5,8 @@ VALUES (2085314565, 35826, 3300262145, 12.5558, 32.4525, 317.705, -0.6820549, 0,
 /* Tomihino, TYPE_CREATURE, Seen 77 times at this location. 0xc4b60101 12.5558 32.4525 317.705 -0.6820549 0 0 0.731301 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2085324519, 36378, 3300261890, 17.321, 42.2799, 318.005, 0.3309373, 0, 0, -0.9436527, False, '2020-08-17 00:00:00');
-/* Hisham al-Evv, TYPE_CREATURE, Seen 3 times at this location. 0xc4b60002 17.321 42.2799 318.005 0.3309373 0 0 -0.9436527 */
+VALUES (0x7C4B6000, 36378, 0xC4B60002, 17.321, 42.2799, 318.005, -0.601093, 0, 0, -0.79918, False, '2020-03-10 00:31:40'); /* Hisham al-Evv */
+/* @teleloc 0xC4B60002 [17.320999 42.279900 318.005005] -0.601093 0.000000 0.000000 -0.799180 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (2085324527, 35909, 3300261890, 21.4279, 30.8856, 318.005, 0.4097927, 0, 0, -0.9121787, False, '2020-08-17 00:00:00');
