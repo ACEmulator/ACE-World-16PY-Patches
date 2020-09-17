d96fd15cff10381228244be163dc84af7ead3f8d
Patrick <phreekn.slacker@gmail.com>
2020-09-17 07:58:07 -0500

    Update 40115 Crystal.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40115 Crystal.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Crystal/40115 Crystal.sql
@@ -13,7 +13,9 @@ VALUES (40115,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (40115,   1, True ) /* Stuck */
-     , (40115,  19, False) /* Attackable */;
+     , (40115,  19, False) /* Attackable */
+     , (40115,  52, True) /* AIImmobile */
+     , (40115,  83, True) /* NPCLookLike */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (40115,  54,       3) /* UseRadius */;
