61b2af10733d793d70b7186c623baa8be2cd9891
Patrick <phreekn.slacker@gmail.com>
2020-09-18 17:06:15 -0500

    Update 40902 Essence Lock.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Generic/Misc/40902 Essence Lock.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Generic/Misc/40902 Essence Lock.sql
@@ -12,7 +12,6 @@ VALUES (40902,   1,        128) /* ItemType - Misc */
      , (40902,  19,          0) /* Value */
      , (40902,  81,          1) /* MaxGeneratedObjects */
      , (40902,  82,          0) /* InitGeneratedObjects */
-     , (40902,  83,       2048) /* ActivationResponse - Emote */
      , (40902,  93,    6292508) /* PhysicsState - Ethereal, ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
      , (40902, 119,          1) /* Active */;
 
@@ -40,10 +39,10 @@ VALUES (40902, 7 /* Use */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id, 0, 30 /* InqQuestSolves */, 0, 1, NULL, 'EssenceLockUsed@4', NULL, 4, 2147483647, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id, 0, 21 /* InqQuest */, 0, 1, NULL, 'EssenceLockUsed', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (40902, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'EssenceLockUsed@4', NULL, NULL, NULL);
+VALUES (40902, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'EssenceLockUsed', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -54,7 +53,7 @@ VALUES (@parent_id, 0, 72 /* Generate */, 0, 1, NULL, NULL, NULL, NULL, NULL, NU
      , (@parent_id, 3, 31 /* EraseQuest */, 0, 1, NULL, 'EssenceLockUsed', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (40902, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'EssenceLockUsed@4', NULL, NULL, NULL);
+VALUES (40902, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'EssenceLockUsed', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
