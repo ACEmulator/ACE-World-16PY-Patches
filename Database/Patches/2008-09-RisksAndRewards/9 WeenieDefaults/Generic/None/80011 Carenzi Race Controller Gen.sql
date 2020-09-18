173fd0605f8163efa5edf0d7d4dc1afeb65a5d71
Ty Conner <tyconner@itcproductions.com>
2020-09-18 01:25:45 -0400

    Update 80011 Carenzi Race Controller Gen.sql

--- Database/Patches/2008-09-RisksAndRewards/9 WeenieDefaults/Generic/None/80011 Carenzi Race Controller Gen.sql
+++ Database/Patches/2008-09-RisksAndRewards/9 WeenieDefaults/Generic/None/80011 Carenzi Race Controller Gen.sql
@@ -29,5 +29,5 @@ VALUES (80011,   1,   33555051) /* Setup */
      , (80011,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (80011, 1, 80015, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Carenzi Race Controller (80015) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */
-     , (80011, 1, 80007, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, -2, 0, 1, 0, 0, 0) /* Generate Landblock KeepAlive (80007) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
+VALUES (80011, -1, 80015, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Carenzi Race Controller (80015) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */
+     , (80011, -1, 80007, 1, 1, 1, 1, 4, -1, 0, 0, 0, 0, -2, 0, 1, 0, 0, 0) /* Generate Landblock KeepAlive (80007) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
