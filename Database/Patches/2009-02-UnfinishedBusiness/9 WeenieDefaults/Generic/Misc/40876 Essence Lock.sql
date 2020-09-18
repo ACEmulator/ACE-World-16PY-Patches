d0d8fe2312720d84e1bbf51a6c90680ffaed9815
Patrick <phreekn.slacker@gmail.com>
2020-09-18 08:02:34 -0500

    Update 40876 Essence Lock.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Generic/Misc/40876 Essence Lock.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Generic/Misc/40876 Essence Lock.sql
@@ -30,10 +30,10 @@ VALUES (40876, 7 /* Use */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id, 0, 30 /* InqQuestSolves */, 0, 1, NULL, 'EssenceLockUsed@3', NULL, 3, 2147483647, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id, 0, 30 /* InqQuestSolves */, 0, 1, NULL, 'EssenceLockUsed@4', NULL, 4, 2147483647, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (40876, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'EssenceLockUsed@3', NULL, NULL, NULL);
+VALUES (40876, 12 /* QuestSuccess */, 1, NULL, NULL, NULL, 'EssenceLockUsed@4', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -46,7 +46,7 @@ VALUES (@parent_id, 0, 18 /* DirectBroadcast */, 0, 1, NULL, 'As you touch the E
      , (@parent_id, 5, 77 /* DeleteSelf */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (40876, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'EssenceLockUsed@3', NULL, NULL, NULL);
+VALUES (40876, 13 /* QuestFailure */, 1, NULL, NULL, NULL, 'EssenceLockUsed@4', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
