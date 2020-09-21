2714cb36d83f507c96c5c614ac0c20250d41fc8f
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-09-21 02:22:53 -0400

    Update from 13days to 20hrs, move to correct folder structure

--- Database/Patches/2006-04-ShiningRunesAndShadowedHands/8 QuestDefDB/RabbitHutchKeyPickupTimer.sql
+++ Database/Patches/2006-04-ShiningRunesAndShadowedHands/8 QuestDefDB/RabbitHutchKeyPickupTimer.sql
@@ -1,4 +1,4 @@
 DELETE FROM quest WHERE name = 'rabbithutchkey';
 
 INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
-VALUES ('rabbithutchkey', 1123200, -1, 'Picked up Rabbit Hutch Key', '2019-03-14 20:34:27');
+VALUES ('rabbithutchkey', 72000, -1, 'Picked up Rabbit Hutch Key', '2020-09-21 20:34:27');
