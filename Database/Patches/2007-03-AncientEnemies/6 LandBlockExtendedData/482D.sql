586026bcf12c851d6e2d26f52218f68a6c3f22b7
Patrick <phreekn.slacker@gmail.com>
2020-10-02 17:52:32 -0500

    Update 482D.sql

--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/482D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/482D.sql
@@ -5,7 +5,7 @@ VALUES (0x7482D000, 80055, 0x482D0024, 115.04, 77.595, 6.006, -0.92388, 0, 0, -0
 /* @teleloc 0x482D0024 [115.040001 77.595001 6.006000] -0.923880 0.000000 0.000000 -0.382684 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7482D001, 35109, 0x482D001F, 79.0399, 158.962, 6.0065, 0.985526, 0, 0, -0.169524, False, '2020-03-30 22:26:08'); /* Apep-tek the Tolerant */
+VALUES (0x7482D901, 35109, 0x482D001F, 79.0399, 158.962, 6.0065, 0.985526, 0, 0, -0.169524, False, '2020-03-30 22:26:08'); /* Apep-tek the Tolerant */
 /* @teleloc 0x482D001F [79.039902 158.962006 6.006500] 0.985526 0.000000 0.000000 -0.169524 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
