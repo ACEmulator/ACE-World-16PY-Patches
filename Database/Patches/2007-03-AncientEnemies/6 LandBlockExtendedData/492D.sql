a238a1bac693d30f52fe7bf1ac11a6b1b4f785b3
Patrick <phreekn.slacker@gmail.com>
2020-10-02 20:36:40 -0500

    Update 492D.sql

--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
@@ -1,11 +1,15 @@
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D100, 36236, 0x492D0017, 64.1195, 155.677, 29.5805, 0.678557, 0, 0, 0.734548, False, '2020-10-01 13:30:20'); /* Lo Shoen */
+VALUES (0x7492D000, 35488, 0x492D0017, 53.7138, 152.896, 29.58046, -0.509899, 0, 0, -0.860234, False, '2020-03-08 04:03:33'); /* Skettek the Wanderer */
+/* @teleloc 0x492D0017 [53.713799 152.895996 29.580460] -0.509899 0.000000 0.000000 -0.860234 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7492D003, 36236, 0x492D0017, 64.1195, 155.677, 29.5805, 0.678557, 0, 0, 0.734548, False, '2020-10-01 13:30:20'); /* Lo Shoen */
 /* @teleloc 0x492D0017 [64.119499 155.677002 29.580500] 0.678557 0.000000 0.000000 0.734548 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D101, 52310, 0x492D0017, 55.6993, 161.671, 29.58046, -0.7358512, 0, 0, -0.6771432,  False, '2019-02-10 00:00:00'); /* Slubbley */
+VALUES (0x7492D004, 52310, 0x492D0017, 55.6993, 161.671, 29.58046, -0.7358512, 0, 0, -0.6771432,  False, '2019-02-10 00:00:00'); /* Slubbley */
 /* @teleloc 0x492D0017 [55.699300 161.671000 29.580460] -0.735851 0.000000 0.000000 -0.677143 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D102, 40922, 0x492D0017, 52.5783, 156.265, 29.58046, 0.7071068, 0, 0, -0.7071068,  False, '2019-02-10 00:00:00'); /* Sharia Blackmist */
+VALUES (0x7492D005, 40922, 0x492D0017, 52.5783, 156.265, 29.58046, 0.7071068, 0, 0, -0.7071068,  False, '2019-02-10 00:00:00'); /* Sharia Blackmist */
 /* @teleloc 0x492D0017 [52.578300 156.265000 29.580460] 0.707107 0.000000 0.000000 -0.707107 */
