b4497d5ed619939308f4e52e1d93707951e55ed8
Patrick <phreekn.slacker@gmail.com>
2020-10-02 22:55:30 -0500

    Update 482D.sql

--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/482D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/482D.sql
@@ -1,5 +1,3 @@
-DELETE FROM `landblock_instance` WHERE `landblock` = 0x482D;
-
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x7482D000, 80055, 0x482D0024, 115.04, 77.595, 6.006, -0.92388, 0, 0, -0.382684, False, '2020-03-30 20:18:44'); /* GY Nighttime NPC Generator */
 /* @teleloc 0x482D0024 [115.040001 77.595001 6.006000] -0.923880 0.000000 0.000000 -0.382684 */
