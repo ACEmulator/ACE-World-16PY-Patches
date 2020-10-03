4a3532254541a4083e16975752875dd228f49c73
Patrick <phreekn.slacker@gmail.com>
2020-10-02 20:07:48 -0500

    Update 492D.sql

--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
@@ -1,25 +1,25 @@
 DELETE FROM `landblock_instance` WHERE `landblock` = 0x492D;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D000, 35488, 0x492D0017, 53.7138, 152.896, 29.58046, -0.5098987, 0, 0, -0.8602344, False, '2019-02-10 00:00:00'); /* Skettek the Wanderer */
+VALUES (0x7492D003, 35488, 0x492D0017, 53.7138, 152.896, 29.58046, -0.5098987, 0, 0, -0.8602344, False, '2019-02-10 00:00:00'); /* Skettek the Wanderer */
 /* @teleloc 0x492D0017 [53.713800 152.896000 29.580460] -0.509899 0.000000 0.000000 -0.860234 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D501, 40919, 0x492D0017, 59.6104, 156.702, 4.02797, -0.468859, 0, 0, -0.883273, False, '2020-03-08 11:34:23'); /* Up to Arcanum Lookout */
+VALUES (0x7492D004, 40919, 0x492D0017, 59.6104, 156.702, 4.02797, -0.468859, 0, 0, -0.883273, False, '2020-03-08 11:34:23'); /* Up to Arcanum Lookout */
 /* @teleloc 0x492D0017 [59.610401 156.701996 4.027970] -0.468859 0.000000 0.000000 -0.883273 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D002, 40920, 0x492D0017, 60.0659, 148.216, 29.5125, 0.029407, 0, 0, 0.999568, False, '2020-03-08 11:36:48'); /* Down to Surface */
+VALUES (0x7492D005, 40920, 0x492D0017, 60.0659, 148.216, 29.5125, 0.029407, 0, 0, 0.999568, False, '2020-03-08 11:36:48'); /* Down to Surface */
 /* @teleloc 0x492D0017 [60.065899 148.216003 29.512501] 0.029407 0.000000 0.000000 0.999568 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D003, 36236, 0x492D0017, 64.1195, 155.677, 29.5805, 0.678557, 0, 0, 0.734548, False, '2020-10-01 13:30:20'); /* Lo Shoen */
+VALUES (0x7492D006, 36236, 0x492D0017, 64.1195, 155.677, 29.5805, 0.678557, 0, 0, 0.734548, False, '2020-10-01 13:30:20'); /* Lo Shoen */
 /* @teleloc 0x492D0017 [64.119499 155.677002 29.580500] 0.678557 0.000000 0.000000 0.734548 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D047, 52310, 0x492D0017, 55.6993, 161.671, 29.58046, -0.7358512, 0, 0, -0.6771432,  True, '2019-02-10 00:00:00'); /* Slubbley */
+VALUES (0x7492D007, 52310, 0x492D0017, 55.6993, 161.671, 29.58046, -0.7358512, 0, 0, -0.6771432,  False, '2019-02-10 00:00:00'); /* Slubbley */
 /* @teleloc 0x492D0017 [55.699300 161.671000 29.580460] -0.735851 0.000000 0.000000 -0.677143 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D049, 40922, 0x492D0017, 52.5783, 156.265, 29.58046, 0.7071068, 0, 0, -0.7071068,  True, '2019-02-10 00:00:00'); /* Sharia Blackmist */
+VALUES (0x7492D008, 40922, 0x492D0017, 52.5783, 156.265, 29.58046, 0.7071068, 0, 0, -0.7071068,  False, '2019-02-10 00:00:00'); /* Sharia Blackmist */
 /* @teleloc 0x492D0017 [52.578300 156.265000 29.580460] 0.707107 0.000000 0.000000 -0.707107 */
