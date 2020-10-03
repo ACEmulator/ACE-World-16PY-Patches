6527267d6bef9487b39168e06a34b4e24bc21cf5
Patrick <phreekn.slacker@gmail.com>
2020-10-02 19:57:11 -0500

    Changed guid in 492D Landblock causing error

---
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/482D.sql
@@ -0,0 +1,57 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x482D;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D000, 80055, 0x482D0024, 115.04, 77.595, 6.006, -0.92388, 0, 0, -0.382684, False, '2020-03-30 20:18:44'); /* GY Nighttime NPC Generator */
+/* @teleloc 0x482D0024 [115.040001 77.595001 6.006000] -0.923880 0.000000 0.000000 -0.382684 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D001, 35109, 0x482D001F, 79.0399, 158.962, 6.0065, 0.985526, 0, 0, -0.169524, False, '2020-03-30 22:26:08'); /* Apep-tek the Tolerant */
+/* @teleloc 0x482D001F [79.039902 158.962006 6.006500] 0.985526 0.000000 0.000000 -0.169524 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D040, 37176, 0x482D0126, 108, 104, -0.345, 1, 0, 0, 0, False, '2019-02-10 00:00:00'); /* Ancient Chest */
+/* @teleloc 0x482D0126 [108.000000 104.000000 -0.345000] 1.000000 0.000000 0.000000 0.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D045, 38729, 0x482D003E, 180, 136, 5.937, 0.92388, 0, 0, -0.382683, False, '2019-02-10 00:00:00'); /* Massilor's Crypt */
+/* @teleloc 0x482D003E [180.000000 136.000000 5.937000] 0.923880 0.000000 0.000000 -0.382683 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D04B, 43842, 0x482D0121, 57.0322, 81.4979, -0.345, -0.00583696, 0, 0, -0.999983, False, '2019-02-10 00:00:00'); /* Enchanted Mnemosyne */
+/* @teleloc 0x482D0121 [57.032200 81.497902 -0.345000] -0.005837 0.000000 0.000000 -0.999983 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D068, 38025, 0x482D002A, 133.109, 43.1089, 2.49999, 0.966001, 0, 0, -0.258539, False, '2019-02-10 00:00:00'); /* Summoning Stone */
+/* @teleloc 0x482D002A [133.108994 43.108898 2.499990] 0.966001 0.000000 0.000000 -0.258539 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D099, 38713, 0x482D0025, 108.637, 110.385, 6.00825, -0.695391, 0, 0, -0.718632, False, '2019-02-10 00:00:00'); /* Shambling Archivist */
+/* @teleloc 0x482D0025 [108.637001 110.385002 6.008250] -0.695391 0.000000 0.000000 -0.718632 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D475, 45754, 0x482D011C, 92.2317, 114.743, -9.595, -0.90099, 0, 0, -0.43384, False, '2019-02-10 00:00:00'); /* Mhoire Soldier  */
+/* @teleloc 0x482D011C [92.231697 114.742996 -9.595000] -0.900990 0.000000 0.000000 -0.433840 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D476, 45685, 0x482D011C, 91.8149, 122.066, -9.595, 0.376999, 0, 0, 0.926214, False, '2019-02-10 00:00:00'); /* Mhoire Soldier  */
+/* @teleloc 0x482D011C [91.814903 122.066002 -9.595000] 0.376999 0.000000 0.000000 0.926214 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D479, 51957, 0x482D011F, 91.9749, 122.991, -14.795, -0.890146, 0, 0, -0.455675, False, '2019-02-10 00:00:00'); /* Yoite */
+/* @teleloc 0x482D011F [91.974899 122.990997 -14.795000] -0.890146 0.000000 0.000000 -0.455675 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D47A, 51958, 0x482D011F, 91.7184, 119.175, -14.795, -0.707107, 0, 0, -0.707107, False, '2019-02-10 00:00:00'); /* Gadi */
+/* @teleloc 0x482D011F [91.718399 119.175003 -14.795000] -0.707107 0.000000 0.000000 -0.707107 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D47B, 51956, 0x482D011F, 91.731, 116.3, -14.795, -0.707107, 0, 0, -0.707107, False, '2019-02-10 00:00:00'); /* Aethelswith */
+/* @teleloc 0x482D011F [91.731003 116.300003 -14.795000] -0.707107 0.000000 0.000000 -0.707107 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D47C, 51955, 0x482D011F, 91.923, 126.347, -14.795, 1, 0, 0, 0, False, '2019-02-10 00:00:00'); /* Renata */
+/* @teleloc 0x482D011F [91.922997 126.347000 -14.795000] 1.000000 0.000000 0.000000 0.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7482D47F, 37146, 0x482D0008, 20.6594, 172.587, 6.38725, 0.975614, 0, 0, -0.219495, False, '2019-02-10 00:00:00'); /* Laylana */
+/* @teleloc 0x482D0008 [20.659401 172.587006 6.387250] 0.975614 0.000000 0.000000 -0.219495 */
--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
@@ -5,7 +5,7 @@ VALUES (0x7492D000, 35488, 0x492D0017, 53.7138, 152.896, 29.58046, -0.5098987, 0
 /* @teleloc 0x492D0017 [53.713800 152.896000 29.580460] -0.509899 0.000000 0.000000 -0.860234 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D001, 40919, 0x492D0017, 59.6104, 156.702, 4.02797, -0.468859, 0, 0, -0.883273, False, '2020-03-08 11:34:23'); /* Up to Arcanum Lookout */
+VALUES (0x7492D501, 40919, 0x492D0017, 59.6104, 156.702, 4.02797, -0.468859, 0, 0, -0.883273, False, '2020-03-08 11:34:23'); /* Up to Arcanum Lookout */
 /* @teleloc 0x492D0017 [59.610401 156.701996 4.027970] -0.468859 0.000000 0.000000 -0.883273 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
