c4e5838d3941af5ace1ccb688fea376fc0279705
Patrick <phreekn.slacker@gmail.com>
2020-10-06 09:34:40 -0500

    Updated Spell Destination and Portal 40104

---
+++ Database/Patches/2009-02-UnfinishedBusiness/2 SpellTableExtendedData/4954.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell` WHERE `id` = 4954;
+
+INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
+VALUES (4954, 'Crystalline Portal', 829227066, 180, 36, 290.005005, -0.438747, 0, 0, 0.898611, '2019-03-18 09:00:00');
+/* @teleloc 0x316D003A [180.000000 36.000000 290.005005] -0.438747 0.000000 0.000000 0.898611 */
--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Unsorted/40104 Resonant Portal.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Unsorted/40104 Resonant Portal.sql
@@ -53,7 +53,7 @@ VALUES (40104, 22 /* TestSuccess */, 1, NULL, NULL, NULL, 'Level_50-999', NULL, 
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id, 0, 99 /* TeleportTarget */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0x316D003A /* 0x316D003A [180 36 290.005] -0.438747 0 0 0.898611 */, 180, 36, 290.005, -0.438747, 0, 0, 0.898611);
+VALUES (@parent_id, 0, 19 /* CastSpellInstant */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4954 /* Crystalline Portal */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (40104, 23 /* TestFailure */, 1, NULL, NULL, NULL, 'Level_50-999', NULL, NULL, NULL);
