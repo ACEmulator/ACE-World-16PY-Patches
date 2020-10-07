afa26ad1d0c3e63c3de60a65fe84859b2da472d2
Patrick <phreekn.slacker@gmail.com>
2020-10-07 13:02:24 -0500

    Fix to Tokashi to accept 20 Shards per timer

---
+++ Database/Patches/2009-02-UnfinishedBusiness/8 QuestDefDB/CrystallineShardTurnIn.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'crystallineshardturnin';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('crystallineshardturnin', 0, 20, 'Turned in Crystalline Shard', '2019-03-14 20:34:27');
