ee25e259804d652bef8494b5f2494320150485b2
Patrick <phreekn.slacker@gmail.com>
2020-10-08 16:19:16 -0500

    Update to Array and 5 minute timer before Fragments expire

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Generic/None/71868 Crystalline Array Gen Generator.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Generic/None/71868 Crystalline Array Gen Generator.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 71868;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (71868, 'generatorcrystallinearrayfragment', 1, '2019-06-23 00:00:00') /* Generic */;
+VALUES (71868, 'generatorcrystallinearraygen', 1, '2019-06-23 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (71868,  81,          4) /* MaxGeneratedObjects */
@@ -29,4 +29,4 @@ VALUES (71868,   1,   33555051) /* Setup */
      , (71868,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (71868, 1, 71867, 1, 4, 4, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Crystalline Array Fragment - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (71868, 1, 71869, 1, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Crystalline Array Fragment - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
