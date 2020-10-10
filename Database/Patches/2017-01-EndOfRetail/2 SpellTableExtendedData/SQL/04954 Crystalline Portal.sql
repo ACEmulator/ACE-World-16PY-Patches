ce7b280515484ca953d894b5235ce4c0eca701a4
Patrick <phreekn.slacker@gmail.com>
2020-10-10 09:03:50 -0500

    Moved and Renamed Spell Data

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04954 Crystalline Portal.sql
+++ Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04954 Crystalline Portal.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell` WHERE `id` = 4954;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
-VALUES (4954, 'Crystalline Portal', 23069349, 80, -160, 0.005, 1, 0, 0, 0, '2019-03-18 09:00:00');
-/* @teleloc 0x016002A5 [80.000000 -160.000000 0.005000] 1.000000 0.000000 0.000000 0.000000 */
+VALUES (4954, 'Crystalline Portal', 829227066, 180, 36, 290.005005, -0.438747, 0, 0, 0.898611, '2019-03-18 09:00:00');
+/* @teleloc 0x316D003A [180.000000 36.000000 290.005005] -0.438747 0.000000 0.000000 0.898611 */
