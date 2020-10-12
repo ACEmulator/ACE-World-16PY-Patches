c3a7258319da9a2da974304c505903d87a2dd72f
Patrick <phreekn.slacker@gmail.com>
2020-10-12 14:44:17 -0500

    Update to 492D

---
+++ Database/Patches/2007-09-DarkMaterials/6 LandBlockExtendedData/492D.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7492D003, 36236, 0x492D0017, 64.1195, 155.677, 29.5805, 0.678557, 0, 0, 0.734548, False, '2020-10-01 13:30:20'); /* Lo Shoen */
+/* @teleloc 0x492D0017 [64.119499 155.677002 29.580500] 0.678557 0.000000 0.000000 0.734548 */
