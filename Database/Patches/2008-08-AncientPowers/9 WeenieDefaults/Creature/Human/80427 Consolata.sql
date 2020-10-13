9f035f65b9958c8e00c384222aae9a54764f2a66
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-10-13 14:29:28 -0400

    Fix botched commit
    Screwed up black market mob clothing on that push.
    
    Fixing.

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/80427 Consolata.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Creature/Human/80427 Consolata.sql
@@ -101,7 +101,6 @@ VALUES (80427,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
      , (80427,  8,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
-VALUES (80427, 2,  2595,  0, 9, 1, False) /* Create Tunic (2595) for Wield */
-     , (80427, 2,  2598,  0, 9, 1, False) /* Create Pants (2598) for Wield */
-     , (80427, 2,  28615,  0, 2, 0.5, False) /* Create Vestiri Robe (28615) for Wield */
-     , (80427, 2,   121,  0, 93, 0, False) /* Create Gloves (121) for Wield */;
+VALUES (80427, 2,  25639,  0, 2, 0, False) /* Create Leather Jerkin (25639) for Wield */
+     , (80427, 2,  25652,  0, 9, 0, False) /* Create Leather Tassets (25652) for Wield */
+     , (80427, 2,   7897,  0, 9, 0, False) /* Create Steel Toed Boots (7897) for Wield */;
