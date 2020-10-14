d44924cc4b317dd70e5b36693a60d62c34cef5c7
Ty Conner <tyconner@itcproductions.com>
2020-10-14 12:58:27 -0400

    Create FreebooterAdvancementWait.sql

---
+++ Database/Patches/2008-08-AncientPowers/8 QuestDefDB/FreebooterAdvancementWait.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'FreebooterAdvancementWait';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('FreebooterAdvancementWait',  72000, -1, 'Cooldown for Freebooter Advancement Gem', '2020-03-31 20:34:27');
