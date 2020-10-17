47921717d89513dad8c1b2eda56b89da42e0bb37
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-10-17 12:57:12 -0400

    Fix MC High Priest Quest Flag
    Had a typo on a questflag name.

--- Database/Patches/2008-08-AncientPowers/8 QuestDefDB/HighPriestFirsteDead.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'HighPriestFirsteDead';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('HighPriestFirsteDead', 0, 1, 'HighPriestFirsteDead', '2020-08-14 18:51:50');
-
