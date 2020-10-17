c3937406c647ec91d0cabb9e9cf538e8ea30bba4
Ty Conner <tyconner@itcproductions.com>
2020-10-17 14:35:01 -0400

    Update 80067 Bloodstone Crystal Stockpile Gen.sql

--- Database/Patches/2010-11-FromDarknessLight/9 WeenieDefaults/Creature/Crystal/80067 Bloodstone Crystal Stockpile Gen.sql
+++ Database/Patches/2010-11-FromDarknessLight/9 WeenieDefaults/Creature/Crystal/80067 Bloodstone Crystal Stockpile Gen.sql
@@ -29,4 +29,4 @@ VALUES (80067,   1,   33555051) /* Setup */
      , (80067,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (80067, -1, 70365, 1200, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Bloodstone Crystal Stockpile (70365) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
+VALUES (80067, -1, 70365, 60, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Bloodstone Crystal Stockpile (70365) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
