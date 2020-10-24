b9e1933b61c716b6420df780072d466107629c76
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-10-24 16:03:41 -0400

    Update F939.sql (#678)
    * Update F939.sql
    
    Fix Z height issue with master statue.
    
    * Update F939.sql
    
    * Update 38370 Statue of Transport.sql
    
    Co-authored-by: Ty Conner <tyconner@itcproductions.com>

--- Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/F939.sql
+++ Database/Patches/2008-08-AncientPowers/6 LandBlockExtendedData/F939.sql
@@ -1,7 +1,12 @@
-  
 DELETE FROM `landblock_instance` WHERE `landblock` = 0xF939;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7F939002, 38370, 0xF9390027, 108.317, 153.149, 127.3845, -0.707107, 0, 0, -0.707107,  False, '2019-02-10 00:00:00'); /* Statue of Transport */
-/* @teleloc 0xF9390027 [108.317000 153.149000 127.384500] -0.707107 0.000000 0.000000 -0.707107 */
+VALUES (0x7F939001,  1154, 0xF9390027, 108.317, 153.149, 127.3945, -0.707107, 0, 0, -0.707107, False, '2019-02-10 00:00:00'); /* Linkable Monster Generator */
+/* @teleloc 0xF9390027 [108.317000 153.149000 127.394500] -0.707107 0.000000 0.000000 -0.707107 */
 
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x7F939001, 0x7F939003, '2019-02-10 00:00:00') /* Statue of Transport (38370) */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7F939003, 38370, 0xF9390027, 108.317, 153.149, 127.3945, -0.707107, 0, 0, -0.707107,  True, '2019-02-10 00:00:00'); /* Statue of Transport */
+/* @teleloc 0xF9390027 [108.317000 153.149000 127.394500] -0.707107 0.000000 0.000000 -0.707107 */
