e2bf990b934cf78047f3e15bbe85482468471ab0
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-10-24 00:45:29 -0400

    level 150

--- Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/PetDevice/49300 Fire K'nath Essence (150).sql
+++ Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/PetDevice/49300 Fire K'nath Essence (150).sql
@@ -1,16 +1,15 @@
 DELETE FROM `weenie` WHERE `class_Id` = 49300;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (49300, 'ace49300-fireknathessence150', 70, '2019-12-07 05:48:22') /* PetDevice */;
+VALUES (49300, 'ace49300-fireknathessence150', 70, '2020-10-23 05:48:22') /* PetDevice */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (49300,   1,        128) /* ItemType - Misc */
      , (49300,   5,         50) /* EncumbranceVal */
      , (49300,  16,          8) /* ItemUseable - Contained */
      , (49300,  18,         32) /* UiEffects - Fire */
-     , (49300,  19,       4000) /* Value */
+     , (49300,  19,       8000) /* Value */
      , (49300,  33,          0) /* Bonded - Normal */
-     , (49300,  65,        101) /* Placement - Resting */
      , (49300,  91,         50) /* MaxStructure */
      , (49300,  92,         50) /* Structure */
      , (49300,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
@@ -25,16 +24,11 @@ VALUES (49300,   1,        128) /* ItemType - Misc */
      , (49300, 369,        140) /* UseRequiresLevel */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (49300,   1, False) /* Stuck */
-     , (49300,  11, True ) /* IgnoreCollisions */
-     , (49300,  13, True ) /* Ethereal */
-     , (49300,  14, True ) /* GravityStatus */
-     , (49300,  19, True ) /* Attackable */
-     , (49300,  22, True ) /* Inscribable */
+VALUES (49300,  22, True ) /* Inscribable */
      , (49300,  69, True ) /* IsSellable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (49300,  39, 0.400000005960464) /* DefaultScale */
+VALUES (49300,  39,     0.4) /* DefaultScale */
      , (49300, 167,      45) /* CooldownDuration */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
--- Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/PetDevice/49349 Lightning Moar Essence (150).sql
+++ Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/PetDevice/49349 Lightning Moar Essence (150).sql
@@ -1,16 +1,15 @@
 DELETE FROM `weenie` WHERE `class_Id` = 49349;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (49349, 'ace49349-lightningmoaressence150', 70, '2019-12-07 05:48:23') /* PetDevice */;
+VALUES (49349, 'ace49349-lightningmoaressence150', 70, '2020-10-23 05:48:23') /* PetDevice */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (49349,   1,        128) /* ItemType - Misc */
      , (49349,   5,         50) /* EncumbranceVal */
      , (49349,  16,          8) /* ItemUseable - Contained */
      , (49349,  18,         64) /* UiEffects - Lightning */
-     , (49349,  19,       4000) /* Value */
+     , (49349,  19,       8000) /* Value */
      , (49349,  33,          0) /* Bonded - Normal */
-     , (49349,  65,        101) /* Placement - Resting */
      , (49349,  91,         50) /* MaxStructure */
      , (49349,  92,         50) /* Structure */
      , (49349,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
@@ -25,16 +24,11 @@ VALUES (49349,   1,        128) /* ItemType - Misc */
      , (49349, 369,        140) /* UseRequiresLevel */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (49349,   1, False) /* Stuck */
-     , (49349,  11, True ) /* IgnoreCollisions */
-     , (49349,  13, True ) /* Ethereal */
-     , (49349,  14, True ) /* GravityStatus */
-     , (49349,  19, True ) /* Attackable */
-     , (49349,  22, True ) /* Inscribable */
+VALUES (49349,  22, True ) /* Inscribable */
      , (49349,  69, True ) /* IsSellable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (49349,  39, 0.400000005960464) /* DefaultScale */
+VALUES (49349,  39,     0.4) /* DefaultScale */
      , (49349, 167,      45) /* CooldownDuration */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
