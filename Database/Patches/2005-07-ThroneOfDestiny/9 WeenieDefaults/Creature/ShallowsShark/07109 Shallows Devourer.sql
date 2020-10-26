253e76e720755ba4771e312a74808f94447a804d
sapphire-knight <69483793+sapphire-knight@users.noreply.github.com>
2020-10-26 13:51:48 -0400

    Update 07109 Shallows Devourer.sql

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/ShallowsShark/07109 Shallows Devourer.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/Creature/ShallowsShark/07109 Shallows Devourer.sql
@@ -91,7 +91,8 @@ VALUES (7109,  6, 0, 3, 0, 100, 0, 0) /* MeleeDefense        Specialized */
      , (7109, 15, 0, 3, 0, 115, 0, 0) /* MagicDefense        Specialized */
      , (7109, 20, 0, 3, 0,  60, 0, 0) /* Deception           Specialized */
      , (7109, 22, 0, 3, 0,  25, 0, 0) /* Jump                Specialized */
-     , (7109, 24, 0, 3, 0,  30, 0, 0) /* Run                 Specialized */;
+     , (7109, 24, 0, 3, 0,  30, 0, 0) /* Run                 Specialized */
+     , (7109, 45, 0, 3, 0, 110, 0, 0) /* LightWeapons        Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (7109,  0,  2, 60, 0.75,  175,  140,  158,  140,  107,  175,  124,  140,    0, 2,  0.4,  0.1,    0,  0.4,  0.1,    0,    0,    0,    0,    0,    0,    0) /* Head */
@@ -167,4 +168,3 @@ INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `w
 VALUES (7109, 9,  6876,  0, 0, 0.01, False) /* Create Sturdy Iron Key (6876) for ContainTreasure */
      , (7109, 9, 22253,  0, 0, 0.0005, False) /* Create Ulgrim's Scroll (22253) for ContainTreasure */
      , (7109, 9,     0,  0, 0, 0.9895, False) /* Create nothing for ContainTreasure */;
-     
