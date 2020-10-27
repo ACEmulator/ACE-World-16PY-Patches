617c80ac2d874bc94f31fff93e2f4be2cf07e29b
Patrick <phreekn.slacker@gmail.com>
2020-10-27 13:57:00 -0500

    Update 492D.sql

--- Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
+++ Database/Patches/2007-03-AncientEnemies/6 LandBlockExtendedData/492D.sql
@@ -1,4 +1,4 @@
-DELETE FROM `landblock_instance` WHERE `landblock` = 0x492D
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x492D;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x7492D000, 35488, 0x492D0017, 53.7138, 152.896, 29.58046, -0.509899, 0, 0, -0.860234, False, '2020-03-08 04:03:33'); /* Skettek the Wanderer */
