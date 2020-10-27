d41ca38f04c81b6387b70d7fc7c9d2070553b442
Patrick <phreekn.slacker@gmail.com>
2020-10-27 14:25:30 -0500

    Merged Landblocks into 1

--- Database/Patches/2013-11-FowlPlay/6 LandBlockExtendedData/492D.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7492D004, 52310, 0x492D0017, 55.6993, 161.671, 29.58046, -0.7358512, 0, 0, -0.6771432,  False, '2019-02-10 00:00:00'); /* Slubbley */
-/* @teleloc 0x492D0017 [55.699300 161.671000 29.580460] -0.735851 0.000000 0.000000 -0.677143 */
