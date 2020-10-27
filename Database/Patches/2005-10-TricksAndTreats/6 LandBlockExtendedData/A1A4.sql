f74edb52f75b722366ddf8f755bde909848049a3
sapphire-knight <69483793+sapphire-knight@users.noreply.github.com>
2020-10-26 21:03:57 -0400

    Update A1A4.sql
    Nudge to Festival Vendor to line up with https://mega.nz/folder/L1MniCKJ#1dQCCFPc2ddcFILa_JGeZw/file/e1VHFAJB
    and Pumpkin buffer moved to match original screenshot http://acpedia.org/images/3/3a/Pumpkin_Buffer_Live.jpg

--- Database/Patches/2005-10-TricksAndTreats/6 LandBlockExtendedData/A1A4.sql
+++ Database/Patches/2005-10-TricksAndTreats/6 LandBlockExtendedData/A1A4.sql
@@ -1,3 +1,7 @@
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (2048540715, 80021, 2711879718, 114.9668, 138.9591, 50.005, -0.943635, 0, 0, -0.330988, False, '2019-08-07 06:01:09'); /* Festival Vendor Generator */
-/* @teleloc 0xA1A40026 [114.966812 138.959106 50.005001] -0.943635 0.000000 0.000000 -0.330988 */
+VALUES (0x7A1A402B, 80021, 0xA1A40026, 118.908, 139.1, 50, -0.943635, 0, 0, -0.330988, False, '2019-08-07 06:01:09'); /* Festival Vendor Generator */
+/* @teleloc 0xA1A40026 [118.907997 139.100006 50.000000] -0.943635 0.000000 0.000000 -0.330988 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7A1A408C, 80022, 0xA1A40026, 107.398, 128.911, 50.005, 0.873799, 0, 0, 0.486287, False, '2019-11-03 00:00:00'); /* Pumpkin Buffer Generator */
+/* @teleloc 0xA1A40026 [107.398003 128.910995 50.005001] 0.873799 0.000000 0.000000 0.486287 */
