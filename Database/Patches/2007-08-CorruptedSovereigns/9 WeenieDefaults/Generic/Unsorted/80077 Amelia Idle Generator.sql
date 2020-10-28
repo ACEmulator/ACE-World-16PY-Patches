67878ac93496e863747b7bcd6c503647aad36b8e
Ty Conner <tyconner@itcproductions.com>
2020-10-28 15:11:48 -0400

    Gen Updates

--- Database/Patches/2007-08-CorruptedSovereigns/9 WeenieDefaults/Generic/Unsorted/80077 Amelia Idle Generator.sql
+++ Database/Patches/2007-08-CorruptedSovereigns/9 WeenieDefaults/Generic/Unsorted/80077 Amelia Idle Generator.sql
@@ -29,4 +29,4 @@ VALUES (80077,   1,   33555051) /* Setup */
      , (80077,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (80077, -1, 35932, 0, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Amelia (35932) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
+VALUES (80077, -1, 35932, 120, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Amelia (35932) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
