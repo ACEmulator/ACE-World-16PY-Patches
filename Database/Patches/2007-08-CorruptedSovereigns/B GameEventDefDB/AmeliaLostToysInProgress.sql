ba0b25433f43f0a9d87fd639197526ef99e08d84
Ty Conner <tyconner@itcproductions.com>
2020-10-29 02:22:25 -0400

    Update AmeliaLostToysInProgress.sql

--- Database/Patches/2007-08-CorruptedSovereigns/B GameEventDefDB/AmeliaLostToysInProgress.sql
+++ Database/Patches/2007-08-CorruptedSovereigns/B GameEventDefDB/AmeliaLostToysInProgress.sql
@@ -1,4 +1,4 @@
-DELETE FROM `event` WHERE `name` = 'AmeliaLostToys';
+DELETE FROM `event` WHERE `name` = 'AmeliaLostToysInProgress';
 
 INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
 VALUES ('AmeliaLostToysInProgress', -1, -1, 3, '2020-09-22 00:00:00');
