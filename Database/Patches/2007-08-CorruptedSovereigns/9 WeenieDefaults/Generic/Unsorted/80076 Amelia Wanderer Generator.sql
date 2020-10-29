52322db3519aa44cf73b91251a93d6372028b338
Ty Conner <tyconner@itcproductions.com>
2020-10-29 01:42:21 -0400

    Update 80076 Amelia Wanderer Generator.sql

--- Database/Patches/2007-08-CorruptedSovereigns/9 WeenieDefaults/Generic/Unsorted/80076 Amelia Wanderer Generator.sql
+++ Database/Patches/2007-08-CorruptedSovereigns/9 WeenieDefaults/Generic/Unsorted/80076 Amelia Wanderer Generator.sql
@@ -5,7 +5,7 @@ VALUES (80076, 'ace80076-ameliawanderergen', 1, '2020-09-22 00:00:00') /* Generi
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (80076,  81,          1) /* MaxGeneratedObjects */
-     , (80076,  82,          0) /* InitGeneratedObjects */
+     , (80076,  82,          1) /* InitGeneratedObjects */
      , (80076,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (80076, 103,          2) /* GeneratorDestructionType - Destroy */
      , (80076, 145,          2) /* GeneratorEndDestructionType - Destroy */
