def123340f7f3148cfe4976cad8017a4bb05fb81
Ty Conner <tyconner@itcproductions.com>
2020-10-29 02:25:32 -0400

    Update 35927 Dirt Pile.sql

--- Database/Patches/2007-08-CorruptedSovereigns/9 WeenieDefaults/Creature/Creature/35927 Dirt Pile.sql
+++ Database/Patches/2007-08-CorruptedSovereigns/9 WeenieDefaults/Creature/Creature/35927 Dirt Pile.sql
@@ -15,7 +15,8 @@ VALUES (35927,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (35927,   1, True ) /* Stuck */
-     , (35927,  19, False) /* Attackable */;
+     , (35927,  19, False) /* Attackable */
+     , (35927,  83, True ) /* NpcLooksLikeObject */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (35927,  39,     0.7) /* DefaultScale */
