14fc16d9fc52420261b95525c3f35d62ee4728d3
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2020-11-03 01:11:12 -0500

    Remove timetorot from pack dolls (#714)
    * patches update
    
    * update weenies from base
    
    Remove timetorot
    Change weenie type from generic to gem to match pcap
    
    Wedding cake - just removed timetorot
    
    * book pack dolls
    
    remove timetorot, change inscribable to true per pcap

--- Database/Patches/2006-09-ComeWhatFollows/9 WeenieDefaults/Gem/Gem/33965 Tipped Pack Cow.sql
+++ Database/Patches/2006-09-ComeWhatFollows/9 WeenieDefaults/Gem/Gem/33965 Tipped Pack Cow.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 33965;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (33965, 'ace33965-tippedpackcow', 1, '2019-03-31 22:51:06') /* Generic */;
+VALUES (33965, 'ace33965-tippedpackcow', 38, '2020-11-02 22:51:06') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (33965,   1,       2048) /* ItemType - Gem */
@@ -11,18 +11,16 @@ VALUES (33965,   1,       2048) /* ItemType - Gem */
      , (33965,   9,          0) /* ValidLocations - None */
      , (33965,  16,          1) /* ItemUseable - No */
      , (33965,  19,         10) /* Value */
-     , (33965,  93,       3092) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn */
+     , (33965,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (33965,  94,         16) /* TargetType - Creature */
      , (33965, 151,          9) /* HookType - Floor, Yard */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (33965,  15, True ) /* LightsStatus */
-     , (33965,  22, True ) /* Inscribable */
+VALUES (33965,  22, True ) /* Inscribable */
      , (33965,  23, True ) /* DestroyOnSell */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (33965,  39, 0.3) /* DefaultScale */
-     , (33965,  44,  -1) /* TimeToRot */;
+VALUES (33965,  39, 0.3) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (33965,   1, 'Tipped Pack Cow') /* Name */
