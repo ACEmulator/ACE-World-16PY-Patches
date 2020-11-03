5ee36a3218de4dd06e228954266be997cfc441ee
Drid <58707922+DridsGhost@users.noreply.github.com>
2020-11-03 00:05:41 -0500

    Nerf Shambling Archivist
    Lower skills
    fix a few bad portals

--- Database/Patches/2008-05-Introductions/9 WeenieDefaults/Portal/37514 Forges.sql
+++ Database/Patches/2008-05-Introductions/9 WeenieDefaults/Portal/37514 Forges.sql
@@ -7,6 +7,7 @@ INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (37514,   1,      65536) /* ItemType - Portal */
      , (37514,  16,         32) /* ItemUseable - Remote */
      , (37514,  93,       3088) /* PhysicsState - IgnoreCollisions, Gravity, LightingOn */
+     , (37514, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */
      , (37514, 133,          4) /* ShowableOnRadar - ShowAlways */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
@@ -23,8 +24,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (37514,   1,   33558268) /* Setup */
      , (37514,   3,  536871008) /* SoundTable */
      , (37514,   8,  100674152) /* Icon */;
-     
+
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (37514, 2, 15335909, 90, -90, -53.446701049805, 1, 0, 0, 0) /* Destination */
-/* @teleloc 0x00ea01e5 90 -90 -53.446701049805 1 0 0 0 */;     
-     
+/* @teleloc 0x00ea01e5 90 -90 -53.446701049805 1 0 0 0 */;
