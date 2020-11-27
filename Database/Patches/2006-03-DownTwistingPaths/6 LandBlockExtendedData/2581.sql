5b61535ac482d238e929fbb4a686a39cd272c4dd
Patrick <phreekn.slacker@gmail.com>
2020-11-27 17:46:51 -0600

    Reverted Landblock 2581

---
+++ Database/Patches/2006-03-DownTwistingPaths/6 LandBlockExtendedData/2581.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7258104C, 32842, 0x25810149, 163.924, 68.9318, 220.00499, 0.713396, 0, 0, 0.700761, False, '2020-02-29 18:07:26'); /* Lieutenant Durgan */
+/* @teleloc 0x25810149 [163.923996 68.931801 220.004990] 0.713396 0.000000 0.000000 0.700761 */
---
+++ Database/Patches/2017-01-EndOfRetail/6 LandBlockExtendedData/SQL/2581.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (1918373963, 43065, 629211180, 138.929, 72.6891, 220.198, -0.7243258, 0, 0, 0.6894578, False); /* Portal to Town Network */
+/* @teleloc 0x2581002C [138.929000 72.689100 220.198000] -0.724326 0.000000 0.000000 0.689458 */
