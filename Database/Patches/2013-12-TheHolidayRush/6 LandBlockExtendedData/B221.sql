286cfa41e8bc748e3eb78dda15e3663c359353ce
Patrick <phreekn.slacker@gmail.com>
2020-11-30 19:27:18 -0600

    Update B221.sql

--- Database/Patches/2013-12-TheHolidayRush/6 LandBlockExtendedData/B221.sql
+++ Database/Patches/2013-12-TheHolidayRush/6 LandBlockExtendedData/B221.sql
@@ -1,4 +1,4 @@
 /* Kara */
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
-VALUES (0x7B222099, 87228,  0xB221000F, 29.124676, 160.472397, 114.005005, 0.283063, 0.000000, 0.000000, 0.959101,  False); /* Gift Box Generator */
+VALUES (0x7B221099, 87228,  0xB221000F, 29.124676, 160.472397, 114.005005, 0.283063, 0.000000, 0.000000, 0.959101,  False); /* Gift Box Generator */
 /* @teleloc  0xB221000F [29.124676 160.472397 114.005005] 0.283063 0.000000 0.000000 0.959101 */
