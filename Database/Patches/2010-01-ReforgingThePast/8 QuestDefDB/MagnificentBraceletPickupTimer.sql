24993e9f81d69842d62c8d2e9ffba0dd8d43879a
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2020-12-04 12:57:26 -0600

    Updates to Heart of Innocence and Oswald's Dirk quests (#734)
    * Updates to existing quest
    
    * Create 24153_Aaminah.es

---
+++ Database/Patches/2010-01-ReforgingThePast/8 QuestDefDB/MagnificentBraceletPickupTimer.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'MagnificentBraceletPickupTimer';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('MagnificentBraceletPickupTimer', 72000, -1, 'Pick up Magnificent Bracelet Wait timer', '2020-11-19 20:34:27');
