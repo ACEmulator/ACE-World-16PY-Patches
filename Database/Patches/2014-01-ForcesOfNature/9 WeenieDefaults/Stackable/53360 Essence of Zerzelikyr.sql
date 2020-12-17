d5023fa1e6435d79d4c2e1c1ae86225437a6adc4
Ty Conner <tyconner@itcproductions.com>
2020-12-17 13:21:45 -0500

    Update 53360 Essence of Zerzelikyr.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/Stackable/53360 Essence of Zerzelikyr.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/Stackable/53360 Essence of Zerzelikyr.sql
@@ -6,19 +6,16 @@ VALUES (53360, 'ace53360-essenceofzerzelikyr', 51, '2020-07-16 17:11:04') /* Sta
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (53360,   1,        128) /* ItemType - Misc */
      , (53360,   5,          1) /* EncumbranceVal */
-     , (53360,  11,        100) /* MaxStackSize */
+     , (53360,  11,          1) /* MaxStackSize */
      , (53360,  12,          1) /* StackSize */
 	 , (53360,  13,          1) /* StackUnitEncumbrance */
 	 , (53360,  15,          0) /* StackUnitValue */
      , (53360,  16,          1) /* ItemUseable - No */
      , (53360,  19,          0) /* Value */
-     , (53360,  33,          1) /* Bonded - Bonded */
-     , (53360,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (53360, 114,          1) /* Attuned - Attuned */;
+     , (53360,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (53360,  22, True ) /* Inscribable */
-	 , (53360,  69, False) /* IsSellable */;
+VALUES (53360,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (53360,   1, 'Essence of Zerzelikyr') /* Name */
