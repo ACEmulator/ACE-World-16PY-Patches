181aa0fd6e19916605e381b378350f9e91a267cf
Ty Conner <tyconner@itcproductions.com>
2020-12-17 13:21:08 -0500

    Update 53366 Essence of Wind Fury.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/Stackable/53366 Essence of Wind Fury.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/Stackable/53366 Essence of Wind Fury.sql
@@ -6,19 +6,16 @@ VALUES (53366, 'ace53366-essenceofwindfury', 51, '2020-07-16 17:11:04') /* Stack
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (53366,   1,        128) /* ItemType - Misc */
      , (53366,   5,          1) /* EncumbranceVal */
-     , (53366,  11,        100) /* MaxStackSize */
+     , (53366,  11,          1) /* MaxStackSize */
      , (53366,  12,          1) /* StackSize */
 	 , (53366,  13,          1) /* StackUnitEncumbrance */
 	 , (53366,  15,          0) /* StackUnitValue */
      , (53366,  16,          1) /* ItemUseable - No */
      , (53366,  19,          0) /* Value */
-     , (53366,  33,          1) /* Bonded - Bonded */
-     , (53366,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (53366, 114,          1) /* Attuned - Attuned */;
+     , (53366,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (53366,  22, True ) /* Inscribable */
-	 , (53366,  69, False) /* IsSellable */;
+VALUES (53366,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (53366,   1, 'Essence of Wind Fury') /* Name */
