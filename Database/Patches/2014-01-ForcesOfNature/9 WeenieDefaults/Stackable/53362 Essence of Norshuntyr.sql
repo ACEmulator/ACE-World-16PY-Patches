1780d7a7f66058db5ead1316fe58f4212e3434cd
Ty Conner <tyconner@itcproductions.com>
2020-12-17 13:22:33 -0500

    Update 53362 Essence of Norshuntyr.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/Stackable/53362 Essence of Norshuntyr.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/Stackable/53362 Essence of Norshuntyr.sql
@@ -6,19 +6,16 @@ VALUES (53362, 'ace53362-essenceofnorshuntyr', 51, '2020-07-16 17:11:04') /* Sta
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (53362,   1,        128) /* ItemType - Misc */
      , (53362,   5,          1) /* EncumbranceVal */
-     , (53362,  11,        100) /* MaxStackSize */
+     , (53362,  11,          1) /* MaxStackSize */
      , (53362,  12,          1) /* StackSize */
 	 , (53362,  13,          1) /* StackUnitEncumbrance */
 	 , (53362,  15,          0) /* StackUnitValue */
      , (53362,  16,          1) /* ItemUseable - No */
      , (53362,  19,          0) /* Value */
-     , (53362,  33,          1) /* Bonded - Bonded */
-     , (53362,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (53362, 114,          1) /* Attuned - Attuned */;
+     , (53362,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (53362,  22, True ) /* Inscribable */
-	 , (53362,  69, False) /* IsSellable */;
+VALUES (53362,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (53362,   1, 'Essence of Norshuntyr') /* Name */
