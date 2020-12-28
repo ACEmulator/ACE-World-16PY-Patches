a97fa0a8df2ab4480ce5135f6aa545cf2f58ab4b
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2020-12-28 17:47:54 -0500

    Moved Spells and Fixed Contract
    Moved fixed spells to End of Retail folder.
    
    Fixed 51679 Contract for Visitors.

--- Database/Patches/2013-07-FeelingsOfDread/2 SpellTableExtendedData/06148 Begone and Be Afraid.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `spell` WHERE `id` = 6148;
-
-INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
-VALUES (6148, 'Begone and Be Afraid', 0x30190033, 156, 60, 18.005, 1, 0, 0, 0, '2019-03-18 09:00:00');
-/* @teleloc 0x30190033 [156 60 18.005] 1 0 0 0 */
--- Database/Patches/2013-07-FeelingsOfDread/9 WeenieDefaults/Gem/Gem/51679 Contract for Visitors.sql
+++ Database/Patches/2013-07-FeelingsOfDread/9 WeenieDefaults/Gem/Gem/51679 Contract for Visitors.sql
@@ -5,6 +5,7 @@ VALUES (51679, 'ace51679-contractforvisitors', 38, '2019-02-10 00:00:00') /* Gem
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (51679,   1,       2048) /* ItemType - Gem */
+     , (51679,   5,          0) /* EncumbranceVal */
      , (51679,  11,          1) /* MaxStackSize */
      , (51679,  12,          1) /* StackSize */
      , (51679,  13,          0) /* StackUnitEncumbrance */
@@ -12,22 +13,26 @@ VALUES (51679,   1,       2048) /* ItemType - Gem */
      , (51679,  16,          8) /* ItemUseable - Contained */
      , (51679,  18,          2) /* UiEffects - Poisoned */
      , (51679,  19,        100) /* Value */
+     , (51679,  33,          1) /* Bonded - Bonded */
      , (51679,  93,       3092) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity, LightingOn */
      , (51679,  94,         16) /* TargetType - Creature */
-     , (51679, 280,        100) /* SharedCooldown */;
+     , (51679, 280,        100) /* SharedCooldown */
+     , (51679, 349,        302) /* UseCreatesContractId */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (51679,  22, True ) /* Inscribable */;
+VALUES (51679,  22, True ) /* Inscribable */
+     , (51679,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (51679, 167,       2) /* CooldownDuration */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (51679,   1, 'Contract for Visitors') /* Name */;
+VALUES (51679,   1, 'Contract for Visitors') /* Name */
+     , (51679,  14, 'Recommended Level: 180') /* Use */
+     , (51679,  16, 'Investigate the Obsidian Plains and discover what is causing the energy fluctuations.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (51679,   1,   33557625) /* Setup */
      , (51679,   3,  536870932) /* SoundTable */
      , (51679,   8,  100691928) /* Icon */
-     , (51679,  22,  872415275) /* PhysicsEffectTable */
-     , (51679, 8044,      14913) /* PCAPPhysicsDIDDataTemplatedFrom - Invitation Wedding Hall */;
+     , (51679,  22,  872415275) /* PhysicsEffectTable */;
