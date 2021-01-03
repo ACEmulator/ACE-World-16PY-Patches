b5abf407719e66377550528047a98745a5c19e85
Ty Conner <tyconner@itcproductions.com>
2021-01-03 16:17:13 -0500

    Update 40779 Lever.sql

--- Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Lever/40779 Lever.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/9 WeenieDefaults/Creature/Lever/40779 Lever.sql
@@ -13,7 +13,8 @@ VALUES (40779,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (40779,   1, True ) /* Stuck */
-     , (40779,  19, False) /* Attackable */;
+     , (40779,  19, False) /* Attackable */
+     , (40779,  83, True ) /* NpcLooksLikeObject */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (40779,  54,       1) /* UseRadius */;
