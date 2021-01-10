b9b021855961667360d12c9cc5daafcd071c0b9f
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-01-09 16:52:52 -0800

    Bandits of the Creepy Chambers

--- Database/Patches/2006-03-DownTwistingPaths/9 WeenieDefaults/Creature/Human/32780 Xanda Har.sql
+++ Database/Patches/2006-03-DownTwistingPaths/9 WeenieDefaults/Creature/Human/32780 Xanda Har.sql
@@ -1,15 +1,16 @@
 DELETE FROM `weenie` WHERE `class_Id` = 32780;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (32780, 'ace32780-xandahar', 10, '2020-02-29 18:15:46') /* Creature */;
+VALUES (32780, 'ace32780-xandahar', 10, '2020-11-25 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32780,   1,         16) /* ItemType - Creature */
      , (32780,   2,         31) /* CreatureType - Human */
-     , (32780,   6,        255) /* ItemsCapacity */
-     , (32780,   7,        255) /* ContainersCapacity */
+     , (32780,   6,         -1) /* ItemsCapacity */
+     , (32780,   7,         -1) /* ContainersCapacity */
      , (32780,  16,         32) /* ItemUseable - Remote */
      , (32780,  25,         80) /* Level */
+     , (32780,  27,          1) /* ArmorType - Cloth */
      , (32780,  93,    6292504) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
      , (32780,  95,          8) /* RadarBlipColor - Yellow */
      , (32780, 113,          2) /* Gender - Female */
@@ -19,12 +20,8 @@ VALUES (32780,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (32780,   1, True ) /* Stuck */
-     , (32780,  11, True ) /* IgnoreCollisions */
-     , (32780,  12, True ) /* ReportCollisions */
-     , (32780,  14, True ) /* GravityStatus */
-     , (32780,  19, False) /* Attackable */
-     , (32780,  41, True ) /* ReportCollisionsAsEnvironment */
-     , (32780,  42, True ) /* AllowEdgeSlide */;
+     , (32780,   8, True ) /* AllowGive */
+     , (32780,  19, False) /* Attackable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (32780,   1,       5) /* HeartbeatInterval */
@@ -93,6 +90,40 @@ VALUES (32780,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
      , (32780,  6,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
      , (32780,  7,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
      , (32780,  8,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (32780,  5 /* HeartBeat */,  0.001, NULL, 2147483709 /* NonCombat */, 1090519043 /* Ready */, NULL, NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id,  0,   8 /* Say */, 0, 1, NULL, 'I wonder what they''re doing out there ...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (32780,  6 /* Give */,      1, 32773 /* Spinning Staff of Death */, NULL, NULL, NULL, NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id,  0,  12 /* TurnToTarget */, 0, 1, NULL, ' ', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  1,   5 /* Motion */, 1, 1, 318767243 /* ScratchHead */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  2,  10 /* Tell */, 1, 1, NULL, 'What''s this? You found this in the Creepy Chambers, eh? Wait ... *this* is the bandit''s super-weapon? Well shoot! No, no, it''s fine... it''s just -- I''m not a staff warrior. I was hoping for a sword.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  3,  49 /* AwardLevelProportionalXP */, 1, 1, NULL, NULL, NULL, NULL, NULL, 0, 3500000, NULL, NULL, NULL, True, NULL, NULL, NULL, 0.05, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  4,  10 /* Tell */, 1, 1, NULL, 'Well, I did say I''d reward you. Thanks anyway, I guess.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (32780,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id,  0,  12 /* TurnToTarget */, 0, 1, NULL, ' ', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  1,   5 /* Motion */, 1, 1, 318767226 /* Beckon */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  2,  10 /* Tell */, 1, 1, NULL, 'Hello! Say, I don''t suppose you could help me out with a small favor? I''ve heard that there''s this group of bandits out in the wilderness, a bit south of the Obsidian Rim. Rumor has it that they''re stockpiling treasure and ... well, doing something strange with creatures. Either breeding them or worshipping them -- no one is quite sure which.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  3,   5 /* Motion */, 1, 1, 318767235 /* Nod */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  4,  10 /* Tell */, 1, 1, NULL, 'But it''s the treasure that I''m interested in! And my curiosity is just killing me! I even heard that they might have some sort of super weapon!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  5,   5 /* Motion */, 1, 1, 318767236 /* Point */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  6,  10 /* Tell */, 1, 1, NULL, 'Will you go and see what you can find out for me? I think their lair is called the Creepy Chambers. The barkeeper here sells a rumor with better directions than I can give you. And if you can find that super-weapon, bring it back to me! I''ll reward you!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (32780, 2,   135,  0, 14, 0.1667, False) /* Create Turban (135) for Wield */
---
+++ Database/Patches/2006-03-DownTwistingPaths/9 WeenieDefaults/MeleeWeapon/32773 Spinning Staff of Death.sql
@@ -0,0 +1,59 @@
+DELETE FROM `weenie` WHERE `class_Id` = 32773;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (32773, 'ace32773-spinningstaffofdeath', 6, '2020-11-25 23:48:00') /* MeleeWeapon */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (32773,   1,          1) /* ItemType - MeleeWeapon */
+     , (32773,   5,        660) /* EncumbranceVal */
+     , (32773,   9,    1048576) /* ValidLocations - MeleeWeapon */
+     , (32773,  16,          1) /* ItemUseable - No */
+     , (32773,  18,          1) /* UiEffects - Magical */
+     , (32773,  19,       6600) /* Value */
+     , (32773,  44,         45) /* Damage */
+     , (32773,  45,          1) /* DamageType - Slash */
+     , (32773,  46,          2) /* DefaultCombatStyle - OneHanded */
+     , (32773,  47,          6) /* AttackType - Thrust, Slash */
+     , (32773,  48,         46) /* WeaponSkill - FinesseWeapons */
+     , (32773,  49,         25) /* WeaponTime */
+     , (32773,  51,          1) /* CombatUse - Melee */
+     , (32773,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (32773, 106,        350) /* ItemSpellcraft */
+     , (32773, 107,       2000) /* ItemCurMana */
+     , (32773, 108,       2000) /* ItemMaxMana */
+     , (32773, 151,          2) /* HookType - Wall */
+     , (32773, 158,          2) /* WieldRequirements - RawSkill */
+     , (32773, 159,         46) /* WieldSkillType - FinesseWeapons */
+     , (32773, 160,        350) /* WieldDifficulty */
+     , (32773, 263,          1) /* ResistanceModifierType */
+     , (32773, 353,          7) /* WeaponType - Staff */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (32773,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (32773,   5,   -0.05) /* ManaRate */
+     , (32773,  21,       0) /* WeaponLength */
+     , (32773,  22,    0.25) /* DamageVariance */
+     , (32773,  26,       0) /* MaximumVelocity */
+     , (32773,  39,    0.67) /* DefaultScale */
+     , (32773,  62,    1.13) /* WeaponOffense */
+     , (32773,  63,       1) /* DamageMod */
+     , (32773, 156,    0.05) /* ProcSpellRate */
+     , (32773, 157,       1) /* ResistanceModifier */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (32773,   1, 'Spinning Staff of Death') /* Name */
+     , (32773,  16, 'The core of this staff is made of bone and the outside is plated with the skin of a Titanium Armoredillo. Despite its weight, it feels light -- as if it seeks to whirl away on its own, slashing the very air to ribbons. ') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (32773,   1,   33559847) /* Setup */
+     , (32773,   3,  536870932) /* SoundTable */
+     , (32773,   8,  100688662) /* Icon */
+     , (32773,  22,  872415275) /* PhysicsEffectTable */
+     , (32773,  55,       1784) /* ProcSpell - Horizon's Blades */;
+
+INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
+VALUES (32773,  1784,      2)  /* Horizon's Blades */
+     , (32773,  2096,      2)  /* Aura of Infected Caress */
+     , (32773,  2106,      2)  /* Aura of Elysa's Sight */;
