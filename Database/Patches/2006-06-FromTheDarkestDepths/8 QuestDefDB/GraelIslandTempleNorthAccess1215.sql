a111416809e9fa3042bb14c254f739a921dc7ef6
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-01-12 12:41:58 -0600

    Raising Grael's Island (Quest) (#766)
    * AddedQuest
    
    * Updates

---
+++ Database/Patches/2006-06-FromTheDarkestDepths/8 QuestDefDB/GraelIslandTempleNorthAccess1215.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'GraelIslandTempleNorthAccess1215';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('GraelIslandTempleNorthAccess1215', 0, 1, 'Player has access to Northern Catacombs Portal', '2020-12-15 06:51:50');
