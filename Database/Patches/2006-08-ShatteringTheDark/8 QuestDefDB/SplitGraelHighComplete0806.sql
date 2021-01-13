e955fbeb8a2d9bbd5f6ec632b7948b23e4358fe4
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-01-13 00:20:25 -0600

    Mukkir Aspect of Grael Quest and Splitting Grael (High) (#767)
    * Quest and All
    
    * Update to Surface Portal Drop
    
    * Update 33458 Mukkir Aspect of Grael.sql
    
    * Moved spell to EoR
    
    * Updates
    
    * Updates Made TrophyRoom Portal Static
    
    * Update 33458 Mukkir Aspect of Grael.sql
    
    * Create 33877_Hoshar_ibn_Jalaq.es
    
    * Balance Grael, fix Souldrinker
    
    Tune Grael
    
    Fix Souldrinker to two hand, get rid of console error
    
    Co-authored-by: Drid <58707922+DridsGhost@users.noreply.github.com>

---
+++ Database/Patches/2006-08-ShatteringTheDark/8 QuestDefDB/SplitGraelHighComplete0806.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'SplitGraelHighComplete0806';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('SplitGraelHighComplete0806', 72000, -1, 'Player has completed Split Grael High', '2020-12-21-20:34:27');
--- Database/Patches/2006-08-ShatteringTheDark/9 WeenieDefaults/Portal/33910 Surface.sql
+++ Database/Patches/2006-08-ShatteringTheDark/9 WeenieDefaults/Portal/33910 Surface.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 33910;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (33910, 'ace33910-surface', 7, '2019-02-04 06:52:23') /* Portal */;
+VALUES (33910, 'ace33910-surface', 7, '2019-02-10 00:00:00') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (33910,   1,      65536) /* ItemType - Portal */
@@ -11,22 +11,19 @@ VALUES (33910,   1,      65536) /* ItemType - Portal */
      , (33910, 133,          4) /* ShowableOnRadar - ShowAlways */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (33910,   1, True ) /* Stuck */
-     , (33910,  12, True ) /* ReportCollisions */
-     , (33910,  13, True ) /* Ethereal */
-     , (33910,  14, True ) /* GravityStatus */
-     , (33910,  15, True ) /* LightsStatus */
-     , (33910,  19, True ) /* Attackable */
-     , (33910,  88, True ) /* PortalShowDestination */;
+VALUES (33910,   1, True ) /* Stuck */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (33910,  54, -0.100000001490116) /* UseRadius */;
+VALUES (33910,  54,    -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (33910,   1, 'Surface') /* Name */
-     , (33910,  38, 'Surface (86.6N, 57.3E).') /* AppraisalPortalDestination */;
+VALUES (33910,   1, 'Surface') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (33910,   1,   33554867) /* Setup */
      , (33910,   2,  150994947) /* MotionTable */
      , (33910,   8,  100667499) /* Icon */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (33910, 2, 3354066958, 33.2956, 128.17, 0.005, 1, 0, 0, 0) /* Destination */
+/* @teleloc 0xC7EB000E [33.295601 128.169998 0.005000] 1.000000 0.000000 0.000000 0.000000 */;
