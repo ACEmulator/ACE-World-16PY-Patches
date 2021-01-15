46fefcfd5970ddf7a1bdb084f225c612c2a0a35e
Adam Plumb <adamplumb@gmail.com>
2021-01-14 19:02:53 -0500

    From the Gaining Ground 2009-12 patch, doubled the success output of alchemy gems

---
+++ Database/Patches/2009-12-GainingGround/4 CraftTable/04610 Gem of Improved Rejuvenation.sql
@@ -0,0 +1,9 @@
+DELETE FROM `recipe` WHERE `id` = 4610;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (4610, 0, 38 /* Alchemy */, 225, 0, 24816 /* Gem of Improved Rejuvenation */, 2, 'The powder congeals into a gem-like substance. The process is a success!', 0, 0, 'The process was an utter failure, leaving you with a messy mass of mush to show for your efforts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-01-14 19:00:00');
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 4610;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (4610, 24750 /* Treated Cinnabar and Amaranth Crucible */,   783 /* Powdered Amber */, '2005-02-09 10:00:00');
