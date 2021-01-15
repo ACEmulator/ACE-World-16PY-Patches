09dda84c5c114371d1f7d18ca2311207eadaa150
Patrick <phreekn.slacker@gmail.com>
2021-01-15 09:11:03 -0600

    Monster Fights!

---
+++ Database/Patches/2008-12-SinsOfTheFathers/9 WeenieDefaults/Generic/87155 Monster Fights Announcer Generator.sql
@@ -0,0 +1,33 @@
+DELETE FROM `weenie` WHERE `class_Id` = 87155;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (87155, 'ace-87155-MonsterFightsAnnouncerGenerator', 1, '2020-05-22 10:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (87155,  81,          1) /* MaxGeneratedObjects */
+     , (87155,  82,          1) /* InitGeneratedObjects */
+     , (87155,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (87155, 103,          2) /* GeneratorDestructionType - Destroy */
+     , (87155, 142,          3) /* GeneratorTimeType - Event */
+     , (87155, 145,          2) /* GeneratorEndDestructionType - Destroy */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (87155,   1, True ) /* Stuck */
+     , (87155,  11, True ) /* IgnoreCollisions */
+     , (87155,  18, True ) /* Visibility */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (87155,  41,  999999) /* RegenerationInterval */
+     , (87155,  43,       4) /* GeneratorRadius */
+     , (87155, 121,      30) /* GeneratorInitialDelay */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (87155,   1, 'Monster Fights Announcer Generator') /* Name */
+     , (87155,  34, 'MonsterFightsAnnouncerevent') /* GeneratorEvent */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (87155,   1,   33555051) /* Setup */
+     , (87155,   8,  100667494) /* Icon */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (87155, -1, 38990, 1, 1, 1, 1, 4, 0, 0, 0, 869924893, 92.4851, 99.2742, 60.005, 0.47699, 0, 0, -0.878909) /* Generate Monster Fights Announcer (38990) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
---
+++ Database/Patches/2008-12-SinsOfTheFathers/9 WeenieDefaults/Generic/87164 Monster Fights Controller Generator.sql
@@ -0,0 +1,33 @@
+DELETE FROM `weenie` WHERE `class_Id` = 87164;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (87164, 'ace-87164-MonsterFightsControllerGenerator', 1, '2020-05-22 10:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (87164,  81,          1) /* MaxGeneratedObjects */
+     , (87164,  82,          1) /* InitGeneratedObjects */
+     , (87164,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (87164, 103,          2) /* GeneratorDestructionType - Destroy */
+     , (87164, 142,          3) /* GeneratorTimeType - Event */
+     , (87164, 145,          2) /* GeneratorEndDestructionType - Destroy */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (87164,   1, True ) /* Stuck */
+     , (87164,  11, True ) /* IgnoreCollisions */
+     , (87164,  18, True ) /* Visibility */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (87164,  41,  999999) /* RegenerationInterval */
+     , (87164,  43,       1) /* GeneratorRadius */
+     , (87164, 121,       1) /* GeneratorInitialDelay */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (87164,   1, 'Monster Fights Controller Generator') /* Name */
+     , (87164,  34, 'MonsterFightsControllerGeneratorevent') /* GeneratorEvent */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (87164,   1,   33555051) /* Setup */
+     , (87164,   8,  100667494) /* Icon */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (87164, -1, 87165, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Monster Fights Controller (87165) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
