3839a5a30328440eb7e991d1dcb4e16f3303df0d
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2021-01-25 10:49:00 -0500

    Moved spells to correct folder

--- Database/Patches/2013-07-FeelingsOfDread/6 LandBlockExtendedData/2D30.sql
+++ Database/Patches/2013-07-FeelingsOfDread/6 LandBlockExtendedData/2D30.sql
@@ -13,10 +13,6 @@ VALUES (0x72D30002, 51564, 0x2D300026, 111.149, 122.053, 63.5832, -0.998459, 0, 
 /* @teleloc 0x2D300026 [111.149002 122.053001 63.583199] -0.998459 0.000000 0.000000 0.055500 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30056, 72143, 0x2D300034, 153.045, 86.0994, 190.105, -0.171574, 0, 0, -0.985171, False, '2020-09-20 11:24:02'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D300034 [153.044998 86.099403 190.104996] -0.171574 0.000000 0.000000 -0.985171 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x72D30057, 72124, 0x2D300023, 118, 60, 120.189, 1, 0, 0, 0, False, '2019-02-10 00:00:00'); /* Torment Ward 2 Gen */
 /* @teleloc 0x2D300023 [118.000000 60.000000 120.189003] 1.000000 0.000000 0.000000 0.000000 */
 
@@ -29,44 +25,24 @@ VALUES (0x72D303FD, 72143, 0x2D30000A, 37.938, 38.6326, 220.105, -0.492095, 0, 0
 /* @teleloc 0x2D30000A [37.938000 38.632599 220.104996] -0.492095 0.000000 0.000000 -0.870542 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D303FF, 72143, 0x2D300023, 108.766, 63.3408, 210.105, 0.410922, 0, 0, 0.91167, False, '2020-09-20 11:21:27'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D300023 [108.765999 63.340801 210.104996] 0.410922 0.000000 0.000000 0.911670 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x72D30400, 72143, 0x2D300024, 105.139, 82.705, 210.105, 0.516451, 0, 0, 0.856317, False, '2020-09-20 11:21:51'); /* Rynthid Platforms 2 Gen */
 /* @teleloc 0x2D300024 [105.139000 82.705002 210.104996] 0.516451 0.000000 0.000000 0.856317 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30401, 72143, 0x2D30001C, 83.2963, 79.9691, 210.105, -0.838139, 0, 0, 0.545457, False, '2020-09-20 11:22:23'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D30001C [83.296303 79.969101 210.104996] -0.838139 0.000000 0.000000 0.545457 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x72D30402, 51604, 0x2D300025, 111.29, 107.765, 62.5321, 0.998599, 0, 0, 0.0529089, False, '2019-02-10 00:00:00'); /* Rynthid Crystal Access Device */
 /* @teleloc 0x2D300025 [111.290001 107.764999 62.532101] 0.998599 0.000000 0.000000 0.052909 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30403, 72143, 0x2D30002C, 130.654, 82.4215, 190.105, 0.968681, 0, 0, -0.248307, False, '2020-09-20 11:23:40'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D30002C [130.654007 82.421501 190.104996] 0.968681 0.000000 0.000000 -0.248307 */
+VALUES (0x72D30403, 72143, 0x2D30002C, 134.18947, 85.95696, 190.105, 0.968681, 0, 0, -0.248307, False, '2020-09-20 11:23:40'); /* Rynthid Platforms 2 Gen */
+/* @teleloc 0x2D30002C [134.189468 85.956963 190.104996] 0.968681 0.000000 0.000000 -0.248307 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x72D30405, 72143, 0x2D300033, 161.133, 59.0129, 190.105, -0.0733514, 0, 0, -0.997306, False, '2020-09-20 11:24:25'); /* Rynthid Platforms 2 Gen */
 /* @teleloc 0x2D300033 [161.132996 59.012901 190.104996] -0.073351 0.000000 0.000000 -0.997306 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30406, 72143, 0x2D30002B, 126.377, 55.1534, 190.105, 0.778406, 0, 0, -0.627761, False, '2020-09-20 11:24:56'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D30002B [126.376999 55.153400 190.104996] 0.778406 0.000000 0.000000 -0.627761 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30407, 72143, 0x2D30002D, 128.972, 106.94, 190.105, 0.999907, 0, 0, 0.0136172, False, '2020-09-20 11:25:40'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D30002D [128.972000 106.940002 190.104996] 0.999907 0.000000 0.000000 0.013617 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x72D30408, 72143, 0x2D30002E, 132.581, 131.299, 190.105, 0.838054, 0, 0, 0.545588, False, '2020-09-20 11:26:01'); /* Rynthid Platforms 2 Gen */
 /* @teleloc 0x2D30002E [132.580994 131.298996 190.104996] 0.838054 0.000000 0.000000 0.545588 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30409, 72143, 0x2D300036, 159.825, 130.996, 190.105, -0.132487, 0, 0, -0.991185, False, '2020-09-20 11:26:21'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D300036 [159.824997 130.996002 190.104996] -0.132487 0.000000 0.000000 -0.991185 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x72D3040A, 72143, 0x2D300035, 158.87, 111.18, 190.105, -0.157222, 0, 0, -0.987563, False, '2020-09-20 11:26:41'); /* Rynthid Platforms 2 Gen */
@@ -93,5 +69,16 @@ VALUES (0x72D3040F, 72143, 0x2D300034, 155.644, 83.7978, 155.065, 0.683562, 0, 0
 /* @teleloc 0x2D300034 [155.643997 83.797798 155.065002] 0.683562 0.000000 0.000000 0.729892 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x72D30410, 72143, 0x2D300012, 61.66017, 42.997837, 220.105, -0.69641924, 0, 0, -0.71763515, False, '2020-09-21 11:36:22'); /* Rynthid Platforms 2 Gen */
-/* @teleloc 0x2D300012 [61.660172 42.997837 220.104996] -0.696419 0.000000 0.000000 -0.717635 */
+VALUES (0x72D30410, 72143, 0x2D300012, 61.6602, 42.9978, 220.105, -0.696419, 0, 0, -0.717635, False, '2020-09-21 11:36:22'); /* Rynthid Platforms 2 Gen */
+/* @teleloc 0x2D300012 [61.660198 42.997799 220.104996] -0.696419 0.000000 0.000000 -0.717635 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x72D30411,  7924, 0x2D30002B, 139.33, 51.8718, 190.105, 1, 0, 0, 0, False, '2021-01-13 13:37:20'); /* Linkable Monster Generator ( 5 Min.) */
+/* @teleloc 0x2D30002B [139.330002 51.871799 190.104996] 1.000000 0.000000 0.000000 0.000000 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x72D30411, 0x72D30412, '2021-01-13 13:38:35') /* Lothus Guardian of Sorrows (51830) */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x72D30412, 51830, 0x2D30002B, 139.33, 51.8718, 190.079, 1, 0, 0, 0,  True, '2021-01-13 13:38:35'); /* Lothus Guardian of Sorrows */
+/* @teleloc 0x2D30002B [139.330002 51.871799 190.078995] 1.000000 0.000000 0.000000 0.000000 */
