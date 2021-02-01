7d8754c04b3dd79d05d84f3fc86d755b8ac41a3f
Patrick <phreekn.slacker@gmail.com>
2021-02-01 15:07:25 -0600

    Update gearxbowwait.sql

--- Database/Patches/2010-02-WintersKnight/8 QuestDefDB/gearxbowwait.sql
+++ Database/Patches/2010-02-WintersKnight/8 QuestDefDB/gearxbowwait.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'gearxbowwait';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('gearxbowwait', 518400, -1, 'Received Rewards for Gear Crossbow Quest', '2020-02-04 06:51:50');
+VALUES ('gearxbowwait', 72000, -1, 'Received Rewards for Gear Crossbow Quest', '2020-02-04 06:51:50');
