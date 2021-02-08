e802e808de27f976fbcca51c9a650af4e89ee4ea
Patrick <phreekn.slacker@gmail.com>
2021-02-08 13:25:59 -0600

    Update to quests/added contract

--- Database/Patches/2011-02-BloodFromStone/8 QuestDefDB/EnergyInfusedRockPickupTimer.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM quest WHERE name = 'EnergyInfusedRockPickupTimer';
-
-INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
-VALUES ('EnergyInfusedRockPickupTimer', 72000, -1, 'Player has picked up Energy Infused Rock', '2021-02-08-20:34:27');
