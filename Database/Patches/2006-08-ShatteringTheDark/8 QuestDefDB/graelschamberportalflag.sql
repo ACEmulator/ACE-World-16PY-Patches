615a4465dcae2c05609a33ef04e8034b01c3c0b4
Patrick <phreekn.slacker@gmail.com>
2021-02-13 09:42:29 -0600

    added portal flag and erase

---
+++ Database/Patches/2006-08-ShatteringTheDark/8 QuestDefDB/graelschamberportalflag.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'graelschamberportalflag';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('graelschamberportalflag', 0, 1, 'Player has access to Graels Chamber', '2020-12-15 06:51:50');
