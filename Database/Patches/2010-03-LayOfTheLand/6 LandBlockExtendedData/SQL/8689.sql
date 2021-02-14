28c46ef32071a307c5981b3c8cdcb576fa431ea4
OptimShi <OptimShi@users.noreply.github.com>
2021-02-14 11:43:00 -0700

    Menhir research (#807)
    * Menhir Research Quest
    
    * Added EmoteScript for 42519
    
    * Work in Progress - Not ready for merge
    
    * Cleaned up some issues, added more Inert ring locations listed on Wiki-talk page
    
    * Tested

---
+++ Database/Patches/2010-03-LayOfTheLand/6 LandBlockExtendedData/SQL/8689.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x78689001, 80210, 0x86890000, 26.9817, 126.69, 106.811, 1, 0, 0, 0, False, '2021-01-18 09:00:00'); /* Menhir Ring Eleven */
+/* @teleloc 0x86890000 [26.981701 126.690002 106.810997] 1.000000 0.000000 0.000000 0.000000 */
---
+++ Database/Patches/2010-03-LayOfTheLand/6 LandBlockExtendedData/SQL/9597.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x79597005, 80209, 0x95970000, 117.926, 109.011, 224.005, -0.749927, 0, 0, 0.661521, False, '2021-01-18 09:00:00'); /* Menhir Ring Ten */
+/* @teleloc 0x95970000 [117.926003 109.011002 224.005005] -0.749927 0.000000 0.000000 0.661521 */
