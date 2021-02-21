402d26337ad1eb6b7dd450f91a719cd57841c5ef
Ty Conner <tyconner@itcproductions.com>
2021-02-21 17:23:50 -0500

    Create 80123 Arena Wand.sql

---
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80123 Arena Wand.sql
@@ -0,0 +1,55 @@
+DELETE FROM `weenie` WHERE `class_Id` = 80123;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (80123, 'DCAArenaWand', 10, '2019-02-10 00:00:00') /* Creature */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (80123,   1,         16) /* ItemType - Creature */
+     , (80123,   6,         -1) /* ItemsCapacity */
+     , (80123,   7,         -1) /* ContainersCapacity */
+     , (80123,  16,         32) /* ItemUseable - Remote */
+     , (80123,  93,    6294556) /* PhysicsState - Ethereal, ReportCollisions, IgnoreCollisions, Gravity, LightingOn, ReportCollisionsAsEnvironment, EdgeSlide */
+     , (80123,  95,          8) /* RadarBlipColor - Yellow */
+     , (80123, 133,          1) /* ShowableOnRadar - ShowNever */
+     , (80123, 267,        300) /* Lifespan */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (80123,   1, True ) /* Stuck */
+     , (80123,  19, False) /* Attackable */
+     , (80123,  52, True ) /* AiImmobile */
+     , (80123,  82, True ) /* DontTurnOrMoveWhenGiving */
+     , (80123,  83, True ) /* NpcLooksLikeObject */
+     , (80123,  90, True ) /* NpcInteractsSilently */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (80123,  54,     0.1) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (80123,   1, 'Arena Wand') /* Name */
+     , (80123,  16, 'An arena wand.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (80123,   1,   33560752) /* Setup */
+     , (80123,   2,  150995446) /* MotionTable */
+     , (80123,   3,  536870932) /* SoundTable */
+     , (80123,   7,  268437353) /* ClothingBase */
+     , (80123,   8,  100690409) /* Icon */;
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (80123,   1, 200, 0, 0) /* Strength */
+     , (80123,   2, 250, 0, 0) /* Endurance */
+     , (80123,   3, 200, 0, 0) /* Quickness */
+     , (80123,   4, 260, 0, 0) /* Coordination */
+     , (80123,   5, 300, 0, 0) /* Focus */
+     , (80123,   6, 300, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (80123,   1,   150, 0, 0, 275) /* MaxHealth */
+     , (80123,   3,   235, 0, 0, 485) /* MaxStamina */
+     , (80123,   5,   300, 0, 0, 600) /* MaxMana */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (80123, 31, 0, 3, 0, 999, 0, 1642.85095430452) /* CreatureEnchantment Specialized */
+     , (80123, 32, 0, 3, 0, 999, 0, 1642.85095430452) /* ItemEnchantment     Specialized */
+     , (80123, 33, 0, 3, 0, 999, 0, 1642.85095430452) /* LifeMagic           Specialized */
+     , (80123, 34, 0, 3, 0, 999, 0, 1642.85095430452) /* WarMagic            Specialized */;
