9273aa15d37060bb279ee7a99fff99ac3e5539a7
Ty Conner <tyconner@itcproductions.com>
2021-02-23 14:07:23 -0500

    Update 00AB.sql

--- Database/Patches/2014-01-ForcesOfNature/6 LandBlockExtendedData/00AB.sql
+++ Database/Patches/2014-01-ForcesOfNature/6 LandBlockExtendedData/00AB.sql
@@ -41,59 +41,59 @@ VALUES (0x700AB00A, 80139, 0x00AB0110, 67.8248, -39.7031, -29.945, 1, 0, 0, 0, T
 /* @teleloc 0x00AB0110 [67.824799 -39.703098 -29.945000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB00B, 80106, 0x00AB013F, 71.3167, -50.3594, -24, 1, 0, 0, 0, True, '2021-02-20 18:37:04'); /* DCA Devices Generator */
+VALUES (0x700AB00B, 80106, 0x00AB013F, 71.3167, -50.3594, -24, 1, 0, 0, 0, False, '2021-02-20 18:37:04'); /* DCA Devices Generator */
 /* @teleloc 0x00AB013F [71.316704 -50.359402 -24.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB00C, 80106, 0x00AB013B, 70.9889, 0.125, -24, 1, 0, 0, 0, True, '2021-02-20 18:38:16'); /* DCA Devices Generator */
+VALUES (0x700AB00C, 80106, 0x00AB013B, 70.9889, 0.125, -24, 1, 0, 0, 0, False, '2021-02-20 18:38:16'); /* DCA Devices Generator */
 /* @teleloc 0x00AB013B [70.988899 0.125000 -24.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB00D, 80106, 0x00AB01FE, 99.5845, -57.9883, -12, 1, 0, 0, 0, True, '2021-02-20 18:38:28'); /* DCA Devices Generator */
+VALUES (0x700AB00D, 80106, 0x00AB01FE, 99.5845, -57.9883, -12, 1, 0, 0, 0, False, '2021-02-20 18:38:28'); /* DCA Devices Generator */
 /* @teleloc 0x00AB01FE [99.584503 -57.988300 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB00E, 80106, 0x00AB011C, 30.3944, -70.0117, -24, 1, 0, 0, 0, True, '2021-02-20 18:38:41'); /* DCA Devices Generator */
+VALUES (0x700AB00E, 80106, 0x00AB011C, 30.3944, -70.0117, -24, 1, 0, 0, 0, False, '2021-02-20 18:38:41'); /* DCA Devices Generator */
 /* @teleloc 0x00AB011C [30.394400 -70.011703 -24.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB00F, 80106, 0x00AB0244, 49.972, -50.1523, -6, 1, 0, 0, 0, True, '2021-02-20 18:38:54'); /* DCA Devices Generator */
+VALUES (0x700AB00F, 80106, 0x00AB0244, 49.972, -50.1523, -6, 1, 0, 0, 0, False, '2021-02-20 18:38:54'); /* DCA Devices Generator */
 /* @teleloc 0x00AB0244 [49.972000 -50.152302 -6.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB010, 80106, 0x00AB0175, 15.356, -19.7227, -12, 1, 0, 0, 0, True, '2021-02-20 18:39:16'); /* DCA Devices Generator */
+VALUES (0x700AB010, 80106, 0x00AB0175, 15.356, -19.7227, -12, 1, 0, 0, 0, False, '2021-02-20 18:39:16'); /* DCA Devices Generator */
 /* @teleloc 0x00AB0175 [15.356000 -19.722700 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB011, 80106, 0x00AB015D, 0.506104, -10.3281, -12, 1, 0, 0, 0, True, '2021-02-20 18:39:31'); /* DCA Devices Generator */
+VALUES (0x700AB011, 80106, 0x00AB015D, 0.506104, -10.3281, -12, 1, 0, 0, 0, False, '2021-02-20 18:39:31'); /* DCA Devices Generator */
 /* @teleloc 0x00AB015D [0.506104 -10.328100 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB012, 80106, 0x00AB01B1, 50.6257, -39.0859, -12, 1, 0, 0, 0, True, '2021-02-20 18:39:47'); /* DCA Devices Generator */
+VALUES (0x700AB012, 80106, 0x00AB01B1, 50.6257, -39.0859, -12, 1, 0, 0, 0, False, '2021-02-20 18:39:47'); /* DCA Devices Generator */
 /* @teleloc 0x00AB01B1 [50.625702 -39.085899 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB013, 80106, 0x00AB01E9, 89.5247, -0.789062, -12, 1, 0, 0, 0, True, '2021-02-20 18:40:01'); /* DCA Devices Generator */
+VALUES (0x700AB013, 80106, 0x00AB01E9, 89.5247, -0.789062, -12, 1, 0, 0, 0, False, '2021-02-20 18:40:01'); /* DCA Devices Generator */
 /* @teleloc 0x00AB01E9 [89.524696 -0.789062 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB014, 80106, 0x00AB0118, 28.155, -18.6133, -24, 1, 0, 0, 0, True, '2021-02-20 18:40:16'); /* DCA Devices Generator */
+VALUES (0x700AB014, 80106, 0x00AB0118, 28.155, -18.6133, -24, 1, 0, 0, 0, False, '2021-02-20 18:40:16'); /* DCA Devices Generator */
 /* @teleloc 0x00AB0118 [28.155001 -18.613300 -24.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB015, 80106, 0x00AB01E6, 81.2721, -49.8828, -12, 1, 0, 0, 0, True, '2021-02-20 18:40:45'); /* DCA Devices Generator */
+VALUES (0x700AB015, 80106, 0x00AB01E6, 81.2721, -49.8828, -12, 1, 0, 0, 0, False, '2021-02-20 18:40:45'); /* DCA Devices Generator */
 /* @teleloc 0x00AB01E6 [81.272102 -49.882801 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB016, 80106, 0x00AB0172, 11.6957, -69.6367, -12, 1, 0, 0, 0, True, '2021-02-20 18:40:58'); /* DCA Devices Generator */
+VALUES (0x700AB016, 80106, 0x00AB0172, 11.6957, -69.6367, -12, 1, 0, 0, 0, False, '2021-02-20 18:40:58'); /* DCA Devices Generator */
 /* @teleloc 0x00AB0172 [11.695700 -69.636703 -12.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB017, 80106, 0x00AB0236, 50.0197, -20.0742, -6, 1, 0, 0, 0, True, '2021-02-20 18:41:09'); /* DCA Devices Generator */
+VALUES (0x700AB017, 80106, 0x00AB0236, 50.0197, -20.0742, -6, 1, 0, 0, 0, False, '2021-02-20 18:41:09'); /* DCA Devices Generator */
 /* @teleloc 0x00AB0236 [50.019699 -20.074200 -6.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x700AB018, 80106, 0x00AB028D, 63.390015, -40.347656, 0, 1, 0, 0, 0, True, '2021-02-20 18:42:21'); /* DCA Devices Generator */
+VALUES (0x700AB018, 80106, 0x00AB028D, 63.390015, -40.347656, 0, 1, 0, 0, 0, False, '2021-02-20 18:42:21'); /* DCA Devices Generator */
 /* @teleloc 0x00AB028D [63.390015 -40.347656 0.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
@@ -101,21 +101,7 @@ VALUES (0x700AB019, 80140, 0x00AB0109, 49.7578, -34.6875, -29.945, 0.707107, 0, 
 /* @teleloc 0x00AB0109 [49.757801 -34.687500 -29.945000] 0.707107 0.000000 0.000000 -0.707107 */
 
 INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
-VALUES (0x700AB019, 0x700AB001, '2005-02-09 10:00:00') /* Statue of Death (80102) */
-     , (0x700AB019, 0x700AB00B, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB00C, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB00D, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB00E, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB00F, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB010, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB011, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB012, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB013, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB014, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB015, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB016, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB017, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */
-     , (0x700AB019, 0x700AB018, '2005-02-09 10:00:00') /* DCA Devices Generator (80106) */;
+VALUES (0x700AB019, 0x700AB001, '2005-02-09 10:00:00') /* Statue of Death (80102) */;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x700AB01A, 80142, 0x00AB0109, 47.3958, -34.5039, -29.945, 0.707107, 0, 0, 0.707107, False, '2021-02-20 18:21:13'); /* DCA Lum1 Linkable Generator */
