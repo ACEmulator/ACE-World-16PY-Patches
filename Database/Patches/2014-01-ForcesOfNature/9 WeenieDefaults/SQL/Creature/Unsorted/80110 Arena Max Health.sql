59725f8e5ab9b38ec0738e2882cd39cf07bbb7c3
Ty Conner <tyconner@itcproductions.com>
2021-02-23 13:49:45 -0500

    Update 80110 Arena Max Health.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80110 Arena Max Health.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80110 Arena Max Health.sql
@@ -32,7 +32,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (80110,   1,   33560761) /* Setup */
      , (80110,   2,  150995447) /* MotionTable */
      , (80110,   3,  536870932) /* SoundTable */
-     , (80110,   8,  100690409) /* Icon */;
+     , (80110,   8,  100690422) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (80110,   1, 200, 0, 0) /* Strength */
