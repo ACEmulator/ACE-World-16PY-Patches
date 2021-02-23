05b4602fc7ff8834145d218fee70453fa252c6d2
Ty Conner <tyconner@itcproductions.com>
2021-02-23 13:56:10 -0500

    Update 80122 Arena Sword.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80122 Arena Sword.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80122 Arena Sword.sql
@@ -32,8 +32,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (80122,   1,   33560744) /* Setup */
      , (80122,   2,  150995446) /* MotionTable */
      , (80122,   3,  536870932) /* SoundTable */
-     , (80122,   7,  268437353) /* ClothingBase */
-     , (80122,   8,  100690409) /* Icon */;
+     , (80122,   8,  100690419) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (80122,   1, 200, 0, 0) /* Strength */
