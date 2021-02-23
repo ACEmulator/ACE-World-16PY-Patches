9b614ce00cc75f86626589e6855c7e6688210bf7
Ty Conner <tyconner@itcproductions.com>
2021-02-23 13:55:49 -0500

    Update 80114 Arena Axe.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80114 Arena Axe.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80114 Arena Axe.sql
@@ -32,8 +32,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (80114,   1,   33560754) /* Setup */
      , (80114,   2,  150995446) /* MotionTable */
      , (80114,   3,  536870932) /* SoundTable */
-     , (80114,   7,  268437353) /* ClothingBase */
-     , (80114,   8,  100690409) /* Icon */;
+     , (80114,   8,  100690423) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (80114,   1, 200, 0, 0) /* Strength */
