0426b24c3c7058955173165fd862b78fca5f96b7
Ty Conner <tyconner@itcproductions.com>
2021-02-23 13:56:05 -0500

    Update 80120 Arena Spear.sql

--- Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80120 Arena Spear.sql
+++ Database/Patches/2014-01-ForcesOfNature/9 WeenieDefaults/SQL/Creature/Unsorted/80120 Arena Spear.sql
@@ -32,8 +32,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (80120,   1,   33560749) /* Setup */
      , (80120,   2,  150995447) /* MotionTable */
      , (80120,   3,  536870932) /* SoundTable */
-     , (80120,   7,  268437353) /* ClothingBase */
-     , (80120,   8,  100690409) /* Icon */;
+     , (80120,   8,  100690417) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (80120,   1, 200, 0, 0) /* Strength */
