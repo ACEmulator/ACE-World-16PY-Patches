6659d33c6a887545e92851abf05161a7c7fef8bf
Ty Conner <tyconner@itcproductions.com>
2021-02-24 15:41:17 -0500

    Update and rename Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Chest/Container/41806 Corpse.sql to Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Container/Container/41806 Corpse.sql

--- Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Container/Container/41806 Corpse.sql
+++ Database/Patches/2008-08-AncientPowers/9 WeenieDefaults/Container/Container/41806 Corpse.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 41806;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (41806, 'ace41806-corpse', 20, '2019-02-10 00:00:00') /* Chest */;
+VALUES (41806, 'ace41806-corpse', 21, '2019-02-10 00:00:00') /* Container */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (41806,   1,        512) /* ItemType - Container */
