3d2c1068ce63343f4865681870cb183d4989a0f0
Ty Conner <tyconner@itcproductions.com>
2021-02-24 23:57:01 -0500

    Cleanup tailoring items

--- Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Gem/42407 Platemail Gauntlets.sql
+++ Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Gem/42407 Platemail Gauntlets.sql
@@ -13,17 +13,11 @@ VALUES (42407,   1,       2048) /* ItemType - Gem */
      , (42407,  15,        653) /* StackUnitValue */
      , (42407,  16,     524296) /* ItemUseable - SourceContainedTargetContained */
      , (42407,  19,        653) /* Value */
-     , (42407,  65,        101) /* Placement - Resting */
      , (42407,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (42407,  94,          6) /* TargetType - Vestements */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (42407,   1, False) /* Stuck */
-     , (42407,  11, True ) /* IgnoreCollisions */
-     , (42407,  13, True ) /* Ethereal */
-     , (42407,  14, True ) /* GravityStatus */
-     , (42407,  19, True ) /* Attackable */
-     , (42407,  22, True ) /* Inscribable */;
+VALUES (42407,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (42407,   1, 'Platemail Gauntlets') /* Name */;
@@ -34,19 +28,4 @@ VALUES (42407,   1,   33554648) /* Setup */
      , (42407,   6,   67108990) /* PaletteBase */
      , (42407,   8,  100667341) /* Icon */
      , (42407,  22,  872415275) /* PhysicsEffectTable */
-     , (42407,  50,  100667895) /* IconOverlay */
-     , (42407, 8001, 1076654104) /* PCAPRecordedWeenieHeader - Value, Usable, StackSize, MaxStackSize, Container, Priority, TargetType, Burden, IconOverlay */
-     , (42407, 8003,         18) /* PCAPRecordedObjectDesc - Inscribable, Attackable */
-     , (42407, 8005,     137217) /* PCAPRecordedPhysicsDesc - CSetup, STable, PeTable, AnimationFrame */;
-
-INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
-VALUES (42407, 8000, 3335446005) /* PCAPRecordedObjectIID */;
-
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (42407, 67110015, 168, 6);
-
-INSERT INTO `weenie_properties_texture_map` (`object_Id`, `index`, `old_Id`, `new_Id`)
-VALUES (42407, 0, 83894333, 83894333);
-
-INSERT INTO `weenie_properties_anim_part` (`object_Id`, `index`, `animation_Id`)
-VALUES (42407, 0, 16778374);
+     , (42407,  50,  100667895) /* IconOverlay */;
--- Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Gem/42409 Yoroi Girth.sql
+++ Database/Patches/2010-04-SheddingSkin/9 WeenieDefaults/Gem/42409 Yoroi Girth.sql
@@ -14,27 +14,21 @@ VALUES (42409,   1,       2048) /* ItemType - Gem */
      , (42409,  16,     524296) /* ItemUseable - SourceContainedTargetContained */
      , (42409,  19,        653) /* Value */
      , (42409,  28,          0) /* ArmorLevel */
-     , (42409,  65,        101) /* Placement - Resting */
      , (42409,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (42409,  94,          6) /* TargetType - Vestements */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (42409,   1, False) /* Stuck */
-     , (42409,  11, True ) /* IgnoreCollisions */
-     , (42409,  13, True ) /* Ethereal */
-     , (42409,  14, True ) /* GravityStatus */
-     , (42409,  19, True ) /* Attackable */
-     , (42409,  22, True ) /* Inscribable */
+VALUES (42409,  22, True ) /* Inscribable */
      , (42409, 100, True ) /* Dyable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (42409,  13, 1.29999995231628) /* ArmorModVsSlash */
+VALUES (42409,  13,     1.3) /* ArmorModVsSlash */
      , (42409,  14,       1) /* ArmorModVsPierce */
      , (42409,  15,       1) /* ArmorModVsBludgeon */
-     , (42409,  16, 0.400000005960464) /* ArmorModVsCold */
-     , (42409,  17, 0.400000005960464) /* ArmorModVsFire */
-     , (42409,  18, 0.600000023841858) /* ArmorModVsAcid */
-     , (42409,  19, 0.400000005960464) /* ArmorModVsElectric */
+     , (42409,  16,     0.4) /* ArmorModVsCold */
+     , (42409,  17,     0.4) /* ArmorModVsFire */
+     , (42409,  18,     0.6) /* ArmorModVsAcid */
+     , (42409,  19,     0.4) /* ArmorModVsElectric */
      , (42409, 165,       1) /* ArmorModVsNether */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
@@ -47,14 +41,3 @@ VALUES (42409,   1,   33554647) /* Setup */
      , (42409,   8,  100668146) /* Icon */
      , (42409,  22,  872415275) /* PhysicsEffectTable */
      , (42409,  50,  100667895) /* IconOverlay */;
-
-INSERT INTO `weenie_properties_palette` (`object_Id`, `sub_Palette_Id`, `offset`, `length`)
-VALUES (42409, 67110015, 80, 12)
-     , (42409, 67110348, 92, 4);
-
-INSERT INTO `weenie_properties_texture_map` (`object_Id`, `index`, `old_Id`, `new_Id`)
-VALUES (42409, 0, 83889072, 83886236)
-     , (42409, 0, 83889342, 83886236);
-
-INSERT INTO `weenie_properties_anim_part` (`object_Id`, `index`, `animation_Id`)
-VALUES (42409, 0, 16778376);
