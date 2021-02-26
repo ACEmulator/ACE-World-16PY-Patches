4028d6dcfb29af66c67fb7dc71060cf193503064
Patrick <phreekn.slacker@gmail.com>
2021-02-26 14:09:25 -0600

    Quest Updated to EOR

---
+++ Database/Patches/2006-11-RememberingThePast/8 QuestDefDB/FragofYajasArmPickupTimer.sql
@@ -0,0 +1,4 @@
+DELETE FROM quest WHERE name = 'FragofYajasArmPickupTimer';
+
+INSERT INTO quest (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FragofYajasArmPickupTimer', 72000, -1, 'Player has picked up Fragment of Yajas Arm', '2021-02-26-20:34:27');
