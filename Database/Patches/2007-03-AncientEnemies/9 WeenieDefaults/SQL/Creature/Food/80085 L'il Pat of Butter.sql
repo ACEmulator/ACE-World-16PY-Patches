e3e8df0f9da7fd99f584f80e2b4f8c791e0b77b8
Ty Conner <tyconner@itcproductions.com>
2021-03-03 20:17:09 -0500

    Update 80085 L'il Pat of Butter.sql

--- Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/SQL/Creature/Food/80085 L'il Pat of Butter.sql
+++ Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/SQL/Creature/Food/80085 L'il Pat of Butter.sql
@@ -84,16 +84,14 @@ VALUES (80085,   1,     0, 0, 0, 90) /* MaxHealth */
      , (80085,   5,     0, 0, 0, 90) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (80085,  6, 0, 2, 0,  30, 0, 914.535082072975) /* MeleeDefense        Trained */
-     , (80085,  7, 0, 2, 0,  50, 0, 914.535082072975) /* MissileDefense      Trained */
-     , (80085, 13, 0, 2, 0,  38, 0, 914.535082072975) /* UnarmedCombat       Trained */
-     , (80085, 14, 0, 2, 0, 110, 0, 914.535082072975) /* ArcaneLore          Trained */
-     , (80085, 15, 0, 2, 0,  10, 0, 914.535082072975) /* MagicDefense        Trained */
-     , (80085, 20, 0, 2, 0, 185, 0, 914.535082072975) /* Deception           Trained */
-     , (80085, 24, 0, 2, 0,  40, 0, 914.535082072975) /* Run                 Trained */
-     , (80085, 31, 0, 2, 0,  40, 0, 914.535082072975) /* CreatureEnchantment Trained */
-     , (80085, 33, 0, 2, 0,  80, 0, 914.535082072975) /* LifeMagic           Trained */
-     , (80085, 34, 0, 2, 0,  40, 0, 914.535082072975) /* WarMagic            Trained */;
+VALUES (80085,  6, 0, 3, 0, 100, 0, 335.880876520311) /* MeleeDefense        Specialized */
+     , (80085,  7, 0, 3, 0,  20, 0, 335.880876520311) /* MissileDefense      Specialized */
+     , (80085, 13, 0, 3, 0,   5, 0, 335.880876520311) /* UnarmedCombat       Specialized */
+     , (80085, 14, 0, 2, 0, 110, 0, 335.880876520311) /* ArcaneLore          Trained */
+     , (80085, 15, 0, 3, 0,  30, 0, 335.880876520311) /* MagicDefense        Specialized */
+     , (80085, 20, 0, 2, 0,  45, 0, 335.880876520311) /* Deception           Trained */
+     , (80085, 24, 0, 2, 0,  40, 0, 335.880876520311) /* Run                 Trained */
+     , (80085, 34, 0, 3, 0,  65, 0, 335.880876520311) /* WarMagic            Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (80085,  0,  4,  2,  0.3,   60,   29,   38,   16,   29,   48,   48,   42,    0, 1,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Head */
@@ -102,10 +100,8 @@ VALUES (80085,  0,  4,  2,  0.3,   60,   29,   38,   16,   29,   48,   48,   42,
      , (80085, 16,  4,  0,    0,   60,   29,   38,   16,   29,   48,   48,   42,    0, 2,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4) /* Torso */;
 
 INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
-VALUES (80085,   171,   2.25)  /* Fester Other I */
-     , (80085,   194,   2.25)  /* Exhaustion Other I */
-     , (80085,   218,   2.25)  /* Mana Depletion Other I */
-     , (80085,  1237,   2.25)  /* Drain Health Other I */;
+VALUES (80085,  1097,    2.5)  /* Flaming Missile */
+     , (80085,  1481,    2.5)  /* Flaming Missile Volley */;
 
 INSERT INTO `weenie_properties_event_filter` (`object_Id`, `event`)
 VALUES (80085,  94) /* ATTACK_NOTIFICATION_EVENT */
@@ -126,6 +122,3 @@ SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (@parent_id,  0,   5 /* Motion */, 0, 1, 268435537 /* Twitch1 */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
-VALUES (80085, 1, 14900,  1, 0, 0, False) /* Create Symbol of Friendship (14900) for Contain */;
