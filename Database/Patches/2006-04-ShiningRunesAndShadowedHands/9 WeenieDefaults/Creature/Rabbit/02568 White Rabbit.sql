729f6f4be20c402d6e2fb985b067b51e034a996d
Ty Conner <tyconner@itcproductions.com>
2021-03-06 14:46:47 -0500

    Update 02568 White Rabbit.sql

--- Database/Patches/2006-04-ShiningRunesAndShadowedHands/9 WeenieDefaults/Creature/Rabbit/02568 White Rabbit.sql
+++ Database/Patches/2006-04-ShiningRunesAndShadowedHands/9 WeenieDefaults/Creature/Rabbit/02568 White Rabbit.sql
@@ -69,7 +69,8 @@ VALUES (2568,   1,       5) /* HeartbeatInterval */
      , (2568, 127,       2) /* AiCounteractEnchantment */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (2568,   1, 'White Rabbit') /* Name */;
+VALUES (2568,   1, 'White Rabbit') /* Name */
+     , (2568,  34, 'springbabies') /* GeneratorEvent */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (2568,   1,   33555579) /* Setup */
@@ -96,17 +97,17 @@ VALUES (2568,   1,  2266, 0, 0, 2599) /* MaxHealth */
      , (2568,   5,     0, 0, 0, 666) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (2568,  6, 0, 3, 0,   0, 0, 335.664341986613) /* MeleeDefense        Specialized */
-     , (2568,  7, 0, 3, 0, 200, 0, 335.664341986613) /* MissileDefense      Specialized */
-     , (2568, 13, 0, 3, 0, 888, 0, 335.664341986613) /* UnarmedCombat       Specialized */
-     , (2568, 14, 0, 2, 0, 400, 0, 335.664341986613) /* ArcaneLore          Trained */
-     , (2568, 15, 0, 3, 0, 210, 0, 335.664341986613) /* MagicDefense        Specialized */
-     , (2568, 20, 0, 2, 0, 100, 0, 335.664341986613) /* Deception           Trained */
-     , (2568, 22, 0, 2, 0,  10, 0, 335.664341986613) /* Jump                Trained */
-     , (2568, 24, 0, 2, 0, 134, 0, 335.664341986613) /* Run                 Trained */
-     , (2568, 31, 0, 3, 0,   0, 0, 335.664341986613) /* CreatureEnchantment Specialized */
-     , (2568, 33, 0, 3, 0,   0, 0, 335.664341986613) /* LifeMagic           Specialized */
-     , (2568, 34, 0, 3, 0,   0, 0, 335.664341986613) /* WarMagic            Specialized */;
+VALUES (2568,  6, 0, 3, 0,   0, 0, 335.591574672111) /* MeleeDefense        Specialized */
+     , (2568,  7, 0, 3, 0, 200, 0, 335.591574672111) /* MissileDefense      Specialized */
+     , (2568, 45, 0, 3, 0, 888, 0, 335.591574672111) /* LightWeapons        Specialized */
+     , (2568, 14, 0, 2, 0, 400, 0, 335.591574672111) /* ArcaneLore          Trained */
+     , (2568, 15, 0, 3, 0, 210, 0, 335.591574672111) /* MagicDefense        Specialized */
+     , (2568, 20, 0, 2, 0, 100, 0, 335.591574672111) /* Deception           Trained */
+     , (2568, 22, 0, 2, 0,  10, 0, 335.591574672111) /* Jump                Trained */
+     , (2568, 24, 0, 2, 0, 134, 0, 335.591574672111) /* Run                 Trained */
+     , (2568, 31, 0, 3, 0,   0, 0, 335.591574672111) /* CreatureEnchantment Specialized */
+     , (2568, 33, 0, 3, 0,   0, 0, 335.591574672111) /* LifeMagic           Specialized */
+     , (2568, 34, 0, 3, 0,   0, 0, 335.591574672111) /* WarMagic            Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (2568,  0,  2, 150, 0.75,  300,  600,  600,  600,  600,  498,  600,  600,    0, 1, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0) /* Head */
@@ -194,3 +195,6 @@ VALUES (2568, 1,  8400,  0, 0, 0.5, False) /* Create Orb of the Bunny Booty (840
      , (2568, 9,     0,  0, 0, 0.5, False) /* Create nothing for ContainTreasure */
      , (2568, 9, 12128,  0, 0, 0.5, False) /* Create White Rabbit Carcass (12128) for ContainTreasure */
      , (2568, 9,     0,  0, 0, 0.5, False) /* Create nothing for ContainTreasure */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (2568, 1, 6080, 0, 1, -1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate White Bunny (6080) (x1 up to max of -1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
