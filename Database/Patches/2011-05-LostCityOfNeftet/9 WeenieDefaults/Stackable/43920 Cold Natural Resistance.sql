b44ba40fca866d6074275f98419c639a0762e55c
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-03-06 11:33:56 -0600

    NPCs/Tokens (#861)

---
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Stackable/43920 Cold Natural Resistance.sql
@@ -0,0 +1,32 @@
+DELETE FROM `weenie` WHERE `class_Id` = 43920;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (43920, 'ace43920-coldnaturalresistance', 51, '2019-02-10 00:00:00') /* Stackable */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43920,   1,        128) /* ItemType - Misc */
+     , (43920,   3,          2) /* PaletteTemplate - Blue */
+     , (43920,   5,          1) /* EncumbranceVal */
+     , (43920,  11,          2) /* MaxStackSize */
+     , (43920,  12,          1) /* StackSize */
+     , (43920,  13,          1) /* StackUnitEncumbrance */
+     , (43920,  15,          1) /* StackUnitValue */
+     , (43920,  16,          1) /* ItemUseable - No */
+     , (43920,  19,          1) /* Value */
+     , (43920,  33,          1) /* Bonded - Bonded */
+     , (43920,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (43920, 114,          1) /* Attuned - Attuned */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43920,  39,     1.2) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43920,   1, 'Cold Natural Resistance') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43920,   1,   33556750) /* Setup */
+     , (43920,   3,  536870932) /* SoundTable */
+     , (43920,   6,   67111919) /* PaletteBase */
+     , (43920,   7,  268435841) /* ClothingBase */
+     , (43920,   8,  100691809) /* Icon */
+     , (43920,  22,  872415275) /* PhysicsEffectTable */;
---
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Stackable/43921 Slashing Natural Resistance.sql
@@ -0,0 +1,32 @@
+DELETE FROM `weenie` WHERE `class_Id` = 43921;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (43921, 'ace43921-slashingnaturalresistance', 51, '2019-02-10 00:00:00') /* Stackable */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43921,   1,        128) /* ItemType - Misc */
+     , (43921,   3,          2) /* PaletteTemplate - Blue */
+     , (43921,   5,          1) /* EncumbranceVal */
+     , (43921,  11,          2) /* MaxStackSize */
+     , (43921,  12,          1) /* StackSize */
+     , (43921,  13,          1) /* StackUnitEncumbrance */
+     , (43921,  15,          1) /* StackUnitValue */
+     , (43921,  16,          1) /* ItemUseable - No */
+     , (43921,  19,          1) /* Value */
+     , (43921,  33,          1) /* Bonded - Bonded */
+     , (43921,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (43921, 114,          1) /* Attuned - Attuned */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43921,  39,     1.2) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43921,   1, 'Slashing Natural Resistance') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43921,   1,   33556750) /* Setup */
+     , (43921,   3,  536870932) /* SoundTable */
+     , (43921,   6,   67111919) /* PaletteBase */
+     , (43921,   7,  268435841) /* ClothingBase */
+     , (43921,   8,  100691811) /* Icon */
+     , (43921,  22,  872415275) /* PhysicsEffectTable */;
