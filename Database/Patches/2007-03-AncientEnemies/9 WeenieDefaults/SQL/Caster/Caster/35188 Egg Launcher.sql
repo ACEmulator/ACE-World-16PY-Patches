27ff56148f11379fc836555fa9a576af1afb5838
Ty Conner <tyconner@itcproductions.com>
2021-03-06 23:20:08 -0500

    Egg Launcher

--- Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/SQL/Caster/Caster/35188 Egg Launcher.sql
+++ Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/SQL/Caster/Caster/35188 Egg Launcher.sql
@@ -18,27 +18,26 @@ VALUES (35188,   1,      32768) /* ItemType - Caster */
      , (35188, 107,        300) /* ItemCurMana */
      , (35188, 108,        300) /* ItemMaxMana */
      , (35188, 117,         10) /* ItemManaCost */
+     , (35188, 150,        103) /* HookPlacement - Hook */
      , (35188, 151,          2) /* HookType - Wall */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (35188,  22, True ) /* Inscribable */
-     , (35188,  23, True ) /* DestroyOnSell */;
+VALUES (35188,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (35188,  29,       1) /* WeaponDefense */
-     , (35188,  39,     0.7) /* DefaultScale */
-     , (35188, 144,       0) /* ManaConversionMod */;
+     , (35188,  39,     0.7) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (35188,   1, 'Egg Launcher') /* Name */
      , (35188,  16, 'A nervous hen.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (35188,    1,   33560288) /* Setup */
-     , (35188,    3,  536870932) /* SoundTable */
-     , (35188,    6,   67110219) /* PaletteBase */
-     , (35188,    7,  268436660) /* ClothingBase */
-     , (35188,    8,  100674625) /* Icon */
-     , (35188,   22,  872415275) /* PhysicsEffectTable */
-     , (35188,   27, 1090519043) /* UseUserAnimation - Ready */
-     , (35188,   28,       4078) /* Spell - EggBolt */;
+VALUES (35188,   1,   33560288) /* Setup */
+     , (35188,   3,  536870932) /* SoundTable */
+     , (35188,   6,   67110219) /* PaletteBase */
+     , (35188,   7,  268436660) /* ClothingBase */
+     , (35188,   8,  100674625) /* Icon */
+     , (35188,  22,  872415275) /* PhysicsEffectTable */
+     , (35188,  27, 1073741873) /* UseUserAnimation - MagicHeal */
+     , (35188,  28,       4078) /* Spell - EggBolt */;
--- Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/SQL/ProjectileSpell/None/35179 Egg.sql
+++ Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/SQL/ProjectileSpell/None/35179 Egg.sql
@@ -8,19 +8,18 @@ VALUES (35179,   1,          0) /* ItemType - None */
      , (35179,   8,         25) /* Mass */
      , (35179,   9,          0) /* ValidLocations - None */
      , (35179,  66,          0) /* CheckpointStatus */
-     , (35179,  93,     133960) /* PhysicsState - Ethereal, IgnoreCollisions, LightingOn, Inelastic */;
+     , (35179,  93,     133140) /* PhysicsState - Ethereal, IgnoreCollisions, LightingOn, Inelastic */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (35179,   1, True ) /* Stuck */
      , (35179,  14, False) /* GravityStatus */
      , (35179,  15, True ) /* LightsStatus */
-     , (35179,  16, True ) /* ScriptedCollision */
      , (35179,  17, True ) /* Inelastic */
      , (35179,  24, True ) /* UiHidden */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (35179,  39,     0.9) /* DefaultScale */
-     , (35179,  26,      15) /* MaximumVelocity */
+	 , (35179,  26,      15) /* MaximumVelocity */
      , (35179,  78,       1) /* Friction */
      , (35179,  79,       0) /* Elasticity */;
 
@@ -28,7 +27,7 @@ INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (35179,   1, 'Egg') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (35179,   1,   33554673) /* Setup */
+VALUES (35179,   1,   33560268) /* Setup */
      , (35179,   3,  536871113) /* SoundTable */
      , (35179,   8,  100667460) /* Icon */
      , (35179,  28,       4078) /* Spell - EggBolt */;
