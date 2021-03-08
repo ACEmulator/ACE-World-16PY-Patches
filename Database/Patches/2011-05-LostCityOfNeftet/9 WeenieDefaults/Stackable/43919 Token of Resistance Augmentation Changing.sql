f09c7428e5e8cdaf9b3a8efe14a8db02e1657a92
Ty Conner <tyconner@itcproductions.com>
2021-03-07 19:38:23 -0500

    Update 43919 Token of Resistance Augmentation Changing.sql

--- Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Stackable/43919 Token of Resistance Augmentation Changing.sql
+++ Database/Patches/2011-05-LostCityOfNeftet/9 WeenieDefaults/Stackable/43919 Token of Resistance Augmentation Changing.sql
@@ -13,17 +13,11 @@ VALUES (43919,   1,       2048) /* ItemType - Gem */
      , (43919,  16,          1) /* ItemUseable - No */
      , (43919,  19,          1) /* Value */
      , (43919,  33,          1) /* Bonded - Bonded */
-     , (43919,  65,        101) /* Placement - Resting */
      , (43919,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (43919, 114,          1) /* Attuned - Attuned */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (43919,   1, False) /* Stuck */
-     , (43919,  11, True ) /* IgnoreCollisions */
-     , (43919,  13, True ) /* Ethereal */
-     , (43919,  14, True ) /* GravityStatus */
-     , (43919,  19, True ) /* Attackable */
-     , (43919,  22, True ) /* Inscribable */;
+VALUES (43919,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (43919,   1, 'Token of Resistance Augmentation Changing') /* Name */
