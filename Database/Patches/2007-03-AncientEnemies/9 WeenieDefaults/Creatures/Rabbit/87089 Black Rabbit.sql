f8ce017d491120dc0c82e1d95254989dfce281b7
Ty Conner <tyconner@itcproductions.com>
2021-03-07 22:51:52 -0500

    Update 87089 Black Rabbit.sql

--- Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Creatures/Rabbit/87089 Black Rabbit.sql
+++ Database/Patches/2007-03-AncientEnemies/9 WeenieDefaults/Creatures/Rabbit/87089 Black Rabbit.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 87089;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (87089, 'ace87089rabbitblack', 10, '2019-09-13 00:00:00') /* Creature */;
+VALUES (87089, 'rabbitblackasheronisland', 10, '2019-09-13 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (87089,   1,         16) /* ItemType - Creature */
@@ -15,19 +15,19 @@ VALUES (87089,   1,         16) /* ItemType - Creature */
      , (87089,  67,         64) /* Tolerance - Retaliate */
      , (87089,  68,          9) /* TargetingTactic - Random, TopDamager */
      , (87089,  72,         41) /* FriendType - Bunny */
-     , (87089,  81,          1) /* MaxGeneratedObjects */
+     , (87089,  81,          2) /* MaxGeneratedObjects */
+     , (87089,  82,          2) /* InitGeneratedObjects */
      , (87089,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
      , (87089, 103,          1) /* GeneratorDestructionType - Nothing */
      , (87089, 133,          4) /* ShowableOnRadar - ShowAlways */
+     , (87089, 142,          3) /* GeneratorTimeType - Event */
      , (87089, 146,        800) /* XpOverride */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (87089,   1, True ) /* Stuck */
      , (87089,  11, False) /* IgnoreCollisions */
      , (87089,  12, True ) /* ReportCollisions */
-     , (87089,  13, False) /* Ethereal */
-     , (87089,  14, True ) /* GravityStatus */
-     , (87089,  19, True ) /* Attackable */;
+     , (87089,  13, False) /* Ethereal */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (87089,   1,       5) /* HeartbeatInterval */
@@ -46,6 +46,7 @@ VALUES (87089,   1,       5) /* HeartbeatInterval */
      , (87089,  34,       3) /* PowerupTime */
      , (87089,  36,       1) /* ChargeSpeed */
      , (87089,  39,     1.5) /* DefaultScale */
+     , (87089,  41,    3600) /* RegenerationInterval */
      , (87089,  43,       1) /* GeneratorRadius */
      , (87089,  64,       1) /* ResistSlash */
      , (87089,  65,       1) /* ResistPierce */
@@ -63,7 +64,8 @@ VALUES (87089,   1,       5) /* HeartbeatInterval */
      , (87089, 125,       1) /* ResistHealthDrain */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (87089,   1, 'Black Rabbit') /* Name */;
+VALUES (87089,   1, 'Black Rabbit') /* Name */
+     , (87089,  34, 'springbunnyevent') /* GeneratorEvent */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (87089,   1,   33555579) /* Setup */
@@ -87,25 +89,20 @@ VALUES (87089,   1,     0, 0, 0, 5) /* MaxHealth */
      , (87089,   5,     0, 0, 0, 10) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (87089,  6, 0, 3, 0,   0, 0, 0) /* MeleeDefense        Specialized */
-     , (87089,  7, 0, 3, 0,   0, 0, 0) /* MissileDefense      Specialized */
-     , (87089, 15, 0, 3, 0,   0, 0, 0) /* MagicDefense        Specialized */
-     , (87089, 22, 0, 2, 0,  10, 0, 0) /* Jump                Trained */
-     , (87089, 24, 0, 2, 0,  10, 0, 0) /* Run                 Trained */
-     , (87089, 45, 0, 3, 0,   0, 0, 0) /* LightWeapons        Specialized */;
+VALUES (87089,  6, 0, 3, 0,   0, 0, 335.519186821404) /* MeleeDefense        Specialized */
+     , (87089,  7, 0, 3, 0,   0, 0, 335.519186821404) /* MissileDefense      Specialized */
+     , (87089, 45, 0, 3, 0,   0, 0, 335.519186821404) /* LightWeapons        Specialized */
+     , (87089, 15, 0, 3, 0,   0, 0, 335.519186821404) /* MagicDefense        Specialized */
+     , (87089, 22, 0, 2, 0,  10, 0, 335.519186821404) /* Jump                Trained */
+     , (87089, 24, 0, 2, 0,  10, 0, 335.519186821404) /* Run                 Trained */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (87089,  0,  2,  2, 0.75,    8,    8,    8,    8,    8,    8,    8,    8,    0, 1, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0, 0.33,  0.4,    0) /* Head */
      , (87089, 16,  4,  4, 0.75,    8,    8,    8,    8,    8,    8,    8,    8,    0, 2, 0.67,  0.4, 0.75, 0.67,  0.4, 0.75, 0.67,  0.4, 0.75, 0.67,  0.4, 0.75) /* Torso */
      , (87089, 17,  4,  0,    0,    8,    8,    8,    8,    8,    8,    8,    8,    0, 3,    0,  0.2, 0.25,    0,  0.2, 0.25,    0,  0.2, 0.25,    0,  0.2, 0.25) /* Tail */;
 
-INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (87089,  3 /* Death */,   0.25, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-SET @parent_id = LAST_INSERT_ID();
-
-INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  72 /* Generate */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `weenie_properties_event_filter` (`object_Id`, `event`)
+VALUES (87089, 414) /* PLAYER_DEATH_EVENT */;
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (87089,  5 /* HeartBeat */,    0.1, NULL, 2147483708 /* HandCombat */, 1090519043 /* Ready */, NULL, NULL, NULL, NULL);
@@ -125,10 +122,7 @@ VALUES (@parent_id,  0,   5 /* Motion */, 0, 1, 268435537 /* Twitch1 */, NULL, N
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (87089, 9,  5633,  0, 0, 0.05, False) /* Create Rabbit Carcass (5633) for ContainTreasure */
-     , (87089, 9,     0,  0, 0, 0.95, False) /* Create nothing for ContainTreasure */
-     , (87089, 9, 27249,  0, 0, 0.5, False) /* Create Spring Cleaner Title Token (27249) for ContainTreasure */
-     , (87089, 9,     0,  0, 0, 0.5, False) /* Create nothing for ContainTreasure */;
+     , (87089, 9,     0,  0, 0, 0.95, False) /* Create nothing for ContainTreasure */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (87089, 0.35, 87094, 3, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate White Rabbit (87094) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (87089, 0.65, 87088, 3, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Hulking Bunny (87088) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (87089, 1, 87090, 0, 1, -1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Black Bunny (87090) (x1 up to max of -1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
