da02883394eeb166c07d8c84d084b3e5d618ae3c
Adam Plumb <adamplumb@gmail.com>
2021-03-08 18:56:17 -0500

    Fix success and fail messages

--- Database/Patches/2007-06-Evolution/4 CraftTable/08836 Mana Phial of Fester.sql
+++ Database/Patches/2007-06-Evolution/4 CraftTable/08836 Mana Phial of Fester.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 8836;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (8836, 0, 38 /* Alchemy */, 425, 0, 41785 /* Mana Phial of Fester */, 5, 'You create 5 Mana Phials of Fester.', 0, 0, 'You fail to create the Mana Phial of Fester.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-03-06 13:00:00');
+VALUES (8836, 0, 38 /* Alchemy */, 425, 0, 41785 /* Mana Phial of Fester */, 5, 'You create 5 Mana Phials of Fester.', 0, 0, 'You fail to create the Mana Phials of Fester.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-03-06 13:00:00');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 8836;
 
