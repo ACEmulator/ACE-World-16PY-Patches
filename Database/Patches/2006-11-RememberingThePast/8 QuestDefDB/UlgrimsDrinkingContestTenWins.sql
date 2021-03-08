8723c69eb408c7e8141dd30b321edf544bd92d7e
Patrick <phreekn.slacker@gmail.com>
2021-03-08 10:34:17 -0600

    DrinkingContest

---
+++ Database/Patches/2006-11-RememberingThePast/8 QuestDefDB/UlgrimsDrinkingContestTenWins.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'UlgrimsDrinkingContestTenWins';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('UlgrimsDrinkingContestTenWins', 0, 10, 'Player stamp for ten wins in Ulgrims Drinking Contest', '2020-03-04 06:51:50');
---
+++ Database/Patches/2006-11-RememberingThePast/B GameEventDefDB/UlgrimsDrinkingContestevent.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'UlgrimsDrinkingContestevent';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('UlgrimsDrinkingContestevent', -1, -1, 3, '2021-03-05 00:00:00');
