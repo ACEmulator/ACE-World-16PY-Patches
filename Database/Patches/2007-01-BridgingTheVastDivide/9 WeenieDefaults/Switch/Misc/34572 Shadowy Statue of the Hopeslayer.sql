8e41dbeaeebed290a47335e3be8d9e42df624544
Ty Conner <tyconner@itcproductions.com>
2021-03-09 13:15:14 -0500

    Change Statue from Generic to Switch

--- Database/Patches/2007-01-BridgingTheVastDivide/9 WeenieDefaults/Switch/Misc/34572 Shadowy Statue of the Hopeslayer.sql
+++ Database/Patches/2007-01-BridgingTheVastDivide/9 WeenieDefaults/Switch/Misc/34572 Shadowy Statue of the Hopeslayer.sql
@@ -1,16 +1,20 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34572;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34572, 'ace34572-shadowystatueofthehopeslayer', 1, '2019-02-10 00:00:00') /* Generic */;
+VALUES (34572, 'ace34572-shadowystatueofthehopeslayer', 26, '2019-02-10 00:00:00') /* Switch */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34572,   1,        128) /* ItemType - Misc */
      , (34572,   5,       6660) /* EncumbranceVal */
+     , (34572,   8,        200) /* Mass */
+     , (34572,   9,          0) /* ValidLocations - None */
      , (34572,  16,         48) /* ItemUseable - ViewedRemote */
      , (34572,  19,          0) /* Value */
-     , (34572,  81,          4) /* MaxGeneratedObjects */
+     , (34572,  81,          1) /* MaxGeneratedObjects */
      , (34572,  82,          0) /* InitGeneratedObjects */
-     , (34572,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */;
+     , (34572,  83,      65554) /* ActivationResponse - Use, Talk, Generate */
+     , (34572,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
+     , (34572, 119,          1) /* Active */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (34572,   1, True ) /* Stuck */;
@@ -23,7 +27,8 @@ VALUES (34572,  39,    0.75) /* DefaultScale */
      , (34572,  76,    0.25) /* Translucency */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (34572,   1, 'Shadowy Statue of the Hopeslayer') /* Name */;
+VALUES (34572,   1, 'Shadowy Statue of the Hopeslayer') /* Name */
+     , (34572,  17, 'The fragile statue, crafted of pure shadow, begins to fade as you touch it.') /* ActivationTalk */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (34572,   1,   33560169) /* Setup */
@@ -31,15 +36,16 @@ VALUES (34572,   1,   33560169) /* Setup */
      , (34572,   8,  100689314) /* Icon */
      , (34572,  22,  872415275) /* PhysicsEffectTable */;
 
+INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
+VALUES (34572,  16,          0) /* ActivationTarget */;
+
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (34572,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  18 /* DirectBroadcast */, 0, 1, NULL, 'The fragile statue, crafted of pure shadow, begins to fade as you touch it.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-     , (@parent_id,  1,  72 /* Generate */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-     , (@parent_id,  2,  77 /* DeleteSelf */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id,  0,  77 /* DeleteSelf */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (34572, -1, 87283, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Void Knight (87283) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
