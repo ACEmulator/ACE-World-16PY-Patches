b4cf588a8daea2b2e269caa33a88af616633085a
Patrick <phreekn.slacker@gmail.com>
2021-03-12 17:02:54 -0600

    Update 87201 Celestial Illuminator.sql

--- Database/Patches/2008-03-FoolishAmbition/9 WeenieDefaults/Creature/Human/87201 Celestial Illuminator.sql
+++ Database/Patches/2008-03-FoolishAmbition/9 WeenieDefaults/Creature/Human/87201 Celestial Illuminator.sql
@@ -88,6 +88,20 @@ VALUES (87201,   1,   785, 0, 0, 890) /* MaxHealth */
      , (87201,   3,   700, 0, 0, 910) /* MaxStamina */
      , (87201,   5,   530, 0, 0, 1000) /* MaxMana */;
 
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (87201,  6, 0, 2, 0, 576, 0, 0) /* MeleeDefense        Trained */
+     , (87201,  7, 0, 2, 0, 149, 0, 0) /* MissileDefense      Trained */
+     , (87201, 15, 0, 2, 0, 170, 0, 0) /* MagicDefense        Trained */
+     , (87201, 16, 0, 2, 0, 416, 0, 0) /* ManaConversion      Trained */
+     , (87201, 31, 0, 2, 0, 416, 0, 0) /* CreatureEnchantment Trained */
+     , (87201, 33, 0, 2, 0, 416, 0, 0) /* LifeMagic           Trained */
+     , (87201, 34, 0, 2, 0, 416, 0, 0) /* WarMagic            Trained */
+     , (87201, 41, 0, 2, 0, 132, 0, 0) /* TwoHandedCombat     Trained */
+     , (87201, 43, 0, 2, 0, 416, 0, 0) /* VoidMagic           Trained */
+     , (87201, 44, 0, 2, 0, 132, 0, 0) /* HeavyWeapons        Trained */
+     , (87201, 45, 0, 2, 0, 132, 0, 0) /* LightWeapons        Trained */
+     , (87201, 46, 0, 2, 0, 132, 0, 0) /* FinesseWeapons      Trained */;
+
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (87201,  0,  4,  0,    0,  475,  475,  475,  475,  475,  475,  475,  475,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
      , (87201,  1,  4,  0,    0,  475,  475,  475,  475,  475,  475,  475,  475,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
