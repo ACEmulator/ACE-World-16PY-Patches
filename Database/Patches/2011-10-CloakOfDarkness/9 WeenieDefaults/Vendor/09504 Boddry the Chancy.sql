9852acf270f80e5db13f9154e88d8894187d530b
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2021-03-12 23:50:14 -0500

    Update Boddry the Chancy, Ashadi bint Samaq and Haya Su Ka (#873)
    * Update 09506 Haya Su Ka.sql
    
    * Update 09504 Boddry the Chancy.sql
    
    * Update 09505 Ashadi bint Samaq.sql
    
    * Update 09505 Ashadi bint Samaq.sql

--- Database/Patches/2011-10-CloakOfDarkness/9 WeenieDefaults/Vendor/09504 Boddry the Chancy.sql
+++ Database/Patches/2011-10-CloakOfDarkness/9 WeenieDefaults/Vendor/09504 Boddry the Chancy.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 9504;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (9504, 'gamblercashieralu', 12, '2020-06-12 00:00:00') /* Vendor */;
+VALUES (9504, 'gamblercashieralu', 12, '2021-03-11 00:00:00') /* Vendor */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (9504,   1,         16) /* ItemType - Creature */
@@ -12,7 +12,7 @@ VALUES (9504,   1,         16) /* ItemType - Creature */
      , (9504,  16,         32) /* ItemUseable - Remote */
      , (9504,  25,          7) /* Level */
      , (9504,  27,          0) /* ArmorType - None */
-     , (9504,  74,     270496) /* MerchandiseItemTypes - Food, Misc, Writable, PromissoryNote */
+     , (9504,  74, 1613561775) /* MerchandiseItemTypes - Food, Useless, SpellComponents, Writable, Key, PromissoryNote, ItemEnchantableTarget, MagicWieldable, TinkeringTool, TinkeringMaterial */
      , (9504,  75,          0) /* MerchandiseMinValue */
      , (9504,  76,    1000000) /* MerchandiseMaxValue */
      , (9504,  93,    2098200) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment */
@@ -45,7 +45,7 @@ VALUES (9504,   1,       5) /* HeartbeatInterval */
      , (9504,  18,       1) /* ArmorModVsAcid */
      , (9504,  19,     0.6) /* ArmorModVsElectric */
      , (9504,  37,     0.8) /* BuyPrice */
-     , (9504,  38,       5) /* SellPrice */
+     , (9504,  38,     1.6) /* SellPrice */
      , (9504,  54,       5) /* UseRadius */
      , (9504,  64,       1) /* ResistSlash */
      , (9504,  65,       1) /* ResistPierce */
@@ -170,17 +170,13 @@ VALUES (9504, 2,   134,  0, 9, 1, False) /* Create Tunic (134) for Wield */
      , (9504, 2,   132,  0, 4, 0.6, False) /* Create Shoes (132) for Wield */
      , (9504, 2,   118,  0, 9, 0, False) /* Create Cap (118) for Wield */
      , (9504, 2, 10696,  0, 17, 0.5, False) /* Create Apron (10696) for Wield */
-     , (9504, 4,  9515, -1, 0, 0, False) /* Create A Guide to Monty's Den of Iniquity (9515) for Shop */
-     , (9504, 4,  2621, -1, 0, 0, False) /* Create Trade Note (100) (2621) for Shop */
-     , (9504, 4,  2622, -1, 0, 0, False) /* Create Trade Note (500) (2622) for Shop */
-     , (9504, 4,  2623, -1, 0, 0, False) /* Create Trade Note (1,000) (2623) for Shop */
-     , (9504, 4,  2624, -1, 0, 0, False) /* Create Trade Note (5,000) (2624) for Shop */
+     , (9504, 4,  9517, -1, 0, 0, False) /* Create A Guide to Gan-Zo's Den of Iniquity (9517) for Shop */
      , (9504, 4,  2625, -1, 0, 0, False) /* Create Trade Note (10,000) (2625) for Shop */
      , (9504, 4,  2626, -1, 0, 0, False) /* Create Trade Note (50,000) (2626) for Shop */
      , (9504, 4,  2627, -1, 0, 0, False) /* Create Trade Note (100,000) (2627) for Shop */
      , (9504, 4, 20628, -1, 0, 0, False) /* Create Trade Note (150,000) (20628) for Shop */
      , (9504, 4, 20629, -1, 0, 0, False) /* Create Trade Note (200,000) (20629) for Shop */
      , (9504, 4, 20630, -1, 0, 0, False) /* Create Trade Note (250,000) (20630) for Shop */
-     , (9504, 4,  2451, -1, 0, 0, False) /* Create Ale (2451) for Shop */
-     , (9504, 4,  2467, -1, 0, 0, False) /* Create Red Wine (2467) for Shop */
-     , (9504, 4,  2471, -1, 0, 0, False) /* Create Stout (2471) for Shop */;
+     , (9504, 4,  2468, -1, 0, 0, False) /* Create Sake (2468) for Shop */
+     , (9504, 4,  2453, -1, 0, 0, False) /* Create Cider (2453) for Shop */
+     , (9504, 4,  2456, -1, 0, 0, False) /* Create Green Tea (2456) for Shop */;
