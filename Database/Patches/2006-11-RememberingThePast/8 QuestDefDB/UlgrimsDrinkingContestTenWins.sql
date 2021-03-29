d7a8edb39a18ff179b11f1cdb2158b17c7fbd9f0
Ty Conner <tyconner@itcproductions.com>
2021-03-29 14:53:34 -0400

    Delete UlgrimsDrinkingContestTenWins.sql

--- Database/Patches/2006-11-RememberingThePast/8 QuestDefDB/UlgrimsDrinkingContestTenWins.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'UlgrimsDrinkingContestTenWins';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('UlgrimsDrinkingContestTenWins', 0, 10, 'Player stamp for ten wins in Ulgrims Drinking Contest', '2020-03-04 06:51:50');
