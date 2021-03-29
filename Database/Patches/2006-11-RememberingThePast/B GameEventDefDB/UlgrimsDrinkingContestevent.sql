753efd34c1b0a78bb26bfd16fc520ecebe6da029
Ty Conner <tyconner@itcproductions.com>
2021-03-29 14:54:01 -0400

    Delete UlgrimsDrinkingContestevent.sql

--- Database/Patches/2006-11-RememberingThePast/B GameEventDefDB/UlgrimsDrinkingContestevent.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `event` WHERE `name` = 'UlgrimsDrinkingContestevent';
-
-INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('UlgrimsDrinkingContestevent', -1, -1, 3, '2021-03-05 00:00:00');
