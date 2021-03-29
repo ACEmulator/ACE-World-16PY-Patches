51a9d6cc5ddb941fd61c57446230c98a6b268eb7
Ty Conner <tyconner@itcproductions.com>
2021-03-29 14:54:05 -0400

    Delete UlgrimsMug2event.sql

--- Database/Patches/2006-11-RememberingThePast/B GameEventDefDB/UlgrimsMug2event.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `event` WHERE `name` = 'UlgrimsMug2event';
-
-INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('UlgrimsMug2event', -1, -1, 3, '2021-03-05 00:00:00');
