f06ef4b086b76fab79071f383819a7a6a1250bfb
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-03-31 13:23:49 -0500

    Quests and Stuff (#822)

---
+++ Database/Patches/2011-04-DarkDeliveries/B GameEventDefDB/DericostCutSceneEvent.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'DericostCutSceneEvent';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('DericostCutSceneEvent', -1, -1, 3, '2021-01-28 00:00:00');
