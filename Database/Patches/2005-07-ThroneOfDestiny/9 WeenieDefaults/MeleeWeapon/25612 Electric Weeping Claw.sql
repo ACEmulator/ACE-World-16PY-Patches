2eafce6f38f8959fcd5441ae2d2ff3ddad231440
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-05-16 13:51:55 -0700

    Fix Typo

--- Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/MeleeWeapon/25612 Electric Weeping Claw.sql
+++ Database/Patches/2005-07-ThroneOfDestiny/9 WeenieDefaults/MeleeWeapon/25612 Electric Weeping Claw.sql
@@ -36,7 +36,7 @@ VALUES (25612,   1,          1) /* ItemType - MeleeWeapon */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (25612,  22, True ) /* Inscribable */
--     , (25612,  69, False) /* IsSellable */
+     , (25612,  69, False) /* IsSellable */
      , (25612,  99, True ) /* Ivoryable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
