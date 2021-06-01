223bd282e55bcb281462a7056b60bc2e8c2ecb73
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2021-06-01 13:32:50 -0400

    Geraine's Study Quest
    Added the Mhoire Infiltration (a.k.a. Geraine's Study) quest.
    http://acpedia.org/wiki/Mhoire_Infiltration

---
+++ Database/Patches/2012-03-RoadToRevenge/B GameEventDefDB/GeraineLibraryBooks.sql
@@ -0,0 +1,3 @@
+DELETE FROM `event` WHERE `name` = 'GeraineLibraryBooks';
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('GeraineLibraryBooks', -1, -1, 3, '2020-01-24 19:57:17');
