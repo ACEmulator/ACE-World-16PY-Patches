823003d9f20f472905a3a9fc5a5ef01e46b7a39b
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2021-06-03 14:19:28 -0400

    Rynthid Fixes
    Rynthid Tentacle Greatspear now has correct base damage (had buffed instead of base before).
    Added missing mask to Rynthid Minions.
    Sanctum Warding Crystals now non-projectile damage immune as in retail.
    Adjusted spellcasting probabilities on Rynthid sorcerer type mobs to cast more ring and war spells, less vuls.

--- Database/Patches/2013-08-EmotionsUnbound/9 WeenieDefaults/Creature/Virindi/51759 Rynthid Sorcerer.sql
+++ Database/Patches/2013-08-EmotionsUnbound/9 WeenieDefaults/Creature/Virindi/51759 Rynthid Sorcerer.sql
@@ -118,11 +118,11 @@ VALUES (51759,  0, 64,  0,    0, 650, 520, 520, 520, 520, 520, 520, 520,  600, 1
      , (51759,  8, 64, 200, 0.5, 650, 520, 520, 520, 520, 520, 520, 520,  600, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
-VALUES (51759,  3940,   2.15)  /* Exsanguinating Wave */
-     , (51759,  3941,   2.176)  /* Heavy Lightning Ring */
-     , (51759,  3989,   2.286)  /* Dark Lightning */
-     , (51759,  4312,   2.3)  /* Incantation of Imperil Other */
-     , (51759,  4483,   2.429)  /* Incantation of Lightning Vulnerability Other */;
+VALUES (51759,  3940,    2.21)  /* Exsanguinating Wave */
+     , (51759,  3941,   2.266)  /* Heavy Lightning Ring */
+     , (51759,  3989,   2.414)  /* Dark Lightning */
+     , (51759,  4312,   2.206)  /* Incantation of Imperil Other */
+     , (51759,  4483,   2.259)  /* Incantation of Lightning Vulnerability Other */;
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (51759, 8, 51858,  1, 0, 0.03, False) /* Mask for Treasure */
