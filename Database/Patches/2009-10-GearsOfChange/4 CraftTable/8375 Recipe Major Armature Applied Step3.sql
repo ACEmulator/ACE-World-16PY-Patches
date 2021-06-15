8ae15498c9738534c7b2f4c7ab1960cfb7c57ec8
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-06-14 22:19:20 -0700

    Trinket Imbue Recipe Updates

--- Database/Patches/2009-10-GearsOfChange/4 CraftTable/8375 Recipe Major Armature Applied Step3.sql
+++ Database/Patches/2009-10-GearsOfChange/4 CraftTable/8375 Recipe Major Armature Applied Step3.sql
@@ -1,33 +1,17 @@
 DELETE FROM `recipe` WHERE `id` = 8375;
 
-INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`,
- `fail_W_C_I_D`,
- `fail_Amount`,
- `fail_Message`,
- `success_Destroy_Source_Chance`,
- `success_Destroy_Source_Amount`,
- `success_Destroy_Source_Message`,
- `success_Destroy_Target_Chance`,
- `success_Destroy_Target_Amount`,
- `success_Destroy_Target_Message`,
- `fail_Destroy_Source_Chance`,
- `fail_Destroy_Source_Amount`,
- `fail_Destroy_Source_Message`,
- `fail_Destroy_Target_Chance`,
- `fail_Destroy_Target_Amount`,
- `fail_Destroy_Target_Message`,
- `data_Id`, `last_Modified`)
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
 VALUES (8375, 0, 18 /* ItemTinkering */, 0, 2, 0, 0, 'You apply the Amber prepared Major Item Tinkering Armature.', 0, 0, 'You apply the Amber prepared Major Item Tinkering Armature, but in the process you destroy the target.', 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2020-02-28 10:00:00');
 
 INSERT INTO `recipe_requirements_int` (`recipe_Id`, `index`, `stat`, `value`, `enum`, `message`)
 VALUES (8375, 0, 105, 1, 2, 'The target item cannot be tinkered!') /* Target.ItemWorkmanship LessThan 1 */
      , (8375, 0, 171, 3, 3, 'The target item has been tinkered too many times already!') /* Target.NumTimesTinkered GreaterThanEqual 3 */
-     , (8375, 0, 179, 1, 3, 'The target item has been imbued already!') /* Target.ImbuedEffect - 8192 - Spellbook_ImbuedEffectType */
+     , (8375, 0, 179, 1, 3, 'The target item has been imbued already!') /* Target.ImbuedEffect - CriticalStrike GreaterThanEqual 1 */
      , (8375, 0, 108, 1, 2, 'The target item must be magical!') /* Target.ItemMaxMana LessThan 1 */
      , (8375, 1,  92, 100, 2, 'The material is not complete!') /* Source.Structure LessThan 100 */;
 
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
-VALUES (8375, True, 0, 0, 0, False, 939524161, 0, 0);
+VALUES (8375, True, 0, 0, 0, False, 0, 0, 0);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -35,7 +19,7 @@ INSERT INTO `recipe_mods_int` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`
 VALUES (@parent_id, 0, 109, 25, 2, 0) /* On Player.SuccessTarget Add ItemDifficulty 25 to Target */
      , (@parent_id, 0, 106, 25, 2, 0) /* On Player.SuccessTarget Add ItemSpellcraft 25 to Target */
      , (@parent_id, 0, 5152, -1, 7, 0) /* On Player.SuccessTarget AddSpell Augmented Stamina III to Target */
-     , (@parent_id, 0, 179, 8192, 1, 0) /* On Player.SuccessTarget Add ImbuedEffect 179 8192 Spellbook_ImbuedEffectType to Target */;
+     , (@parent_id, 0, 179, 8192, 2, 0) /* On Player.SuccessTarget Add ImbuedEffect Spellbook_ImbuedEffectType to Target */;
 
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
 VALUES (8375, True, 0, 0, 0, False, 0, 0, 0);
@@ -46,7 +30,7 @@ INSERT INTO `recipe_mods_string` (`recipe_Mod_Id`, `index`, `stat`, `value`, `en
 VALUES (@parent_id, 3,  39, NULL, 3, 0) /* On Player.SuccessResult CopyFromSourceToTarget TinkerName to Result */;
 
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
-VALUES (8375, True, 0, 0, 0, False, 939524158, 0, 0);
+VALUES (8375, True, 0, 0, 0, False, 0, 0, 0);
 
 SET @parent_id = LAST_INSERT_ID();
 
--- Database/Patches/2009-10-GearsOfChange/4 CraftTable/8405 Recipe Moderate Armature Applied Step3.sql
+++ Database/Patches/2009-10-GearsOfChange/4 CraftTable/8405 Recipe Moderate Armature Applied Step3.sql
@@ -1,39 +1,23 @@
 DELETE FROM `recipe` WHERE `id` = 8405;
 
-INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`,
- `fail_W_C_I_D`,
- `fail_Amount`,
- `fail_Message`,
- `success_Destroy_Source_Chance`,
- `success_Destroy_Source_Amount`,
- `success_Destroy_Source_Message`,
- `success_Destroy_Target_Chance`,
- `success_Destroy_Target_Amount`,
- `success_Destroy_Target_Message`,
- `fail_Destroy_Source_Chance`,
- `fail_Destroy_Source_Amount`,
- `fail_Destroy_Source_Message`,
- `fail_Destroy_Target_Chance`,
- `fail_Destroy_Target_Amount`,
- `fail_Destroy_Target_Message`,
- `data_Id`, `last_Modified`)
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
 VALUES (8405, 0, 18 /* ItemTinkering */, 0, 1, 0, 0, 'You apply the Sapphire prepared Moderate Item Tinkering Armature.', 0, 0, 'You apply the Sapphire prepared Moderate Item Tinkering Armature, but in the process you destroy the target.', 1, 1, NULL, 0, 0, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2020-02-28 10:00:00');
 
 INSERT INTO `recipe_requirements_int` (`recipe_Id`, `index`, `stat`, `value`, `enum`, `message`)
 VALUES (8405, 0, 105, 1, 2, 'The target item cannot be tinkered!') /* Target.ItemWorkmanship LessThan 1 */
      , (8405, 0, 171, 3, 3, 'The target item has been tinkered too many times already!') /* Target.NumTimesTinkered GreaterThanEqual 3 */
-     , (8405, 0, 303, 1, 3, 'The target item has been imbued already!') /* Target.ImbuedEffect - 8192 - Spellbook_ImbuedEffectType */
+     , (8405, 0, 303, 1, 3, 'The target item has been imbued already!') /* Target.ImbuedEffect2 - CriticalStrike GreaterThanEqual 1 */
      , (8405, 0, 108, 1, 2, 'The target item must be magical!') /* Target.ItemMaxMana LessThan 1 */
      , (8405, 1,  92, 100, 2, 'The material is not complete!') /* Source.Structure LessThan 100 */;
 
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
-VALUES (8405, True, 0, 0, 0, False, 939524161, 0, 0);
+VALUES (8405, True, 0, 0, 0, False, 0, 0, 0);
 
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `recipe_mods_int` (`recipe_Mod_Id`, `index`, `stat`, `value`, `enum`, `source`)
-VALUES (@parent_id, 0, 5148, -1, 7, 0) /* On Player.SuccessTarget AddSpell Augmented Mana I to Target */
-     , (@parent_id, 0, 303, 8192, 1, 0) /* On Player.SuccessTarget Add ImbuedEffect 303 8192 Spellbook_ImbuedEffectType to Target */;
+VALUES (@parent_id, 0, 5148, -1, 7, 0) /* On Player.SuccessTarget AddSpell Augmented Mana II to Target */
+     , (@parent_id, 0, 303, 8192, 2, 0) /* On Player.SuccessTarget Add ImbuedEffect2 Spellbook_ImbuedEffectType to Target */;
 
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
 VALUES (8405, True, 0, 0, 0, False, 0, 0, 0);
