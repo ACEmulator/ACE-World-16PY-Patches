6411c589a75373454e82ba3267f0f573081c9c8c
Ty Conner <tyconner@itcproductions.com>
2021-07-17 22:45:32 -0400

    Update compile-pr-and-add-to-zip.bat

--- AppVeyor/compile-pr-and-add-to-zip.bat
+++ AppVeyor/compile-pr-and-add-to-zip.bat
@@ -2,5 +2,6 @@ IF DEFINED APPVEYOR_PULL_REQUEST_HEAD_COMMIT (
 cd Database\compiled
 call compile-pr.bat
 REM 7z a ..\..\ACE-World-Database-v%APPVEYOR_BUILD_VERSION%.sql.zip %APPVEYOR_PULL_REQUEST_NUMBER%-%APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH%.zip
+move PR*.* ..\..
 cd ..\..
 )
