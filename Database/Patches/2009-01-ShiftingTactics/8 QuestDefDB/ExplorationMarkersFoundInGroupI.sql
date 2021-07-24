15589cb53f8ea72271ae5e6a990ad1b766d47b52
Ty Conner <tyconner@itcproductions.com>
2021-07-24 14:02:17 -0400

    Create ExplorationMarkersFoundInGroupI.sql

---
+++ Database/Patches/2009-01-ShiftingTactics/8 QuestDefDB/ExplorationMarkersFoundInGroupI.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'ExplorationMarkersFoundInGroupI';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('ExplorationMarkersFoundInGroupI', 0, 0x7FF, 'Amount of Group I Exploration Markers found.', '2020-01-30 00:00:00');
