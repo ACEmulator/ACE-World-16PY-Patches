2bef30a0e72ff016a962cae978533c7cb1deac75
Ty Conner <tyconner@itcproductions.com>
2021-07-24 14:43:43 -0400

    Update ExplorationMarkersFoundInGroupH.sql

--- Database/Patches/2009-01-ShiftingTactics/8 QuestDefDB/ExplorationMarkersFoundInGroupH.sql
+++ Database/Patches/2009-01-ShiftingTactics/8 QuestDefDB/ExplorationMarkersFoundInGroupH.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'ExplorationMarkersFoundInGroupH';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('ExplorationMarkersFoundInGroupH', 0, 0x7FF, 'Amount of Group H Exploration Markers found.', '2020-01-30 00:00:00');
+VALUES ('ExplorationMarkersFoundInGroupH', 0, 0x3FF, 'Amount of Group H Exploration Markers found.', '2020-01-30 00:00:00');
