52ec4c4f96b3651d69b689a28500df7fc2e329dc
Ty Conner <tyconner@itcproductions.com>
2021-07-24 14:43:47 -0400

    Update ExplorationMarkersFoundInGroupJ.sql

--- Database/Patches/2009-01-ShiftingTactics/8 QuestDefDB/ExplorationMarkersFoundInGroupJ.sql
+++ Database/Patches/2009-01-ShiftingTactics/8 QuestDefDB/ExplorationMarkersFoundInGroupJ.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'ExplorationMarkersFoundInGroupJ';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('ExplorationMarkersFoundInGroupJ', 0, 0x7FF, 'Amount of Group J Exploration Markers found.', '2020-01-30 00:00:00');
+VALUES ('ExplorationMarkersFoundInGroupJ', 0, 0x3FF, 'Amount of Group J Exploration Markers found.', '2020-01-30 00:00:00');
