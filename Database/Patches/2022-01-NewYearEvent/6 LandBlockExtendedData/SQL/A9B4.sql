50d8a3157c054799b69649d7e33bf218125e8d7d
Ty Conner <tyconner@itcproductions.com>
2021-07-26 02:53:42 -0400

    ....

---
+++ Database/Patches/2022-01-NewYearEvent/6 LandBlockExtendedData/SQL/A9B4.sql
@@ -0,0 +1,3 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`)
+VALUES (0x7A9B4081, 80225, 0xA9B40162, 113.197983, 32.396770, 94.005005, -0.925914, 0, 0, 0.377735, False); /* Holiday Planner Generator */
+/* @teleloc 0xA9B40162 [113.197983 32.396770 94.005005] -0.925914 0.000000 0.000000 0.377735 */
