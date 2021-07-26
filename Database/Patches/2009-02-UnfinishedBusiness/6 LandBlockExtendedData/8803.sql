a253d13e9821b4d5cf445628f762126f6235a2ae
Ty Conner <tyconner@itcproductions.com>
2021-07-25 22:10:39 -0400

    Update 8803.sql

--- Database/Patches/2009-02-UnfinishedBusiness/6 LandBlockExtendedData/8803.sql
+++ Database/Patches/2009-02-UnfinishedBusiness/6 LandBlockExtendedData/8803.sql
@@ -2859,3 +2859,14 @@ VALUES (0x78803405, 40116, 0x880307AD, 326.748047, -0.160034, -24.00, 1.000000, 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x78803406, 40117, 0x8803093D, 436.453125, -219.996460, -24.000000, 1.000000, 0, 0, 0,  True, '2019-02-10 00:00:00'); /* Crystal */
 /* @teleloc 0x8803093D [436.453125 -219.996460 -24.000000] 1.000000 0.000000 0.000000 0.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x788036A5,  1154, 0x88030119, 17.9445, -182.139, -27.80706, 0.4207969, 0, 0, 0.9071549, False, '2020-01-30 00:00:00'); /* Linkable Monster Generator */
+/* @teleloc 0x88030119 [17.944500 -182.139000 -27.807060] 0.420797 0.000000 0.000000 0.907155 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x788036A5, 0x788036A6, '2020-01-30 00:00:00') /* Exploration Marker */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x788036A6, 39809, 0x88030119, 17.9445, -182.139, -27.80706, 0.4207969, 0, 0, 0.9071549,  True, '2020-01-30 00:00:00'); /* Exploration Marker */
+/* @teleloc 0x88030119 [17.944500 -182.139000 -27.807060] 0.420797 0.000000 0.000000 0.907155 */
