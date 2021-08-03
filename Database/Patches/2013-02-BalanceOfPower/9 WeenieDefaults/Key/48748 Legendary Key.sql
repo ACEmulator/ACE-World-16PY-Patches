5bf5cde8777bd2833877266a62be7042b7b224bc
Ty Conner <tyconner@itcproductions.com>
2021-08-03 02:33:02 -0400

    Update 48748 Legendary Key.sql

--- Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/Key/48748 Legendary Key.sql
+++ Database/Patches/2013-02-BalanceOfPower/9 WeenieDefaults/Key/48748 Legendary Key.sql
@@ -13,7 +13,6 @@ VALUES (48748,   1,      16384) /* ItemType - Key */
      , (48748,  92,          2) /* Structure */
      , (48748,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (48748,  94,        640) /* TargetType - LockableMagicTarget */
-     , (48748, 114,          0) /* Attuned - Normal */
      , (48748, 267,      86400) /* Lifespan */
      , (48748, 369,        150) /* UseRequiresLevel */;
 
