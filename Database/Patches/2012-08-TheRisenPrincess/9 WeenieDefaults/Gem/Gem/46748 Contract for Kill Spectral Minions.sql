f999babfaa3c4d774c8f2ad7e6111e41d3c1c1fb
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2021-08-03 20:31:07 -0400

    Update 46748 Contract for Kill Spectral Minions.sql

--- Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/Gem/Gem/46748 Contract for Kill Spectral Minions.sql
+++ Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/Gem/Gem/46748 Contract for Kill Spectral Minions.sql
@@ -36,8 +36,7 @@ VALUES (46748, 167,       2) /* CooldownDuration */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (46748,   1, 'Contract for Kill: Spectral Minions') /* Name */
-     , (46748,  14, 'Recommended Level: 200') /* Use */
-;
+     , (46748,  14, 'Recommended Level: 200') /* Use */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (46748,   1,   33557625) /* Setup */
