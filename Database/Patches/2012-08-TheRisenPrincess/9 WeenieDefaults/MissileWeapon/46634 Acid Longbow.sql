85c83082978a0149dbc974485c2fcb28acb54bac
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2021-08-05 13:11:50 -0400

    weapon updates - tectonic rifts

--- Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/MissileWeapon/46634 Acid Longbow.sql
+++ Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/MissileWeapon/46634 Acid Longbow.sql
@@ -4,51 +4,44 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (46634, 'ace46634-acidlongbow', 3, '2020-10-23 23:53:27') /* MissileLauncher */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (46634,   1,        256) /* ItemType - MissileWeapon */
-     , (46634,   3,          7) /* PaletteTemplate - DeepGreen */
-     , (46634,   5,        980) /* EncumbranceVal */
-     , (46634,   9,    4194304) /* ValidLocations - MissileWeapon */
-     , (46634,  16,          1) /* ItemUseable - No */
-     , (46634,  18,        256) /* UiEffects - Acid */
-     , (46634,  19,        375) /* Value */	 
-     , (46634,  33,         -2) /* Bonded - Destroy */
-     , (46634,  45,         32) /* DamageType - Acid */
-     , (46634,  46,         16) /* DefaultCombatStyle - Bow */
-     , (46634,  48,         47) /* WeaponSkill - MissileWeapons */
-     , (46634,  49,         -1) /* WeaponTime */
-     , (46634,  50,          1) /* AmmoType - Arrow */
-     , (46634,  51,          2) /* CombatUse - Missile */
-     , (46634,  52,          2) /* ParentLocation - LeftHand */
-     , (46634,  53,          3) /* PlacementPosition - LeftHand */
-     , (46634,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (46634, 114,          1) /* Attuned - Attuned */
-     , (46634, 151,          2) /* HookType - Wall */
-     , (46634, 204,         16) /* ElementalDamageBonus */
-     , (46634, 353,          8) /* WeaponType - Bow */;
+VALUES (46634,   1,     256) /* ItemType - MissileWeapon */
+     , (46634,   3,       8) /* PaletteTemplate */
+     , (46634,   5,     980) /* EncumbranceVal */
+     , (46634,   9, 4194304) /* ValidLocations - MissileWeapon */
+     , (46634,  16,       1) /* ItemUseable - No */
+     , (46634,  18,     256) /* UiEffects - Acid */
+     , (46634,  19,       0) /* Value */
+     , (46634,  33,      -2) /* Bonded - Destroy */
+     , (46634,  45,      32) /* DamageType - Acid */
+     , (46634,  46,      16) /* DefaultCombatStyle - Bow */
+     , (46634,  48,      47) /* WeaponSkill - MissileWeapons */
+     , (46634,  49,      -1) /* WeaponTime */
+     , (46634,  50,       1) /* AmmoType - Arrow */
+     , (46634,  51,       2) /* CombatUse - Missile */
+     , (46634,  52,       2) /* ParentLocation - LeftHand */
+     , (46634,  53,       3) /* PlacementPosition - LeftHand */
+     , (46634,  93,    1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (46634, 151,       2) /* HookType - Wall */
+     , (46634, 204,      16) /* ElementalDamageBonus */
+     , (46634, 353,       8) /* WeaponType - Bow */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (46634,  11, True ) /* IgnoreCollisions */
-     , (46634,  13, True ) /* Ethereal */
-     , (46634,  14, True ) /* GravityStatus */
-     , (46634,  19, True ) /* Attackable */
-     , (46634,  69, False) /* IsSellable */
-     , (46634,  99, False) /* Ivoryable */;
+VALUES (46634,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (46634,  21,       0) /* WeaponLength */
+VALUES (46634,  12,     0.5) /* Shade */
      , (46634,  26,    26.3) /* MaximumVelocity */
-     , (46634,  29,    0.75) /* WeaponDefense */
+     , (46634,  29,       1) /* WeaponDefense */
      , (46634,  39,     1.1) /* DefaultScale */
-     , (46634,  62,     1.5) /* WeaponOffense */
-     , (46634,  63,     1.5) /* DamageMod */
-     , (46634,  76,     0.7) /* Translucency */
-     , (46634, 136,     0.6) /* CriticalMultiplier */;
+     , (46634,  62,       1) /* WeaponOffense */
+     , (46634,  63,       6) /* DamageMod */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (46634,   1, 'Acid Longbow') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (46634,   1,   33559029) /* Setup */
-     , (46634,   3,  536870932) /* SoundTable */
-     , (46634,   8,  100677121) /* Icon */
-     , (46634,  22,  872415275) /* PhysicsEffectTable */;
+VALUES (46634,  1,  33559029) /* Setup */
+     , (46634,  3, 536870932) /* SoundTable */
+     , (46634,  7, 268436873) /* ClothingBase */
+     , (46634,  8, 100677121) /* Icon */
+     , (46634, 22, 872415275) /* PhysicsEffectTable */;
--- Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/MissileWeapon/46636 Frost Longbow.sql
+++ Database/Patches/2012-08-TheRisenPrincess/9 WeenieDefaults/MissileWeapon/46636 Frost Longbow.sql
@@ -4,52 +4,44 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (46636, 'ace46636-frostlongbow', 3, '2020-05-15 18:44:50') /* MissileLauncher */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (46636,   1,        256) /* ItemType - MissileWeapon */
-     , (46636,   3,          2) /* PaletteTemplate - Blue */
-     , (46636,   5,        980) /* EncumbranceVal */
-     , (46636,   9,    4194304) /* ValidLocations - MissileWeapon */
-     , (46636,  16,          1) /* ItemUseable - No */
-     , (46636,  18,        128) /* UiEffects - Frost */
-     , (46636,  19,        375) /* Value */
-     , (46636,  33,         -2) /* Bonded - Destroy */
-     , (46636,  45,         32) /* DamageType - Acid */
-     , (46636,  46,         16) /* DefaultCombatStyle - Bow */
-     , (46636,  48,         47) /* WeaponSkill - MissileWeapons */
-     , (46636,  49,         -1) /* WeaponTime */
-     , (46636,  50,          1) /* AmmoType - Arrow */
-     , (46636,  51,          2) /* CombatUse - Missile */
-     , (46636,  52,          2) /* ParentLocation - LeftHand */
-     , (46636,  53,          3) /* PlacementPosition - LeftHand */
-     , (46636,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (46636, 114,          1) /* Attuned - Attuned */
-     , (46636, 151,          2) /* HookType - Wall */
-     , (46636, 204,         16) /* ElementalDamageBonus */
-     , (46636, 353,          8) /* WeaponType - Bow */;
+VALUES (46636,   1,     256) /* ItemType - MissileWeapon */
+     , (46636,   3,       2) /* PaletteTemplate */
+     , (46636,   5,     980) /* EncumbranceVal */
+     , (46636,   9, 4194304) /* ValidLocations - MissileWeapon */
+     , (46636,  16,       1) /* ItemUseable - No */
+     , (46636,  18,     128) /* UiEffects - Frost */
+     , (46636,  19,       0) /* Value */
+     , (46636,  33,      -2) /* Bonded - Destroy */
+     , (46636,  45,       8) /* DamageType - Cold */
+     , (46636,  46,      16) /* DefaultCombatStyle - Bow */
+     , (46636,  48,      47) /* WeaponSkill - MissileWeapons */
+     , (46636,  49,      -1) /* WeaponTime */
+     , (46636,  50,       1) /* AmmoType - Arrow */
+     , (46636,  51,       2) /* CombatUse - Missile */
+     , (46636,  52,       2) /* ParentLocation - LeftHand */
+     , (46636,  53,       3) /* PlacementPosition - LeftHand */
+     , (46636,  93,    1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (46636, 151,       2) /* HookType - Wall */
+     , (46636, 204,      16) /* ElementalDamageBonus */
+     , (46636, 353,       8) /* WeaponType - Bow */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (46636,  11, True ) /* IgnoreCollisions */
-     , (46636,  13, True ) /* Ethereal */
-     , (46636,  14, True ) /* GravityStatus */
-     , (46636,  19, True ) /* Attackable */
-     , (46636,  69, False) /* IsSellable */
-     , (46636,  99, False) /* Ivoryable */;
+VALUES (46636,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (46636,  21,       0) /* WeaponLength */
-     , (46636,  22,    0.25) /* DamageVariance */
+VALUES (46636,  12,     0.5) /* Shade */
      , (46636,  26,    26.3) /* MaximumVelocity */
-     , (46636,  29,    0.75) /* WeaponDefense */
+     , (46636,  29,       1) /* WeaponDefense */
      , (46636,  39,     1.1) /* DefaultScale */
-     , (46636,  62,     1.5) /* WeaponOffense */
-     , (46636,  63,     1.5) /* DamageMod */
-     , (46636,  76,     0.7) /* Translucency */
-     , (46636, 136,     0.6) /* CriticalMultiplier */;
+     , (46636,  62,       1) /* WeaponOffense */
+     , (46636,  63,       6) /* DamageMod */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (46636,   1, 'Frost Longbow') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (46636,   1,   33559026) /* Setup */
-     , (46636,   3,  536870932) /* SoundTable */
-     , (46636,   8,  100668815) /* Icon */
-     , (46636,  22,  872415275) /* PhysicsEffectTable */;
+VALUES (46636,  1,  33559026) /* Setup */
+     , (46636,  3, 536870932) /* SoundTable */
+     , (46636,  7, 268436873) /* ClothingBase */
+     , (46636,  8, 100668815) /* Icon */
+     , (46636, 22, 872415275) /* PhysicsEffectTable */;
