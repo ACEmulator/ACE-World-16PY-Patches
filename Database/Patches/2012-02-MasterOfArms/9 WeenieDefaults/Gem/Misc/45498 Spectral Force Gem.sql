e678ddefba4abc89772985f2a5754fec33c136fc
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-08-05 18:15:08 -0700

    Update 45498 Spectral Force Gem.sql

--- Database/Patches/2012-02-MasterOfArms/9 WeenieDefaults/Gem/Misc/45498 Spectral Force Gem.sql
+++ Database/Patches/2012-02-MasterOfArms/9 WeenieDefaults/Gem/Misc/45498 Spectral Force Gem.sql
@@ -13,7 +13,8 @@ VALUES (45498,   1,        128) /* ItemType - Misc */
      , (45498,  16,          8) /* ItemUseable - Contained */
      , (45498,  19,          1) /* Value */
      , (45498,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (45498,  94,         16) /* TargetType - Creature */;
+     , (45498,  94,         16) /* TargetType - Creature */
+     , (45498, 269,          1) /* UseCreateQuantity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (45498,  22, True ) /* Inscribable */
@@ -29,4 +30,5 @@ VALUES (45498,   1,   33556769) /* Setup */
      , (45498,   6,   67111919) /* PaletteBase */
      , (45498,   8,  100673039) /* Icon */
      , (45498,  22,  872415275) /* PhysicsEffectTable */
+     , (45498,  38,      35492) /* UseCreateItem - Gem of Spectral Force */
      , (45498,  50,  100689501) /* IconOverlay */;
