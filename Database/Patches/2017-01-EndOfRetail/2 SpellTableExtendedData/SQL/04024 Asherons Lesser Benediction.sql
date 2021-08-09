6d12ec0e607f2ebf6bfa3d5d1a71b10b05e339d7
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-08-09 11:37:05 -0500

    Spell Name Update (#992)
    * SpellNameUpdate
    
    * Update and rename 03810 Asherons Benediction.sql to 03810 Asheron's Benediction.sql
    
    * Update and rename 04024 Asherons Lesser Benediction.sql to 04024 Asheron's Lesser Benediction.sql
    
    Co-authored-by: Ty Conner <tyconner@itcproductions.com>

--- Database/Patches/2017-01-EndOfRetail/2 SpellTableExtendedData/SQL/04024 Asherons Lesser Benediction.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell` WHERE `id` = 4024;
-
-INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (4024, 'Asherons Lesser Benediction', 20482 /* SecondAtt, SingleStat, Multiplicative */, 1 /* MaxHealth */, 1.1, '2019-03-18 09:00:00');
