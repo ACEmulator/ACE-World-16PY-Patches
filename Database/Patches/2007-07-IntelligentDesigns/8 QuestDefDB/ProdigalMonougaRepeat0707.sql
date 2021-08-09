27ab1343c57b24b78f9248d4bfacd867d3bb9ed9
Patrick <phreekn.slacker@gmail.com>
2021-08-09 14:51:40 -0500

    UpdateXPandText

---
+++ Database/Patches/2007-07-IntelligentDesigns/8 QuestDefDB/ProdigalMonougaRepeat0707.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'ProdigalMonougaRepeat0707';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('ProdigalMonougaRepeat0707', 0, 1, 'Player has completed Monouga Laboratory at least once before', '2021-08-09 06:51:50');
