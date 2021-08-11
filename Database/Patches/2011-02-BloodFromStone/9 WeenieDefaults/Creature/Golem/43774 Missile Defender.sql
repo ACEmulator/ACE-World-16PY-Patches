207107745ad99ecdd0292ff0d8c5b94f6c90eab8
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-08-10 19:16:40 -0500

    Updates (#1005)

--- Database/Patches/2011-02-BloodFromStone/9 WeenieDefaults/Creature/Golem/43774 Missile Defender.sql
+++ Database/Patches/2011-02-BloodFromStone/9 WeenieDefaults/Creature/Golem/43774 Missile Defender.sql
@@ -96,7 +96,7 @@ VALUES (43774,  6, 0, 3, 0, 300, 0, 0) /* MeleeDefense        Specialized */
      , (43774, 15, 0, 3, 0, 265, 0, 0) /* MagicDefense        Specialized */
      , (43774, 20, 0, 3, 0, 100, 0, 0) /* Deception           Specialized */
      , (43774, 24, 0, 3, 0,  10, 0, 0) /* Run                 Specialized */
-     , (43774, 47, 0, 3, 0, 285, 0, 0) /* MissileWeapons      Specialized */;
+     , (43774, 47, 0, 3, 0, 525, 0, 0) /* MissileWeapons      Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
 VALUES (43774,  0,  4,  0,    0,  300,  240,  240,  240,  300,  300,  300,  300,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
