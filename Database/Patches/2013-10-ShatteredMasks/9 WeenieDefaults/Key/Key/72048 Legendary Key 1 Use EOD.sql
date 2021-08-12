a20d7ab6be077e4beab2ba3f602ddc9f76f3bd9e
Ty Conner <tyconner@itcproductions.com>
2021-08-12 13:15:16 -0400

    Update 72048 Legendary Key 1 Use EOD.sql

--- Database/Patches/2013-10-ShatteredMasks/9 WeenieDefaults/Key/Key/72048 Legendary Key 1 Use EOD.sql
+++ Database/Patches/2013-10-ShatteredMasks/9 WeenieDefaults/Key/Key/72048 Legendary Key 1 Use EOD.sql
@@ -4,29 +4,24 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (72048, 'ace72048-legendarykey', 22, '2019-02-10 00:00:00') /* Key */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (72048,   1,   16384) /* ItemType - Key */
-     , (72048,   5,      30) /* EncumbranceVal */
-     , (72048,  16, 2097160) /* ItemUseable - SourceContainedTargetRemote */
-     , (72048,  18,      64) /* UiEffects - Lightning */
-     , (72048,  19,   10000) /* Value */
-     , (72048,  33,       1) /* Bonded - Normal */
-     , (72048,  65,     101) /* Placement - Resting */
-     , (72048,  91,       1) /* MaxStructure */
-     , (72048,  92,       1) /* Structure */
-     , (72048,  93,    1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (72048,  94,     640) /* TargetType - LockableMagicTarget */
-     , (72048, 267,   86400) /* Lifespan */
-     , (72048, 369,     150) /* UseRequiresLevel */;
+VALUES (72048,   1,      16384) /* ItemType - Key */
+     , (72048,   5,         30) /* EncumbranceVal */
+     , (72048,  16,    2097160) /* ItemUseable - SourceContainedTargetRemote */
+     , (72048,  18,         64) /* UiEffects - Lightning */
+     , (72048,  19,      10000) /* Value */
+     , (72048,  33,          0) /* Bonded - Normal */
+     , (72048,  91,          1) /* MaxStructure */
+     , (72048,  92,          1) /* Structure */
+     , (72048,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (72048,  94,        640) /* TargetType - LockableMagicTarget */
+     , (72048, 114,          0) /* Attuned - Normal */
+     , (72048, 267,      86400) /* Lifespan */
+     , (72048, 369,        150) /* UseRequiresLevel */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (72048,  1, False) /* Stuck */
-     , (72048, 11,  True) /* IgnoreCollisions */
-     , (72048, 13,  True) /* Ethereal */
-     , (72048, 14,  True) /* GravityStatus */
-     , (72048, 19,  True) /* Attackable */
-     , (72048, 22,  True) /* Inscribable */
-     , (72048, 69, False) /* IsSellable */
-     , (72048, 99, False) /* Ivoryable */;
+VALUES (72048,  22, True ) /* Inscribable */
+     , (72048,  69, False) /* IsSellable */
+     , (72048,  99, False) /* Ivoryable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (72048,  1, 'Legendary Key') /* Name */
