66270b63d7f390a3f9573f9659a41aa4b1759b61
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-08-21 18:04:42 -0700

    Update 24858 Issk.sql

--- Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/Creature/Sclavus/24858 Issk.sql
+++ Database/Patches/2006-02-AChangeInTactics/9 WeenieDefaults/Creature/Sclavus/24858 Issk.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 24858;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (24858, 'npcsclavusharbinger', 10, '2005-02-09 10:00:00') /* Creature */;
+VALUES (24858, 'npcsclavusharbinger', 10, '2021-08-21 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (24858,   1,         16) /* ItemType - Creature */
