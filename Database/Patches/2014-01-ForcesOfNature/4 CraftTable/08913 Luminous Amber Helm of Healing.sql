6046b3a7bad6bf76547342b84aa6bb556b72382f
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-08-24 16:50:02 -0700

    Update 08913 Luminous Amber Helm of Healing.sql

--- Database/Patches/2014-01-ForcesOfNature/4 CraftTable/08913 Luminous Amber Helm of Healing.sql
+++ Database/Patches/2014-01-ForcesOfNature/4 CraftTable/08913 Luminous Amber Helm of Healing.sql
@@ -5,7 +5,6 @@ VALUES (8913, 0, 29 /* ArmorTinkering */, 400, 0, 0, 0, 'You successfully imbue 
 
 INSERT INTO `recipe_requirements_int` (`recipe_Id`, `index`, `stat`, `value`, `enum`, `message`)
 VALUES (8913, 0,   9,  1, 4, 'You can only apply this augmentaion to a helm-slot item!') /* ValidLocations - HeadWear */
-     , (8913, 0, 105,  1, 2, 'You can only apply this augmentation to a loot-generated item!')  /* Target.ItemWorkmanship - LessThan 1 */
 	 , (8913, 0, 376,  1, 3, 'This item has already been imbued!') /* Target.GearHealingBoostInt - GreaterThanEqual 1 */;
 
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
