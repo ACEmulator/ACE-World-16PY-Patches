3f2e988f1f15e3dc6177c9f00654bde43711a586
Ross Elliott <ross@yotesfan.com>
2021-09-09 17:13:55 -0700

    Alembic Incanta Recipes

--- Database/Patches/2013-12-TheHolidayRush/4 CraftTable/09086 Bloodseeker Oil.sql
+++ Database/Patches/2013-12-TheHolidayRush/4 CraftTable/09086 Bloodseeker Oil.sql
@@ -1,9 +1,9 @@
-DELETE FROM `recipe` WHERE `id` = 9088;
+DELETE FROM `recipe` WHERE `id` = 9086;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (9088, 0, 38 /* Alchemy */, 140, 0, 5333 /* Health Oil */, 1, 'You create health oil.', 0, 0, 'You fail to create health oil.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2005-02-09 10:00:00');
+VALUES (9086, 0, 38 /* Alchemy */, 125, 0, 5337 /* Bloodseeker Oil */, 1, 'You create bloodseeker oil.', 0, 0, 'You fail to create bloodseeker oil.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-09-09 10:00:00');
 
-DELETE FROM `cook_book` WHERE `recipe_Id` = 9088;
+DELETE FROM `cook_book` WHERE `recipe_Id` = 9086;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (9088, 52525 /* Alembic Incanta */,   764 /* Vitriol */, '2005-02-09 10:00:00');
+VALUES (9086, 52525 /* Alembic Incanta */,  759 /* Quicksilver */, '2021-09-09 10:00:00');
--- Database/Patches/2013-12-TheHolidayRush/4 CraftTable/09106 Concentrated Slashing Oil.sql
+++ Database/Patches/2013-12-TheHolidayRush/4 CraftTable/09106 Concentrated Slashing Oil.sql
@@ -1,9 +1,9 @@
-DELETE FROM `recipe` WHERE `id` = 9087;
+DELETE FROM `recipe` WHERE `id` = 9106;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (9087, 0, 38 /* Alchemy */, 125, 0, 5331 /* Frost Oil */, 1, 'You create frost oil.', 0, 0, 'You fail to create frost oil.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2005-02-09 10:00:00');
+VALUES (9106, 0, 38 /* Alchemy */, 155, 0, 36741 /* Concentrated Slashing Oil */, 1, 'You create concentrated slashing oil.', 0, 0, 'You fail to create concentrated slashing oil.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-09-09 10:00:00');
 
-DELETE FROM `cook_book` WHERE `recipe_Id` = 9087;
+DELETE FROM `cook_book` WHERE `recipe_Id` = 9106;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (9087, 52525 /* Alembic Incanta */,   757 /* Colcothar */, '2005-02-09 10:00:00');
+VALUES (9106, 52524 /* Concentrated Alembic Incanta */,  8312 /* Verdigris Pea */, '2021-09-09 10:00:00');
