750c95758c2adfd13925253b4796c6cb58f0d303
Patrick <phreekn.slacker@gmail.com>
2021-09-16 13:29:16 -0500

    Delete BAA0.sql

--- Database/Patches/2012-07-SoullessSisters/6 LandBlockExtendedData/BAA0.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7BAA0000, 46356, 0xBAA00101, 59.0658, 38.5595, 53.705, 0.15787, 0, 0, -0.98746, False, '2021-08-30 14:57:49'); /* Popkin of the Gate */
-/* @teleloc 0xBAA00101 [59.065800 38.559502 53.705002] 0.157870 0.000000 0.000000 -0.987460 */
