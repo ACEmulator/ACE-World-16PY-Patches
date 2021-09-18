eab68be31832239e141bb5b86387e2ea60797873
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-09-18 09:10:29 -0700

    Rynthid wands (#1062)
    * Update 51989 Rynthid Tentacle Wand.sql
    
    * Update 51990 Life-attuned Rynthid Tentacle Wand.sql
    
    * Update 51991 Nether-attuned Rynthid Tentacle Wand.sql
    
    * Update 51989 Rynthid Tentacle Wand.sql

--- Database/Patches/2013-10-ShatteredMasks/9 WeenieDefaults/Caster/51990 Life-attuned Rynthid Tentacle Wand.sql
+++ Database/Patches/2013-10-ShatteredMasks/9 WeenieDefaults/Caster/51990 Life-attuned Rynthid Tentacle Wand.sql
@@ -13,8 +13,6 @@ VALUES (51990,   1,      32768) /* ItemType - Caster */
      , (51990,  33,          1) /* Bonded - Bonded */
      , (51990,  45,         16) /* DamageType - Fire */
      , (51990,  46,        512) /* DefaultCombatStyle - Magic */
-     , (51990,  52,          1) /* ParentLocation - RightHand */
-     , (51990,  53,          1) /* PlacementPosition - RightHandCombat */
      , (51990,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (51990,  94,         16) /* TargetType - Creature */
      , (51990, 106,        475) /* ItemSpellcraft */
@@ -29,11 +27,7 @@ VALUES (51990,   1,      32768) /* ItemType - Caster */
      , (51990, 353,         12) /* WeaponType - Magic */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (51990,  11, True ) /* IgnoreCollisions */
-     , (51990,  13, True ) /* Ethereal */
-     , (51990,  14, True ) /* GravityStatus */
-     , (51990,  19, True ) /* Attackable */
-     , (51990,  22, True ) /* Inscribable */
+VALUES (51990,  22, True ) /* Inscribable */
      , (51990,  69, False) /* IsSellable */
      , (51990,  99, True ) /* Ivoryable */;
 
