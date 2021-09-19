7de7a1bf4526680b47f8814a2362dc85fb149849
An Adventurer <an.adventurer.ac@gmail.com>
2021-09-19 17:24:28 -0500

    fixing text issue with Hamud's Last Testament.

--- Database/Patches/2010-11-FromDarknessLight/9 WeenieDefaults/Book/06878 Hamud's Last Testament.sql
+++ Database/Patches/2010-11-FromDarknessLight/9 WeenieDefaults/Book/06878 Hamud's Last Testament.sql
@@ -35,4 +35,8 @@ VALUES (6878, 2, 1000);
 
 INSERT INTO `weenie_properties_book_page_data` (`object_Id`, `page_Id`, `author_Id`, `author_Name`, `author_Account`, `ignore_Author`, `page_Text`)
 VALUES (6878, 0, 4294967295, 'Hamud ibn Rafik', 'prewritten', False, 'My daughter,
+My periods of lucidity now last only a few moments at a time.  I am sending this note to you with one of the new recruits in the Tenebrous Edge.  I am unable to leave my chambers in this ancient, cursed fortress.  I know now what will become of me.  The Dark Master himself spoke to me and told me what lies in store.   I know I will never reach the gardens of delight spoken of by ibn Salayyar.  In my years of service with the Zharalim, I became familiar with many horrors, but what awaits me now surpasses even the depraved rites of the Milantans.
+')
+     , (6878, 1, 4294967295, 'Hamud ibn Rafik', 'prewritten', False, 'Do not fall as I have fallen, my daughter.  Seek a way to escape Dereth.  I have heard that others have disappeared from this undead world.  My hope is that you will find a way to disappear as well.
+Your devoted father
 ');
