d12a60c50ab8a7130b239b0230e8462c049fd404
An Adventurer <an.adventurer.ac@gmail.com>
2021-09-22 08:55:29 -0500

    removing NPC generator, additional fix for 2013-03 NPC relocaiton.

--- Database/Patches/2013-03-DisturbanceInTheDesert/6 LandBlockExtendedData/A360.sql
+++ Database/Patches/2013-03-DisturbanceInTheDesert/6 LandBlockExtendedData/A360.sql
@@ -1,5 +1,1 @@
 DELETE FROM `landblock_instance` WHERE `landblock` = 0xA360;
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7A360003,  3953, 0xA3600100, 61.1315, 178.797, 17.705, -0.346289, 0, 0, 0.938128, False, '2005-02-09 10:00:00'); /* Linkable Monster Gen (30 min.) */
-/* @teleloc 0xA3600100 [61.131500 178.796997 17.705000] -0.346289 0.000000 0.000000 0.938128 */
