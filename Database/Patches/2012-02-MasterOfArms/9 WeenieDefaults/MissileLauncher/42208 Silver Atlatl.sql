d29cd4046419e925a98cd12179761690bd72b0a1
An Adventurer <an.adventurer.ac@gmail.com>
2021-09-26 10:22:23 -0500

    Fac hub item fixes (#1075)
    * Fixes for various Fac Hub related items. Mostly cosmetic fixes, a few stat updates
    
    * deleting older copies of updated weenies, per PR comments
    
    * removing unneeded weenie properties, per PR comments
    
    * Update 01240 Oversized Banderling Ogre.sql
    
    Few adjustments
    
    * Update 42209 Tetsubo Slugger.sql
    
    * Update 42210 Ancient Arbalest.sql
    
    * Update 01240 Oversized Banderling Ogre.sql
    
    Co-authored-by: ziang4891 <56269610+ziang4891@users.noreply.github.com>

--- Database/Patches/2012-02-MasterOfArms/9 WeenieDefaults/MissileLauncher/42208 Silver Atlatl.sql
+++ Database/Patches/2012-02-MasterOfArms/9 WeenieDefaults/MissileLauncher/42208 Silver Atlatl.sql
@@ -1,10 +1,11 @@
 DELETE FROM `weenie` WHERE `class_Id` = 42208;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (42208, 'ace42208-silveratlatl', 3, '2020-02-01 21:01:19') /* MissileLauncher */;
+VALUES (42208, 'ace42208-silveratlatl', 3, '2021-09-20 08:26:52') /* MissileLauncher */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (42208,   1,        256) /* ItemType - MissileWeapon */
+     , (42208,   3,         20) /* PaletteTemplate */
      , (42208,   5,        200) /* EncumbranceVal */
      , (42208,   9,    4194304) /* ValidLocations - MissileWeapon */
      , (42208,  16,          1) /* ItemUseable - No */
@@ -15,7 +16,7 @@ VALUES (42208,   1,        256) /* ItemType - MissileWeapon */
      , (42208,  48,         47) /* WeaponSkill - MissileWeapons */
      , (42208,  49,         15) /* WeaponTime */
      , (42208,  50,          4) /* AmmoType - Atlatl */
-     , (42208,  51,          2) /* CombatUse - Missle */
+     , (42208,  51,          2) /* CombatUse - Missile */
      , (42208,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (42208, 106,        150) /* ItemSpellcraft */
      , (42208, 107,        400) /* ItemCurMana */
@@ -25,22 +26,20 @@ VALUES (42208,   1,        256) /* ItemType - MissileWeapon */
      , (42208, 353,         10) /* WeaponType - Thrown */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (42208,  11, True ) /* IgnoreCollisions */
-     , (42208,  13, True ) /* Ethereal */
-     , (42208,  14, True ) /* GravityStatus */
-     , (42208,  19, True ) /* Attackable */
+VALUES (42208,  19, True ) /* Attackable */
      , (42208,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (42208,   5, -0.025) /* ManaRate */
-     , (42208,  21,      0) /* WeaponLength */
-     , (42208,  22,      0) /* DamageVariance */
-     , (42208,  26,   24.9) /* MaximumVelocity */
-     , (42208,  29,      1) /* WeaponDefense */
-     , (42208,  39,    1.1) /* DefaultScale */
-     , (42208,  62,      1) /* WeaponOffense */
-     , (42208,  63,    2.2) /* DamageMod */
-     , (42208, 147,   0.18) /* CriticalFrequency */;
+VALUES (42208,   5,  -0.025) /* ManaRate */
+     , (42208,  12,       0) /* Shade */
+     , (42208,  21,       0) /* WeaponLength */
+     , (42208,  22,       0) /* DamageVariance */
+     , (42208,  26,    24.9) /* MaximumVelocity */
+     , (42208,  29,       1) /* WeaponDefense */
+     , (42208,  39,     1.1) /* DefaultScale */
+     , (42208,  62,       1) /* WeaponOffense */
+     , (42208,  63,     2.2) /* DamageMod */
+     , (42208, 147,    0.18) /* CriticalFrequency */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (42208,   1, 'Silver Atlatl') /* Name */;
@@ -49,9 +48,10 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (42208,   1,   33557433) /* Setup */
      , (42208,   3,  536870932) /* SoundTable */
      , (42208,   6,   67111919) /* PaletteBase */
+     , (42208,   7,  268436304) /* Clothingbase */
      , (42208,   8,  100672413) /* Icon */
      , (42208,  22,  872415275) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
-VALUES (42208,   535,      2)  /* Missile Weapon Mastery Other III */
-     , (42208,  1612,      2)  /* Aura of Blood Drinker Self II */;
+VALUES (42208,   535,      2) /* Missile Weapon Mastery Other III */
+     , (42208,  1612,      2) /* Aura of Blood Drinker Self II */;
