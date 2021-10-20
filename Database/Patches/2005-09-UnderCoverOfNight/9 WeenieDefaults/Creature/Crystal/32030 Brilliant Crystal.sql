21d83a7bbb31d585219d862cd6d4a3947f2e362e
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2021-10-19 19:54:46 -0500

    add create list to drop in 1 pile

--- Database/Patches/2005-09-UnderCoverOfNight/9 WeenieDefaults/Creature/Crystal/32030 Brilliant Crystal.sql
+++ Database/Patches/2005-09-UnderCoverOfNight/9 WeenieDefaults/Creature/Crystal/32030 Brilliant Crystal.sql
@@ -97,18 +97,13 @@ VALUES (32030,  0,  4, 50, 0.75,  350,  350,  350,  350,  350,  420,  378, 35000
      , (32030, 16,  4,  0,    0,  350,  350,  350,  350,  350,  420,  378, 35000,    0, 2,  0.3,  0.4, 0.25,  0.3,  0.4, 0.25,  0.6,  0.5, 0.25,  0.6,  0.5, 0.25) /* Torso */
      , (32030, 17,  4, 50, 0.75,  350,  350,  350,  350,  350,  420,  378, 35000,    0, 2,    0,    0,    0,    0,    0,    0,  0.1,  0.1,    0,  0.1,  0.1,    0) /* Tail */;
 
-INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (32030,  3 /* Death */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-SET @parent_id = LAST_INSERT_ID();
-
-INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  23 /* StartEvent */, 0, 1, NULL, 'BrilliantShardEvent', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (32030,  9 /* Generation */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-SET @parent_id = LAST_INSERT_ID();
-
-INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  24 /* StopEvent */, 0, 1, NULL, 'BrilliantShardEvent', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */
+     , (32030, 9, 31881,  1, 0, 0, False) /* Create Brilliant Shard (31879) for ContainTreasure */;
