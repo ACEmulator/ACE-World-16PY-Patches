3ac8b3c43489f6a21b470f94908d5bf14e2a1199
Patrick <phreekn.slacker@gmail.com>
2021-10-21 16:14:51 -0500

    AddedTimerToCompleted

--- Database/Patches/2006-10-DanceOfTheDead/8 QuestDefDB/BlackSpearIceTotemCompleted.sql
+++ Database/Patches/2006-10-DanceOfTheDead/8 QuestDefDB/BlackSpearIceTotemCompleted.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'BlackSpearIceTotemCompleted';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('BlackSpearIceTotemCompleted', 0, 1, 'Player has completed the Black Spear Ice Totem Quest', '2021-10-15 06:51:50');
+VALUES ('BlackSpearIceTotemCompleted', 72000, -1, 'Player has completed the Black Spear Ice Totem Quest', '2021-10-15 06:51:50');
