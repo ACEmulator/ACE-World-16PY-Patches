3881134eaae6a2c4025174aa4b724288ab3fff35
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2021-10-26 00:52:04 -0500

    Add Small Fledgling Mukkir Kill Task (#1108)

---
+++ Database/Patches/2006-08-ShatteringTheDark/8 QuestDefDB/KillTaskSmallFledglingMukkirCompleted0806.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskSmallFledglingMukkirCompleted0806';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskSmallFledglingMukkirCompleted0806', 72000, -1, 'Small Fledgling Mukkir Kill Task Timer', '2021-10-11 17:30:00');
