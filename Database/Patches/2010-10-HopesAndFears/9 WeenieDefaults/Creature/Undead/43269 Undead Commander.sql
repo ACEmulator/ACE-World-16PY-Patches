95b95a909de3e8ea2cda3fc65a15699d48a13f86
Patrick <phreekn.slacker@gmail.com>
2021-10-30 14:04:37 -0500

    RemovedLum

--- Database/Patches/2010-10-HopesAndFears/9 WeenieDefaults/Creature/Undead/43269 Undead Commander.sql
+++ Database/Patches/2010-10-HopesAndFears/9 WeenieDefaults/Creature/Undead/43269 Undead Commander.sql
@@ -16,8 +16,7 @@ VALUES (43269,   1,         16) /* ItemType - Creature */
      , (43269,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
      , (43269, 133,          2) /* ShowableOnRadar - ShowMovement */
      , (43269, 146,    1250000) /* XpOverride */
-     , (43269, 307,          2) /* DamageRating */
-     , (43269, 332,        100) /* LuminanceAward */;
+     , (43269, 307,          2) /* DamageRating */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (43269,   1, True ) /* Stuck */
@@ -71,14 +70,14 @@ INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (43269,   1, 'Undead Commander') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43269,   1,   33558541) /* Setup */
-     , (43269,   2,  150994967) /* MotionTable */
-     , (43269,   3,  536870934) /* SoundTable */
-     , (43269,   4,  805306368) /* CombatTable */
-     , (43269,   6,   67114692) /* PaletteBase */
-     , (43269,   7,  268436726) /* ClothingBase */
-     , (43269,   8,  100667942) /* Icon */
-     , (43269,  22,  872415272) /* PhysicsEffectTable */
+VALUES (43269,   1, 0x0200100D) /* Setup */
+     , (43269,   2, 0x09000017) /* MotionTable */
+     , (43269,   3, 0x20000016) /* SoundTable */
+     , (43269,   4, 0x30000000) /* CombatTable */
+     , (43269,   6, 0x040016C4) /* PaletteBase */
+     , (43269,   7, 0x100004F6) /* ClothingBase */
+     , (43269,   8, 0x06001226) /* Icon */
+     , (43269,  22, 0x34000028) /* PhysicsEffectTable */
      , (43269,  35,       2000) /* DeathTreasureType - Loot Tier: 8 */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
