b66c096c41f9ec4e2ac4c16b6bed2ff4fa37d6a6
Patrick <phreekn.slacker@gmail.com>
2021-11-03 14:48:11 -0500

    QuestandStuff

---
+++ Database/Patches/6 LandBlockExtendedData/1934.sql
@@ -0,0 +1,19 @@
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x71934000, 35343, 0x19340002, 4.36483, 42.6455, 0.00880003, -0.65336, 0, 0, 0.757047, False, '2021-03-19 10:33:18'); /* Chief Taraash */
+/* @teleloc 0x19340002 [4.364830 42.645500 0.008800] -0.653360 0.000000 0.000000 0.757047 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x71934015,  5085, 0x19340002, 8.71607, 40.7937, 0.06, 0.995754, 0, 0, -0.092059, False, '2019-02-10 00:00:00'); /* Linkable Item Gen - 25 seconds */
+/* @teleloc 0x19340002 [8.716070 40.793701 0.060000] 0.995754 0.000000 0.000000 -0.092059 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x71934015, 0x71934016, '2019-02-10 00:00:00') /* Pile of Short Sticks (6118) */
+     , (0x71934015, 0x71934017, '2019-02-10 00:00:00') /* Campfire (4128) */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x71934016,  6118, 0x19340002, 8.71607, 40.7937, 0.06, 0.995754, 0, 0, -0.092059,  True, '2019-02-10 00:00:00'); /* Pile of Short Sticks */
+/* @teleloc 0x19340002 [8.716070 40.793701 0.060000] 0.995754 0.000000 0.000000 -0.092059 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x71934017,  4128, 0x19340002, 6.67268, 42.0863, 2.98023E-08, 0.995754, 0, 0, -0.092059,  True, '2019-02-10 00:00:00'); /* Campfire */
+/* @teleloc 0x19340002 [6.672680 42.086300 0.000000] 0.995754 0.000000 0.000000 -0.092059 */
--- Database/Patches/9 WeenieDefaults/Portal/Portal/35563 Archive.sql
+++ Database/Patches/9 WeenieDefaults/Portal/Portal/35563 Archive.sql
@@ -30,3 +30,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (35563,   1, 0x020005D5) /* Setup */
      , (35563,   2, 0x09000003) /* MotionTable */
      , (35563,   8, 0x0600106B) /* Icon */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (35563, 2, 0x193604BC, 146.681, -8.62729, -4.8255, 0.779756, 0, 0, -0.626083) /* Destination */
+/* @teleloc 0x193604BC [146.681000 -8.627290 -4.825500] 0.779756 0.000000 0.000000 -0.626083 */;
