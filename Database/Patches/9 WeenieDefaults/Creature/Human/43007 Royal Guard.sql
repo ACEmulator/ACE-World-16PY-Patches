fa5319a076ccd8c3aa7a79ebcf25bd096a315d83
Ty Conner <tyconner@itcproductions.com>
2021-11-04 14:06:31 -0400

    Fix some npcs in Arwic (#1152)
    * Fix some npcs in Arwic
    
    * .
    
    * Update 43009 Royal Guard.sql
    
    * Add Cloaked Covenant Breastplate
    
    * .

--- Database/Patches/9 WeenieDefaults/Creature/Human/43007 Royal Guard.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/43007 Royal Guard.sql
@@ -6,12 +6,10 @@ VALUES (43007, 'ace43007-royalguard', 10, '2021-11-01 00:00:00') /* Creature */;
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (43007,   1,         16) /* ItemType - Creature */
      , (43007,   2,         31) /* CreatureType - Human */
-     , (43007,   3,          9) /* PaletteTemplate - Grey */
      , (43007,   6,         -1) /* ItemsCapacity */
      , (43007,   7,         -1) /* ContainersCapacity */
      , (43007,  16,         32) /* ItemUseable - Remote */
      , (43007,  25,        275) /* Level */
-     , (43007,  68,         13) /* TargetingTactic - Random, LastDamager, TopDamager */
      , (43007,  93,    6292504) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
      , (43007,  95,          8) /* RadarBlipColor - Yellow */
      , (43007, 113,          1) /* Gender - Male */
@@ -60,7 +58,6 @@ VALUES (43007,   1, 0x02000001) /* Setup */
      , (43007,   2, 0x09000001) /* MotionTable */
      , (43007,   3, 0x20000001) /* SoundTable */
      , (43007,   6, 0x0400007E) /* PaletteBase */
-     , (43007,   7, 0x100006C7) /* ClothingBase */
      , (43007,   8, 0x06001036) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
@@ -137,4 +134,15 @@ VALUES (@parent_id,  0,  12 /* TurnToTarget */, 0, 1, NULL, NULL, NULL, NULL, NU
 
 INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
 VALUES (43007, 2, 24611,  1, 0, 0, False) /* Create Sword of Lost Light (24611) for Wield */
-     , (43007, 2, 42717,  1, 0, 0, False) /* Create Shield of Borelean's Royal Guard (42717) for Wield */;
+     , (43007, 2, 42717,  1, 0, 0, False) /* Create Shield of Borelean's Royal Guard (42717) for Wield */
+     , (43007, 2,  2587,  0, 14, 0.5, False) /* Create Shirt (2587) for Wield */
+     , (43007, 2,  2601,  0, 14, 0.5, False) /* Create Pants (2601) for Wield */
+     , (43007, 2, 21150,  0, 93, 1, False) /* Create Covenant Sollerets (21150) for Wield */
+     , (43007, 2, 21151,  0, 93, 1, False) /* Create Covenant Bracers (21151) for Wield */
+     , (43007, 2, 80226,  0, 93, 1, False) /* Create Covenant Breastplate (80226) for Wield */
+     , (43007, 2, 21153,  0, 93, 1, False) /* Create Covenant Gauntlets (21153) for Wield */
+     , (43007, 2, 21154,  0, 93, 1, False) /* Create Covenant Girth (21154) for Wield */
+     , (43007, 2, 21155,  0, 93, 1, False) /* Create Covenant Greaves (21155) for Wield */
+     , (43007, 2, 21156,  0, 93, 1, False) /* Create Covenant Helm (21156) for Wield */
+     , (43007, 2, 21157,  0, 93, 1, False) /* Create Covenant Pauldrons (21157) for Wield */
+     , (43007, 2, 21159,  0, 93, 1, False) /* Create Covenant Tassets (21159) for Wield */;
