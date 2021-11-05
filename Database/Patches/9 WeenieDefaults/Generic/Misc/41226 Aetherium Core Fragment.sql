10ddb6449f5fe99b0220f2673509108121ae1da9
Patrick <phreekn.slacker@gmail.com>
2021-11-05 10:45:09 -0500

    Update 41226 Aetherium Core Fragment.sql

--- Database/Patches/9 WeenieDefaults/Generic/Misc/41226 Aetherium Core Fragment.sql
+++ Database/Patches/9 WeenieDefaults/Generic/Misc/41226 Aetherium Core Fragment.sql
@@ -7,13 +7,16 @@ INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (41226,   1,        128) /* ItemType - Misc */
      , (41226,   5,          4) /* EncumbranceVal */
      , (41226,  16,          1) /* ItemUseable - No */
-     , (41226,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+     , (41226,  33,          1) /* Bonded - Bonded */
+     , (41226,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (41226, 114,          1) /* Attuned - Attuned */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (41226,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (41226,   1, 'Aetherium Core Fragment') /* Name */
+     , (41226,  16, 'A complicated amalgamation of Aetherium ore and forged metal parts, apparently part of the power core of some kind of mechanical creature.') /* LongDesc */
      , (41226,  33, 'AetheriumCoreFragmentPickupTimer') /* Quest */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
