07ccce72ec4c11656fbeb8d8e74fbdff4a62dc01
Patrick <phreekn.slacker@gmail.com>
2021-11-05 21:08:16 -0500

    UpdatedMotionTableRemovedEmote

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/35104 Corpse of a Servitor of Asheron.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/35104 Corpse of a Servitor of Asheron.sql
@@ -54,7 +54,7 @@ VALUES (35104,   1, 'Corpse of a Servitor of Asheron') /* Name */
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (35104,   1, 0x02000F5F) /* Setup */
-     , (35104,   2, 0x09000082) /* MotionTable */
+     , (35104,   2, 0x090001A0) /* MotionTable */
      , (35104,   3, 0x20000015) /* SoundTable */
      , (35104,   7, 0x1000049A) /* ClothingBase */
      , (35104,   8, 0x06001070) /* Icon */;
@@ -74,14 +74,6 @@ VALUES (35104,  0,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
      , (35104,  6,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
      , (35104,  7,  4,  0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
      , (35104,  8,  4,  2, 0.75,    0,    0,    0,    0,    0,    0,    0,    0,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
-
-INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (35104,  5 /* HeartBeat */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-SET @parent_id = LAST_INSERT_ID();
-
-INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,   5 /* Motion */, 0, 1, 0x40000011 /* Dead */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (35104,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/35104_Corpse_of_a_Servitor_of_Asheron.es
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/35104_Corpse_of_a_Servitor_of_Asheron.es
@@ -1,4 +1,2 @@
-Heartbeat:
-	- Motion: Dead
 Use:
 	- DirectBroadcast: This golem has been utterly destroyed. It cannot be reactivated.
