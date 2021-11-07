4e0a0f48aa9f72919abd544af61a51e1fb6ba729
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2021-11-07 00:42:30 -0400

    Hoshino Door Fix (#1166)
    * Update 4BE2.sql
    
    Move door 46310 slightly to prevent bypassing the first part of the Hoshino Fortress Infiltration quest.
    
    * Update 4AE2.sql
    
    Move door 46310 (GUID 74AE2000) in landblock 4AE2 slightly (by 0.5) to prevent skipping it.
    
    * Update 4AE3.sql
    
    Move door 74AE3000 in 4AE3 slightly to prevent players from bypassing it.

--- Database/Patches/6 LandBlockExtendedData/4AE2.sql
+++ Database/Patches/6 LandBlockExtendedData/4AE2.sql
@@ -1,15 +1,15 @@
 DELETE FROM `landblock_instance` WHERE `landblock` = 0x4AE2;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x74AE2000, 46310, 0x4AE2002F, 133, 144.012, 172, 1, 0, 0, 0, False, '2021-11-01 00:00:00'); /* Door */
-/* @teleloc 0x4AE2002F [133.000000 144.011993 172.000000] 1.000000 0.000000 0.000000 0.000000 */
+VALUES (0x74AE2000, 46310, 0x4AE2002E, 133, 143.5, 172, 1, 0, 0, 0, False, '2021-11-01 00:00:00'); /* Door */
+/* @teleloc 0x4AE2002E [133.000000 143.511993 172.000000] 1.000000 0.000000 0.000000 0.000000 */
 
 INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
 VALUES (0x74AE2000, 0x74AE2001, '2021-08-10 13:21:51') /* Door (46311) */;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x74AE2001, 46311, 0x4AE2002F, 123.5, 149.175, 172, -0, 0, 0, -1,  True, '2021-11-01 00:00:00'); /* Door */
-/* @teleloc 0x4AE2002F [123.500000 149.175003 172.000000] -0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x74AE2001, 46311, 0x4AE2002F, 123.5, 149.175, 172, 0, 0, 0, -1,  True, '2021-11-01 00:00:00'); /* Door */
+/* @teleloc 0x4AE2002F [123.500000 149.175003 172.000000] 0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x74AE2002, 46312, 0x4AE2002E, 123.5, 138.827, 172, 1, 0, 0, 0, False, '2021-11-01 00:00:00'); /* Door */
--- Database/Patches/6 LandBlockExtendedData/4AE3.sql
+++ Database/Patches/6 LandBlockExtendedData/4AE3.sql
@@ -1,8 +1,8 @@
 DELETE FROM `landblock_instance` WHERE `landblock` = 0x4AE3;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x74AE3000, 46310, 0x4AE30013, 71.988, 56.5, 172, 0.707107, 0, 0, -0.707107, False, '2021-11-01 00:00:00'); /* Door */
-/* @teleloc 0x4AE30013 [71.987999 56.500000 172.000000] 0.707107 0.000000 0.000000 -0.707107 */
+VALUES (0x74AE3000, 46310, 0x4AE30013, 71.5, 56.5, 172, 0.707107, 0, 0, -0.707107, False, '2021-11-01 00:00:00'); /* Door */
+/* @teleloc 0x4AE30013 [71.588005 56.500000 172.000000] 0.707107 0.000000 0.000000 -0.707107 */
 
 INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
 VALUES (0x74AE3000, 0x74AE3001, '2021-08-10 13:21:51') /* Door (46311) */;
