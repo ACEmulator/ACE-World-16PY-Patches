b921868088e6e22878fa7f289c85fa168519fbf0
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-11-07 23:20:39 -0600

    QuestUpdate (#1145)

---
+++ Database/Patches/9 WeenieDefaults/Generic/None/87417 Withered Atoll Tumerok Generator.sql
@@ -0,0 +1,33 @@
+DELETE FROM `weenie` WHERE `class_Id` = 87417;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (87417, 'ace87417-WitheredAtollTumerokGenerator', 1, '2020-08-17 00:06:48') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (87417,  81,          3) /* MaxGeneratedObjects */
+     , (87417,  82,          3) /* InitGeneratedObjects */
+     , (87417,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (87417,   1, True ) /* Stuck */
+     , (87417,  11, True ) /* IgnoreCollisions */
+     , (87417,  18, True ) /* Visibility */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (87417,  41,     300) /* RegenerationInterval */
+     , (87417,  43,       5) /* GeneratorRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (87417,   1, 'Withered Atoll Tumerok Generator') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (87417,   1,   33555051) /* Setup */
+     , (87417,   8,  100667494) /* Icon */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (87417, 0.16, 35835, 30, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Withered Transcendent Tumerok (35835) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (87417, 0.32, 30687, 30, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Withered Revered Tumerok Shaman (30687) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (87417, 0.48, 35835, 30, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Withered Transcendent Tumerok (35835) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (87417, 0.54, 30687, 30, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Withered Revered Tumerok Shaman (30687) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (87417, 0.7, 35835, 30, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Withered Transcendent Tumerok (35835) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (87417, 0.88, 30687, 30, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Withered Revered Tumerok Shaman (30687) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
