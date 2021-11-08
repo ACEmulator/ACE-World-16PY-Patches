e7ae9b3d42f3e76e9a9a824d2bb07a0ca7801b62
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-11-07 22:18:30 -0600

    Aerbax's Prodigal Banderling and The Archivist Quests (#1143)
    * QuestandStuff
    
    * Updates
    
    * UpdateDiyastomatchotherPR
    
    * UpdateDiyas
    
    * UpdateDiyasAgain

---
+++ Database/Patches/8 QuestDefDB/GuardWinterbornFlag.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'GuardWinterbornFlag';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('GuardWinterbornFlag', 0, 1, 'Player has completed Aerbax Prodigal Banderling Quest at least once.', '2021-10-21 06:51:50');
---
+++ Database/Patches/8 QuestDefDB/HarraagsLairFlag.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'HarraagsLairFlag';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('HarraagsLairFlag', 0, 1, 'Player flag for drop location portal in Harraags Hideout', '2021-03-18 06:51:50');
