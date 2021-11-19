cf63f9c3d0654858a25c052cce72037a4abe07fa
An Adventurer <an.adventurer.ac@gmail.com>
2021-11-19 10:10:35 -0600

    Updating Asheron's Sanctum (5E47) with EoR spawns, creating Asheron's Report (37062)

--- Database/Patches/6 LandBlockExtendedData/5E47.sql
+++ Database/Patches/6 LandBlockExtendedData/5E47.sql
@@ -1,17 +1,32 @@
 DELETE FROM `landblock_instance` WHERE `landblock` = 0x5E47;
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x75E47000, 24432, 0x5E470103, 12.25, 4, 0.005, 0, 0, 0, -1,  True, '2005-02-09 10:00:00'); /* Guardian */
-/* @teleloc 0x5E470103 [12.250000 4.000000 0.005000] 0.000000 0.000000 0.000000 -1.000000 */
+VALUES (0x75E47002, 33590, 0x5E470105, 10.3632, -22.7972, 0, 0, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Asheron's Tower */
+/* @teleloc 0x5E470105 [10.363200 -22.797200 0.000000] 0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x75E47001, 24432, 0x5E470103, 7.75, 4, 0.005, 0, 0, 0, -1,  True, '2005-02-09 10:00:00'); /* Guardian */
-/* @teleloc 0x5E470103 [7.750000 4.000000 0.005000] 0.000000 0.000000 0.000000 -1.000000 */
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x75E47003,  7923, 0x5E470104, 11.8286, -9.20461, 0.005, 1, 0, 0, 0, False, '2005-02-09 10:00:00'); /* Linkable Monster Generator ( 3 Min.) */
-/* @teleloc 0x5E470104 [11.828600 -9.204610 0.005000] 1.000000 0.000000 0.000000 0.000000 */
+VALUES (0x75E47003,  1154, 0x5E470103, 8, 4, 0.01, 0, 0, 0, -1, False, '2019-02-10 00:00:00'); /* Linkable Monster Generator */
+/* @teleloc 0x5E470103 [8.000000 4.000000 0.010000] 0.000000 0.000000 0.000000 -1.000000 */
 
 INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
-VALUES (0x75E47003, 0x75E47000, '2005-02-09 10:00:00') /* Guardian (24432) */
-     , (0x75E47003, 0x75E47001, '2005-02-09 10:00:00') /* Guardian (24432) */;
+VALUES (0x75E47003, 0x75E47004, '2019-02-10 00:00:00') /* Guardian (24432) */
+     , (0x75E47003, 0x75E47005, '2019-02-10 00:00:00') /* Guardian (24432) */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x75E47004, 24432, 0x5E470103, 8, 4, 0.01, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Guardian */
+/* @teleloc 0x5E470103 [8.000000 4.000000 0.010000] 0.000000 0.000000 0.000000 -1.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x75E47005, 24432, 0x5E470103, 12, 4, 0.01, 0, 0, 0, -1,  True, '2019-02-10 00:00:00'); /* Guardian */
+/* @teleloc 0x5E470103 [12.000000 4.000000 0.010000] 0.000000 0.000000 0.000000 -1.000000 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x75E47006,  1542, 0x5E470107, 20.4978, -11.4963, 1.13063, 0.516241, 0, 0, -0.856444, False, '2019-02-10 00:00:00'); /* Linkable Item Generator */
+/* @teleloc 0x5E470107 [20.497800 -11.496300 1.130630] 0.516241 0.000000 0.000000 -0.856444 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x75E47006, 0x75E47007, '2019-02-10 00:00:00') /* Asheron's Report (37062) */;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x75E47007, 37062, 0x5E470107, 20.4978, -11.4963, 1.13063, 0.516241, 0, 0, -0.856444,  True, '2019-02-10 00:00:00'); /* Asheron's Report */
+/* @teleloc 0x5E470107 [20.497800 -11.496300 1.130630] 0.516241 0.000000 0.000000 -0.856444 */
