7626163df3b3e711772235eea76dc6aa56e16dd6
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-11-19 10:22:07 -0800

    AddedGivetoLeopold

---
+++ Database/Patches/8 QuestDefDB/VisionQuestRepeat1007.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'VisionQuestRepeat1007';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('VisionQuestRepeat1007', 0, 1, 'Player has completed Delving into Claudes Mind once.', '2021-11-17 16:56:08');
