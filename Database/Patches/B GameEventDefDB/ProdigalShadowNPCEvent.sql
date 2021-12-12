087fa8c0e9fb49b4a10e6cca3a303c5a50c25078
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-12-11 19:07:44 -0600

    ForgotEvents

---
+++ Database/Patches/B GameEventDefDB/ProdigalShadowNPCEvent.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'ProdigalShadowNPCEvent';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('ProdigalShadowNPCEvent', -1, -1, 4, '2021-12-09 00:00:00');
