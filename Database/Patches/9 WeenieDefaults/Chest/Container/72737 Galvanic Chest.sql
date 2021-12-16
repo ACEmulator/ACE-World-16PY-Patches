95c7a496b32abc268897a84f2ee58b402ad87df6
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2021-12-16 09:34:36 -0500

    Update 72737 Galvanic Chest.sql

--- Database/Patches/9 WeenieDefaults/Chest/Container/72737 Galvanic Chest.sql
+++ Database/Patches/9 WeenieDefaults/Chest/Container/72737 Galvanic Chest.sql
@@ -51,4 +51,4 @@ VALUES (72737,   1,   33557001) /* Setup */
      , (72737,  28,       5972) /* Spell - Galvanic Bomb */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (72737, -1, 72417, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Bar of Curved Metal (72417) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
+VALUES (72737, -1, 72738, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Bar of Curved Metal (72738) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
