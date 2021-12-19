6c305e7b848cc4b81b8b708ea4b06eafdc88a707
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-12-19 14:53:30 -0600

    UpdatedEmotes

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/87243.es
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/87243.es
@@ -3,51 +3,59 @@ Use:
         QuestSuccess:
             - DirectBroadcast: You have already discovered this gift box and all of the other gift boxes hidden around Dereth!
         QuestFailure:
-            - InqQuestBitsOn: GiftBoxesFound, 0x64
+            - InqQuestBitsOn: GiftBoxesFound, 0x40
                 QuestSuccess:
                     - DirectBroadcast: You have already discovered this gift box.
                 QuestFailure:
-                    - SetQuestBitsOn: GiftBoxesFound, 0x64
+                    - SetQuestBitsOn: GiftBoxesFound, 0x40
                     - StampQuest: GiftBoxesFoundCounter
                     - InqIntStat: Level, 1 - 49
                         TestSuccess:
                             - DirectBroadcast: You reach into the gift box and discover the gift of knowledge!
-                            - AwardLevelProportionalXP: 100%, 0 - 1,500,000
+                            - AwardLevelProportionalXP: 100%, Min: 1, Max: 1,500,000
                             - Goto: TitleRewards
                         TestFailure:
                             - Goto: RandomRewards
-                                GotoSet: Probability: 0.125
-                                    - DirectBroadcast: You reach into the gift box and discover an A'nekshay Token!
-                                    - Give: 44240
-                                    - Goto: TitleRewards
-                                GotoSet: Probability: 0.25
-                                    - DirectBroadcast: You reach into the gift box and discover an Ancient Mhoire Coin!
-                                    - Give: Ancient Mhoire Coin (35383)
-                                    - Goto: TitleRewards
-                                GotoSet: Probability: 0.375
-                                    - DirectBroadcast: You reach into the gift box and discover a Colosseum Coin!
-                                    - Give: Colosseum Coin (36518)
-                                    - Goto: TitleRewards
-                                GotoSet: Probability: 0.5
-                                    - DirectBroadcast: You reach into the gift box and discover an Exquisite Casino Key!
-                                    - Give: 52033
-                                    - Goto: TitleRewards
-                                GotoSet: Probability: 0.625
-                                    - DirectBroadcast: You reach into the gift box and discover a Legendary Key!
-                                    - Give: 48750
-                                    - Goto: TitleRewards
-                                GotoSet: Probability: 0.75
-                                    - DirectBroadcast: You reach into the gift box and discover an Ornate Gear Marker!
-                                    - Give: 43142
-                                    - Goto: TitleRewards
-                                GotoSet: Probability: 0.875
-                                    - DirectBroadcast: You reach into the gift box and discover a Small Olthoi Venom Sac!
-                                    - Give: 36376
-                                    - Goto: TitleRewards
-                                GotoSet:
-                                    - DirectBroadcast: You reach into the gift box and discover a Stipend!
-                                    - Give: Stipend (46423)
-                                    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.125
+    - DirectBroadcast: You reach into the gift box and discover an A'nekshay Token!
+    - Give: 44240
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.25
+    - DirectBroadcast: You reach into the gift box and discover an Ancient Mhoire Coin!
+    - Give: 35383
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.375
+    - DirectBroadcast: You reach into the gift box and discover a Colosseum Coin!
+    - Give: 36518
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.5
+    - DirectBroadcast: You reach into the gift box and discover an Exquisite Casino Key!
+    - Give: 52033
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.625
+    - DirectBroadcast: You reach into the gift box and discover a Legendary Key!
+    - Give: 48750
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.75
+    - DirectBroadcast: You reach into the gift box and discover an Ornate Gear Marker!
+    - Give: 43142
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 0.875
+    - DirectBroadcast: You reach into the gift box and discover a Small Olthoi Venom Sac!
+    - Give: 36376
+    - Goto: TitleRewards
+
+GotoSet: RandomRewards, Probability: 1
+    - DirectBroadcast: You reach into the gift box and discover a Stipend!
+    - Give: 46423
+    - Goto: TitleRewards
 
 GotoSet: TitleRewards, Probability: 0.33
     - InqQuest: GiftBoxesFoundCounter
--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/87244 Gift Box.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/87244 Gift Box.sql
@@ -137,7 +137,7 @@ VALUES (87244, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'GiftBoxesWait',
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0, 102 /* InqQuestBitsOn */, 0, 1, NULL, 'GiftBoxesFound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 296, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id,  0, 102 /* InqQuestBitsOn */, 0, 1, NULL, 'GiftBoxesFound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 128, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (87244, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'GiftBoxesFound', NULL, NULL, NULL);
@@ -145,7 +145,7 @@ VALUES (87244, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'GiftBoxesFound'
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0, 106 /* SetQuestBitsOn */, 0, 1, NULL, 'GiftBoxesFound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 296, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+VALUES (@parent_id,  0, 106 /* SetQuestBitsOn */, 0, 1, NULL, 'GiftBoxesFound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 128, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
      , (@parent_id,  1,  22 /* StampQuest */, 0, 1, NULL, 'GiftBoxesFoundCounter', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
      , (@parent_id,  2,  36 /* InqIntStat */, 0, 1, NULL, 'Level_1-49', NULL, 1, 49, NULL, NULL, NULL, NULL, 25 /* PropertyInt.Level */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
@@ -204,7 +204,7 @@ SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (@parent_id,  0,  18 /* DirectBroadcast */, 0, 1, NULL, 'You reach into the gift box and discover the gift of knowledge!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-     , (@parent_id,  1,  49 /* AwardLevelProportionalXP */, 0, 1, NULL, NULL, NULL, NULL, NULL, 0, 1500000, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  1,  49 /* AwardLevelProportionalXP */, 0, 1, NULL, NULL, NULL, 1, 1500000, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
      , (@parent_id,  2,  67 /* Goto */, 0, 1, NULL, 'TitleRewards', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
