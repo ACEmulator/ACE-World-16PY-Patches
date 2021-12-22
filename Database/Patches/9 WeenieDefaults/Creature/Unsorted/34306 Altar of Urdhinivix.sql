c707fa438521a522c87815f2913bfc3a2a865f9d
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-12-22 14:50:26 -0600

    Update 34306 Altar of Urdhinivix.sql

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/34306 Altar of Urdhinivix.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/34306 Altar of Urdhinivix.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34306;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34306, 'ace34306-altarofurdhinivix', 10, '2021-11-17 16:56:08') /* Creature */;
+VALUES (34306, 'ace34306-altarofurdhinivix', 10, '2021-12-22 00:00:00') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34306,   1,         16) /* ItemType - Creature */
@@ -14,19 +14,38 @@ VALUES (34306,   1,         16) /* ItemType - Creature */
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (34306,   1, True ) /* Stuck */
      , (34306,   8, True ) /* AllowGive */
-     , (34306,  19, False) /* Attackable */;
+     , (34306,  12, True ) /* ReportCollisions */
+     , (34306,  13, False) /* Ethereal */
+     , (34306,  19, False) /* Attackable */
+     , (34306,  52, True ) /* AiImmobile */
+     , (34306,  82, True ) /* DontTurnOrMoveWhenGiving */
+     , (34306,  83, True ) /* NpcLooksLikeObject */
+     , (34306,  90, True ) /* NpcInteractsSilently */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (34306,  54,       3) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (34306,   1, 'Altar of Urdhinivix') /* Name */;
+VALUES (34306,   1, 'Altar of Urdhinivix') /* Name */
+     , (34306,  16, 'This altar seems to glow with an unholy light. It seems to be consecrated to a being named Urdhinivix.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (34306,   1, 0x02001653) /* Setup */
      , (34306,   2, 0x0900013D) /* MotionTable */
      , (34306,   3, 0x20000015) /* SoundTable */
      , (34306,   8, 0x060030CC) /* Icon */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (34306, 31, 0, 3, 0, 500, 0, 0) /* CreatureEnchantment Specialized */
+     , (34306, 32, 0, 3, 0, 500, 0, 0) /* ItemEnchantment     Specialized */
+     , (34306, 33, 0, 3, 0, 500, 0, 0) /* LifeMagic           Specialized */
+     , (34306, 34, 0, 3, 0, 500, 0, 0) /* WarMagic            Specialized */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (34306,  0,  8,  3,  0.5,   20,   16,   16,   16,   20,   20,   20,   20,    0, 1,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Head */
+     , (34306, 16, 64,  3,  0.5,   20,   16,   16,   16,   20,   20,   20,   20,    0, 2,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4) /* Torso */
+     , (34306, 17, 64,  3, 0.75,   20,   16,   16,   16,   20,   20,   20,   20,    0, 2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Tail */
+     , (34306, 21, 64,  3,  0.5,   10,    8,    8,    8,   10,   10,   10,   10,    0, 2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2) /* Wings */;
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (34306,  6 /* Give */,      1, 34309 /* Filled Sacrificial Goblet */, NULL, NULL, NULL, NULL, NULL, NULL);
