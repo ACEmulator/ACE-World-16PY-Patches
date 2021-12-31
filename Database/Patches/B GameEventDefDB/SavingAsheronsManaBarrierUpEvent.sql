149cfda0cb268c62ee2af466e41bf32faeef50fe
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2021-12-31 13:04:29 -0600

    Updates

--- Database/Patches/B GameEventDefDB/SavingAsheronsManaBarrierUpEvent.sql
+++ Database/Patches/B GameEventDefDB/SavingAsheronsManaBarrierUpEvent.sql
@@ -1,4 +1,4 @@
 DELETE FROM `event` WHERE `name` = 'SavingAsheronsManaBarrierUpEvent';
 
 INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('SavingAsheronsManaBarrierUpEvent', -1, -1, 3 /* GameEventState.Off */, '2021-11-01 00:00:00');
+VALUES ('SavingAsheronsManaBarrierUpEvent', -1, -1, 4 /* GameEventState.Off */, '2021-11-01 00:00:00');
