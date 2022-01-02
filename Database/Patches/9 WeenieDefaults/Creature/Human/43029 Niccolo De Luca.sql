337d2e9c75316d414cdce26826b894e70f6cceaf
bobigor377 <61636128+bobigor377@users.noreply.github.com>
2022-01-01 22:08:55 -0500

    Update 43029 Niccolo De Luca.sql

--- Database/Patches/9 WeenieDefaults/Creature/Human/43029 Niccolo De Luca.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/43029 Niccolo De Luca.sql
@@ -50,7 +50,7 @@ VALUES (43029,   1,     0, 0, 0, 125) /* MaxHealth */
      , (43029,   5,     0, 0, 0, 170) /* MaxMana */;
      
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (43030,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (43029,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -59,7 +59,7 @@ VALUES (@parent_id,  0,  12 /* TurnToTarget */, 0, 1, NULL, NULL, NULL, NULL, NU
      , (@parent_id,  1,  21 /* InqQuest */, 0, 1, NULL, 'MonthlyQuestWeeklyOne0710', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (43030, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'MonthlyQuestWeeklyOne0710', NULL, NULL, NULL);
+VALUES (43029, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'MonthlyQuestWeeklyOne0710', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -68,7 +68,7 @@ VALUES (@parent_id,  0,  10 /* Tell */, 0, 1, NULL, 'You have already been rewar
      , (@parent_id,  1,  18 /* DirectBroadcast */, 1, 1, NULL, 'You must wait %tqt to be rewarded again.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (43030, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'QuestBanderlingHauntCompleted0105', NULL, NULL, NULL);
+VALUES (43029, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'boneylumpoffleshpickuptimer', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -79,15 +79,15 @@ VALUES (@parent_id,  0,  49 /* AwardLevelProportionalXP */, 0, 1, NULL, NULL, NU
      , (@parent_id,  3,  22 /* StampQuest */, 0, 1, NULL, 'MonthlyQuestWeeklyOne0710', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (43030, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'MonthlyQuestWeeklyOne0710', NULL, NULL, NULL);
+VALUES (43029, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'MonthlyQuestWeeklyOne0710', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  21 /* InqQuest */, 0, 1, NULL, 'QuestBanderlingHauntCompleted0105', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id,  0,  21 /* InqQuest */, 0, 1, NULL, 'boneylumpoffleshpickuptimer', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (43030, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'QuestBanderlingHauntCompleted0105', NULL, NULL, NULL);
+VALUES (43029, 13 /* QuestFailure */,      1, NULL, NULL, NULL, 'boneylumpoffleshpickuptimer', NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
