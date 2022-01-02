bb83562d7318e0377e4911520c338f3bb727b048
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2022-01-01 22:25:17 -0500

    Serpent Burial Grounds Quest (#1237)
    * Serpent Burial Grounds Quest
    
    Adds the quest.
    
    http://acpedia.org/wiki/Serpent_Burial_Grounds_Quest
    
    * update xp reward
    
    Set XP reward to level proportional instead of flat.
    
    * revisions based on testing
    
    Increased visual awareness on Sclavus Neophytes to 35 from 24.
    Corrected class name for Sclavus Sentinels.
    Added persistent missile to slithis tentacles to prevent movement due to sticky melee.
    Reordered items in create list for Shikken Moriyaki to match retail video.
    
    * Update 584E.sql
    
    Added missing slithis in the boss room.

---
+++ Database/Patches/8 QuestDefDB/SerpentBurialGroundsKeyPickup.sql
@@ -0,0 +1,3 @@
+DELETE FROM `quest` WHERE `name` = 'SerpentBurialGroundsKeyPickup';
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('SerpentBurialGroundsKeyPickup', 72000, -1, 'quest timer', '2020-01-24 19:57:17');
