90db65700457fd5db0c946eb4b1435b30d8996ce
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-01-19 11:17:17 -0600

    Updates (#1261)

---
+++ Database/Patches/8 QuestDefDB/DireChampDireHuntTitle.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'DireChampDireHuntTitle';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('DireChampDireHuntTitle', 0, 1, 'Player has received the title Dire Huntsman.', '2022-01-11 16:56:08');
---
+++ Database/Patches/8 QuestDefDB/DireChampHonoraryReaTitle.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'DireChampHonoraryReaTitle';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('DireChampHonoraryReaTitle', 0, 1, 'Player has received the title Honorary Rea of the Aun.', '2022-01-11 16:56:08');
