fc0804347266f6afda421a86d160de6ca61ecaa5
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2022-01-21 21:45:23 -0600

    missed a couple

--- Database/Patches/9 WeenieDefaults/Caster/Caster/27836 Singularity Scepter of Life Magic.sql
+++ Database/Patches/9 WeenieDefaults/Caster/Caster/27836 Singularity Scepter of Life Magic.sql
@@ -37,7 +37,7 @@ VALUES (27836,  22, True ) /* Inscribable */
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (27836,   5,  -0.033) /* ManaRate */
      , (27836,  29,    1.07) /* WeaponDefense */
-     , (27836, 138,     1.4) /* SlayerDamageBonus */
+     , (27836, 138,     1.8) /* SlayerDamageBonus */
      , (27836, 144,    0.07) /* ManaConversionMod */
      , (27836, 147,    0.25) /* CriticalFrequency */;
 
