843befe418334354146c56c61a27a66a0e334064
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2022-01-25 20:54:43 -0600

    Update 33517 Surface.sql
    added destination and teleloc which fixed the error destination not yet implemented

--- Database/Patches/9 WeenieDefaults/Portal/Portal/33517 Surface.sql
+++ Database/Patches/9 WeenieDefaults/Portal/Portal/33517 Surface.sql
@@ -22,9 +22,14 @@ INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (33517,  54,    -0.1) /* UseRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (33517,   1, 'Surface') /* Name */;
+VALUES (33517,   1, 'Surface') /* Name */
+     , (33517,  38, 'Surface (75.8N, 70.2E)') /* AppraisalPortalDestination */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (33517,   1, 0x020001B3) /* Setup */
      , (33517,   2, 0x09000003) /* MotionTable */
      , (33517,   8, 0x0600106B) /* Icon */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (33517,  2, 3621650442, 34.219322, 36.04955, 62.005, -0.70025, 0, 0, 0.713897) /*Destination*/
+/* @teleloc 0xD7DE000A [34.219322 36.049549 62.005001] -0.700250 0.000000 0.000000 0.713897 */;
