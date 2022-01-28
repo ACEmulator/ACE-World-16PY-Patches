a112857b190d3648d8e123f8fd9dcb3dbf9707f5
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2022-01-28 07:29:35 -0600

    Delete KothMoxStaffPickedUp.sql
    removed pick up timer

--- Database/Patches/8 QuestDefDB/KothMoxStaffPickedUp.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'KothMoxStaffPickedUp';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('KothMoxStaffPickedUp', 72000, -1, 'You have obtained Kothmox Staff.', '2021-11-01 00:00:00');
