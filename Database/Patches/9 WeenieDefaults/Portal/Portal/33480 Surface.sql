662bc4ec820843a6dab89df506362656db21ed6c
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2022-01-28 12:05:33 -0600

    Update 33480 Surface.sql (#1274)
    * Update 33480 Surface.sql
    
    add portal bitmask 49 to prevent from being able to get free access to viss island without doing the quest
    
    * Update 33480 Surface.sql
    
    * Update 33480 Surface.sql
    
    Co-authored-by: ziang4891 <56269610+ziang4891@users.noreply.github.com>

--- Database/Patches/9 WeenieDefaults/Portal/Portal/33480 Surface.sql
+++ Database/Patches/9 WeenieDefaults/Portal/Portal/33480 Surface.sql
@@ -1,12 +1,13 @@
 DELETE FROM `weenie` WHERE `class_Id` = 33480;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (33480, 'ace33480-surface', 7, '2021-11-01 00:00:00') /* Portal */;
+VALUES (33480, 'ace33480-surface', 7, '2022-01-25 07:32:30') /* Portal */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (33480,   1,      65536) /* ItemType - Portal */
      , (33480,  16,         32) /* ItemUseable - Remote */
      , (33480,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
+     , (33480, 111,         49) /* PortalBitmask */
      , (33480, 133,          4) /* ShowableOnRadar - ShowAlways */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
@@ -26,3 +27,4 @@ VALUES (33480,   1, 0x020001B3) /* Setup */
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (33480, 2, 0xD7DE0102, 95.4745, 35.8457, -6.34, -0.707107, 0, 0, 0.707107) /* Destination */
 /* @teleloc 0xD7DE0102 [95.474503 35.845699 -6.340000] -0.707107 0.000000 0.000000 0.707107 */;
+
