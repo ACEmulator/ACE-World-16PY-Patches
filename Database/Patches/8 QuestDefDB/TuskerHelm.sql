b5d328c8c196ecb4fc34d804a41623c5e50124ac
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-03-02 13:20:57 -0600

    Oolutanga Update (#1301)
    * Updates
    
    * RemovedExtent1

---
+++ Database/Patches/8 QuestDefDB/TuskerHelm.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'TuskerHelm';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('TuskerHelm', 0, 1, 'Player has put on the Tusker Skull Helm.', '2022-01-24 16:56:08');
