40c61242b3492a470c079ca1c68e1211b260d851
Ty Conner <tyconner@itcproductions.com>
2022-03-05 16:48:38 -0500

    Update RBGauntletArenaTwoBooter.sql

--- Database/Patches/B GameEventDefDB/RBGauntletArenaTwoBooter.sql
+++ Database/Patches/B GameEventDefDB/RBGauntletArenaTwoBooter.sql
@@ -1,4 +1,4 @@
 DELETE FROM `event` WHERE `name` = 'RBGauntletArenaTwoBooter';
 
 INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('RBGauntletArenaTwoBooter', -1, -1, 3, '2021-12-09 00:00:00');
+VALUES ('RBGauntletArenaTwoBooter', -1, -1, 4, '2021-12-09 00:00:00');
