34abd49e8a0493175481a58de1725de5899230c1
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-03-09 15:25:58 -0600

    RemovedPalettes

--- Database/Patches/9 WeenieDefaults/Creature/Human/31692 Jun Sawashima.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/31692 Jun Sawashima.sql
@@ -39,13 +39,7 @@ VALUES (31692,   1, 0x02000001) /* Setup */
      , (31692,   2, 0x09000001) /* MotionTable */
      , (31692,   3, 0x20000001) /* SoundTable */
      , (31692,   6, 0x0400007E) /* PaletteBase */
-     , (31692,   8, 0x06001036) /* Icon */
-     , (31692,   9, 0x0500112E) /* EyesTexture */
-     , (31692,  10, 0x0500115D) /* NoseTexture */
-     , (31692,  11, 0x05001195) /* MouthTexture */
-     , (31692,  15, 0x04001FDF) /* HairPalette */
-     , (31692,  16, 0x040002BD) /* EyesPalette */
-     , (31692,  17, 0x040004A9) /* SkinPalette */;
+     , (31692,   8, 0x06001036) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (31692,   1, 220, 0, 0) /* Strength */
