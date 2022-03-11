9b5c959d326c82627e07b313c02e6b79e8d6e4e9
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-03-11 13:15:10 -0600

    Updates

---
+++ Database/Patches/8 QuestDefDB/TrustBetrayalBugPickedUp0905.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'TrustBetrayalBugPickedUp0905';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('TrustBetrayalBugPickedUp0905', 72000, -1, 'Pickup timer for Grazer Bug', '2021-11-01 00:00:00');
--- Database/Patches/9 WeenieDefaults/Generic/Misc/70047 Grazer Bug.sql
+++ Database/Patches/9 WeenieDefaults/Generic/Misc/70047 Grazer Bug.sql
@@ -26,7 +26,7 @@ VALUES (70047,  39,     0.5) /* DefaultScale */;
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (70047,   1, 'Grazer Bug') /* Name */
      , (70047,  16, 'Grazer Bugs are small parasitic creatures that live inside the bowels of warm blooded animals. They feed off the half-digested food of their host where they will reproduce with their life mate counterpart. In cases of accidental expulsion from its host, the Grazer Bug has the unique ability to track down the host animal in order to reunite itself with its mate.') /* LongDesc */
-     , (70047,  33, 'TrustBetrayalBug0905') /* Quest */;
+     , (70047,  33, 'TrustBetrayalBugPickedUp0905') /* Quest */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (70047,   1, 0x02000181) /* Setup */
