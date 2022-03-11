0428cc3bac8fc21e713561d81d8268f785f6e7a2
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-03-11 12:30:49 -0600

    Updates (#1310)

---
+++ Database/Patches/8 QuestDefDB/EnricoAugmentationLetterTurnIn.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'EnricoAugmentationLetterTurnIn';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('EnricoAugmentationLetterTurnIn', 0, 1, 'Player has turned in Augmentation letter.', '2021-11-01 00:00:00');
