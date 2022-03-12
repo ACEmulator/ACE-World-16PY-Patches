52ddd020c31ac51305c21b62d07e10466e269f30
Ty Conner <tyconner@itcproductions.com>
2022-03-12 14:57:11 -0500

    Create AE62.sql

---
+++ Database/Patches/6 LandBlockExtendedData/AE62.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xAE62;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7AE62000, 12646, 0xAE620006, 1.45307, 128.808, 51.75782, 0.630368, 0, 0, -0.776296, False, '2019-02-10 00:00:00'); /* Arida Butte */
+/* @teleloc 0xAE620006 [1.453070 128.808000 51.757820] 0.630368 0.000000 0.000000 -0.776296 */
