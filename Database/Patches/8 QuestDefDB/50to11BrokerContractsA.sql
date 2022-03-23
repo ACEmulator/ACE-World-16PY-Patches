fbd3ec7adc88f6ca53c46cf3f0cfdc65eba0da27
Ty Conner <tyconner@itcproductions.com>
2022-03-23 12:34:08 -0400

    Update 50to11BrokerContractsA.sql

--- Database/Patches/8 QuestDefDB/50to11BrokerContractsA.sql
+++ Database/Patches/8 QuestDefDB/50to11BrokerContractsA.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = '50to11BrokerContractsA';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('50to11BrokerContractsA', 0, 4194302, 'Tracks which quests have been completed for Contract Brokers quest', '2021-11-01 00:00:00');
+VALUES ('50to11BrokerContractsA', 0, 0x1FFFFF, 'Tracks which quests have been completed for Contract Brokers quest', '2021-11-01 00:00:00');
