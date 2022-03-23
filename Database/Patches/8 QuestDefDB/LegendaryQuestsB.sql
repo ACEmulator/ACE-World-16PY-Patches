6928ad293219a9452504eb378b2cc73176a5bee9
Ty Conner <tyconner@itcproductions.com>
2022-03-23 12:34:16 -0400

    Update LegendaryQuestsB.sql

--- Database/Patches/8 QuestDefDB/LegendaryQuestsB.sql
+++ Database/Patches/8 QuestDefDB/LegendaryQuestsB.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'LegendaryQuestsB';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('LegendaryQuestsB', 0, 2097150, 'BitField Stamp for Legendary Quests Completed', '2021-11-01 00:00:00');
+VALUES ('LegendaryQuestsB', 0, 0xFFFFF, 'BitField Stamp for Legendary Quests Completed', '2021-11-01 00:00:00');
