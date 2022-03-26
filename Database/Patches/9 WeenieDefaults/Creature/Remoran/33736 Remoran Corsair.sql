c7494b0c6b1e76c0d16a382c7f0170f2a7fb56f6
Ty Conner <tyconner@itcproductions.com>
2022-03-26 14:50:16 -0400

    Update 33736 Remoran Corsair.sql

--- Database/Patches/9 WeenieDefaults/Creature/Remoran/33736 Remoran Corsair.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Remoran/33736 Remoran Corsair.sql
@@ -138,4 +138,4 @@ VALUES (33736, 9, 41979,  1, 0, 0.02, False) /* Create Shattered Mana Forge Key 
      , (33736, 9,     0,  0, 0, 0.98, False) /* Create nothing for ContainTreasure */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (33736, -1, 33629, -1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) /* Generate Fouled Remoran (33629) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (33736, -1, 33629, 0, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) /* Generate Fouled Remoran (33629) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
