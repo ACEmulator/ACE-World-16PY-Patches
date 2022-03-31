638d0b7f66684b19021b0b37c026e691e7958f94
Ty Conner <tyconner@itcproductions.com>
2022-03-31 01:04:05 -0400

    Create MonthlyQuestWeeklyOne0710.sql

---
+++ Database/Patches/8 QuestDefDB/MonthlyQuestWeeklyOne0710.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'MonthlyQuestBiWeeklyOne0710';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('MonthlyQuestBiWeeklyOne0710', 518400, -1, 'Player has completed the Weekly Monthly Explorer Society Quest.', '2021-11-01 00:00:00');
