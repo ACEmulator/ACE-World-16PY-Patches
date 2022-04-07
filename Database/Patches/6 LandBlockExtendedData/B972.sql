9b0ad1b33d5a25b1ed93be87518e749e35685e51
An Adventurer <an.adventurer.ac@gmail.com>
2022-04-07 15:06:07 -0500

    Removing all old weenie instances from the former site of the Yanshi refugee camp. Only LS remained at EoR

---
+++ Database/Patches/6 LandBlockExtendedData/B972.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xB972;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7B972022,   509, 0xB9720000, 147.054, 93.6479, 34.005, -0.613228, 0, 0, 0.789906, False, '2005-02-09 10:00:00'); /* Life Stone */
+/* @teleloc 0xB9720000 [147.054001 93.647903 34.005001] -0.613228 0.000000 0.000000 0.789906 */
