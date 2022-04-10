a24c3b6b1bd177f461b37456b4ff3144a1a0a9a3
Ty Conner <tyconner@itcproductions.com>
2022-04-10 14:29:47 -0400

    Update 49600 Ivory Crafter.sql

--- Database/Patches/9 WeenieDefaults/Creature/Human/49600 Ivory Crafter.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/49600 Ivory Crafter.sql
@@ -73,13 +73,7 @@ VALUES (49600,   1, 0x02000001) /* Setup */
      , (49600,   3, 0x20000083) /* SoundTable */
      , (49600,   4, 0x30000000) /* CombatTable */
      , (49600,   6, 0x0400007E) /* PaletteBase */
-     , (49600,   8, 0x06001036) /* Icon */
-     , (49600,   9, 0x05001154) /* EyesTexture */
-     , (49600,  10, 0x0500116C) /* NoseTexture */
-     , (49600,  11, 0x050011A7) /* MouthTexture */
-     , (49600,  15, 0x0400200E) /* HairPalette */
-     , (49600,  16, 0x040002BF) /* EyesPalette */
-     , (49600,  17, 0x040002B2) /* SkinPalette */;
+     , (49600,   8, 0x06001036) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (49600,   1,  80, 0, 0) /* Strength */
