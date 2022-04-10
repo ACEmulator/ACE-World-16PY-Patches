c239e56552d4ee6dcfa6408a8fa4edf02d02aec1
Ty Conner <tyconner@itcproductions.com>
2022-04-10 17:17:36 -0400

    normalize

--- Database/Patches/9 WeenieDefaults/Generic/None/87836 Blighted Dire Ursuin Camp Generator.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/87836 Blighted Dire Ursuin Camp Generator.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 87836;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (87836, 'blightedursuindirecampgen', 1, '2005-02-09 10:00:00') /* Generic */;
+VALUES (87836, 'ace87836-blighteddireursuincampgenerator', 1, '2005-02-09 10:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (87836,  81,          3) /* MaxGeneratedObjects */
