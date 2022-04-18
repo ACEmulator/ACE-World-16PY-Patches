ca78fa1e983262f197046245ca791e5724b10189
Ty Conner <tyconner@itcproductions.com>
2022-04-18 15:26:59 -0400

    Update 42961 Rock Pile.sql

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/42961 Rock Pile.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/42961 Rock Pile.sql
@@ -14,7 +14,11 @@ VALUES (42961,   1,         16) /* ItemType - Creature */
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (42961,   1, True ) /* Stuck */
-     , (42961,  19, False) /* Attackable */;
+     , (42961,  19, False) /* Attackable */
+     , (42961,  52, True ) /* AiImmobile */
+     , (42961,  82, True ) /* DontTurnOrMoveWhenGiving */
+     , (42961,  83, True ) /* NpcLooksLikeObject */
+     , (42961,  90, True ) /* NpcInteractsSilently */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (42961,  54,       3) /* UseRadius */;
