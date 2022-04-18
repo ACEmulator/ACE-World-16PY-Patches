4c7504f9bd1e132f1e45d3eb39949c60cb0ec219
Ty Conner <tyconner@itcproductions.com>
2022-04-18 01:05:28 -0400

    Update 42667 Top Hat.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Armor/42667 Top Hat.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/42667 Top Hat.sql
@@ -5,21 +5,25 @@ VALUES (42667, 'ace42667-tophat', 2, '2019-02-10 00:00:00') /* Clothing */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (42667,   1,          2) /* ItemType - Armor */
+     , (42667,   3,         21) /* PaletteTemplate - Brown */
      , (42667,   4,      16384) /* ClothingPriority - Head */
      , (42667,   5,        100) /* EncumbranceVal */
      , (42667,   9,          1) /* ValidLocations - HeadWear */
      , (42667,  16,          1) /* ItemUseable - No */
      , (42667,  19,       1200) /* Value */
+     , (42667,  27,          2) /* ArmorType - Leather */
      , (42667,  28,        110) /* ArmorLevel */
      , (42667,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (42667, 105,         10) /* ItemWorkmanship */
+     , (42667, 150,        103) /* HookPlacement - Hook */
      , (42667, 151,          2) /* HookType - Wall */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (42667,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (42667,  13,     1.3) /* ArmorModVsSlash */
+VALUES (42667,  12,    0.66) /* Shade */
+     , (42667,  13,     1.3) /* ArmorModVsSlash */
      , (42667,  14,       1) /* ArmorModVsPierce */
      , (42667,  15,       1) /* ArmorModVsBludgeon */
      , (42667,  16,     0.4) /* ArmorModVsCold */
@@ -36,5 +40,6 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (42667,   1, 0x02001974) /* Setup */
      , (42667,   3, 0x20000014) /* SoundTable */
      , (42667,   6, 0x0400007E) /* PaletteBase */
-     , (42667,   8, 0x06006159) /* Icon */
+     , (42667,   7, 0x100007A0) /* ClothingBase */
+     , (42667,   8, 0x06006D9F) /* Icon */
      , (42667,  22, 0x3400002B) /* PhysicsEffectTable */;
