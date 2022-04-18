e0e3a59789e0dd4715e48d8d7309a69ef6bc916c
Ty Conner <tyconner@itcproductions.com>
2022-04-18 05:14:04 -0400

    Update 42977 Game Coordinator.sql

--- Database/Patches/9 WeenieDefaults/Creature/Human/42977 Game Coordinator.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/42977 Game Coordinator.sql
@@ -33,13 +33,7 @@ VALUES (42977,   1, 0x02000001) /* Setup */
      , (42977,   2, 0x09000001) /* MotionTable */
      , (42977,   3, 0x20000001) /* SoundTable */
      , (42977,   6, 0x0400007E) /* PaletteBase */
-     , (42977,   8, 0x06001036) /* Icon */
-     , (42977,   9, 0x0500113F) /* EyesTexture */
-     , (42977,  10, 0x05001177) /* NoseTexture */
-     , (42977,  11, 0x050011CF) /* MouthTexture */
-     , (42977,  15, 0x04002016) /* HairPalette */
-     , (42977,  16, 0x040004B0) /* EyesPalette */
-     , (42977,  17, 0x04001B7D) /* SkinPalette */;
+     , (42977,   8, 0x06001036) /* Icon */;
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (42977, 8040, 0xB3700022, 116.366, 26.7017, 26.005, -0.197122, 0, 0, -0.980379) /* PCAPRecordedLocation */
@@ -57,3 +51,7 @@ INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`
 VALUES (42977,   1,   5, 0, 0, 65) /* MaxHealth */
      , (42977,   3,   110, 0, 0, 230) /* MaxStamina */
      , (42977,   5,   5, 0, 0, 105) /* MaxMana */;
+
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (42977, 2, 28615,  1, 2, 0.5, False) /* Create Royal Bouquet (42709) for Wield */
+     , (42977, 2, 56,  1, 93, 0, False) /* Create Royal Bouquet (42709) for Wield */;
