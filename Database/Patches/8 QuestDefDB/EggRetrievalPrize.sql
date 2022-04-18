b5e99694bc0c829b1eeb6bd6b5eb48ebdb2e788d
Ty Conner <tyconner@itcproductions.com>
2022-04-18 13:57:37 -0400

    Create EggRetrievalPrize.sql

---
+++ Database/Patches/8 QuestDefDB/EggRetrievalPrize.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'EggRetrievalPrize';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('EggRetrievalPrize', 0, 3, 'Additional reward counter for Candle Retrieval', '2021-11-01 00:00:00');
