b925033347b88b3518f90d742510219f41dc04bc
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2022-04-18 20:57:49 -0500

    jester marker changed to 20 hr timer
    https://asheron.fandom.com/wiki/Jester%27s_Lost_Marbles#Update_History

--- Database/Patches/8 QuestDefDB/foundjestermarker1.sql
+++ Database/Patches/8 QuestDefDB/foundjestermarker1.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'foundjestermarker1';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('foundjestermarker1', 0, 1, 'Found Jester Marker', '2021-11-01 00:00:00');
+VALUES ('foundjestermarker1', 72000, -1, 'Found Jester Marker', '2021-11-01 00:00:00');
--- Database/Patches/8 QuestDefDB/foundjestermarker2.sql
+++ Database/Patches/8 QuestDefDB/foundjestermarker2.sql
@@ -1,4 +1,4 @@
 DELETE FROM `quest` WHERE `name` = 'foundjestermarker2';
 
 INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('foundjestermarker2', 0, 1, 'Found Jester Marker', '2021-11-01 00:00:00');
+VALUES ('foundjestermarker2', 72000, -1, 'Found Jester Marker', '2021-11-01 00:00:00');
