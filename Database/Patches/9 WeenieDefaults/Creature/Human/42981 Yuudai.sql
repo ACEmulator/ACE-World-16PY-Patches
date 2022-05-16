ccc86696f49bd3beecf4cedfea0b60662b031fd7
Ty Conner <tyconner@itcproductions.com>
2022-05-15 21:56:15 -0400

    Update 42981 Yuudai.sql

--- Database/Patches/9 WeenieDefaults/Creature/Human/42981 Yuudai.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/42981 Yuudai.sql
@@ -36,8 +36,7 @@ VALUES (42981,   1, 0x02000001) /* Setup */
      , (42981,   2, 0x09000001) /* MotionTable */
      , (42981,   3, 0x20000001) /* SoundTable */
      , (42981,   6, 0x0400007E) /* PaletteBase */
-     , (42981,   8, 0x06001036) /* Icon */
-     , (42981,  22, 0x34000004) /* PhysicsEffectTable */;
+     , (42981,   8, 0x06001036) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (42981,   1, 150, 0, 0) /* Strength */
