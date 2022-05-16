0c3044d3c53763388e51a75754671f009dc211be
Ty Conner <tyconner@itcproductions.com>
2022-05-15 21:56:19 -0400

    Update 42987 Messenger for The Stranger.sql

--- Database/Patches/9 WeenieDefaults/Creature/Human/42987 Messenger for The Stranger.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/42987 Messenger for The Stranger.sql
@@ -32,8 +32,7 @@ VALUES (42987,   1, 0x02000001) /* Setup */
      , (42987,   2, 0x09000001) /* MotionTable */
      , (42987,   3, 0x20000001) /* SoundTable */
      , (42987,   6, 0x0400007E) /* PaletteBase */
-     , (42987,   8, 0x06001036) /* Icon */
-     , (42987,  22, 0x34000004) /* PhysicsEffectTable */;
+     , (42987,   8, 0x06001036) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (42987,   1, 290, 0, 0) /* Strength */
