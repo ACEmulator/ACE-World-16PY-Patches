0f8ddc8e87af8f9505ec1bd173ad17a96ad2a3e0
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-05-19 15:20:13 -0500

    WeenieConflictFix

--- Database/Patches/6 LandBlockExtendedData/380A.sql
+++ Database/Patches/6 LandBlockExtendedData/380A.sql
@@ -5,5 +5,5 @@ VALUES (0x7380A001, 43120, 0x380A0102, 132.106, 92.8269, -5.663, 0.999826, 0, 0,
 /* @teleloc 0x380A0102 [132.106003 92.826897 -5.663000] 0.999826 0.000000 0.000000 0.018646 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7380A002, 88069, 0x380A0010, 35.99902, 180, 0.055, 1, 0, 0, 0, False, '2022-05-19 14:42:51'); /* Tch'Keryk the Emissary Generator */
+VALUES (0x7380A002, 88079, 0x380A0010, 35.99902, 180, 0.055, 1, 0, 0, 0, False, '2022-05-19 14:42:51'); /* Tch'Keryk the Emissary Generator */
 /* @teleloc 0x380A0010 [35.999020 180.000000 0.055000] 1.000000 0.000000 0.000000 0.000000 */
