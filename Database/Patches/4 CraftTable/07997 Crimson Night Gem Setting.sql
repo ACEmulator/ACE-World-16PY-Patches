225a7a8917fafbaf42407a451b572004ec82eed7
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2022-05-27 09:52:13 -0700

    Update 07997 Crimson Night Gem Setting.sql

--- Database/Patches/4 CraftTable/07997 Crimson Night Gem Setting.sql
+++ Database/Patches/4 CraftTable/07997 Crimson Night Gem Setting.sql
@@ -3,6 +3,9 @@ DELETE FROM `recipe` WHERE `id` = 7997;
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
 VALUES (7997, 0, 0, 0, 0, 0, 0, 'You''ve added the setting to the Silifi of Crimson Stars.', 0, 0, 'This should never happen.', 1, 1, '', 0, 0, '', 1, 1, '', 0, 0, '', 0, '2021-11-01 00:00:00');
 
+INSERT INTO `recipe_requirements_float` (`recipe_Id`, `index`, `stat`, `value`, `enum`, `message`)
+VALUES (7997, 0, 157, 1, 3, 'The Silifi only accepts one Ruby of Crimson Night!') /* Target.ItemWorkmanship GreaterThanEqual 1 */
+
 INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
 VALUES (7997, True, 0, 0, 0, False, 0, 1, 0);
 
