df54c83b755aaa4ba0e5d32c584d0d133254697d
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2022-06-09 07:32:42 -0700

    TradeAgreements

--- Database/Patches/9 WeenieDefaults/Generic/Misc/28844 Renald's Old Mug.sql
+++ Database/Patches/9 WeenieDefaults/Generic/Misc/28844 Renald's Old Mug.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 28844;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (28844, 'mugrenald', 1, '2021-11-17 16:56:08') /* Generic */;
+VALUES (28844, 'mugrenald', 1, '2022-06-09 00:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (28844,   1,        128) /* ItemType - Misc */
@@ -16,7 +16,7 @@ INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (28844,   1, 'Renald''s Old Mug') /* Name */
      , (28844,  16, 'A battered old pewter mug. By the filthy condition it is in, this mug has never seen soap nor water in it''s lifetime. Etched into the side of it is the name Renald. Return this mug to Renald the Eldest.') /* LongDesc */
      , (28844,  33, 'renaldsoldmug') /* Quest */
-     , (28844,  37, 'RenaldRefusal') /* QuestRestriction */;
+     , (28844,  37, 'RenaldMug') /* QuestRestriction */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (28844,   1, 0x020000E6) /* Setup */
