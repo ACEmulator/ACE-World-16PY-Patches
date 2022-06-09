4a4c8d042f73ca524807ff9475fd130b0033f1b9
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2022-06-09 13:23:29 -0400

    Vissidal Clear Drop
    Cleared the spawns from the Vissidal drop to match Hells retail video:
    
    https://www.twitch.tv/videos/48337756?t=0h48m18s
    
    There were no spawns at the drop, and going toward town.

--- Database/Patches/1 RegionDescExtendedData/C8E2.sql
+++ Database/Patches/1 RegionDescExtendedData/C8E2.sql
@@ -8,12 +8,4 @@ VALUES (0xC8E2, 70106, 0, 2, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
      , (0xC8E2, 70106, 1, 6, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
      , (0xC8E2, 70106, 2, 4, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
      , (0xC8E2, 70106, 2, 5, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 3, 4, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 4, 2, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 4, 7, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 5, 2, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 5, 7, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 6, 1, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 6, 2, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 6, 4, '2021-11-01 00:00:00') /* Vissidal West Area Gen */
-     , (0xC8E2, 70106, 7, 1, '2021-11-01 00:00:00') /* Vissidal West Area Gen */;
+     , (0xC8E2, 70106, 4, 7, '2021-11-01 00:00:00') /* Vissidal West Area Gen */;
