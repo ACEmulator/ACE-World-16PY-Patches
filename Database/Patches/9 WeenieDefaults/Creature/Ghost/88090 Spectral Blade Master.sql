e014c65bd29c192b8c6b672a8ece1ba36295c775
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-06-16 19:25:41 -0500

    CreatureFix (#1385)

---
+++ Database/Patches/9 WeenieDefaults/Creature/Ghost/88090 Spectral Blade Master.sql
@@ -0,0 +1,123 @@
+DELETE FROM `weenie` WHERE `class_Id` = 88090;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (88090, 'ace88090-spectralblademaster', 10, '2022-05-17 03:47:03') /* Creature */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (88090,   1,         16) /* ItemType - Creature */
+     , (88090,   2,         77) /* CreatureType - Ghost */
+     , (88090,   6,         -1) /* ItemsCapacity */
+     , (88090,   7,         -1) /* ContainersCapacity */
+     , (88090,  16,          1) /* ItemUseable - No */
+     , (88090,  25,        245) /* Level */
+     , (88090,  48,         44) /* WeaponSkill - HeavyWeapons */
+     , (88090,  68,          5) /* TargetingTactic - Random, LastDamager */
+     , (88090,  93,    4195336) /* PhysicsState - ReportCollisions, Gravity, EdgeSlide */
+     , (88090, 101,        263) /* AiAllowedCombatStyle - Unarmed, OneHanded, OneHandedAndShield, DualWield */
+     , (88090, 133,          2) /* ShowableOnRadar - ShowMovement */
+     , (88090, 146,    1850000) /* XpOverride */
+     , (88090, 307,         10) /* DamageRating */
+     , (88090, 308,         10) /* DamageResistRating */
+     , (88090, 313,          5) /* CritRating */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (88090,   1, True ) /* Stuck */
+     , (88090,   6, True ) /* AiUsesMana */
+     , (88090,  11, False) /* IgnoreCollisions */
+     , (88090,  12, True ) /* ReportCollisions */
+     , (88090,  13, False) /* Ethereal */
+     , (88090,  50, True ) /* NeverFailCasting */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (88090,   1,       5) /* HeartbeatInterval */
+     , (88090,   2,       0) /* HeartbeatTimestamp */
+     , (88090,   3,       2) /* HealthRate */
+     , (88090,   4,       5) /* StaminaRate */
+     , (88090,   5,       1) /* ManaRate */
+     , (88090,  13,       1) /* ArmorModVsSlash */
+     , (88090,  14,       1) /* ArmorModVsPierce */
+     , (88090,  15,       1) /* ArmorModVsBludgeon */
+     , (88090,  16,       1) /* ArmorModVsCold */
+     , (88090,  17,       1) /* ArmorModVsFire */
+     , (88090,  18,       1) /* ArmorModVsAcid */
+     , (88090,  19,       1) /* ArmorModVsElectric */
+     , (88090,  31,      35) /* VisualAwarenessRange */
+     , (88090,  34,       1) /* PowerupTime */
+     , (88090,  36,       1) /* ChargeSpeed */
+     , (88090,  64,    0.45) /* ResistSlash */
+     , (88090,  65,    0.35) /* ResistPierce */
+     , (88090,  66,    0.35) /* ResistBludgeon */
+     , (88090,  67,     0.5) /* ResistFire */
+     , (88090,  68,    0.35) /* ResistCold */
+     , (88090,  69,     0.5) /* ResistAcid */
+     , (88090,  70,    0.35) /* ResistElectric */
+     , (88090,  76,     0.5) /* Translucency */
+     , (88090,  80,       3) /* AiUseMagicDelay */
+     , (88090, 104,      10) /* ObviousRadarRange */
+     , (88090, 122,       2) /* AiAcquireHealth */
+     , (88090, 125,       1) /* ResistHealthDrain */
+     , (88090, 166,       1) /* ResistNether */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (88090,   1, 'Spectral Blade Master') /* Name */
+     , (88090,  45, 'SanctumGuardianKillTask') /* KillQuest */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (88090,   1, 0x02001B87) /* Setup */
+     , (88090,   2, 0x09000001) /* MotionTable */
+     , (88090,   3, 0x2000001E) /* SoundTable */
+     , (88090,   4, 0x30000000) /* CombatTable */
+     , (88090,   6, 0x0400007E) /* PaletteBase */
+     , (88090,   7, 0x10000828) /* ClothingBase */
+     , (88090,   8, 0x060016C4) /* Icon */
+     , (88090,  22, 0x34000025) /* PhysicsEffectTable */
+     , (88090,  35,       1015) /* DeathTreasureType - Loot Tier: 7 */;
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (88090,   1, 350, 0, 0) /* Strength */
+     , (88090,   2, 350, 0, 0) /* Endurance */
+     , (88090,   3, 320, 0, 0) /* Quickness */
+     , (88090,   4, 380, 0, 0) /* Coordination */
+     , (88090,   5, 480, 0, 0) /* Focus */
+     , (88090,   6, 480, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (88090,   1,  2500, 0, 0, 2675) /* MaxHealth */
+     , (88090,   3,  3000, 0, 0, 3350) /* MaxStamina */
+     , (88090,   5,  4800, 0, 0, 5280) /* MaxMana */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (88090,  6, 0, 2, 0, 427, 0, 0) /* MeleeDefense        Trained */
+     , (88090,  7, 0, 2, 0, 580, 0, 0) /* MissileDefense      Trained */
+     , (88090, 15, 0, 2, 0, 363, 0, 0) /* MagicDefense        Trained */
+     , (88090, 31, 0, 2, 0, 220, 0, 0) /* CreatureEnchantment Trained */
+     , (88090, 33, 0, 2, 0, 220, 0, 0) /* LifeMagic           Trained */
+     , (88090, 34, 0, 2, 0, 220, 0, 0) /* WarMagic            Trained */
+     , (88090, 41, 0, 2, 0, 517, 0, 0) /* TwoHandedCombat     Trained */
+     , (88090, 43, 0, 2, 0, 220, 0, 0) /* VoidMagic           Trained */
+     , (88090, 44, 0, 2, 0, 517, 0, 0) /* HeavyWeapons        Trained */
+     , (88090, 45, 0, 2, 0, 517, 0, 0) /* LightWeapons        Trained */
+     , (88090, 46, 0, 2, 0, 507, 0, 0) /* FinesseWeapons      Trained */
+     , (88090, 49, 0, 2, 0, 457, 0, 0) /* DualWield           Trained */
+     , (88090, 51, 0, 2, 0, 517, 0, 0) /* SneakAttack         Trained */
+     , (88090, 52, 0, 2, 0, 517, 0, 0) /* DirtyFighting       Trained */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (88090,  0,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (88090,  1,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (88090,  2,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (88090,  3,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (88090,  4,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (88090,  5,  4, 600, 0.75,  400,  275,  275,  225,  250,  400,  225,  400,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (88090,  6,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (88090,  7,  4,  0,    0,  400,  275,  275,  225,  250,  400,  225,  400,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (88090,  8,  4, 600, 0.75,  400,  275,  275,  225,  250,  400,  225,  400,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+
+INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
+VALUES (88090,  4435,   2.06)  /* Incantation of Blade Blast */
+     , (88090,  4475,  2.064)  /* Incantation of Blade Vulnerability Other */
+     , (88090,  4489,  2.068)  /* Incantation of Fester Other */;
+
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (88090, 2, 46372,  1, 0, 0.5, False) /* Create Spectral Tachi (46372) for Wield */
+     , (88090, 2, 46373,  1, 0, 0.5, False) /* Create Spectral Tachi (46373) for Wield */;
---
+++ Database/Patches/9 WeenieDefaults/Creature/Undead/87614 Mu-miyah Champion.sql
@@ -0,0 +1,124 @@
+DELETE FROM `weenie` WHERE `class_Id` = 87614;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (87614, 'ace87614-mumiyahchampion', 10, '2022-05-17 03:47:03') /* Creature */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (87614,   1,         16) /* ItemType - Creature */
+     , (87614,   2,         14) /* CreatureType - Undead */
+     , (87614,   3,         44) /* PaletteTemplate - Tanred */
+     , (87614,   6,         -1) /* ItemsCapacity */
+     , (87614,   7,         -1) /* ContainersCapacity */
+     , (87614,  16,          1) /* ItemUseable - No */
+     , (87614,  25,        220) /* Level */
+     , (87614,  27,          0) /* ArmorType - None */
+     , (87614,  68,         13) /* TargetingTactic - Random, LastDamager, TopDamager */
+     , (87614,  93,       1032) /* PhysicsState - ReportCollisions, Gravity */
+     , (87614, 133,          2) /* ShowableOnRadar - ShowMovement */
+     , (87614, 146,    1400000) /* XpOverride */
+     , (87614, 307,          7) /* DamageRating */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (87614,   1, True ) /* Stuck */
+     , (87614,   6, True ) /* AiUsesMana */
+     , (87614,  11, False) /* IgnoreCollisions */
+     , (87614,  12, True ) /* ReportCollisions */
+     , (87614,  13, False) /* Ethereal */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (87614,   1,       5) /* HeartbeatInterval */
+     , (87614,   2,       0) /* HeartbeatTimestamp */
+     , (87614,   3,       4) /* HealthRate */
+     , (87614,   4,      10) /* StaminaRate */
+     , (87614,   5,       3) /* ManaRate */
+     , (87614,  12,       0) /* Shade */
+     , (87614,  13,    0.67) /* ArmorModVsSlash */
+     , (87614,  14,     0.9) /* ArmorModVsPierce */
+     , (87614,  15,    0.75) /* ArmorModVsBludgeon */
+     , (87614,  16,       1) /* ArmorModVsCold */
+     , (87614,  17,    0.67) /* ArmorModVsFire */
+     , (87614,  18,    0.67) /* ArmorModVsAcid */
+     , (87614,  19,       1) /* ArmorModVsElectric */
+     , (87614,  31,      22) /* VisualAwarenessRange */
+     , (87614,  34,       1) /* PowerupTime */
+     , (87614,  36,       1) /* ChargeSpeed */
+     , (87614,  39,     1.2) /* DefaultScale */
+     , (87614,  64,     0.8) /* ResistSlash */
+     , (87614,  65,    0.25) /* ResistPierce */
+     , (87614,  66,     0.7) /* ResistBludgeon */
+     , (87614,  67,     0.8) /* ResistFire */
+     , (87614,  68,     0.3) /* ResistCold */
+     , (87614,  69,     0.8) /* ResistAcid */
+     , (87614,  70,     0.4) /* ResistElectric */
+     , (87614,  71,       1) /* ResistHealthBoost */
+     , (87614,  72,       1) /* ResistStaminaDrain */
+     , (87614,  73,       1) /* ResistStaminaBoost */
+     , (87614,  74,       1) /* ResistManaDrain */
+     , (87614,  75,       1) /* ResistManaBoost */
+     , (87614,  80,       3) /* AiUseMagicDelay */
+     , (87614, 104,      10) /* ObviousRadarRange */
+     , (87614, 117,     0.5) /* FocusedProbability */
+     , (87614, 122,       2) /* AiAcquireHealth */
+     , (87614, 125,       1) /* ResistHealthDrain */
+     , (87614, 166,     1.1) /* ResistNether */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (87614,   1, 'Mu-miyah Champion') /* Name */
+     , (87614,  45, 'SanctumGuardianKillTask') /* KillQuest */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (87614,   1, 0x02000001) /* Setup */
+     , (87614,   2, 0x09000025) /* MotionTable */
+     , (87614,   3, 0x2000001E) /* SoundTable */
+     , (87614,   4, 0x30000008) /* CombatTable */
+     , (87614,   6, 0x0400007E) /* PaletteBase */
+     , (87614,   7, 0x100000BD) /* ClothingBase */
+     , (87614,   8, 0x060016C2) /* Icon */
+     , (87614,  22, 0x34000028) /* PhysicsEffectTable */
+     , (87614,  35,       1000) /* DeathTreasureType - Loot Tier: 7 */;
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (87614,   1, 190, 0, 0) /* Strength */
+     , (87614,   2, 200, 0, 0) /* Endurance */
+     , (87614,   3, 350, 0, 0) /* Quickness */
+     , (87614,   4, 210, 0, 0) /* Coordination */
+     , (87614,   5, 300, 0, 0) /* Focus */
+     , (87614,   6, 310, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (87614,   1,  4100, 0, 0, 4300) /* MaxHealth */
+     , (87614,   3,  1660, 0, 0, 1860) /* MaxStamina */
+     , (87614,   5,  4000, 0, 0, 4300) /* MaxMana */;
+
+INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
+VALUES (87614,  6, 0, 2, 0, 470, 0, 0) /* MeleeDefense        Trained */
+     , (87614,  7, 0, 2, 0, 390, 0, 0) /* MissileDefense      Trained */
+     , (87614, 15, 0, 2, 0, 320, 0, 0) /* MagicDefense        Trained */
+     , (87614, 24, 0, 2, 0, 255, 0, 0) /* Run                 Trained */
+     , (87614, 33, 0, 2, 0, 350, 0, 0) /* LifeMagic           Trained */
+     , (87614, 34, 0, 2, 0, 350, 0, 0) /* WarMagic            Trained */
+     , (87614, 45, 0, 3, 0, 500, 0, 0) /* LightWeapons        Specialized */
+     , (87614, 46, 0, 3, 0, 500, 0, 0) /* FinesseWeapons      Specialized */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (87614,  0,  4,  0,    0,  460,  250,  150,  275,  250,  100,  250,  150,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (87614,  1,  4,  0,    0,  350,  250,  150,  275,  250,  100,  250,  150,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (87614,  2,  4,  0,    0,  350,  250,  150,  275,  250,  100,  250,  150,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (87614,  3,  4,  0,    0,  350,  250,  150,  275,  250,  100,  250,  150,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (87614,  4,  4,  0,    0,  350,  250,  150,  275,  250,  100,  250,  150,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (87614,  5,  4, 300, 0.75,  350,  250,  150,  275,  250,  100,  250,  150,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (87614,  6,  4,  0,    0,  350,  250,  150,  275,  250,  100,  250,  150,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (87614,  7,  4,  0,    0,  350,  250,  150,  275,  250,  100,  250,  150,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (87614,  8,  4, 300, 0.75,  350,  250,  150,  275,  250,  100,  250,  150,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
+
+INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
+VALUES (87614,  2074,   2.06)  /* Gossamer Flesh */
+     , (87614,  2170,   2.06)  /* Inferno's Gift */
+     , (87614,  4423,   2.06)  /* Incantation of Flame Arc */;
+
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (87614, 2, 44265,  1, 0, 0.1, False) /* Create Burning Sands Katar (44265) for Wield */
+     , (87614, 9, 44240,  1, 0, 0.1, False) /* Create A'nekshay Token (44240) for ContainTreasure */
+     , (87614, 9,     0,  0, 0, 0.9, False) /* Create nothing for ContainTreasure */
+     , (87614, 9, 48908,  1, 0, 0.03, False) /* Create Shattered Legendary Key (48908) for ContainTreasure */
+     , (87614, 9,     0,  0, 0, 0.97, False) /* Create nothing for ContainTreasure */;
