7e103e2be3e90e4e564d3c6d8183fde75cdb4867
harliq <30803300+harliq@users.noreply.github.com>
2022-07-05 23:51:27 -0400

    Update 90110 Daimyo Idah Gen.sql
    Fixed Radius and Regen

--- Database/Patches/9 WeenieDefaults/Generic/None/90110 Daimyo Idah Gen.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/90110 Daimyo Idah Gen.sql
@@ -18,7 +18,7 @@ VALUES (90110,   1, True ) /* Stuck */
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (90110,  41,    3600) /* RegenerationInterval */
-     , (90110,  43,      40) /* GeneratorRadius */;
+     , (90110,  43,      10) /* GeneratorRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (90110,   1, 'Daimyo Idah Gen') /* Name */;
@@ -28,5 +28,5 @@ VALUES (90110,   1, 0x0200026B) /* Setup */
      , (90110,   8, 0x06001066) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (90110, 0.21, 90108, 180, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Daimyo Idah (90108) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (90110, 1, 46525, 180, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Spectral Minion (46525) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (90110, 0.21, 90108, 10, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Daimyo Idah (90108) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (90110, 1, 46525, 10, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Spectral Minion (46525) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
