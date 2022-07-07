0732f456d9cb7ffe9de35558e1485948afb21c5f
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-07-07 08:33:44 -0500

    Nerf

--- Database/Patches/9 WeenieDefaults/Creature/Undead/39350 Minik Ra T'thuunixis.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Undead/39350 Minik Ra T'thuunixis.sql
@@ -90,15 +90,15 @@ VALUES (39350,   1, 149800, 0, 0, 150000) /* MaxHealth */
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
 VALUES (39350,  6, 0, 3, 0, 505, 0, 0) /* MeleeDefense        Specialized */
-     , (39350,  7, 0, 3, 0, 525, 0, 0) /* MissileDefense      Specialized */
+     , (39350,  7, 0, 3, 0, 499, 0, 0) /* MissileDefense      Specialized */
      , (39350, 14, 0, 3, 0, 253, 0, 0) /* ArcaneLore          Specialized */
-     , (39350, 15, 0, 3, 0, 501, 0, 0) /* MagicDefense        Specialized */
-     , (39350, 20, 0, 3, 0, 250, 0, 0) /* Deception           Specialized */
-     , (39350, 31, 0, 3, 0, 450, 0, 0) /* CreatureEnchantment Specialized */
-     , (39350, 33, 0, 3, 0, 450, 0, 0) /* LifeMagic           Specialized */
-     , (39350, 34, 0, 3, 0, 450, 0, 0) /* WarMagic            Specialized */
-     , (39350, 44, 0, 3, 0, 465, 0, 0) /* HeavyWeapons        Specialized */
-     , (39350, 45, 0, 3, 0, 465, 0, 0) /* LightWeapons        Specialized */
+     , (39350, 15, 0, 3, 0, 437, 0, 0) /* MagicDefense        Specialized */
+     , (39350, 20, 0, 3, 0,  90, 0, 0) /* Deception           Specialized */
+     , (39350, 31, 0, 3, 0, 430, 0, 0) /* CreatureEnchantment Specialized */
+     , (39350, 33, 0, 3, 0, 430, 0, 0) /* LifeMagic           Specialized */
+     , (39350, 34, 0, 3, 0, 430, 0, 0) /* WarMagic            Specialized */
+     , (39350, 44, 0, 3, 0, 455, 0, 0) /* HeavyWeapons        Specialized */
+     , (39350, 45, 0, 3, 0, 455, 0, 0) /* LightWeapons        Specialized */
      , (39350, 46, 0, 3, 0, 440, 0, 0) /* FinesseWeapons      Specialized */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
