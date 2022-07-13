2d69d8c37f2964ec64366712c759780e08e15565
Ty Conner <tyconner@itcproductions.com>
2022-07-13 12:25:47 -0400

    Update 43472 Luminous Pearl of Blood Drinking.sql

--- Database/Patches/9 WeenieDefaults/Gem/Gem/43472 Luminous Pearl of Blood Drinking.sql
+++ Database/Patches/9 WeenieDefaults/Gem/Gem/43472 Luminous Pearl of Blood Drinking.sql
@@ -12,9 +12,15 @@ VALUES (43472,   1,       2048) /* ItemType - Gem */
      , (43472,  15,          0) /* StackUnitValue */
      , (43472,  16,          8) /* ItemUseable - Contained */
      , (43472,  18,          1) /* UiEffects - Magical */
-     , (43472,  65,        101) /* Placement - Resting */
+     , (43472,  19,          0) /* Value */
+     , (43472,  33,          1) /* Bonded - Bonded */
      , (43472,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (43472,  94,         16) /* TargetType - Creature */
+     , (43472, 106,        325) /* ItemSpellcraft */
+     , (43472, 107,      10000) /* ItemCurMana */
+     , (43472, 108,      10000) /* ItemMaxMana */
+     , (43472, 109,          0) /* ItemDifficulty */
+     , (43472, 114,          1) /* Attuned - Attuned */
      , (43472, 151,         11) /* HookType - Floor, Wall, Yard */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
@@ -22,10 +28,12 @@ VALUES (43472,   1, False) /* Stuck */
      , (43472,  11, True ) /* IgnoreCollisions */
      , (43472,  13, True ) /* Ethereal */
      , (43472,  14, True ) /* GravityStatus */
-     , (43472,  19, True ) /* Attackable */;
+     , (43472,  19, True ) /* Attackable */
+     , (43472,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (43472,   1, 'Luminous Pearl of Blood Drinking') /* Name */
+     , (43472,  16, 'Using this gem will cast Incantation of Blood Drinker upon your equipped melee or missile weapon.') /* LongDesc */
      , (43472,  20, 'Luminous Pearls of Blood Drinking') /* PluralName */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
