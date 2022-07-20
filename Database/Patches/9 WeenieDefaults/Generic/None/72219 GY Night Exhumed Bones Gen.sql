47103975f6fcd40fd1bfeadedc8b65923185293a
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2022-07-20 10:41:07 -0400

    GY Spawn Fix
    Added line item delays to prevent instant respawn. These were previously set to 0.
    Adjusted regen interval on some generators to match the rest.

--- Database/Patches/9 WeenieDefaults/Generic/None/72219 GY Night Exhumed Bones Gen.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/72219 GY Night Exhumed Bones Gen.sql
@@ -29,4 +29,4 @@ VALUES (72219,   1, 0x0200026B) /* Setup */
      , (72219,   8, 0x06001066) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (72219, -1, 35251, 0, 4, 4, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Exhumed Bones Piles (35251) (x4 up to max of 4) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (72219, -1, 35251, 60, 4, 4, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Exhumed Bones Piles (35251) (x4 up to max of 4) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
--- Database/Patches/9 WeenieDefaults/Generic/None/72224 GY Night Pyre Champion Camp Gen.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/72224 GY Night Pyre Champion Camp Gen.sql
@@ -29,6 +29,6 @@ VALUES (72224,   1, 0x0200026B) /* Setup */
      , (72224,   8, 0x06001066) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (72224, -1, 41963, 0, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Pyre Skeleton (41963) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (72224, -1, 52308, 0, 3, 3, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Pyre Minion (52308) (x3 up to max of 3) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
-     , (72224, -1, 46816, 0, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Pyre Champion (46816) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (72224, -1, 41963, 120, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Pyre Skeleton (41963) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (72224, -1, 52308, 120, 3, 3, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Pyre Minion (52308) (x3 up to max of 3) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (72224, -1, 46816, 120, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Pyre Champion (46816) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
