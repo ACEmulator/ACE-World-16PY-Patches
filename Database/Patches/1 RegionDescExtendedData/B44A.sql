475b44c9394ae94c34bf95e382a118cc7c9a4b04
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2022-07-20 20:36:34 -0400

    Viridian Rise Spawn Fix (#1411)
    Cleared more encounters that had been forgotten, resulting in double spawns.
    Adjusted generator respawn rate.

--- Database/Patches/1 RegionDescExtendedData/B44A.sql
+++ Database/Patches/1 RegionDescExtendedData/B44A.sql
@@ -1,9 +1,2 @@
 DELETE FROM `encounter` WHERE `landblock` = 0xB44A;
 
-INSERT INTO `encounter` (`landblock`, `weenie_Class_Id`, `cell_X`, `cell_Y`, `last_Modified`)
-VALUES (0xB44A, 70851, 2, 0, '2021-11-01 00:00:00') /* Viridian Entrance Camp Gen */
-     , (0xB44A, 70851, 2, 2, '2021-11-01 00:00:00') /* Viridian Entrance Camp Gen */
-     , (0xB44A, 70851, 3, 5, '2021-11-01 00:00:00') /* Viridian Entrance Camp Gen */
-     , (0xB44A, 70851, 3, 7, '2021-11-01 00:00:00') /* Viridian Entrance Camp Gen */
-     , (0xB44A, 70851, 0, 6, '2021-11-01 00:00:00') /* Viridian Entrance Camp Gen */
-     , (0xB44A, 70851, 0, 4, '2021-11-01 00:00:00') /* Viridian Entrance Camp Gen */;
