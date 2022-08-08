f078bb4a1c6601f75b5e1e0ca7b37881599c648d
Ty Conner <tyconner@itcproductions.com>
2022-08-08 13:45:29 -0400

    Create SpokeWithSharia.sql

---
+++ Database/Patches/8 QuestDefDB/SpokeWithSharia.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'SpokeWithSharia';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('SpokeWithSharia', 0, 1, 'Spoke to Sharia Blackmist and started Ghost Hunter quest', '2021-11-01 00:00:00');
