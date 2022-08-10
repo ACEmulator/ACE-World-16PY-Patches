5b514e4104ad1238d1f9c5044958a5de3a6d07c6
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-08-09 21:47:06 -0500

    UpdatedDelay (#1418)

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/38026 Summoning Stone.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/38026 Summoning Stone.sql
@@ -63,4 +63,4 @@ INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `dela
 VALUES (@parent_id,  0,  18 /* DirectBroadcast */, 0, 1, NULL, 'Nothing happens.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (38026, -1, 38017, 300, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Jester (38017) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (38026, -1, 38017, 10, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Jester (38017) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
