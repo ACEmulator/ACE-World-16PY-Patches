e817ffa5ef3d4d21c791aa87787998ed714c34b6
TheDoctor-AC <ross@yotesfan.com>
2022-08-18 22:02:15 -0700

    Updated yield values for fletching with Wrapped Bundles of Shafts

--- Database/Patches/4 CraftTable/02678 Deadly Broadhead Quarrel.sql
+++ Database/Patches/4 CraftTable/02678 Deadly Broadhead Quarrel.sql
@@ -1,9 +1,9 @@
-DELETE FROM `recipe` WHERE `id` = 6021;
+DELETE FROM `recipe` WHERE `id` = 2678;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6021, 0, 37 /* Fletching */, 390, 0, 43953 /* Deadly Prismatic Atlatl Dart */, 1000, 'You make a bundle of deadly prismatic atlatl darts.', 0, 0, 'You fail to make a bundle of deadly prismatic atlatl darts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2022-08-18 21:52:04');
+VALUES (2678, 0, 37 /* Fletching */, 180, 0, 15442 /* Deadly Broadhead Quarrel */, 1000, 'You make a big bundle of deadly broadhead quarrels.', 0, 0, 'You fail to make any deadly broadhead quarrels.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2022-08-18 21:48:50');
 
-DELETE FROM `cook_book` WHERE `recipe_Id` = 6021;
+DELETE FROM `cook_book` WHERE `recipe_Id` = 2678;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (6021, 44072 /* Wrapped Bundle of Deadly Prismatic Arrowheads */, 15298 /* Wrapped Bundle of Atlatl Dartshafts */, '2021-11-01 00:00:00');
+VALUES (2678, 15424 /* Wrapped Bundle of Deadly Broad Arrowheads */,  9378 /* Wrapped Bundle of Quarrelshafts */, '2005-02-09 10:00:00');
--- Database/Patches/4 CraftTable/04470 Barbed Arrow.sql
+++ Database/Patches/4 CraftTable/04470 Barbed Arrow.sql
@@ -1,9 +1,9 @@
-DELETE FROM `recipe` WHERE `id` = 6021;
+DELETE FROM `recipe` WHERE `id` = 4470;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6021, 0, 37 /* Fletching */, 390, 0, 43953 /* Deadly Prismatic Atlatl Dart */, 1000, 'You make a bundle of deadly prismatic atlatl darts.', 0, 0, 'You fail to make a bundle of deadly prismatic atlatl darts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2022-08-18 21:52:04');
+VALUES (4470, 0, 37 /* Fletching */, 50, 0, 8826 /* Barbed Arrow */, 1000, 'You make a big bundle of barbed arrows.', 0, 0, 'You fail to make any barbed arrows.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2022-08-18 21:46:01');
 
-DELETE FROM `cook_book` WHERE `recipe_Id` = 6021;
+DELETE FROM `cook_book` WHERE `recipe_Id` = 4470;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (6021, 44072 /* Wrapped Bundle of Deadly Prismatic Arrowheads */, 15298 /* Wrapped Bundle of Atlatl Dartshafts */, '2021-11-01 00:00:00');
+VALUES (4470, 24544 /* Wrapped Bundle of Barbed Arrowheads */,  9377 /* Wrapped Bundle of Arrowshafts */, '2005-02-09 10:00:00');
