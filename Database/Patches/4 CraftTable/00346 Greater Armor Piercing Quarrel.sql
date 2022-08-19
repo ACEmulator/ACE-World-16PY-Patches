d619c6ca0840fff6c1de44e6bacc9387e6296931
TheDoctor-AC <ross@yotesfan.com>
2022-08-18 21:42:56 -0700

    Updated yield values for fletching with Bundles of Shafts

---
+++ Database/Patches/4 CraftTable/00346 Greater Armor Piercing Quarrel.sql
@@ -0,0 +1,9 @@
+DELETE FROM `recipe` WHERE `id` = 346;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (346, 0, 37 /* Fletching */, 160, 0, 5318 /* Greater Armor Piercing Quarrel */, 100, 'You make a bundle of greater armor-piercing quarrels.', 0, 0, 'You fail to make any greater armor-piercing quarrels.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2022-08-18 21:28:55');
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 346;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (346, 5356 /* Bundle of Greater Armor Piercing Arrowheads */,  5339 /* Bundle of Quarrelshafts */, '2005-02-09 10:00:00');
--- Database/Patches/4 CraftTable/06018 Greater Prismatic Atlatl Dart.sql
+++ Database/Patches/4 CraftTable/06018 Greater Prismatic Atlatl Dart.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 6018;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (6018, 0, 37 /* Fletching */, 370, 0, 43954 /* Greater Prismatic Atlatl Dart */, 10, 'You make a bundle of greater prismatic atlatl darts.', 0, 0, 'You fail to make a bundle of greater prismatic atlatl darts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-11-01 00:00:00');
+VALUES (6018, 0, 37 /* Fletching */, 370, 0, 43954 /* Greater Prismatic Atlatl Dart */, 100, 'You make a bundle of greater prismatic atlatl darts.', 0, 0, 'You fail to make a bundle of greater prismatic atlatl darts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2022-08-18 21:32:58');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 6018;
 
