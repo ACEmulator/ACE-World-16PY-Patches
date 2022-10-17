4b738a95579b94281623d362373722e6e321b274
OptimShi <OptimShi@users.noreply.github.com>
2022-10-17 10:51:59 -0700

    Update rare life protects to be 99.9% from 99% (#1444)

--- Database/Patches/2 SpellTableExtendedData/03687 Prodigal Blade Protection.sql
+++ Database/Patches/2 SpellTableExtendedData/03687 Prodigal Blade Protection.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 3687;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (3687, 'Prodigal Blade Protection', 20488 /* Float, SingleStat, Multiplicative */, 64 /* ResistSlash */, 0.01, '2021-11-01 00:00:00');
+VALUES (3687, 'Prodigal Blade Protection', 20488 /* Float, SingleStat, Multiplicative */, 64 /* ResistSlash */, 0.001, '2021-11-01 00:00:00');
