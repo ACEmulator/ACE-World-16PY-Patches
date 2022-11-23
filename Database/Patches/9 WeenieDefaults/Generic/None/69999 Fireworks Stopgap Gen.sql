2c7373cead5971e28d5cab39e34bb925562a7793
Ty Conner <tyconner@itcproductions.com>
2022-11-23 13:19:05 -0500

    .

--- Database/Patches/9 WeenieDefaults/Generic/None/69999 Fireworks Stopgap Gen.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/69999 Fireworks Stopgap Gen.sql
@@ -8,8 +8,8 @@ VALUES (69999,  81,          1) /* MaxGeneratedObjects */
      , (69999,  82,          1) /* InitGeneratedObjects */
      , (69999,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (69999, 142,          1) /* GeneratorTimeType - RealTime */
-     , (69999, 143, 1667365260) /* GeneratorStartTime - 11/02/2022 05:01:00 */
-     , (69999, 144, 1667710740) /* GeneratorEndTime - 11/06/2022 04:59:00 */
+     , (69999, 143, 1698901260) /* GeneratorStartTime - 11/02/2023 05:01:00 */
+     , (69999, 144, 1699246740) /* GeneratorEndTime - 11/06/2023 04:59:00 */
      , (69999, 145,          3) /* GeneratorEndDestructionType - Kill */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
