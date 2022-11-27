a960b80a445a94e25c209d4238f68e3cecb26a95
Ty Conner <tyconner@itcproductions.com>
2022-11-27 12:11:07 -0500

    Update 43605 Soldier Abdomen Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Armor/43605 Soldier Abdomen Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/43605 Soldier Abdomen Metamorphi.sql
@@ -36,9 +36,10 @@ VALUES (43605,   1, 'Soldier Abdomen Metamorphi') /* Name */
      , (43605,  16, 'When equipped, this Metamorphi will increase the Soldier''s Damage Rating.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43605,   1, 0x020000DD) /* Setup */
+VALUES (43605,   1, 0x020009D1) /* Setup */
      , (43605,   3, 0x20000014) /* SoundTable */
      , (43605,   6, 0x0400007E) /* PaletteBase */
+     , (43605,   7, 0x100007C3) /* ClothingBase */
      , (43605,   8, 0x06006E9C) /* Icon */
      , (43605,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43605,  52, 0x06006E9A) /* IconUnderlay */
