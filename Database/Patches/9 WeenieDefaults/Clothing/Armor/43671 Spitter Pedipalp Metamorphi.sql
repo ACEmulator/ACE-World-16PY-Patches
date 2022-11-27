a66495aba2120dc82e6a7e62916330cdf1f4b1e8
Ty Conner <tyconner@itcproductions.com>
2022-11-27 13:33:28 -0500

    Create 43671 Spitter Pedipalp Metamorphi.sql

---
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/43671 Spitter Pedipalp Metamorphi.sql
@@ -0,0 +1,28 @@
+DELETE FROM `weenie` WHERE `class_Id` = 43671;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (43671, 'ace43671-spitterpedipalpmetamorphi', 2, '2019-02-10 00:00:00') /* Clothing */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43671,   1,          2) /* ItemType - Armor */
+     , (43671,   4,       8192) /* ClothingPriority - OuterwearLowerArms */
+     , (43671,   5,        150) /* EncumbranceVal */
+     , (43671,   9,       4096) /* ValidLocations - LowerArmArmor */
+     , (43671,  16,          1) /* ItemUseable - No */
+     , (43671,  19,        250) /* Value */
+     , (43671,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (43671,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43671,   1, 'Spitter Pedipalp Metamorphi') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43671,   1, 0x020000DD) /* Setup */
+     , (43671,   3, 0x20000014) /* SoundTable */
+     , (43671,   6, 0x0400007E) /* PaletteBase */
+     , (43671,   8, 0x06006EA3) /* Icon */
+     , (43671,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (43671,  52, 0x06006E9B) /* IconUnderlay */
+     , (43671, 8044,       2597) /* PCAPPhysicsDIDDataTemplatedFrom - Flared Pants */;
