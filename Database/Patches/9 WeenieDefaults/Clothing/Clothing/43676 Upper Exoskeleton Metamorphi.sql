a0d0ac93677c9fcc4ac243ec7a558473b48964dc
Ty Conner <tyconner@itcproductions.com>
2022-11-27 02:32:30 -0500

    Update 43676 Upper Exoskeleton Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Clothing/43676 Upper Exoskeleton Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Clothing/43676 Upper Exoskeleton Metamorphi.sql
@@ -14,7 +14,7 @@ VALUES (43676,   1,          4) /* ItemType - Clothing */
      , (43676,  33,          1) /* Bonded - Bonded */
      , (43676,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
      , (43676, 114,          1) /* Attuned - Attuned */
-     , (43676, 265,         42) /* EquipmentSetId - OlthoiArmorDRed */
+     , (43676, 265,         42) /* EquipmentSetId - OlthoiArmorCRed */
      , (43676, 324,         13) /* HeritageSpecificArmor - OlthoiAcid */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
@@ -36,9 +36,10 @@ VALUES (43676,   1, 'Upper Exoskeleton Metamorphi') /* Name */
      , (43676,  16, 'When equipped, this Metamorphi will decrease incoming damage.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43676,   1, 0x020000DD) /* Setup */
+VALUES (43676,   1, 0x020009D1) /* Setup */
      , (43676,   3, 0x20000014) /* SoundTable */
      , (43676,   6, 0x0400007E) /* PaletteBase */
+     , (43676,   7, 0x100007C3) /* ClothingBase */
      , (43676,   8, 0x06006EA7) /* Icon */
      , (43676,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43676,  52, 0x06006E9B) /* IconUnderlay */
