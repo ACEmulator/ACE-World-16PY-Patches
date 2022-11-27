eda75d34655a35e339cc01de755d7c8b6e6023cb
Ty Conner <tyconner@itcproductions.com>
2022-11-27 02:38:14 -0500

    Update 43678 Lower Exoskeleton Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Clothing/43678 Lower Exoskeleton Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Clothing/43678 Lower Exoskeleton Metamorphi.sql
@@ -10,18 +10,36 @@ VALUES (43678,   1,          4) /* ItemType - Clothing */
      , (43678,   9,        196) /* ValidLocations - AbdomenWear, UpperLegWear, LowerLegWear */
      , (43678,  16,          1) /* ItemUseable - No */
      , (43678,  19,       2000) /* Value */
-     , (43678,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+     , (43678,  28,          0) /* ArmorLevel */
+     , (43678,  33,          1) /* Bonded - Bonded */
+     , (43678,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (43678, 114,          1) /* Attuned - Attuned */
+     , (43678, 265,         44) /* EquipmentSetId - OlthoiArmorDRed */
+     , (43678, 324,         13) /* HeritageSpecificArmor - Olthoi */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (43678,  22, True ) /* Inscribable */;
+VALUES (43678,  22, True ) /* Inscribable */
+     , (43678,  23, True ) /* DestroyOnSell */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43678,  13,       1) /* ArmorModVsSlash */
+     , (43678,  14,       1) /* ArmorModVsPierce */
+     , (43678,  15,       1) /* ArmorModVsBludgeon */
+     , (43678,  16,       1) /* ArmorModVsCold */
+     , (43678,  17,       1) /* ArmorModVsFire */
+     , (43678,  18,       1) /* ArmorModVsAcid */
+     , (43678,  19,       1) /* ArmorModVsElectric */
+     , (43678, 165,       1) /* ArmorModVsNether */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (43678,   1, 'Lower Exoskeleton Metamorphi') /* Name */;
+VALUES (43678,   1, 'Lower Exoskeleton Metamorphi') /* Name */
+     , (43678,  16, 'When equipped, this Metamorphi will decrease the incoming Critical Strike damage.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43678,   1, 0x020000DD) /* Setup */
+VALUES (43678,   1, 0x020009D1) /* Setup */
      , (43678,   3, 0x20000014) /* SoundTable */
      , (43678,   6, 0x0400007E) /* PaletteBase */
+     , (43678,   7, 0x100007C3) /* ClothingBase */
      , (43678,   8, 0x06006EA6) /* Icon */
      , (43678,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43678,  52, 0x06006E99) /* IconUnderlay */
