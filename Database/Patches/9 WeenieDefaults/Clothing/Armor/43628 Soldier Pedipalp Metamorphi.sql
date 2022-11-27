88daf0be289605452f2ef91db89e288e3e25b2f4
Ty Conner <tyconner@itcproductions.com>
2022-11-27 12:41:16 -0500

    Update 43628 Soldier Pedipalp Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Armor/43628 Soldier Pedipalp Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/43628 Soldier Pedipalp Metamorphi.sql
@@ -36,9 +36,10 @@ VALUES (43628,   1, 'Soldier Pedipalp Metamorphi') /* Name */
      , (43628,  16, 'When equipped, this Metamorphi will decrease the incoming Critical Strike damage.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43628,   1, 0x020000DD) /* Setup */
+VALUES (43628,   1, 0x020009D1) /* Setup */
      , (43628,   3, 0x20000014) /* SoundTable */
      , (43628,   6, 0x0400007E) /* PaletteBase */
+     , (43628,   7, 0x100007C3) /* ClothingBase */
      , (43628,   8, 0x06006EA3) /* Icon */
      , (43628,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43628,  52, 0x06006E99) /* IconUnderlay */
