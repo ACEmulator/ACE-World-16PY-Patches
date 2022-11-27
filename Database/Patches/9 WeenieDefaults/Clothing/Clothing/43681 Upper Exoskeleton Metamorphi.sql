9717f63b70ab8f22526ca9d672ab7e9e9f69a600
Ty Conner <tyconner@itcproductions.com>
2022-11-27 02:11:16 -0500

    Update 43681 Upper Exoskeleton Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Clothing/43681 Upper Exoskeleton Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Clothing/43681 Upper Exoskeleton Metamorphi.sql
@@ -10,18 +10,35 @@ VALUES (43681,   1,          4) /* ItemType - Clothing */
      , (43681,   9,         30) /* ValidLocations - ChestWear, AbdomenWear, UpperArmWear, LowerArmWear */
      , (43681,  16,          1) /* ItemUseable - No */
      , (43681,  19,       2000) /* Value */
-     , (43681,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+     , (43681,  28,          0) /* ArmorLevel */
+     , (43681,  33,          1) /* Bonded - Bonded */
+     , (43681,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (43681, 114,          1) /* Attuned - Attuned */
+     , (43681, 265,         43) /* EquipmentSetId - OlthoiArmorDRat */
+     , (43681, 324,         12) /* HeritageSpecificArmor - Olthoi */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (43681,  22, True ) /* Inscribable */;
 
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43681,  13,       1) /* ArmorModVsSlash */
+     , (43681,  14,       1) /* ArmorModVsPierce */
+     , (43681,  15,       1) /* ArmorModVsBludgeon */
+     , (43681,  16,       1) /* ArmorModVsCold */
+     , (43681,  17,       1) /* ArmorModVsFire */
+     , (43681,  18,       1) /* ArmorModVsAcid */
+     , (43681,  19,       1) /* ArmorModVsElectric */
+     , (43681, 165,       1) /* ArmorModVsNether */;
+
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (43681,   1, 'Upper Exoskeleton Metamorphi') /* Name */;
+VALUES (43681,   1, 'Upper Exoskeleton Metamorphi') /* Name */
+     , (43681,  16, 'When equipped, this Metamorphi will increase the Ripper''s Critical Damage Rating') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43681,   1, 0x020000DD) /* Setup */
+VALUES (43681,   1, 0x020009D1) /* Setup */
      , (43681,   3, 0x20000014) /* SoundTable */
      , (43681,   6, 0x0400007E) /* PaletteBase */
+     , (43681,   7, 0x100007C3) /* ClothingBase */
      , (43681,   8, 0x06006EA7) /* Icon */
      , (43681,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43681,  52, 0x06006E9D) /* IconUnderlay */
