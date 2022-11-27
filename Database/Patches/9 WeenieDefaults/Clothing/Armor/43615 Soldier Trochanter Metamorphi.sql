1040620d7d564477c0a9c55fd1dcdce7a95068b3
Ty Conner <tyconner@itcproductions.com>
2022-11-27 12:25:22 -0500

    Create 43615 Soldier Trochanter Metamorphi.sql

---
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/43615 Soldier Trochanter Metamorphi.sql
@@ -0,0 +1,28 @@
+DELETE FROM `weenie` WHERE `class_Id` = 43615;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (43615, 'ace43615-soldiertrochantermetamorphi', 2, '2019-02-10 00:00:00') /* Clothing */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (43615,   1,          2) /* ItemType - Armor */
+     , (43615,   4,       4096) /* ClothingPriority - OuterwearUpperArms */
+     , (43615,   5,        150) /* EncumbranceVal */
+     , (43615,   9,       2048) /* ValidLocations - UpperArmArmor */
+     , (43615,  16,          1) /* ItemUseable - No */
+     , (43615,  19,        500) /* Value */
+     , (43615,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (43615,  22, True ) /* Inscribable */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (43615,   1, 'Soldier Trochanter Metamorphi') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (43615,   1, 0x020000DD) /* Setup */
+     , (43615,   3, 0x20000014) /* SoundTable */
+     , (43615,   6, 0x0400007E) /* PaletteBase */
+     , (43615,   8, 0x06006EA0) /* Icon */
+     , (43615,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (43615,  52, 0x06006E9D) /* IconUnderlay */
+     , (43615, 8044,       2597) /* PCAPPhysicsDIDDataTemplatedFrom - Flared Pants */;
