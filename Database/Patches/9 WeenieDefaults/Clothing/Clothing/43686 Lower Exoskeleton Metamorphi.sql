6cd0b25341491892f7d3208b785fdd309d5facf5
Ty Conner <tyconner@itcproductions.com>
2022-11-27 02:11:28 -0500

    Update 43686 Lower Exoskeleton Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Clothing/43686 Lower Exoskeleton Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Clothing/43686 Lower Exoskeleton Metamorphi.sql
@@ -36,9 +36,10 @@ VALUES (43686,   1, 'Lower Exoskeleton Metamorphi') /* Name */
      , (43686,  16, 'When equipped, this Metamorphi will decrease the incoming Critical Strike damage.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43686,   1, 0x020000DD) /* Setup */
+VALUES (43686,   1, 0x020009D1) /* Setup */
      , (43686,   3, 0x20000014) /* SoundTable */
      , (43686,   6, 0x0400007E) /* PaletteBase */
+     , (43686,   7, 0x100007C3) /* ClothingBase */
      , (43686,   8, 0x06006EA6) /* Icon */
      , (43686,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43686,  52, 0x06006E99) /* IconUnderlay */
