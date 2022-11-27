e6226f2b0ca4a2cc02d9a478b2992e8bd54f6548
Ty Conner <tyconner@itcproductions.com>
2022-11-27 12:10:58 -0500

    Update 43603 Soldier Abdomen Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Armor/43603 Soldier Abdomen Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/43603 Soldier Abdomen Metamorphi.sql
@@ -10,18 +10,36 @@ VALUES (43603,   1,          2) /* ItemType - Armor */
      , (43603,   9,       1024) /* ValidLocations - AbdomenArmor */
      , (43603,  16,          1) /* ItemUseable - No */
      , (43603,  19,       1300) /* Value */
-     , (43603,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+     , (43603,  28,          0) /* ArmorLevel */
+     , (43603,  33,          1) /* Bonded - Bonded */
+     , (43603,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (43603, 114,          1) /* Attuned - Attuned */
+     , (43603, 265,         43) /* EquipmentSetId - OlthoiArmorDRat */
+     , (43603, 324,         12) /* HeritageSpecificArmor - Olthoi */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (43603,  22, True ) /* Inscribable */;
+VALUES (43603,  22, True ) /* Inscribable */
+     , (43603,  23, True ) /* DestroyOnSell */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43603,  13,       1) /* ArmorModVsSlash */
+     , (43603,  14,       1) /* ArmorModVsPierce */
+     , (43603,  15,       1) /* ArmorModVsBludgeon */
+     , (43603,  16,       1) /* ArmorModVsCold */
+     , (43603,  17,       1) /* ArmorModVsFire */
+     , (43603,  18,       1) /* ArmorModVsAcid */
+     , (43603,  19,       1) /* ArmorModVsElectric */
+     , (43603, 165,       1) /* ArmorModVsNether */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (43603,   1, 'Soldier Abdomen Metamorphi') /* Name */;
+VALUES (43603,   1, 'Soldier Abdomen Metamorphi') /* Name */
+     , (43603,  16, 'When equipped, this Metamorphi will increase the Soldier''s Critical Damage Rating') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43603,   1, 0x020000DD) /* Setup */
+VALUES (43603,   1, 0x020009D1) /* Setup */
      , (43603,   3, 0x20000014) /* SoundTable */
      , (43603,   6, 0x0400007E) /* PaletteBase */
+     , (43603,   7, 0x100007C3) /* ClothingBase */
      , (43603,   8, 0x06006E9C) /* Icon */
      , (43603,  22, 0x3400002B) /* PhysicsEffectTable */
      , (43603,  52, 0x06006E9D) /* IconUnderlay */
