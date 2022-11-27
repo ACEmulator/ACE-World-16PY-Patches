d7800e37e593a9e684cbc46ede832692bfc54023
Ty Conner <tyconner@itcproductions.com>
2022-11-27 14:15:39 -0500

    Update 43641 Spitter Pincer Metamorphi.sql

--- Database/Patches/9 WeenieDefaults/Clothing/Armor/43641 Spitter Pincer Metamorphi.sql
+++ Database/Patches/9 WeenieDefaults/Clothing/Armor/43641 Spitter Pincer Metamorphi.sql
@@ -10,19 +10,36 @@ VALUES (43641,   1,          2) /* ItemType - Armor */
      , (43641,   9,         32) /* ValidLocations - HandWear */
      , (43641,  16,          1) /* ItemUseable - No */
      , (43641,  19,        250) /* Value */
-     , (43641,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+     , (43641,  28,          0) /* ArmorLevel */
+     , (43641,  33,          1) /* Bonded - Bonded */
+     , (43641,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (43641, 114,          1) /* Attuned - Attuned */
+     , (43641, 265,         44) /* EquipmentSetId - OlthoiArmorCRed */
+     , (43641, 324,         13) /* HeritageSpecificArmor - OlthoiAcid */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (43641,  22, True ) /* Inscribable */;
+VALUES (43641,  22, True ) /* Inscribable */
+     , (43641,  23, True ) /* DestroyOnSell */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (43641,  13,       1) /* ArmorModVsSlash */
+     , (43641,  14,       1) /* ArmorModVsPierce */
+     , (43641,  15,       1) /* ArmorModVsBludgeon */
+     , (43641,  16,       1) /* ArmorModVsCold */
+     , (43641,  17,       1) /* ArmorModVsFire */
+     , (43641,  18,       1) /* ArmorModVsAcid */
+     , (43641,  19,       1) /* ArmorModVsElectric */
+     , (43641, 165,       1) /* ArmorModVsNether */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (43641,   1, 'Spitter Pincer Metamorphi') /* Name */;
+VALUES (43641,   1, 'Spitter Pincer Metamorphi') /* Name */
+     , (43641,  16, 'When equipped, this Metamorphi will decrease the incoming Critical Strike damage.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (43641,   1, 0x020000DD) /* Setup */
+VALUES (43641,   1, 0x020009D1) /* Setup */
      , (43641,   3, 0x20000014) /* SoundTable */
      , (43641,   6, 0x0400007E) /* PaletteBase */
+     , (43641,   7, 0x100007C3) /* ClothingBase */
      , (43641,   8, 0x06006E98) /* Icon */
      , (43641,  22, 0x3400002B) /* PhysicsEffectTable */
-     , (43641,  52, 0x06006E99) /* IconUnderlay */
-     , (43641, 8044,       2597) /* PCAPPhysicsDIDDataTemplatedFrom - Flared Pants */;
+     , (43641,  52, 0x06006E99) /* IconUnderlay */;
