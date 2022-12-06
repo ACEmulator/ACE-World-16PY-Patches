247233d271091c2e54f21d5e2df9e93cbe89523e
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2022-12-06 15:51:22 -0600

    UpdatedtoRemoveInqItem

---
+++ Database/Patches/8 QuestDefDB/ShatteredCoreSampleReceived.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'ShatteredCoreSampleReceived';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('ShatteredCoreSampleReceived', 300, -1, 'Player stamp for receiving the Shattered Aetherium Core Sample.', '2022-07-02 15:33:11');
