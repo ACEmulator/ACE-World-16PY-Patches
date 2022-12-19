f94c8c2dd862a41b545b5bfab357ec0e1dc60c24
Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
2022-12-19 11:23:35 -0600

    Update 70921 Atlan's Coded Notebook.sql
    icon did not match what either wiki or acpedia had

--- Database/Patches/9 WeenieDefaults/Book/Writable/70921 Atlan's Coded Notebook.sql
+++ Database/Patches/9 WeenieDefaults/Book/Writable/70921 Atlan's Coded Notebook.sql
@@ -21,9 +21,9 @@ VALUES (70921,   1, 'Atlan''s Coded Notebook') /* Name */
      , (70921,  16, 'A translation of the notebook found in the Empyrean Workshop.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (70921,   1, 0x02001517) /* Setup */
+VALUES (70921,   1, 0x02000153) /* Setup */
      , (70921,   3, 0x20000014) /* SoundTable */
-     , (70921,   8, 0x06002B67) /* Icon */
+     , (70921,   8, 0x060012D5) /* Icon */
      , (70921,  22, 0x3400002B) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_book` (`object_Id`, `max_Num_Pages`, `max_Num_Chars_Per_Page`)
