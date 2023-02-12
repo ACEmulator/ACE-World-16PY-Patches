fb1ca7405c1630212b6b8b4bbbdb696bc92fcb4c
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2023-02-12 11:37:47 -0600

    Moarsman Spawning Pools Updates (#1479)
    * Updates
    
    * ColorFix

--- Database/Patches/6 LandBlockExtendedData/3B0B.sql
+++ Database/Patches/6 LandBlockExtendedData/3B0B.sql
@@ -12,12 +12,5 @@ VALUES (0x73B0B00A, 38264, 0x3B0B0036, 156, 132, 2.94674, 0.626475, 0, 0, -0.779
 /* @teleloc 0x3B0B0036 [156.000000 132.000000 2.946740] 0.626475 0.000000 0.000000 -0.779442 */
 
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x73B0B00B,  4219, 0x3B0B0013, 67.7659, 67.7662, -0.342597, 0.911039, 0, 0, -0.412321, False, '2021-11-01 00:00:00'); /* Linkable Monster Generator ( 7 Min.) */
-/* @teleloc 0x3B0B0013 [67.765900 67.766197 -0.342597] 0.911039 0.000000 0.000000 -0.412321 */
-
-INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
-VALUES (0x73B0B00B, 0x73B0B00C, '2021-11-01 00:00:00') /* Brood Mother (38283) */;
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x73B0B00C, 38283, 0x3B0B0013, 71.0376, 70.6403, -0.4444, 0.997189, 0, 0, 0.07493,  True, '2021-11-01 00:00:00'); /* Brood Mother */
-/* @teleloc 0x3B0B0013 [71.037598 70.640297 -0.444400] 0.997189 0.000000 0.000000 0.074930 */
+VALUES (0x73B0B00B, 88423, 0x3B0B0013, 71.6008, 70.6818, -0.395, 1, 0, 0, 0, False, '2023-01-25 11:18:51'); /* Moarsman Spawning Pool 4 Generator */
+/* @teleloc 0x3B0B0013 [71.600800 70.681801 -0.395000] 1.000000 0.000000 0.000000 0.000000 */
---
+++ Database/Patches/9 WeenieDefaults/Creature/Moarsman/38292.es
@@ -0,0 +1,23 @@
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.045
+    - Motion: Twitch3
+
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.095
+    - Motion: Twitch2
+
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.1
+    - Motion: Twitch1
+
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.2
+    - Turn: East
+
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.3
+    - Turn: South
+	
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.5
+    - Move: [5 0 0]
+	
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.6
+    - Move: [0 5 0]
+	
+HeartBeat: Style: NonCombat, Substyle: Ready, Probability: 0.8
+    - MoveHome
