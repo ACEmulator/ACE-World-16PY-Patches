8d73142fc63644a4deb620e0ec865772cc9d35e3
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2023-03-11 16:46:17 -0500

    Update Mage Academy
    Updates the Mage Academy quest, including Burial Temple and Mage Academy dungeons with the new high level retail spawns.
    
    http://acpedia.org/wiki/The_Mage_Academy
    
    Adds Lady Jaria's Wand, and updates 05763 Tazal al-Ashfai with new rewards for the wand, and Black Gem from the Burial Temple.
    
    Burial Temple now has a 150+ restriction like in retail.
    
    Adds door, lever and surface portal in the new section of Burial Temple, used to access the Warded Portal during the seasonal Undead Attack on Al-Jalima quest. This DOES NOT add the Warded Portal itself.

---
+++ Database/Patches/9 WeenieDefaults/Chest/Container/06042 Lady Jaera's Tomb.sql
@@ -0,0 +1,56 @@
+DELETE FROM `weenie` WHERE `class_Id` = 6042;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (6042, 'mageacademycoffinwarriorhighlocked', 20, '2005-02-09 10:00:00') /* Chest */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (6042,   1,        512) /* ItemType - Container */
+     , (6042,   5,       6000) /* EncumbranceVal */
+     , (6042,   6,         -1) /* ItemsCapacity */
+     , (6042,   7,         -1) /* ContainersCapacity */
+     , (6042,   8,       3000) /* Mass */
+     , (6042,  16,         48) /* ItemUseable - ViewedRemote */
+     , (6042,  19,        200) /* Value */
+     , (6042,  37,        300) /* ResistItemAppraisal */
+     , (6042,  38,       9999) /* ResistLockpick */
+     , (6042,  81,          1) /* MaxGeneratedObjects */
+     , (6042,  82,          1) /* InitGeneratedObjects */
+     , (6042,  83,       4096) /* ActivationResponse - CastSpell */
+     , (6042,  93,       1048) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity */
+     , (6042,  96,        500) /* EncumbranceCapacity */
+     , (6042, 100,          1) /* GeneratorType - Relative */
+     , (6042, 106,        450) /* ItemSpellcraft */
+     , (6042, 119,      65535) /* Active */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (6042,   1, True ) /* Stuck */
+     , (6042,   2, False) /* Open */
+     , (6042,   3, True ) /* Locked */
+     , (6042,  12, True ) /* ReportCollisions */
+     , (6042,  13, False) /* Ethereal */
+     , (6042,  33, False) /* ResetMessagePending */
+     , (6042,  34, False) /* DefaultOpen */
+     , (6042,  35, True ) /* DefaultLocked */
+     , (6042,  86, True ) /* ChestRegenOnClose */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (6042,  41,      60) /* RegenerationInterval */
+     , (6042,  43,       1) /* GeneratorRadius */
+     , (6042,  54,       1) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (6042,   1, 'Lady Jaera''s Tomb') /* Name */
+     , (6042,  12, 'keymageacademycoffin') /* LockCode */
+     , (6042,  14, 'Use this item to open it and see its contents.') /* Use */
+     , (6042,  16, 'A coffin of sand-colored stone. You notice a suspicious glow near the lock.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (6042,   1, 0x020000CE) /* Setup */
+     , (6042,   2, 0x09000024) /* MotionTable */
+     , (6042,   3, 0x20000025) /* SoundTable */
+     , (6042,   8, 0x060012C7) /* Icon */
+     , (6042,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (6042,  28,       2074) /* Spell - Gossamer Flesh */;
+
+INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
+VALUES (6042, 1, 72958,  0, 0, 0, False) /* Create Lady Jaera's Wand (72958) for Contain */;
---
+++ Database/Patches/9 WeenieDefaults/Switch/Misc/72959 Flame Trap.sql
@@ -0,0 +1,37 @@
+DELETE FROM `weenie` WHERE `class_Id` = 72959;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (72959, 'ace72959-trapfirelv7', 26, '2005-02-09 10:00:00') /* Switch */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (72959,   1,        128) /* ItemType - Misc */
+     , (72959,   5,       6000) /* EncumbranceVal */
+     , (72959,   8,       3000) /* Mass */
+     , (72959,  16,          1) /* ItemUseable - No */
+     , (72959,  19,        200) /* Value */
+     , (72959,  83,       4096) /* ActivationResponse - CastSpell */
+     , (72959,  93,         20) /* PhysicsState - Ethereal, IgnoreCollisions */
+     , (72959, 106,        450) /* ItemSpellcraft */
+     , (72959, 119,          1) /* Active */
+     , (72959, 134,          8) /* PlayerKillerStatus - Creature */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (72959,   1, True ) /* Stuck */
+     , (72959,  12, False) /* ReportCollisions */
+     , (72959,  13, True ) /* Ethereal */
+     , (72959,  14, False) /* GravityStatus */
+     , (72959,  18, True ) /* Visibility */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (72959,  11,      30) /* ResetInterval */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (72959,   1, 'Flame Trap') /* Name */
+     , (72959,  22, 'You hear a faint clicking sound.') /* ActivationFailure */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (72959,   1, 0x020000EB) /* Setup */
+     , (72959,   3, 0x20000014) /* SoundTable */
+     , (72959,   8, 0x06001066) /* Icon */
+     , (72959,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (72959,  28,       2128) /* Spell - Ilservian's Flame */;
