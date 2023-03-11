b28a88f159cd11df43913a52012d145eed4b41b5
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2023-03-11 17:20:07 -0500

    Update 43162 Zombie Mage.sql

--- Database/Patches/9 WeenieDefaults/Creature/Undead/43162 Zombie Mage.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Undead/43162 Zombie Mage.sql
@@ -69,6 +69,7 @@ VALUES (43162,   1, 0x02000197) /* Setup */
      , (43162,   7, 0x10000066) /* ClothingBase */
      , (43162,   8, 0x06001226) /* Icon */
      , (43162,  22, 0x34000028) /* PhysicsEffectTable */
+     , (43162,  32,       4007) /* WieldedTreasureType */
      , (43162,  35,        420) /* DeathTreasureType - Loot Tier: 6 */;
 
 INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
