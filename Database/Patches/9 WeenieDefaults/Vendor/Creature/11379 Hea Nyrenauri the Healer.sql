245d96c02e58d824dc4027794f1aa80f2655f063
OptimShi <OptimShi@users.noreply.github.com>
2023-03-23 17:17:29 -0700

    Updated appearance to match EOR

--- Database/Patches/9 WeenieDefaults/Vendor/Creature/11379 Hea Nyrenauri the Healer.sql
+++ Database/Patches/9 WeenieDefaults/Vendor/Creature/11379 Hea Nyrenauri the Healer.sql
@@ -78,7 +78,7 @@ VALUES (11379,   1, 0x02001401) /* Setup */
      , (11379,   3, 0x20000013) /* SoundTable */
      , (11379,   4, 0x3000000C) /* CombatTable */
      , (11379,   6, 0x04001E51) /* PaletteBase */
-     , (11379,   7, 0x100000BF) /* ClothingBase */
+     , (11379,   7, 0x1000061E) /* ClothingBase */
      , (11379,   8, 0x0600103C) /* Icon */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
