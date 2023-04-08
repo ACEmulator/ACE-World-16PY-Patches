65dffb04e7900927ce1df2f1b0569da518dc5b0d
OptimShi <OptimShi@users.noreply.github.com>
2023-04-08 09:42:26 -0700

    Rynthid Training Update (#1524)
    * Add Contract and update quest stamps to match for Rynthid Training
    
    * Deleted old quest stamps
    
    * Fixed setup for contract
    
    ---------
    
    Co-authored-by: Ty Conner <tyconner@itcproductions.com>

---
+++ Database/Patches/8 QuestDefDB/RynthidTrainingCompleted_1013.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'RynthidTrainingCompleted_1013';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('RynthidTrainingCompleted_1013', 72000, -1, 'Player cooldown for completing Rynthid Training.', '2022-06-21 15:22:25');
