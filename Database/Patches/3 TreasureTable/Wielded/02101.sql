b8b93c34790924487465b321a2d6447d61fa6c00
Ty Conner <tyconner@itcproductions.com>
2023-04-08 16:04:19 -0400

    Update 02101.sql

--- Database/Patches/3 TreasureTable/Wielded/02101.sql
+++ Database/Patches/3 TreasureTable/Wielded/02101.sql
@@ -1,13 +1,13 @@
 DELETE FROM `treasure_wielded` WHERE `treasure_Type` = 2101;
 
 INSERT INTO `treasure_wielded` (`treasure_Type`, `weenie_Class_Id`, `palette_Id`, `unknown_1`, `shade`, `stack_Size`, `stack_Size_Variance`, `probability`, `unknown_3`, `unknown_4`, `unknown_5`, `set_Start`, `has_Sub_Set`, `continues_Previous_Set`, `unknown_9`, `unknown_10`, `unknown_11`, `unknown_12`, `last_Modified`)
-VALUES (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arbalest */
-     , (2101, 48080,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Quarrel */
-     , (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arbalest */
-     , (2101, 48080,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Quarrel */
-     , (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arbalest */
-     , (2101, 48080,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Quarrel */
-     , (2101, 48083,  0, 0,    0, 0, 0,   0.121, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Acid Katar */
+VALUES (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arbalest */
+     , (2101, 48080,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Quarrel */
+     , (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arbalest */
+     , (2101, 48080,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Quarrel */
+     , (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arbalest */
+     , (2101, 48080,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Quarrel */
+     , (2101, 48083,  0, 0,    0, 0, 0,   0.121, 0, 0, 0,  True, False, True, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Acid Katar */
      , (2101, 48084,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Katar */
      , (2101, 48085,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Lightning Katar */
      , (2101, 48086,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Nekode */
@@ -18,7 +18,7 @@ VALUES (2101, 47852,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False, False, 0
      , (2101,    44,  0, 0,    0, 0, 0,     0.3, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Buckler */
      , (2101,    91,  0, 0,    0, 0, 0,     0.3, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Kite Shield */
      , (2101,    93,  0, 0,    0, 0, 0,     0.3, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Round Shield */;
-/* 
+/*
     # Set: 1
     |   7.70% chance of Arbalest (47852)
     |  92.30% chance of nothing from this set
