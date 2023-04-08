3bb8e37af457c5572c8acff1c849f45cd0fcf6a7
Ty Conner <tyconner@itcproductions.com>
2023-04-08 16:04:10 -0400

    Update 02030.sql

--- Database/Patches/3 TreasureTable/Wielded/02030.sql
+++ Database/Patches/3 TreasureTable/Wielded/02030.sql
@@ -1,13 +1,13 @@
 DELETE FROM `treasure_wielded` WHERE `treasure_Type` = 2030;
 
 INSERT INTO `treasure_wielded` (`treasure_Type`, `weenie_Class_Id`, `palette_Id`, `unknown_1`, `shade`, `stack_Size`, `stack_Size_Variance`, `probability`, `unknown_3`, `unknown_4`, `unknown_5`, `set_Start`, `has_Sub_Set`, `continues_Previous_Set`, `unknown_9`, `unknown_10`, `unknown_11`, `unknown_12`, `last_Modified`)
-VALUES (2030, 23735,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Yumi */
-     , (2030, 47072,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arrow */
-     , (2030, 48239,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False,  True, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Fire Bow */
-     , (2030, 48304,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arrow */
-     , (2030, 48238,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False,  True, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Electric Bow */
-     , (2030, 48285,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arrow */
-     , (2030, 47664,  0, 0,    0, 0, 0,   0.121, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Lightning Tachi */
+VALUES (2030, 23735,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, True, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Yumi */
+     , (2030, 47072,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arrow */
+     , (2030, 48239,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, True,  True, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Fire Bow */
+     , (2030, 48304,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arrow */
+     , (2030, 48238,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, True,  True, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Electric Bow */
+     , (2030, 48285,  0, 0,    0, 30, 0.25,       1, 0, 0, 0,  True,  False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Arrow */
+     , (2030, 47664,  0, 0,    0, 0, 0,   0.121, 0, 0, 0,  True, False, True, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Lightning Tachi */
      , (2030, 47630,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Acid Tachi */
      , (2030, 47682,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Flaming Tachi */
      , (2030, 48086,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Nekode */
@@ -16,9 +16,9 @@ VALUES (2030, 23735,  0, 0,    0, 0, 0,   0.077, 0, 0, 0,  True, False, False, 0
      , (2030, 48501,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Flaming Katar */
      , (2030, 48073,  0, 0,    0, 0, 0,   0.121, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Lightning Nekode */
      , (2030, 23685,  0, 0,    0, 0, 0,     0.3, 0, 0, 0,  True, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Kite Shield */
-     , (2030, 23685,  0, 0,    0, 0, 0,     0.3, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Kite Shield */
-     , (2030,    93,  0, 0,    0, 0, 0,     0.3, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Round Shield */;
-/* 
+     , (2030, 93,  0, 0,    0, 0, 0,     0.3, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Kite Shield */
+     , (2030,    44,  0, 0,    0, 0, 0,     0.3, 0, 0, 0, False, False, False, 0, 0, 0, 0, '2021-11-01 00:00:00') /* Round Shield */;
+/*
     # Set: 1
     |   7.70% chance of Yumi (23735)
     |  92.30% chance of nothing from this set
