c85494db26ed4fbcebcf8c1130cd6489bc0cd14c
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2023-04-08 10:08:03 -0500

    addedanomalyandshardupdates

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/43248 Visible Portalspace Anomaly.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/43248 Visible Portalspace Anomaly.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 43248;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (43248, 'ace43248-visibleportalspaceanomaly', 10, '2021-11-01 00:00:00') /* Creature */;
+VALUES (43248, 'ace43248-visibleportalspaceanomaly', 10, '2023-04-08 09:06:41') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (43248,   1,         16) /* ItemType - Creature */
@@ -9,16 +9,19 @@ VALUES (43248,   1,         16) /* ItemType - Creature */
      , (43248,   7,         -1) /* ContainersCapacity */
      , (43248,  16,          1) /* ItemUseable - No */
      , (43248,  81,          1) /* MaxGeneratedObjects */
-     , (43248,  82,          1) /* InitGeneratedObjects */
+     , (43248,  82,          0) /* InitGeneratedObjects */
      , (43248,  93,    2100236) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn, ReportCollisionsAsEnvironment */
-     , (43248, 133,          1) /* ShowableOnRadar - ShowNever */;
+     , (43248, 133,          1) /* ShowableOnRadar - ShowNever */
+     , (43248, 290,          1) /* HearLocalSignals */
+     , (43248, 291,          5) /* HearLocalSignalsRadius */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (43248,   1, True ) /* Stuck */
-     , (43248,  19, False) /* Attackable */;
+     , (43248,  19, False) /* Attackable */
+     , (43248,  52, True ) /* AiImmobile */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (43248,  41,     720) /* RegenerationInterval */
+VALUES (43248,  41,       0) /* RegenerationInterval */
      , (43248,  43,     0.1) /* GeneratorRadius */
      , (43248,  76,       1) /* Translucency */;
 
@@ -33,5 +36,14 @@ VALUES (43248,   1, 0x020019E4) /* Setup */
      , (43248,   8, 0x060016C4) /* Icon */
      , (43248,  22, 0x34000025) /* PhysicsEffectTable */;
 
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (43248, 37 /* ReceiveLocalSignal */,      1, NULL, NULL, NULL, 'sparkport', NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id,  0,  72 /* Generate */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  1,  77 /* DeleteSelf */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (43248, -1, 43255, 1, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0.5, 1, 0, 0, 0) /* Generate Sparkling Portal (43255) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (43248, -1, 43255, 1, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0.5, 1, 0, 0, 0) /* Generate Sparkling Portal (43255) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
