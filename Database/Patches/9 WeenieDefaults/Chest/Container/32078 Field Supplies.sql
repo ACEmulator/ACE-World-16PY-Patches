6c424c3008e8859ba7ccefc53b5275a44e92a2d1
OptimShi <OptimShi@users.noreply.github.com>
2023-04-08 09:35:33 -0700

    Generators (#1520)
    * Fixed some generator errors
    
    * More gen fixes

--- Database/Patches/9 WeenieDefaults/Chest/Container/32078 Field Supplies.sql
+++ Database/Patches/9 WeenieDefaults/Chest/Container/32078 Field Supplies.sql
@@ -47,5 +47,5 @@ VALUES (32078,   1, 0x0200007C) /* Setup */
      , (32078,  22, 0x3400002B) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (32078, 1, 32083, 0, 1, 1, 2, 8, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Instructions (32083) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
-     , (32078, 1, 448, 0, 1, 1, 2, 72, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate RANDOMLY GENERATED TREASURE from Loot Tier 4 from Death Treasure Table id: 448 (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: ContainTreasure */;
+VALUES (32078, -1, 32083, 0, 1, 1, 2, 8, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Instructions (32083) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
+     , (32078, -1, 448, 0, 1, 1, 2, 72, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate RANDOMLY GENERATED TREASURE from Loot Tier 4 from Death Treasure Table id: 448 (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: ContainTreasure */;
