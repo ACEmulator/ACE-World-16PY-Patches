358ba83b459b10514d378190ede5060832d51bca
Ty Conner <tyconner@itcproductions.com>
2023-04-09 03:53:07 -0400

    Update 32079 Field Supplies.sql

--- Database/Patches/9 WeenieDefaults/Chest/Container/32079 Field Supplies.sql
+++ Database/Patches/9 WeenieDefaults/Chest/Container/32079 Field Supplies.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 32079;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (32079, 'ace32079-fieldsupplies', 20, '2021-11-01 00:00:00') /* Chest */;
+VALUES (32079, 'ace32079-fieldsupplies', 20, '2023-04-08 23:52:08') /* Chest */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (32079,   1,        512) /* ItemType - Container */
