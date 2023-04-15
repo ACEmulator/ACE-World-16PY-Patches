4415803b627b709cb424dfefa70c58307cd92911
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2023-04-15 15:10:18 -0400

    Rynthid Testing Fix Broken Reset
    Currently, the door tests in this quest only reset when the landblock resets, meaning players are stuck waiting for a respawn that will never come. This corrects this issue by having the door controller stopgap delete itself after it closes its door, allowing the generator it is on to start its cycle from the beginning.
    
    Drudge Wilter scale is adjusted to match retail video.
    
    Gurog Bruiser stats and art are adjusted to match retail video. These Gurog do not appear to ever cast and only melee with their fists. They do not wield an axe. Also, their HP was lower than it should be based on how much damage it takes to kill them on the video.
    
    No corpse is set on both of these mobs, and loot profile removed, as seen on the video.

--- Database/Patches/9 WeenieDefaults/Creature/Gurog/88145 Gurog Bruiser.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Gurog/88145 Gurog Bruiser.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 88145;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (88145, 'ace88145-gurogbruiser', 10, '2022-12-04 19:04:52') /* Creature */;
+VALUES (88145, 'ace88145-gurogbruiser', 10, '2023-04-15 02:39:48') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (88145,   1,         16) /* ItemType - Creature */
@@ -23,6 +23,7 @@ VALUES (88145,   1, True ) /* Stuck */
      , (88145,  11, False) /* IgnoreCollisions */
      , (88145,  12, True ) /* ReportCollisions */
      , (88145,  13, False) /* Ethereal */
+     , (88145,  29, True ) /* NoCorpse */
      , (88145,  50, True ) /* NeverFailCasting */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
@@ -64,13 +65,23 @@ INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (88145,   1, 'Gurog Bruiser') /* Name */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (88145,   1, 0x02001A2C) /* Setup */
+VALUES (88145,   1, 0x02001A2B) /* Setup */
      , (88145,   2, 0x090001A8) /* MotionTable */
      , (88145,   3, 0x200000D5) /* SoundTable */
      , (88145,   4, 0x30000000) /* CombatTable */
      , (88145,   8, 0x06002B2E) /* Icon */
-     , (88145,  22, 0x340000CD) /* PhysicsEffectTable */
-     , (88145,  35,       1000) /* DeathTreasureType - Loot Tier: 7 */;
+     , (88145,  22, 0x340000CD) /* PhysicsEffectTable */;
+
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (88145,  0,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
+     , (88145,  1,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
+     , (88145,  2,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
+     , (88145,  3,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
+     , (88145,  4,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
+     , (88145,  5,  4,200,  0.5,  500,  250,  250,  250,  250,  250,  250,  250,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
+     , (88145,  6,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
+     , (88145,  7,  4,  0,    0,  500,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
+     , (88145,  8,  4,200,  0.5,  500,  250,  250,  250,  250,  250,  250,  250,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
 
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (88145,   1, 550, 0, 0) /* Strength */
@@ -81,7 +92,7 @@ VALUES (88145,   1, 550, 0, 0) /* Strength */
      , (88145,   6, 410, 0, 0) /* Self */;
 
 INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
-VALUES (88145,   1,  1655, 0, 0, 1900) /* MaxHealth */
+VALUES (88145,   1,  4755, 0, 0, 5000) /* MaxHealth */
      , (88145,   3,  3500, 0, 0, 3990) /* MaxStamina */
      , (88145,   5,  1000, 0, 0, 1410) /* MaxMana */;
 
@@ -96,21 +107,3 @@ VALUES (88145,  6, 0, 3, 0, 470, 0, 0) /* MeleeDefense        Specialized */
      , (88145, 45, 0, 3, 0, 420, 0, 0) /* LightWeapons        Specialized */
      , (88145, 46, 0, 3, 0, 420, 0, 0) /* FinesseWeapons      Specialized */;
 
-INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
-VALUES (88145,  0,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 1, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0, 0.33,    0,    0) /* Head */
-     , (88145,  1,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 2, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0, 0.44, 0.17,    0) /* Chest */
-     , (88145,  2,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 3,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0,    0, 0.17,    0) /* Abdomen */
-     , (88145,  3,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 1, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0, 0.23, 0.03,    0) /* UpperArm */
-     , (88145,  4,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 2,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0,    0,  0.3,    0) /* LowerArm */
-     , (88145,  5,  4, 200,  0.5,  500,  500,  275,  500,  500,  275,  500,  500,    0, 2,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0,    0,  0.2,    0) /* Hand */
-     , (88145,  6,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 3,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18,    0, 0.13, 0.18) /* UpperLeg */
-     , (88145,  7,  4,  0,    0,  500,  500,  275,  500,  500,  275,  500,  500,    0, 3,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6,    0,    0,  0.6) /* LowerLeg */
-     , (88145,  8,  4, 200,  0.5,  500,  500,  275,  500,  500,  275,  500,  500,    0, 3,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22,    0,    0, 0.22) /* Foot */;
-
-INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
-VALUES (88145,  4312,   2.02)  /* Incantation of Imperil Other */
-     , (88145,  4446,   2.02)  /* Incantation of Frost Blast */
-     , (88145,  4447,   2.25)  /* Incantation of Frost Bolt */;
-
-INSERT INTO `weenie_properties_create_list` (`object_Id`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`)
-VALUES (88145, 2, 43397,  0, 0, 1, False) /* Create Frost Greataxe (43397) for Wield */;
--- Database/Patches/9 WeenieDefaults/Generic/None/88150 Grievver Test Master Generator.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/88150 Grievver Test Master Generator.sql
@@ -25,5 +25,5 @@ VALUES (88150,   1, 0x0200026B) /* Setup */
      , (88150,   8, 0x06001066) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (88150, -1, 88151, 600, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 0.707107, 0, 0, 0.707107) /* Generate Grievver Violator Generator (88151) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */
-     , (88150, -1, 88109, 600, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 0.707107, 0, 0, 0.707107) /* Generate Rynthid Assessment Door Controller (88109) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
+VALUES (88150, -1, 88151, 300, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 0.707107, 0, 0, 0.707107) /* Generate Grievver Violator Generator (88151) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */
+     , (88150, -1, 88109, 120, 1, 1, 1, 4, -1, 0, 0, 0, 0, 0, 0, 0.707107, 0, 0, 0.707107) /* Generate Rynthid Assessment Door Controller (88109) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Specific */;
