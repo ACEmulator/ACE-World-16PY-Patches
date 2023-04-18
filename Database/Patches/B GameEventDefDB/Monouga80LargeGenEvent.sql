ef74e45d34f601b3ff55ec82e01f9d6e387fa189
sLackeR-hub <70003173+sLackeR-hub@users.noreply.github.com>
2023-04-18 14:45:15 -0500

    Updates

--- Database/Patches/B GameEventDefDB/Monouga80LargeGenEvent.sql
+++ Database/Patches/B GameEventDefDB/Monouga80LargeGenEvent.sql
@@ -1,4 +1,4 @@
 DELETE FROM `event` WHERE `name` = 'Monouga80LargeGenEvent';
 
 INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('Monouga80LargeGenEvent', -1, -1, 3 /* GameEventState.Off */, '2021-11-01 00:00:00');
+VALUES ('Monouga80LargeGenEvent', -1, -1, 4 /* GameEventState.On */, '2021-11-01 00:00:00');
