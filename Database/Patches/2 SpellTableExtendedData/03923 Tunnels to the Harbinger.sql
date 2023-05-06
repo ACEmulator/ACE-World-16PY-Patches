10eb034a5c843e2268b9223c86e6181120d8ef01
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2023-05-06 09:06:00 -0400

    Update 03923 Tunnels to the Harbinger.sql

--- Database/Patches/2 SpellTableExtendedData/03923 Tunnels to the Harbinger.sql
+++ Database/Patches/2 SpellTableExtendedData/03923 Tunnels to the Harbinger.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell` WHERE `id` = 3923;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
-VALUES (3923, 'Tunnels to the Harbinger', 0x64470240, 49.9144, -66.3803, 0.01, 1, 0, 0, 0, '2021-11-01 00:00:00');
-/* @teleloc 0x64470240 [49.914398 -66.380302 0.010000] 1.000000 0.000000 0.000000 0.000000 */
+VALUES (3923, 'Tunnels to the Harbinger', 0x005E0317, 280, -66, 0.01, 1, 0, 0, 0, '2021-11-01 00:00:00');
+/* @teleloc 0x005E0317 [280.000000 -66.000000 0.010000] 1.000000 0.000000 0.000000 0.000000 */
