1335e67308b7342180d5a9dd5cfac949c77baace
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2023-08-27 04:00:01 -0500

    Fix Nimble Fingers buffs

--- Database/Patches/2 SpellTableExtendedData/04984 Nimble Fingers - Cooking.sql
+++ Database/Patches/2 SpellTableExtendedData/04984 Nimble Fingers - Cooking.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4984;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (4984, 'Nimble Fingers - Cooking', 36880 /* Skill, SingleStat, Additive */, 22 /* Jump */, -20, '2021-11-01 00:00:00');
+VALUES (4984, 'Nimble Fingers - Cooking', 36880 /* Skill, SingleStat, Additive */, 39 /* Cooking */, 5, '2021-11-01 00:00:00');
