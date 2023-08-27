584025a709133e42144da99e85e5ea7c2ab01d0e
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2023-08-27 01:17:08 -0500

    Fix Red Rune Silveran Mace TurnIn

--- Database/Patches/9 WeenieDefaults/Creature/Human/33015 Meerana du Renari.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/33015 Meerana du Renari.sql
@@ -287,7 +287,7 @@ INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `dela
 VALUES (@parent_id,  0,  67 /* Goto */, 0, 1, NULL, 'WeaponTrading', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (33015,  6 /* Give */,      1, 87534 /* Binding Realm */, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (33015,  6 /* Give */,      1, 87555 /* Red Rune Silveran Mace */, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
