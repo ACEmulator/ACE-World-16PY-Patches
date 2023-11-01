2ce652d8f8e800770c49d9ca3b49cc1dad1f7f19
Fire-Chimp <hastings_david@hotmail.com>
2023-11-01 07:18:24 -0500

    society fix for retail meta
    error in text that retail meta would not proceed due to the missing ,

--- Database/Patches/9 WeenieDefaults/Creature/Human/38697 Hador the Vengeful.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Human/38697 Hador the Vengeful.sql
@@ -121,7 +121,7 @@ VALUES (38697, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'TaskGrave1JawCo
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  10 /* Tell */, 0, 1, NULL, 'Haven''t picked up any jaws yet? Get to it youngster! The Society waits for no one.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id,  0,  10 /* Tell */, 0, 1, NULL, 'Haven''t picked up any jaws yet? Get to it, youngster! The Society waits for no one.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (38697, 12 /* QuestSuccess */,      1, NULL, NULL, NULL, 'TaskGrave1JawCollectWait@2', NULL, NULL, NULL);
