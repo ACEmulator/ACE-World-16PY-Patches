b439f0020738bc2898a9177a203c4fd1c009ff7e
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2023-11-09 11:08:31 -0600

    Remove Swarm Demon Life Immunity (#1594)

--- Database/Patches/9 WeenieDefaults/Creature/Olthoi/35151 Swarm Demon.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Olthoi/35151 Swarm Demon.sql
@@ -21,8 +21,7 @@ INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (35151,   1, True ) /* Stuck */
      , (35151,  12, True ) /* ReportCollisions */
      , (35151,  14, True ) /* GravityStatus */
-     , (35151,  19, True ) /* Attackable */
-     , (35151, 103, True ) /* NonProjectileMagicImmune */;
+     , (35151,  19, True ) /* Attackable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (35151,   1,       5) /* HeartbeatInterval */
