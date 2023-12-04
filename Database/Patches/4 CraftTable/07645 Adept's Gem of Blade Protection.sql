8db58f35b5647f9fc54ccede4a969fe0fa0adcbf
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2023-12-03 22:17:48 -0600

    Fix adept gem of blade prot recipe to create 2 gems

--- Database/Patches/4 CraftTable/07645 Adept's Gem of Blade Protection.sql
+++ Database/Patches/4 CraftTable/07645 Adept's Gem of Blade Protection.sql
@@ -1,7 +1,7 @@
 DELETE FROM `recipe` WHERE `id` = 7645;
 
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
-VALUES (7645, 0, 38 /* Alchemy */, 325, 0, 34170 /* Adept's Gem of Blade Protection */, 1, 'The powder congeals into a gem-like substance. The process is a success!', 0, 0, 'The process was an utter failure, leaving you with a messy mass of mush to show for your efforts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-11-01 00:00:00');
+VALUES (7645, 0, 38 /* Alchemy */, 325, 0, 34170 /* Adept's Gem of Blade Protection */, 2, 'The powder congeals into a gem-like substance. The process is a success!', 0, 0, 'The process was an utter failure, leaving you with a messy mass of mush to show for your efforts.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2021-11-01 00:00:00');
 
 DELETE FROM `cook_book` WHERE `recipe_Id` = 7645;
 
