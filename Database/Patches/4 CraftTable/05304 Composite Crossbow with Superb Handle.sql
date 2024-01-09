6e5650815c8cbb2d1fcdc36043871183e91dacf0
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2024-01-09 11:52:46 -0500

    Misc Fixes
    Corrects composite crossbow with handle recipes to use the crossbow (not bow) as the base.
    Removes nether resistance from Decaying Ruschk Laktar, and sets other resistances to be similar to other undead Ruschk.
    Sets attack type to thrust for 46392 Shadowfire Isparian Spear.
    Corrects stats on Composite Crossbow with Fine Handle based on what's listed on both wikis.

--- Database/Patches/4 CraftTable/05304 Composite Crossbow with Superb Handle.sql
+++ Database/Patches/4 CraftTable/05304 Composite Crossbow with Superb Handle.sql
@@ -17,6 +17,6 @@ VALUES (@parent_id, 3,  25, NULL, 4, 0) /* On Player.SuccessResult CopyFromSourc
 DELETE FROM `cook_book` WHERE `recipe_Id` = 5304;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
-VALUES (5304, 33983 /* Superb Bone Handle */,  6963 /* Composite Bow */, '2022-07-02 15:33:11')
+VALUES (5304, 33983 /* Superb Bone Handle */,  7035 /* Composite Crossbow */, '2022-07-02 15:33:11')
      , (5304, 33983 /* Superb Bone Handle */, 33999 /* Composite Crossbow with Handle */, '2022-07-02 15:33:11')
      , (5304, 33983 /* Superb Bone Handle */, 87751 /* Composite Crossbow with Fine Handle */, '2022-07-02 15:33:11');
