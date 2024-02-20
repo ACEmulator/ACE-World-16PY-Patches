23fcf9afbb383688752df93516b850fb8f07dcb7
OptimShi <ross@yotesfan.com>
2024-02-20 11:51:13 -0700

    Add Vacant Golem pet

--- Database/Patches/9 WeenieDefaults/PetDevice/Misc/34916 Inactive Blank Golem.sql
+++ Database/Patches/9 WeenieDefaults/PetDevice/Misc/34916 Inactive Blank Golem.sql
@@ -1,15 +1,17 @@
 DELETE FROM `weenie` WHERE `class_Id` = 34916;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (34916, 'ace34916-inactiveblankgolem', 1, '2021-11-07 08:12:46') /* Generic */;
+VALUES (34916, 'ace34916-inactiveblankgolem', 70, '2021-11-07 08:12:46') /* PetDevice */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (34916,   1,        128) /* ItemType - Misc */
      , (34916,   5,         50) /* EncumbranceVal */
-     , (34916,  16,          1) /* ItemUseable - No */
+     , (34916,  16,          8) /* ItemUseable - Contained */
      , (34916,  19,          0) /* Value */
      , (34916,  33,          1) /* Bonded - Bonded */
-     , (34916, 114,          1) /* Attuned - Attuned */;
+     , (34916,  94,         16) /* TargetType - Creature */
+     , (34916, 114,          1) /* Attuned - Attuned */
+     , (34916, 266,      34898) /* PetClass - Vacant Golem */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (34916,  22, True ) /* Inscribable */
