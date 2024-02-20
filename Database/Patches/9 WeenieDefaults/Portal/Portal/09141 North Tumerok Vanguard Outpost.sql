fef2d64afdbcdd7570fe8bde80baf175e2521b3f
Ty Conner <tyconner@itcproductions.com>
2024-02-20 00:36:01 -0500

    normalize

--- Database/Patches/9 WeenieDefaults/Portal/Portal/09141 North Tumerok Vanguard Outpost.sql
+++ Database/Patches/9 WeenieDefaults/Portal/Portal/09141 North Tumerok Vanguard Outpost.sql
@@ -30,6 +30,5 @@ VALUES (9141,   1, 0x020005D3) /* Setup */
      , (9141,   8, 0x0600106B) /* Icon */;
 
 INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (9141, 2, 0x2A50296, 60, -680, 6, 0.707107, 0, 0, -0.707107) /* Destination */
-/* @teleloc 0x2A50296 [60.000000 -680.000000 6.000000] 0.707107 0.000000 0.000000 -0.707107 */;
-
+VALUES (9141, 2, 0x02A50296, 60, -680, 6, 0.707107, 0, 0, -0.707107) /* Destination */
+/* @teleloc 0x02A50296 [60.000000 -680.000000 6.000000] 0.707107 0.000000 0.000000 -0.707107 */;
