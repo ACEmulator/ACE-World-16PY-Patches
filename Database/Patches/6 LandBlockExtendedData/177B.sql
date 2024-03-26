1b04a7045de272500b908683311cc1c6bbd92cd0
lugielord <156704507+lugielord@users.noreply.github.com>
2024-03-26 16:57:59 -0500

    0177B Strange Humming Crystal Exit Portals
    -- 0177B.sql
    -- https://asheron.fandom.com/wiki/Humming_Crystal
    -- strange humming crystal missing ne & nw exit portals @ 2.8S, 82.8W

---
+++ Database/Patches/6 LandBlockExtendedData/177B.sql
@@ -0,0 +1,25 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x177B;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7177B000,   412, 0x177B0105, 178.24, 180.95, 572.908, 0.707107, 0, 0, -0.707107, False, '2005-02-09 10:00:00'); /* Door */
+/* @teleloc 0x177B0105 [178.240005 180.949997 572.908020] 0.707107 0.000000 0.000000 -0.707107 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7177B001,  9075, 0x177B0000, 180, 179.749, 560, 0.042599, 0, 0, -0.999092, False, '2005-02-09 10:00:00'); /* Strange Humming Crystal */
+/* @teleloc 0x177B0000 [180.000000 179.748993 560.000000] 0.042599 0.000000 0.000000 -0.999092 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7177B002,  9070, 0x177B0000, 170.93, 170.601, 560.005, -0.404541, 0, 0, -0.91452, False, '2005-02-09 10:00:00'); /* Exit Portal sw */
+/* @teleloc 0x177B0000 [170.929993 170.600998 560.005005] -0.404541 0.000000 0.000000 -0.914520 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7177B003,  9069, 0x177B0000, 189.244, 170.735, 560.005, 0.415827, 0, 0, -0.909444, False, '2005-02-09 10:00:00'); /* Exit Portal se */
+/* @teleloc 0x177B0000 [189.244003 170.735001 560.005005] 0.415827 0.000000 0.000000 -0.909444 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7177B004,  9068, 0x177B0040, 171.164490, 189.317139, 560.005981, -0.917047, 0.000000, 0.000000, -0.398779, False, '2023-03-25 20:00:00'); /* Exit Portal nw */
+/* @teleloc 0x177B0040 [171.164490 189.317139 560.005981] -0.917047 0.000000 0.000000 -0.398779 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7177B005,  9067, 0x177B0040, 189.655746, 189.048752, 560.005981, -0.936353, 0.000000, 0.000000, 0.351059, False, '2023-03-25 20:00:00'); /* Exit Portal ne */
+/* @teleloc 0x177B0040 [189.655746 189.048752 560.005981] -0.936353 0.000000 0.000000 0.351059 */
