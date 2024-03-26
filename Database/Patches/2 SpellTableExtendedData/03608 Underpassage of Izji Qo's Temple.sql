93df09d79bc21984554380800f062dc73fdb210a
lugielord <156704507+lugielord@users.noreply.github.com>
2024-03-26 16:54:39 -0500

    03608 Underpassage of Izji Qo's Temple.sql - Update Spell destination
    -- 03608 Underpassage of Izji Qo's Temple.sql
    -- https://asheron.fandom.com/wiki/The_Temple_of_Izji_Qo
    -- 29745	gatewayreeshangauntlet casts spell 3608, 'Underpassage of Izji Qo''s Temple' which is BROKEN.
    -- location should be: 0x016A02B0
    -- same as 29799	portalthreebagsexit

---
+++ Database/Patches/2 SpellTableExtendedData/03608 Underpassage of Izji Qo's Temple.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell` WHERE `id` = 3608;
+
+INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
+VALUES (3608, 'Underpassage of Izji Qo''s Temple', 0x016A02B0, 10.000000, -20.000000, 0.006000, 1.000000, 0.000000, 0.000000, 0.000000, '2024-03-26 20:00:00');
+/* @teleloc 0x016A02B0 [10.000000 -20.000000 0.006000] 1.000000 0.000000 0.000000 0.000000 */
