43336f2fb7cf05ff7055315610077aafd3bf5a6a
lugielord <156704507+lugielord@users.noreply.github.com>
2024-03-26 16:48:32 -0500

    8474 Add Mud Cave to 79.0S, 84.1E
    -- E81C.sql
    -- 8474 Add Mud Cave instance to 79.0S, 84.1E
    -- https://asheron.fandom.com/wiki/Mud_Cave

---
+++ Database/Patches/6 LandBlockExtendedData/E81C.sql
@@ -0,0 +1,51 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xE81C;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C001,  4246, 0xE81C0100, 105.882, 144.949, -5.195, 0.350992, 0, 0, 0.936378,  True, '2005-02-09 10:00:00'); /* Rank Moarsman */
+/* @teleloc 0xE81C0100 [105.882004 144.949005 -5.195000] 0.350992 0.000000 0.000000 0.936378 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C002,  4246, 0xE81C0000, 113.338, 131.26, 0.004552, 0.360431, 0, 0, 0.932786,  True, '2005-02-09 10:00:00'); /* Rank Moarsman */
+/* @teleloc 0xE81C0000 [113.337997 131.259995 0.004552] 0.360431 0.000000 0.000000 0.932786 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C003,  4246, 0xE81C0000, 98.5148, 128.902, 0.004552, -0.484919, 0, 0, 0.874559,  True, '2005-02-09 10:00:00'); /* Rank Moarsman */
+/* @teleloc 0xE81C0000 [98.514801 128.901993 0.004552] -0.484919 0.000000 0.000000 0.874559 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C004, 27854, 0xE81C0100, 105.033, 147.25, -5.1952, -0.076296, 0, 0, -0.997085,  True, '2005-02-09 10:00:00'); /* Fetid Moarsman */
+/* @teleloc 0xE81C0100 [105.032997 147.250000 -5.195200] -0.076296 0.000000 0.000000 -0.997085 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C005, 27854, 0xE81C0000, 102.38, 135.36, 3.90545, -0.074841, 0, 0, 0.997195,  True, '2005-02-09 10:00:00'); /* Fetid Moarsman */
+/* @teleloc 0xE81C0000 [102.379997 135.360001 3.905450] -0.074841 0.000000 0.000000 0.997195 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C006, 27854, 0xE81C0000, 100.173, 121.254, 0.005, 0.993514, 0, 0, -0.113713,  True, '2005-02-09 10:00:00'); /* Fetid Moarsman */
+/* @teleloc 0xE81C0000 [100.172997 121.253998 0.005000] 0.993514 0.000000 0.000000 -0.113713 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C007, 27858, 0xE81C0100, 100.873, 151.146, -5.195, 0.131455, 0, 0, -0.991322,  True, '2005-02-09 10:00:00'); /* Muculent Moarsman */
+/* @teleloc 0xE81C0100 [100.873001 151.145996 -5.195000] 0.131455 0.000000 0.000000 -0.991322 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C008, 27858, 0xE81C0100, 104.343, 152.272, -5.195, -0.216437, 0, 0, -0.976297,  True, '2005-02-09 10:00:00'); /* Muculent Moarsman */
+/* @teleloc 0xE81C0100 [104.343002 152.272003 -5.195000] -0.216437 0.000000 0.000000 -0.976297 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C009,  4219, 0xE81C0100, 98.4759, 141.959, -5.195, -0.173478, 0, 0, -0.984838, False, '2005-02-09 10:00:00'); /* Linkable Monster Generator ( 7 Min.) */
+/* @teleloc 0xE81C0100 [98.475899 141.959000 -5.195000] -0.173478 0.000000 0.000000 -0.984838 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7E81C010,  8474, 0xE81C0100, 102.499847, 152.441330, -5.194000, 0.999920, 0.000000, 0.000000, 0.012681, False, '2024-03-25 20:00:00'); /* Mud Cave 79.0S, 84.1E */
+/* @teleloc 0xE81C0100 [102.499847 152.441330 -5.194000] 0.999920 0.000000 0.000000 0.012681 */
+
+INSERT INTO `landblock_instance_link` (`parent_GUID`, `child_GUID`, `last_Modified`)
+VALUES (0x7E81C009, 0x7E81C001, '2005-02-09 10:00:00') /* Rank Moarsman (4246) */
+     , (0x7E81C009, 0x7E81C002, '2005-02-09 10:00:00') /* Rank Moarsman (4246) */
+     , (0x7E81C009, 0x7E81C003, '2005-02-09 10:00:00') /* Rank Moarsman (4246) */
+     , (0x7E81C009, 0x7E81C004, '2005-02-09 10:00:00') /* Fetid Moarsman (27854) */
+     , (0x7E81C009, 0x7E81C005, '2005-02-09 10:00:00') /* Fetid Moarsman (27854) */
+     , (0x7E81C009, 0x7E81C006, '2005-02-09 10:00:00') /* Fetid Moarsman (27854) */
+     , (0x7E81C009, 0x7E81C007, '2005-02-09 10:00:00') /* Muculent Moarsman (27858) */
+     , (0x7E81C009, 0x7E81C008, '2005-02-09 10:00:00') /* Muculent Moarsman (27858) */;
