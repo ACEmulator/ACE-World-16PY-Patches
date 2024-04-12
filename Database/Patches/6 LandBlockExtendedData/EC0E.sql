cf62a5c901a890fcdec7cd2603bc2feb56306572
lugielord <156704507+lugielord@users.noreply.github.com>
2024-04-12 02:17:11 -0500

    Delete Database/Patches/6 LandBlockExtendedData/EC0E.sql
    Roll back this new file as it is causing issues.

--- Database/Patches/6 LandBlockExtendedData/EC0E.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x7EC0E01F, 37068, 0xEC0E002D, 120.912, 115.15, -0.163, 1, 0, 0, 0, False, '2024-04-01 20:00:00'); /* Roots of the Deru Tree */
-/* @teleloc 0xEC0E002D 120.912 115.15 -0.163 1 0 0 0 */
