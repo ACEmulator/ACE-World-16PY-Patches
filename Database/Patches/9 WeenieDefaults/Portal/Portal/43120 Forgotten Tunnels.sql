d0cb6c2315deff5f554fe6661e66d1f589bd75c5
lugielord <156704507+lugielord@users.noreply.github.com>
2024-04-13 16:04:15 -0500

    Update Forgotten Tunnnels 43118, 43119, 43120 portals 'not yet implemented'
    Database/Patches/9 WeenieDefaults/Portal/Portal/43118 Forgotten Tunnels.sql
    Database/Patches/9 WeenieDefaults/Portal/Portal/43119 Forgotten Tunnels.sql
    Database/Patches/9 WeenieDefaults/Portal/Portal/43120 Forgotten Tunnels.sql
    
    -- Set Destinations according to Landblock Google Sheet PCAP data
    
    43118 Source: 93.1S, 56.9W
    @teleloc 0x380B0102 [60.0445 20.7545 -5.663] -0.999965 0 0 0.008334
    -- Set Destination:
    @teleloc 0x8B02026b [0 -20 18.004999160767] 0.70710700750351 0 0 -0.70710700750351
    -- W side of dungeon
    
    43119 Source 92.9S, 56.4W
    @teleloc 0x380B0106 [179.998 69.0345 -5.663] -0.999579 0 0 -0.028995
    -- Set Destination:
    @teleloc 0x8B020282 [130 -200 18.004999160767] 1 0 0 0
    -- S side of dungeon
    
    43120 Source 93.6S, 56.6W
    @teleloc 0x380A0102 [132.106 92.8269 -5.663] 0.999826 0 0 0.018646
    -- Set Destination:
    @teleloc 0x8B020292 [260 -20 18.004999160767] -0.70710700750351 0 0 -0.70710700750351
    -- E side of dungeon

--- Database/Patches/9 WeenieDefaults/Portal/Portal/43120 Forgotten Tunnels.sql
+++ Database/Patches/9 WeenieDefaults/Portal/Portal/43120 Forgotten Tunnels.sql
@@ -24,3 +24,7 @@ INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (43120,   1, 0x020005D5) /* Setup */
      , (43120,   2, 0x09000003) /* MotionTable */
      , (43120,   8, 0x0600106B) /* Icon */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`,`position_Type`,`obj_Cell_Id`,`origin_X`,`origin_Y`,`origin_Z`,`angles_W`,`angles_X`,`angles_Y`,`angles_Z`)
+VALUES (43120, 2, 0x8B020292, 260, -20, 18.004999160767, -0.70710700750351, 0, 0, -0.70710700750351); /* Destination E*/
+/* @teleloc 0x8B020292 [260 -20 18.004999160767] -0.70710700750351 0 0 -0.70710700750351  */
