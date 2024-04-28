27ee9e405faa9cfff0d9d1092fd2e46c91b4b632
lugielord <156704507+lugielord@users.noreply.github.com>
2024-04-28 14:12:20 -0500

    Update 90130 90131 E3D5 E3D6 Olthoi Tunnel Final 2 Portals
    Database/Patches/9 WeenieDefaults/Creature/Shadow/90130 Olthoi Tunnel.sql
    Database/Patches/9 WeenieDefaults/Creature/Shadow/90131 Olthoi Tunnel.sql
    Database/Patches/6 LandBlockExtendedData/E3D5.sql
    Database/Patches/6 LandBlockExtendedData/E3D6.sql
    
    https://asheron.fandom.com/wiki/Olthoi_Island
    Of the twelve pair (24 total) of Olthoi Tunnels, we have 22 weenies, we are missing 2.
    Requested and received approval for 2 new weenies: 90130 & 90131.
    
    -- Dark Isle Pair:
    90130 (new) Olthoi Tunnel Source 69.4N, 80.3E
    Destination 85.4N, 57.3E
    Note: ace43705-olthoitunnel is existing return portal
    ---
    
    -- Candeth Keep Pair:
    90131 (new) Olthoi Tunnel Source 68.5N, 79.7E
    Destination 87.7S, 65.1W
    Note: ace43559-olthoitunnel is existing return portal

---
+++ Database/Patches/6 LandBlockExtendedData/E3D5.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xE3D5;
+
+INSERT INTO `landblock_instance`(`weenie_Class_Id`,`obj_Cell_Id`,`origin_X`,`origin_Y`,`origin_Z`,`angles_W`,`angles_X`,`angles_Y`,`angles_Z`,`is_Link_Child`,`last_Modified`)
+VALUES(90131, 0xE3D50001, 8.941406, 11.824219, 0, 1, 0, 0, 0, False, '2024-04-28 00:00:00'); /* Olthoi Tunnel */
+/* @teleloc 0xE3D50001 [8.941406 11.824219 0] 1 0 0 0 */
