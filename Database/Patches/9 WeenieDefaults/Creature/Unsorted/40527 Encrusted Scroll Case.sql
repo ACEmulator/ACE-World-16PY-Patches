d97703a0727957f8224296e23d44c6c676fcbd52
Ty Conner <tyconner@itcproductions.com>
2024-05-11 14:15:04 -0400

    Update 40527 Encrusted Scroll Case.sql

--- Database/Patches/9 WeenieDefaults/Creature/Unsorted/40527 Encrusted Scroll Case.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Unsorted/40527 Encrusted Scroll Case.sql
@@ -1,16 +1,39 @@
 DELETE FROM `weenie` WHERE `class_Id` = 40527;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (40527, 'ace40527-encrustedscrollcase', 10, '2021-11-17 16:56:08') /* Creature */;
+VALUES (40527, 'ace40527-encrustedscrollcase', 10, '2024-03-23 02:50:59') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (40527,   1,         16) /* ItemType - Creature */
      , (40527,   6,         -1) /* ItemsCapacity */
      , (40527,   7,         -1) /* ContainersCapacity */
      , (40527,  16,         32) /* ItemUseable - Remote */
-     , (40527,  67,         40) /* Tolerance - Provoke, Target */
      , (40527,  93,    6292504) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment, EdgeSlide */
-     , (40527,  95,          8) /* RadarBlipColor - Yellow */;
+     , (40527,  95,          8) /* RadarBlipColor - Yellow */
+     , (40527, 307,          0) /* DamageRating */
+     , (40527, 308,          0) /* DamageResistRating */
+     , (40527, 313,          0) /* CritRating */
+     , (40527, 314,          0) /* CritDamageRating */
+     , (40527, 315,          0) /* CritResistRating */
+     , (40527, 316,          0) /* CritDamageResistRating */
+     , (40527, 370,          0) /* GearDamage */
+     , (40527, 371,          0) /* GearDamageResist */
+     , (40527, 372,          0) /* GearCrit */
+     , (40527, 373,          0) /* GearCritResist */
+     , (40527, 374,          0) /* GearCritDamage */
+     , (40527, 375,          0) /* GearCritDamageResist */
+     , (40527, 376,          0) /* GearHealingBoost */
+     , (40527, 377,          0) /* GearNetherResist */
+     , (40527, 378,          0) /* GearLifeResist */
+     , (40527, 379,          0) /* GearMaxHealth */
+     , (40527, 381,          0) /* PKDamageRating */
+     , (40527, 382,          0) /* PKDamageResistRating */
+     , (40527, 383,          0) /* GearPKDamageRating */
+     , (40527, 384,          0) /* GearPKDamageResistRating */
+     , (40527, 386,          0) /* Overpower */
+     , (40527, 387,          0) /* OverpowerResist */
+     , (40527, 388,          0) /* GearOverpower */
+     , (40527, 389,          0) /* GearOverpowerResist */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (40527,   1, True ) /* Stuck */
@@ -21,7 +44,8 @@ VALUES (40527,   1, True ) /* Stuck */
      , (40527,  41, True ) /* ReportCollisionsAsEnvironment */
      , (40527,  42, True ) /* AllowEdgeSlide */
      , (40527,  52, True ) /* AiImmobile */
-     , (40527, 118, True ) /* NeverAttack */;
+     , (40527,  82, True ) /* DontTurnOrMoveWhenGiving */
+     , (40527,  83, True ) /* NpcLooksLikeObject */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
 VALUES (40527,  54,       3) /* UseRadius */;
@@ -37,15 +61,6 @@ VALUES (40527,   1, 0x02000158) /* Setup */
      , (40527,   8, 0x06003540) /* Icon */
      , (40527,  22, 0x3400002B) /* PhysicsEffectTable */;
 
-INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
-VALUES (40527,   1,  10, 0, 0) /* Strength */
-     , (40527,   2,  10, 0, 0) /* Endurance */
-     , (40527,   3,  10, 0, 0) /* Quickness */
-     , (40527,   4,  10, 0, 0) /* Coordination */
-     , (40527,   5,  10, 0, 0) /* Focus */
-     , (40527,   6,  10, 0, 0) /* Self */;
+INSERT INTO `weenie_properties_i_i_d` (`object_Id`, `type`, `value`)
+VALUES (40527,  16, 0x73F320B2) /* ActivationTarget */;
 
-INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
-VALUES (40527,   1,     0, 0, 0, 0) /* MaxHealth */
-     , (40527,   3,     0, 0, 0, 0) /* MaxStamina */
-     , (40527,   5,     0, 0, 0, 0) /* MaxMana */;
