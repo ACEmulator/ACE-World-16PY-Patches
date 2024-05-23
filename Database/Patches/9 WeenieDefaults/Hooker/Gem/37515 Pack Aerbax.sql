dedd16ceebb07844c38d569535b7be1a1cb84bea
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2024-05-23 00:20:34 -0400

    Update Pack Dolls (#1660)
    Adds on the hook animations to Pack Harbinger, Aerbax, and Tthuun (cast spell and dead).
    Adds pcapped dolls that were once available (e.g., from live events) - Pack Grael, Rare Tthuun, Max, and Gurog.
    Adds missing palette template and clothing base to Pack Gold Remoran to correct appearance.

--- Database/Patches/9 WeenieDefaults/Hooker/Gem/37515 Pack Aerbax.sql
+++ Database/Patches/9 WeenieDefaults/Hooker/Gem/37515 Pack Aerbax.sql
@@ -1,39 +1,38 @@
-DELETE FROM `weenie` WHERE `class_Id` = 39369;
+DELETE FROM `weenie` WHERE `class_Id` = 37515;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (39369, 'ace39369-packtthuun', 64, '2024-05-16 12:50:39') /* Hooker */;
+VALUES (37515, 'ace37515-packaerbax', 64, '2024-05-16 01:35:44') /* Hooker */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (39369,   1,       2048) /* ItemType - Gem */
-     , (39369,   5,         10) /* EncumbranceVal */
-     , (39369,  16,         32) /* ItemUseable - Remote */
-     , (39369,  19,          0) /* Value */
-     , (39369,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (39369,  94,         16) /* TargetType - Creature */
-     , (39369, 150,        103) /* HookPlacement - Hook */
-     , (39369, 151,          9) /* HookType - Floor, Yard */
-     , (39369, 197,          1) /* HookGroup - NoisemakingItems */;
+VALUES (37515,   1,       2048) /* ItemType - Gem */
+     , (37515,   5,         10) /* EncumbranceVal */
+     , (37515,  16,          1) /* ItemUseable - No */
+     , (37515,  19,          0) /* Value */
+     , (37515,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (37515,  94,         16) /* TargetType - Creature */
+     , (37515, 150,        103) /* HookPlacement - Hook */
+     , (37515, 151,          9) /* HookType - Floor, Yard */
+     , (37515, 197,          1) /* HookGroup - NoisemakingItems */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (39369,  22, True ) /* Inscribable */;
+VALUES (37515,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (39369,  39,    0.25) /* DefaultScale */;
+VALUES (37515,  39,     0.3) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (39369,   1, 'Pack T''thuun') /* Name */
-     , (39369,  14, 'The Pack T''thuun can be placed on floor and yard hooks.') /* Use */
-     , (39369,  16, 'A miniature doll of what we have seen of T''thuun, the Whispering One.') /* LongDesc */;
+VALUES (37515,   1, 'Pack Aerbax') /* Name */
+     , (37515,  14, 'The Pack Aerbax can be placed on floor and yard hooks.') /* Use */
+     , (37515,  16, 'A miniature doll of Aerbax - once a valued member of the Virindi Singularity who split from it and gradually began to realize its own independence.  Its research culminated in the discovery of a Kemeroi, one of the great evil beings hidden deep within the world, which further twisted it towards Shadow and destruction.  Aerbax hatched a plot to dominate the creatures of Auberean by crafting prodigal versions of several selected different races.  Aerbax''s shadow has now been sundered from it, but Aerbax still exists and may one day again attempt to guide the course of history from afar.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (39369,   1, 0x02001856) /* Setup */
-     , (39369,   2, 0x090001F4) /* MotionTable */
-     , (39369,   3, 0x20000067) /* SoundTable */
-     , (39369,   8, 0x06001ED2) /* Icon */
-     , (39369,  22, 0x34000064) /* PhysicsEffectTable */;
+VALUES (37515,   1, 0x020017F5) /* Setup */
+     , (37515,   2, 0x090001EB) /* MotionTable */
+     , (37515,   8, 0x060067D9) /* Icon */
+     , (37515,  22, 0x34000029) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (39369,  7 /* Use */,    0.5, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (37515,  7 /* Use */,    0.5, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -42,7 +41,7 @@ VALUES (@parent_id,  0,   5 /* Motion */, 0, 1, 0x400000D3 /* CastSpell */, NULL
      , (@parent_id,  1,   5 /* Motion */, 0.5, 1, 0x41000003 /* Ready */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (39369,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (37515,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
--- Database/Patches/9 WeenieDefaults/Hooker/Gem/39370 Rare Pack T'thuun.sql
+++ Database/Patches/9 WeenieDefaults/Hooker/Gem/39370 Rare Pack T'thuun.sql
@@ -1,39 +1,39 @@
-DELETE FROM `weenie` WHERE `class_Id` = 39369;
+DELETE FROM `weenie` WHERE `class_Id` = 39370;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (39369, 'ace39369-packtthuun', 64, '2024-05-16 12:50:39') /* Hooker */;
+VALUES (39370, 'ace39370-rarepacktthuun', 64, '2024-05-16 12:49:57') /* Hooker */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (39369,   1,       2048) /* ItemType - Gem */
-     , (39369,   5,         10) /* EncumbranceVal */
-     , (39369,  16,         32) /* ItemUseable - Remote */
-     , (39369,  19,          0) /* Value */
-     , (39369,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (39369,  94,         16) /* TargetType - Creature */
-     , (39369, 150,        103) /* HookPlacement - Hook */
-     , (39369, 151,          9) /* HookType - Floor, Yard */
-     , (39369, 197,          1) /* HookGroup - NoisemakingItems */;
+VALUES (39370,   1,       2048) /* ItemType - Gem */
+     , (39370,   5,         10) /* EncumbranceVal */
+     , (39370,  16,         32) /* ItemUseable - Remote */
+     , (39370,  19,          0) /* Value */
+     , (39370,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (39370,  94,         16) /* TargetType - Creature */
+     , (39370, 150,        103) /* HookPlacement - Hook */
+     , (39370, 151,          9) /* HookType - Floor, Yard */
+     , (39370, 197,          1) /* HookGroup - NoisemakingItems */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (39369,  22, True ) /* Inscribable */;
+VALUES (39370,  22, True ) /* Inscribable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (39369,  39,    0.25) /* DefaultScale */;
+VALUES (39370,  39,     0.5) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (39369,   1, 'Pack T''thuun') /* Name */
-     , (39369,  14, 'The Pack T''thuun can be placed on floor and yard hooks.') /* Use */
-     , (39369,  16, 'A miniature doll of what we have seen of T''thuun, the Whispering One.') /* LongDesc */;
+VALUES (39370,   1, 'Rare Pack T''thuun') /* Name */
+     , (39370,  14, 'The Rare Pack T''thuun can be placed on floor and yard hooks.') /* Use */
+     , (39370,  16, 'A not-so-miniature doll of what we have seen of T''thuun, the Whispering One.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (39369,   1, 0x02001856) /* Setup */
-     , (39369,   2, 0x090001F4) /* MotionTable */
-     , (39369,   3, 0x20000067) /* SoundTable */
-     , (39369,   8, 0x06001ED2) /* Icon */
-     , (39369,  22, 0x34000064) /* PhysicsEffectTable */;
+VALUES (39370,   1, 0x02001856) /* Setup */
+     , (39370,   2, 0x090001F4) /* MotionTable */
+     , (39370,   3, 0x20000067) /* SoundTable */
+     , (39370,   8, 0x06001ED2) /* Icon */
+     , (39370,  22, 0x34000064) /* PhysicsEffectTable */;
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (39369,  7 /* Use */,    0.5, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (39370,  7 /* Use */,    0.5, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
@@ -42,7 +42,7 @@ VALUES (@parent_id,  0,   5 /* Motion */, 0, 1, 0x400000D3 /* CastSpell */, NULL
      , (@parent_id,  1,   5 /* Motion */, 0.5, 1, 0x41000003 /* Ready */, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (39369,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (39370,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
