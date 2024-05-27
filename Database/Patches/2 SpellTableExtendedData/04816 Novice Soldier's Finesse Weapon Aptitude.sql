a5efc967fac10332e56074ca12142b7f5bfe36b5
Nalindar <nalindargit@gmail.com>
2024-05-27 16:49:39 -0400

    Update 04816 Novice Soldier's Finesse Weapon Aptitude.sql
    Update from Dagger to Finesse Weapons

--- Database/Patches/2 SpellTableExtendedData/04816 Novice Soldier's Finesse Weapon Aptitude.sql
+++ Database/Patches/2 SpellTableExtendedData/04816 Novice Soldier's Finesse Weapon Aptitude.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4816;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (4816, 'Novice Soldier''s Finesse Weapon Aptitude', 33591312 /* Skill, SingleStat, Additive, Beneficial */, 4 /* Dagger */, 3, '2021-11-01 00:00:00');
+VALUES (4816, 'Novice Soldier''s Finesse Weapon Aptitude', 33591312 /* Skill, SingleStat, Additive, Beneficial */, 46 /* FinesseWeapons */, 3, '2021-11-01 00:00:00');
