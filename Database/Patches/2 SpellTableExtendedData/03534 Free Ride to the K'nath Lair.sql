f1f89a77bdc92d241f1d7d1d7625ad2516ae3908
Ty Conner <tyconner@itcproductions.com>
2024-06-24 13:45:07 -0400

    Update 03534 Free Ride to the K'nath Lair.sql

--- Database/Patches/2 SpellTableExtendedData/03534 Free Ride to the K'nath Lair.sql
+++ Database/Patches/2 SpellTableExtendedData/03534 Free Ride to the K'nath Lair.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell` WHERE `id` = 3534;
 
 INSERT INTO `spell` (`id`, `name`, `position_Obj_Cell_ID`, `position_Origin_X`, `position_Origin_Y`, `position_Origin_Z`, `position_Angles_W`, `position_Angles_X`, `position_Angles_Y`, `position_Angles_Z`, `last_Modified`)
-VALUES (3534, 'Free Ride to the K''nath Lair', 0x0193012A, 40, -10, 0, 0.027346, 0, 0, -0.999626, '2021-11-01 00:00:00');
+VALUES (3534, 'Free Ride to the K''nath Lair', 0x01930151, 40, -10, 0.005, 0.027346, 0, 0, -0.999626, '2021-11-01 00:00:00');
 /* @teleloc 0x0193012A [40.000000 -10.000000 0.000000] 0.027346 0.000000 0.000000 -0.999626 */
