7bdb64f259fc449167fc151654702ae6e0b9b4b7
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2024-07-07 12:02:28 -0400

    Add Prismatic Amuli and Celdon Recipes (#1693)
    * Add Prismatic Amuli and Celdon Recipes
    
    Adds recipes to create these Shadow Armor pieces. Although retired, they could still be made at the end of retail if a person had left over armor pieces, and could be useful for server operators who wish to make this armor available again.
    
    * no longer consume glyph
    
    Only one glyph is required as per wiki description.

---
+++ Database/Patches/4 CraftTable/09226 Prismatic Amuli Leggings.sql
@@ -0,0 +1,9 @@
+DELETE FROM `recipe` WHERE `id` = 9226;
+
+INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
+VALUES (9226, 0, 0, 0, 0, 32757 /* Prismatic Amuli Leggings */, 1, 'You''ve altered your armor!', 0, 0, '', 0, 0, '', 1, 1, '', 0, 0, '', 1, 1, '', 0, '2021-11-01 00:00:00');
+
+DELETE FROM `cook_book` WHERE `recipe_Id` = 9226;
+
+INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
+VALUES (9226, 32945 /* Prismatic Glyph */, 14839 /* Greater Amuli Shadow Leggings */, '2021-11-01 00:00:00');
