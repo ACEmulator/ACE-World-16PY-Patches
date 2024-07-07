52bb2afcc35acb589b22bf0affa4945331c7b66a
Ty Conner <tyconner@itcproductions.com>
2024-07-07 13:06:35 -0400

    Create 80050 Crystal Gen.sql

---
+++ Database/Patches/9 WeenieDefaults/Generic/None/80050 Crystal Gen.sql
@@ -0,0 +1,29 @@
+DELETE FROM `weenie` WHERE `class_Id` = 80050;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (80050, 'ace80050-ameliaidlegenerator', 1, '2021-11-01 00:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (80050,  81,          1) /* MaxGeneratedObjects */
+     , (80050,  82,          1) /* InitGeneratedObjects */
+     , (80050,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (80050,   1, True ) /* Stuck */
+     , (80050,  11, True ) /* IgnoreCollisions */
+     , (80050,  18, True ) /* Visibility */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (80050,  41,      60) /* RegenerationInterval */
+     , (80050, 121,       1) /* GeneratorInitialDelay */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (80050,   1, 'Crystal Generator') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (80050,   1, 0x0200026B) /* Setup */
+     , (80050,   8, 0x06001066) /* Icon */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (80050, -1, 40124, 120, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate 40124 (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
+     , (80050, -1, 40146, 1, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate 40146 (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */
