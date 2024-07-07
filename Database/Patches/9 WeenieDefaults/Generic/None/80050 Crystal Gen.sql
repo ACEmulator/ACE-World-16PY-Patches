aaec5835fbb9573e15b9902a648755a35ae472db
Ty Conner <tyconner@itcproductions.com>
2024-07-07 13:15:58 -0400

    Update 80050 Crystal Gen.sql

--- Database/Patches/9 WeenieDefaults/Generic/None/80050 Crystal Gen.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/80050 Crystal Gen.sql
@@ -25,5 +25,5 @@ VALUES (80050,   1, 0x0200026B) /* Setup */
      , (80050,   8, 0x06001066) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (80050, -1, 40124, 120, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate 40124 (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
-     , (80050, -1, 40146, 1, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate 40146 (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */
+VALUES (80050, -1, 40124, 120, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate 40124 (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */
+     , (80050, -1, 40146, 1, 1, 1, 1, 1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate 40146 (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: OnTop */;
