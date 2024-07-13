c4d79174b9591a24b34e5f22eb1b60936d5d704a
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2024-07-13 12:05:23 -0500

    Fix Olthoi Sentinel Tracker LW/MeD values based off PCAP estimates (#1698)

--- Database/Patches/9 WeenieDefaults/Creature/Apparition/36773 Olthoi Sentinel Tracker.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Apparition/36773 Olthoi Sentinel Tracker.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 36773;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (36773, 'ace36773-olthoisentineltracker', 10, '2021-11-01 00:00:00') /* Creature */;
+VALUES (36773, 'ace36773-olthoisentineltracker', 10, '2024-07-09 07:51:04') /* Creature */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (36773,   1,         16) /* ItemType - Creature */
@@ -73,6 +73,14 @@ VALUES (36773,   1, 0x02000FB3) /* Setup */
      , (36773,  22, 0x340000A8) /* PhysicsEffectTable */
      , (36773,  30,         86) /* PhysicsScript - BreatheAcid */;
 
+INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
+VALUES (36773,  0,  2,400,    0,  350,  175,  175,  175,  175,  175,  175,  175,    0, 1,  0.1,    0,    0,  0.1,    0,    0,  0.1,    0,    0,  0.1,    0,    0) /* Head */
+     , (36773, 16,  4,400,    0,  350,  175,  175,  175,  175,  175,  175,  175,    0, 2, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45) /* Torso */
+     , (36773, 18,  2,400,  0.5,  350,  175,  175,  175,  175,  175,  175,  175,    0, 2,    0,  0.2,  0.1,    0,  0.2,  0.1,    0,  0.2,  0.1,    0,  0.2,  0.1) /* Arm */
+     , (36773, 19,  2,400, 0.75,  350,  175,  175,  175,  175,  175,  175,  175,    0, 3,    0,  0.2, 0.45,    0,  0.2, 0.45,    0,  0.2, 0.45,    0,  0.2, 0.45) /* Leg */
+     , (36773, 20,  1,400, 0.75,  350,  175,  175,  175,  175,  175,  175,  175,    0, 2, 0.45,  0.2,    0, 0.45,  0.2,    0, 0.45,  0.2,    0, 0.45,  0.2,    0) /* Claw */
+     , (36773, 22, 32,300,  0.5,    0,    0,    0,    0,    0,    0,    0,    0,    0, 0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0) /* Breath */;
+
 INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
 VALUES (36773,   1, 600, 0, 0) /* Strength */
      , (36773,   2, 600, 0, 0) /* Endurance */
@@ -84,29 +92,22 @@ VALUES (36773,   1, 600, 0, 0) /* Strength */
 INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
 VALUES (36773,   1,   900, 0, 0, 1200) /* MaxHealth */
      , (36773,   3,   900, 0, 0, 1500) /* MaxStamina */
-     , (36773,   5,     0, 0, 0, 600) /* MaxMana */;
+     , (36773,   5,     0, 0, 0,  600) /* MaxMana */;
 
 INSERT INTO `weenie_properties_skill` (`object_Id`, `type`, `level_From_P_P`, `s_a_c`, `p_p`, `init_Level`, `resistance_At_Last_Check`, `last_Used_Time`)
-VALUES (36773,  6, 0, 3, 0, 500, 0, 0) /* MeleeDefense        Specialized */
+VALUES (36773,  6, 0, 3, 0, 210, 0, 0) /* MeleeDefense         Specialized */
      , (36773,  7, 0, 3, 0, 340, 0, 0) /* MissileDefense      Specialized */
-     , (36773, 13, 0, 3, 0, 480, 0, 0) /* UnarmedCombat       Specialized */
+     , (36773, 45, 0, 3, 0, 170, 0, 0) /* LightWeapons         Specialized */
      , (36773, 15, 0, 3, 0, 271, 0, 0) /* MagicDefense        Specialized */
      , (36773, 20, 0, 2, 0, 100, 0, 0) /* Deception           Trained */
      , (36773, 22, 0, 2, 0, 700, 0, 0) /* Jump                Trained */
      , (36773, 24, 0, 2, 0, 700, 0, 0) /* Run                 Trained */;
 
-INSERT INTO `weenie_properties_body_part` (`object_Id`, `key`, `d_Type`, `d_Val`, `d_Var`, `base_Armor`, `armor_Vs_Slash`, `armor_Vs_Pierce`, `armor_Vs_Bludgeon`, `armor_Vs_Cold`, `armor_Vs_Fire`, `armor_Vs_Acid`, `armor_Vs_Electric`, `armor_Vs_Nether`, `b_h`, `h_l_f`, `m_l_f`, `l_l_f`, `h_r_f`, `m_r_f`, `l_r_f`, `h_l_b`, `m_l_b`, `l_l_b`, `h_r_b`, `m_r_b`, `l_r_b`)
-VALUES (36773,  0,  2, 400,    0,  350,  420,  420,  420,  350,  350,  350,  350,    0, 1,  0.1,    0,    0,  0.1,    0,    0,  0.1,    0,    0,  0.1,    0,    0) /* Head */
-     , (36773, 16,  4, 400,    0,  350,  420,  420,  420,  350,  350,  350,  350,    0, 2, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45, 0.45,  0.4, 0.45) /* Torso */
-     , (36773, 18,  2, 400,  0.5,  350,  420,  420,  420,  350,  350,  350,  350,    0, 2,    0,  0.2,  0.1,    0,  0.2,  0.1,    0,  0.2,  0.1,    0,  0.2,  0.1) /* Arm */
-     , (36773, 19,  2, 400, 0.75,  350,  420,  420,  420,  350,  350,  350,  350,    0, 3,    0,  0.2, 0.45,    0,  0.2, 0.45,    0,  0.2, 0.45,    0,  0.2, 0.45) /* Leg */
-     , (36773, 20,  1, 400, 0.75,  350,  420,  420,  420,  350,  350,  350,  350,    0, 2, 0.45,  0.2,    0, 0.45,  0.2,    0, 0.45,  0.2,    0, 0.45,  0.2,    0) /* Claw */
-     , (36773, 22, 32, 300,  0.5,    0,    0,    0,    0,    0,    0,    0,    0,    0, 0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0) /* Breath */;
-
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
-VALUES (36773,  3 /* Death */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (36773, 3 /* Death */, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,  88 /* LocalSignal */, 0, 1, NULL, 'OlthoiSentinelTrackerKilled', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id, 0, 88 /* LocalSignal */, 0, 1, NULL, 'OlthoiSentinelTrackerKilled', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
