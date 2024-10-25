4eb061d6db90f7dd3c5f5473893259c0c0006747
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2024-10-25 11:37:47 -0400

    Add Deewain to Asheron's Island (#1716)
    Adds to final pcapped location on Asheron's Island.
    
    Thanks to OptimShi for the list of missing NPCs.

---
+++ Database/Patches/9 WeenieDefaults/Creature/Golem/42543 Deewain.sql
@@ -0,0 +1,56 @@
+DELETE FROM `weenie` WHERE `class_Id` = 42543;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (42543, 'ace42543-deewain', 10, '2024-10-17 08:15:50') /* Creature */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (42543,   1,         16) /* ItemType - Creature */
+     , (42543,   2,         13) /* CreatureType - Golem */
+     , (42543,   6,         -1) /* ItemsCapacity */
+     , (42543,   7,         -1) /* ContainersCapacity */
+     , (42543,  16,         32) /* ItemUseable - Remote */
+     , (42543,  25,        350) /* Level */
+     , (42543,  93,    2098200) /* PhysicsState - ReportCollisions, IgnoreCollisions, Gravity, ReportCollisionsAsEnvironment */
+     , (42543,  95,          8) /* RadarBlipColor - Yellow */
+     , (42543, 133,          4) /* ShowableOnRadar - ShowAlways */
+     , (42543, 134,         16) /* PlayerKillerStatus - RubberGlue */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (42543,   1, True ) /* Stuck */
+     , (42543,  19, False) /* Attackable */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (42543,  39,     2.1) /* DefaultScale */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (42543,   1, 'Deewain') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (42543,   1, 0x020007CA) /* Setup */
+     , (42543,   2, 0x09000081) /* MotionTable */
+     , (42543,   3, 0x20000015) /* SoundTable */
+     , (42543,   8, 0x06001224) /* Icon */
+     , (42543,  22, 0x3400005B) /* PhysicsEffectTable */;
+
+INSERT INTO `weenie_properties_attribute` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`)
+VALUES (42543,   1, 450, 0, 0) /* Strength */
+     , (42543,   2, 400, 0, 0) /* Endurance */
+     , (42543,   3, 450, 0, 0) /* Quickness */
+     , (42543,   4, 450, 0, 0) /* Coordination */
+     , (42543,   5, 450, 0, 0) /* Focus */
+     , (42543,   6, 450, 0, 0) /* Self */;
+
+INSERT INTO `weenie_properties_attribute_2nd` (`object_Id`, `type`, `init_Level`, `level_From_C_P`, `c_P_Spent`, `current_Level`)
+VALUES (42543,   1, 50000, 0, 0, 50200) /* MaxHealth */
+     , (42543,   3, 15000, 0, 0, 15400) /* MaxStamina */
+     , (42543,   5, 50000, 0, 0, 50450) /* MaxMana */;
+
+INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
+VALUES (42543,  7 /* Use */,      1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+SET @parent_id = LAST_INSERT_ID();
+
+INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (@parent_id,  0,  12 /* TurnToTarget */, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  1,  10 /* Tell */, 0, 1, NULL, 'The chaotic one did not prevail.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+     , (@parent_id,  2,  10 /* Tell */, 0, 1, NULL, 'Perhaps now the rest of this world will accept me as Asheron does.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
