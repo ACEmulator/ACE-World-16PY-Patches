fd1efd5141689d3e3608ac4cf826e43d9d93a58a
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2024-10-25 11:34:08 -0400

    Update Empyrean Rescue (#1706)
    * Update Empyrean Rescue
    
    Corrected an issue where duplicates from the same wave could spawn during the final fight.
    Removed destroyed portals from dungeon 2 spawns. These were only found in dungeons 1 and 3.
    Changed gateway summoned by Virindi boss in dungeon 2 to no summon (can now be tied, recalled) to match retail behavior as per reports from players.
    
    * revise wave gens
    
    To match other wave gens (e.g., GYColo).

--- Database/Patches/9 WeenieDefaults/Generic/None/87495 Empyrean Rescue Engorged Bloodstone Gen.sql
+++ Database/Patches/9 WeenieDefaults/Generic/None/87495 Empyrean Rescue Engorged Bloodstone Gen.sql
@@ -15,7 +15,7 @@ VALUES (87495,   1, True ) /* Stuck */
      , (87495,  18, True ) /* Visibility */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (87495,  41,     300) /* RegenerationInterval */
+VALUES (87495,  41,  999999) /* RegenerationInterval */
      , (87495,  43,       0) /* GeneratorRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
@@ -26,4 +26,4 @@ VALUES (87495,   1, 0x0200026B) /* Setup */
      , (87495,   8, 0x06001066) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (87495, -1, 43272, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Engorged Bloodstone (43272) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
+VALUES (87495, -1, 43272, 0, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Engorged Bloodstone (43272) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
