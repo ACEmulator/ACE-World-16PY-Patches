1e3681f2038d26e44e47433022fbdb1f7def5b93
Ty Conner <tyconner@itcproductions.com>
2024-10-29 11:25:34 -0400

    Update 37457 Pyre Champion.sql

--- Database/Patches/9 WeenieDefaults/Creature/Skeleton/37457 Pyre Champion.sql
+++ Database/Patches/9 WeenieDefaults/Creature/Skeleton/37457 Pyre Champion.sql
@@ -64,7 +64,8 @@ VALUES (37457,   1,       5) /* HeartbeatInterval */
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (37457,   1, 'Pyre Champion') /* Name */
-     , (37457,  45, 'KilltaskGraveyardSkeleton_1309') /* KillQuest */;
+     , (37457,  45, 'KilltaskGraveyardSkeleton_1309') /* KillQuest */
+     , (37457,  49, 'KillTaskMGHPyreChampion') /* KillQuest2 */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (37457,   1, 0x020016A5) /* Setup */
