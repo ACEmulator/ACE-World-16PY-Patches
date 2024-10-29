eacc50d20ea1d8c83e3e2755f10dac0fa3283eb7
Ty Conner <tyconner@itcproductions.com>
2024-10-29 11:20:44 -0400

    Create KillTaskMGHUndeadSailor.sql

---
+++ Database/Patches/8 QuestDefDB/KillTaskMGHUndeadSailor.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskMGHUndeadSailor';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskMGHUndeadSailor', 0, 10, 'Monthly Game Hunter Kill Task', '2022-06-06 04:05:48');
