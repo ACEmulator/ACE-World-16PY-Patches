bae07d281a88a208bda07676471cb82d6cc56463
Ty Conner <tyconner@itcproductions.com>
2024-10-29 11:20:40 -0400

    Create KillTaskMGHHarvestReaper.sql

---
+++ Database/Patches/8 QuestDefDB/KillTaskMGHHarvestReaper.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'KillTaskMGHHarvestReaper';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('KillTaskMGHHarvestReaper', 0, 10, 'Monthly Game Hunter Kill Task', '2022-06-06 04:05:48');
