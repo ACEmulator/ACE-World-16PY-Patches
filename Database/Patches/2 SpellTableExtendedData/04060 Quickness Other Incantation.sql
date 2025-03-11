7f27d6068491fda5c45aa7768ec8fa7fbb073f6d
Boop-AC <90673082+Boop-AC@users.noreply.github.com>
2025-03-11 04:03:22 -0500

    Fix Endurance/Quickness Other Incantations

--- Database/Patches/2 SpellTableExtendedData/04060 Quickness Other Incantation.sql
+++ Database/Patches/2 SpellTableExtendedData/04060 Quickness Other Incantation.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4060;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (4060, 'Quickness Other Incantation', 36865 /* Attribute, SingleStat, Additive */, 4 /* Coordination */, 25, '2021-11-01 00:00:00');
+VALUES (4060, 'Quickness Other Incantation', 36865 /* Attribute, SingleStat, Additive */, 3 /* Quickness */, 45, '2021-11-01 00:00:00');
