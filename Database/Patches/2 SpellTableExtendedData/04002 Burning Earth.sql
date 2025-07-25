74f8bd27cb5983bc1887491747b01d9801ddcd90
TectonicRifts <60861066+TectonicRifts@users.noreply.github.com>
2025-07-25 12:39:45 -0400

    fix more spells

--- Database/Patches/2 SpellTableExtendedData/04002 Burning Earth.sql
+++ Database/Patches/2 SpellTableExtendedData/04002 Burning Earth.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 4002;
 
 INSERT INTO `spell` (`id`, `name`, `e_Type`, `base_Intensity`, `variance`, `wcid`, `num_Projectiles`, `num_Projectiles_Variance`, `spread_Angle`, `vertical_Angle`, `default_Launch_Angle`, `non_Tracking`, `create_Offset_Origin_X`, `create_Offset_Origin_Y`, `create_Offset_Origin_Z`, `padding_Origin_X`, `padding_Origin_Y`, `padding_Origin_Z`, `dims_Origin_X`, `dims_Origin_Y`, `dims_Origin_Z`, `peturbation_Origin_X`, `peturbation_Origin_Y`, `peturbation_Origin_Z`, `imbued_Effect`, `slayer_Creature_Type`, `slayer_Damage_Bonus`, `crit_Freq`, `crit_Multiplier`, `ignore_Magic_Resist`, `elemental_Modifier`, `last_Modified`)
-VALUES (4002, 'Burning Earth', 16 /* Fire */, 100, 100, 7277 /* Flame Bolt */, 10, 0, 0, 0, 0, False, 0, 2, 0, 0.5, 1, 1, 10, 1, 1, 0, 0, 0, 0 /* Undef */, 0 /* Invalid */, 1, 0, 0, 0, 1, '2021-11-01 00:00:00');
+VALUES (4002, 'Burning Earth', 16 /* Fire */, 100, 100, 33526, 10, 0, 0, 0, 0, False, 0, 2, 0, 0.5, 1, 1, 10, 1, 1, 0, 0, 0, 0 /* Undef */, 0 /* Invalid */, 1, 0, 0, 0, 1, '2021-11-01 00:00:00');
---
+++ Database/Patches/9 WeenieDefaults/ProjectileSpell/None/33526 Burning Earth.sql
@@ -0,0 +1,29 @@
+DELETE FROM `weenie` WHERE `class_Id` = 33526;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (33526, 'ace33526-burningearth', 33, '2019-02-10 00:00:00') /* ProjectileSpell */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (33526,   1,          0) /* ItemType - None */
+     , (33526,  93,     166728) /* PhysicsState - ReportCollisions, Missile, AlignPath, PathClipped, LightingOn, ScriptedCollision, Inelastic */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (33526,   1, True ) /* Stuck */
+     , (33526,  24, True ) /* UiHidden */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (33526,  26,       3) /* MaximumVelocity */
+     , (33526,  77,       1) /* PhysicsScriptIntensity */
+     , (33526,  78,       1) /* Friction */
+     , (33526,  79,       0) /* Elasticity */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (33526,   1, 'Burning Earth') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (33526,   1, 0x020015FC) /* Setup */
+     , (33526,   3, 0x20000037) /* SoundTable */
+     , (33526,   8, 0x06001066) /* Icon */
+     , (33526,  22, 0x340000BD) /* PhysicsEffectTable */
+     , (33526,  28,       3943) /* Spell - Burning Earth */
+     , (33526,  30,         90) /* PhysicsScript - ProjectileCollision */;
