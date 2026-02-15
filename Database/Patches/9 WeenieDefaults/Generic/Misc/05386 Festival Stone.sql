9526de434443c39ddc5ccb54e8de4c0091dbd634
Ty Conner <tyconner@itcproductions.com>
2026-02-15 14:58:35 -0500

    finalize

--- Database/Patches/9 WeenieDefaults/Generic/Misc/05386 Festival Stone.sql
+++ Database/Patches/9 WeenieDefaults/Generic/Misc/05386 Festival Stone.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 5386;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (5386, 'festivalstoneseedsow', 1, '2025-03-16 03:42:04') /* Generic */;
+VALUES (5386, 'festivalstoneseedsow', 1, '2026-02-15 19:42:48') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (5386,   1,        128) /* ItemType - Misc */
