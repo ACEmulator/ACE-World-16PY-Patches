23406ddd68d30454fd6d867fec7c0877300a17e3
Carthar <wade_grant@hotmail.com>
2026-03-25 03:03:57 -0400

    Fix junk data in 5 fellowship life vuln spells (#1794)
    * Update 03875 Acidic Curse.sql
    
    Fixed stat mod and mod key to be consistent with acid vuln spells
    
    * Update 03876 Curse of the Blades.sql
    
    Updated Stat mod and Stat mod key to be consistent with blade vuln
    
    * Update 03917 Numbing Chill.sql
    
    Updated Stat mod and stat mod key to be consistent with frost vuln
    
    * Update 03918 Flammable.sql
    
    Updated stat mod and stat mod key to be consistent with fire vuln
    
    * Update 03919 Lightning Rod.sql
    
    updated stat mod and stat mod key to be consistent with lightning vuln
    
    * Update 03917 Numbing Chill.sql
    
    ---------
    
    Co-authored-by: Ty Conner <tyconner@itcproductions.com>

--- Database/Patches/2 SpellTableExtendedData/03876 Curse of the Blades.sql
+++ Database/Patches/2 SpellTableExtendedData/03876 Curse of the Blades.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 3876;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (3876, 'Curse of the Blades', 36865 /* Attribute, SingleStat, Additive */, 3 /* Quickness */, 2.85, '2021-11-01 00:00:00');
+VALUES (3876, 'Curse of the Blades', 20488 /* Float, SingleStat, Multiplicative */, 64 /* RESIST_SLASH_FLOAT  */, 2.85, '2026-03-24 00:00:00');
--- Database/Patches/2 SpellTableExtendedData/03919 Lightning Rod.sql
+++ Database/Patches/2 SpellTableExtendedData/03919 Lightning Rod.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 3919;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (3919, 'Lightning Rod', 36865 /* Attribute, SingleStat, Additive */, 3 /* Quickness */, 2.85, '2021-11-01 00:00:00');
+VALUES (3919, 'Lightning Rod', 20488 /* Float, SingleStat, Multiplicative */, 70 /* Resist_electic_float */, 2.85, '2026-03-24 00:00:00');
