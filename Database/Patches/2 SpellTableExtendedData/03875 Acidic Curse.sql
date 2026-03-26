afb2ef984c38320ae626a9aab591f957b5c838f7
Ty Conner <tyconner@itcproductions.com>
2026-03-26 01:40:54 -0400

    normalize

--- Database/Patches/2 SpellTableExtendedData/03875 Acidic Curse.sql
+++ Database/Patches/2 SpellTableExtendedData/03875 Acidic Curse.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 3875;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (3875, 'Acidic Curse', 20488 /* Float, SingleStat, Multiplicative */, 69 /* RESIST_ACID_FLOAT  */, 2.85, '2026-03-24 00:00:00');
+VALUES (3875, 'Acidic Curse', 20488 /* Float, SingleStat, Multiplicative */, 69 /* ResistAcid */, 2.85, '2026-03-24 00:00:00');
--- Database/Patches/2 SpellTableExtendedData/03876 Curse of the Blades.sql
+++ Database/Patches/2 SpellTableExtendedData/03876 Curse of the Blades.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell` WHERE `id` = 3876;
 
 INSERT INTO `spell` (`id`, `name`, `stat_Mod_Type`, `stat_Mod_Key`, `stat_Mod_Val`, `last_Modified`)
-VALUES (3876, 'Curse of the Blades', 20488 /* Float, SingleStat, Multiplicative */, 64 /* RESIST_SLASH_FLOAT  */, 2.85, '2026-03-24 00:00:00');
+VALUES (3876, 'Curse of the Blades', 20488 /* Float, SingleStat, Multiplicative */, 64 /* ResistSlash */, 2.85, '2026-03-24 00:00:00');
